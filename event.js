1e8cf27f9b52e190bb5598a5f2b9036ebe73e610
pongdang <pongdang0121@gmail.com>
2022-05-19 22:03:00 +0900

    feat: submit 버튼 누르면 입력창 초기화

--- event.js
+++ event.js
@@ -1,6 +1,7 @@
 let discussionAnswerButtons = document.querySelectorAll(".discussion__answer__button");
 let discussionAnswerContents = document.querySelectorAll(".discussion__answer__content");
 
+// 버튼에 맞는 게시글 보이게 하기
 discussionAnswerButtons.forEach((button, i) =>
   button.addEventListener("click", function () {
     if (discussionAnswerContents[i].classList[1] === "show") {
@@ -17,10 +18,19 @@ const inputStory = document.querySelector("#story");
 const form = document.querySelector(".form");
 const submitButton = document.querySelector(".submit-button");
 
+// 기본 동작 이벤트 막기
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+
+  inputTitle.value = "";
+  inputName.value = "";
+  inputStory.value = "";
 });
 
+// name 입력창에 커서 가도록 하기
+inputName.focus();
+
+// submit 버튼 누르면 정보가 객체 안에 저장되고 화면에 렌더되게 하는 함수
 function inputValue(e) {
   if (e.target.type === "submit") {
     agoraStatesDiscussions.push({
@@ -34,9 +44,6 @@ function inputValue(e) {
     ul.prepend(convertToDiscussion(agoraStatesDiscussions[agoraStatesDiscussions.length - 1]));
     console.log(agoraStatesDiscussions);
   }
-  inputTitle.value = "";
-  inputName.value = "";
-  inputStory.value = "";
 }
 
-submitButton.addEventListener("click", inputValue);
+submitButton.addEventListener("click", inputValue); // { a: 'b' }
