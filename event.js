8b54999db4708d29d35a40157a25fb26d3307af0
pongdang <pongdang0121@gmail.com>
2022-05-19 21:41:51 +0900

    feat: submit 버튼 누르면 폼 내용 추가되는 함수 작성

--- event.js
+++ event.js
@@ -1,5 +1,5 @@
-const discussionAnswerButtons = document.querySelectorAll(".discussion__answer__button");
-const discussionAnswerContents = document.querySelectorAll(".discussion__answer__content");
+let discussionAnswerButtons = document.querySelectorAll(".discussion__answer__button");
+let discussionAnswerContents = document.querySelectorAll(".discussion__answer__content");
 
 discussionAnswerButtons.forEach((button, i) =>
   button.addEventListener("click", function () {
@@ -10,3 +10,33 @@ discussionAnswerButtons.forEach((button, i) =>
     }
   })
 );
+
+const inputName = document.querySelector("#name");
+const inputTitle = document.querySelector("#title");
+const inputStory = document.querySelector("#story");
+const form = document.querySelector(".form");
+const submitButton = document.querySelector(".submit-button");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+
+function inputValue(e) {
+  if (e.target.type === "submit") {
+    agoraStatesDiscussions.push({
+      createdAt: new Date().toISOString(),
+      title: inputTitle.value,
+      author: inputName.value,
+      answer: null,
+      bodyHTML: inputStory.value,
+      avatarUrl: "https://avatars.githubusercontent.com/u/76990149?v=4",
+    });
+    ul.prepend(convertToDiscussion(agoraStatesDiscussions[agoraStatesDiscussions.length - 1]));
+    console.log(agoraStatesDiscussions);
+  }
+  inputTitle.value = "";
+  inputName.value = "";
+  inputStory.value = "";
+}
+
+submitButton.addEventListener("click", inputValue);
