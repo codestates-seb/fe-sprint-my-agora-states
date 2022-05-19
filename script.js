ef5700e0c4479bf0ca59d22dca26936879283fc2
cjsgmail <cjs0097@gmail.com>
2022-05-19 20:35:21 +0900

    디스커션 가운데 정렬, js 디스커션 레이아웃

--- script.js
+++ script.js
@@ -3,20 +3,55 @@ console.log(agoraStatesDiscussions);
 
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
 const convertToDiscussion = (obj) => {
+  
   const li = document.createElement("li"); // li 요소 생성
   li.className = "discussion__container"; // 클래스 이름 지정
-
+  
   const avatarWrapper = document.createElement("div");
   avatarWrapper.className = "discussion__avatar--wrapper";
   const discussionContent = document.createElement("div");
   discussionContent.className = "discussion__content";
   const discussionAnswered = document.createElement("div");
   discussionAnswered.className = "discussion__answered";
+  
+  
+  const avatarImg = document.createElement('img');
+  avatarImg.className = "discussion__avatar--image";
+  avatarImg.src = obj.avatarUrl;
+  avatarImg.alt = 'avatar of ' + obj.author;
+  avatarWrapper.append(avatarImg);
+  
+  const title = document.createElement("h2");
+  const title_link = document.createElement("a");
+  title.className = "discussion__title";
+  title_link.href = obj.url;
+  title_link.textContent = obj.title;
+  
+  
+  title.append(title_link);
+  discussionContent.append(title);
+  
+  
+
+  const information = document.createElement("div");
+  information.className = "discussion__information";
+  information.textContent = `${obj.author} / ${obj.createdAt}`;
+  discussionContent.append(information);
+  
+  
+  const answerd = document.createElement("div");
+  const answerdP = document.createElement("p");
+  answerd.className = "discussion__answered";
+  answerdP.textContent = "☑";
+  answerd.append(answerdP);
+  
+
+  
 
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
-
-
-
+  
+  
+  
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
 };
