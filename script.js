5db03d8687107b8ed8bb7c8dc61dc72378468663
dldnjswns31 <dldnjswns32@gmail.com>
2022-05-20 00:15:39 +0900

    data.js 의 discussion 배열 아이템을 DOM을 통해 html에 추가

--- script.js
+++ script.js
@@ -15,7 +15,33 @@ const convertToDiscussion = (obj) => {
 
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
 
+  const avatarImg = document.createElement("img");
+  avatarImg.src = obj.avatarUrl;
+  avatarImg.alt = `avatar of ${obj.author}`;
+  avatarImg.classList.add("discussion__avatar--image");
+  avatarWrapper.append(avatarImg);
 
+  // 질문과 하이퍼링크 추가
+  const title = document.createElement("h3");
+  const titleLink = document.createElement("a");
+  titleLink.href = obj.url;
+  titleLink.textContent = obj.title;
+  title.append(titleLink);
+  discussionContent.append(title);
+
+  // 작성자 닉네임과 작성 시간 추가
+  const information = document.createElement("div");
+  information.classList.add("discussion__information");
+  information.textContent = `${obj.author} / ${obj.createdAt}`;
+  discussionContent.append(information);
+
+  // 답변 유무 추가
+  if (obj.answer === null) {
+    // ☑
+    discussionAnswered.textContent = "❌";
+  } else {
+    discussionAnswered.textContent = "⭕️";
+  }
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
