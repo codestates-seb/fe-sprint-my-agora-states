c7508e91f5ea90f0998c9bc9900bf0e1a4c4abc9
Kyoorim <kyoorim88@gmail.com>
2022-05-20 15:45:45 +0900

    아고라스테이츠만들기 v1

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+// // input value 가져오기 
+// let userName = document.querySelector('#name').textContent;
+// console.log(userName);
+
+// userName.onkeyup = function() {
+//   return userName.textContent;
+// }
 // index.html을 열어서 agoraStatesDiscussions 배열 요소를 확인하세요.
 console.log(agoraStatesDiscussions);
 
@@ -14,7 +21,34 @@ const convertToDiscussion = (obj) => {
   discussionAnswered.className = "discussion__answered";
 
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
+  // 이미지 넣기 
+    // <img class = "discussion__avatar--image">
+    // src = "https://avatars.githubusercontent.com/u/12145019?s=64&u=5c97f25ee02d87898457e23c0e61b884241838e3&v=4"
+    // alt = "avatar of kimploo"
+  const avatarImg = document.createElement('img');
+  avatarImg.src = obj.avatarUrl;
+  avatarImg.alt = 'avatar of ' + obj.author;
+  avatarWrapper.append(avatarImg);
 
+  // 제목
+  const discussionTitle = document.createElement("h2");
+  // discussionTitle.className = "discussion__title";
+  const titleAnchor = document.createElement("a");
+  titleAnchor.href = obj.url;
+  titleAnchor.textContent = obj.title;
+  discussionTitle.append(titleAnchor);
+
+  // 작성자
+  const discussionInformation = document.createElement("div");
+  discussionInformation.className = "discussion__information";
+  discussionInformation.textContent = `${obj.author} / ${new Date(obj.createdAt).toLocaleTimeString()}`;
+
+  discussionContent.append(discussionTitle, discussionInformation);
+
+  // 체크박스
+  const checked = document.createElement("p");
+    checked.textContent = obj.answer ? "☑":"☒";
+    discussionAnswered.append(checked);
 
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
@@ -32,3 +66,18 @@ const render = (element) => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+//시간
+// const  statusBar = document.createElement("div")
+// statusBar.className = "status-bar";
+// const now = new Date();
+// const statusBarTime = document.getElementsByClassName("time");
+
+// function getTime(){
+//   const hour = now.getHours;
+//   const minutes = now.getMinutes;
+//   const seconds = now.getSeconds
+//   statusBarTime.innerText = `${hour}:${minutes}:${seconds}`;
+// }
+// getTime();
+
