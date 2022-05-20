d41c8a4dcbdd2dc84dd611bcb0a4122f3941ecbe
yoni <sdy32@naver.com>
2022-05-20 15:27:24 +0900

    feat: 레이아웃 구현 완료

--- script.js
+++ script.js
@@ -61,3 +61,7 @@ const render = (element) => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+
+
+// 질문 추가 기능 구현
