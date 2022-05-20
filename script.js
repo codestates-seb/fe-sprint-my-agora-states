0e654cf2d4a0064b880a86bae1b2e41026d5a406
dukjjang <jhd7292@naver.com>
2022-05-20 18:13:17 +0900

    ls 수정

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const li = document.querySelector(".discussion__container");
 const downBtn = document.querySelector(".downBtn");
 
 const toggle = (function () {
-  let isShow = false;
+  let isShow = true;
 
   // ① 클로저를 반환
   return function () {
@@ -149,7 +149,6 @@ const render = (element) => {
 
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
-render(ul);
 
 const paginationEl = document.querySelector(".pagination");
 
