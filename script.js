a06abe1093d3dfdcf39139412459f3ade1af4dcf
SEB_FE_39_고영석 <104332815+ttfcl@users.noreply.github.com>
2022-05-20 14:56:56 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,64 @@
-// index.html을 열어서 agoraStatesDiscussions 배열 요소를 확인하세요.
-console.log(agoraStatesDiscussions);
+let i = 0;
+let count = 0;
 
-// convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
+let submitBox = document.querySelector("#submitbox")
+let abcd = {};
+
+submitBox.onclick = function() {
+  let masterName = document.querySelector(".mastername")
+  let masterTitle = document.querySelector(".mastertitle")
+  let masterQuestion = document.querySelector(".masterquestion")
+ 
+  abcd.author = masterName.value;
+  abcd.title = masterTitle.value;
+  abcd.bodyHTML = masterQuestion.value;
+  agoraStatesDiscussions.unshift(abcd)
+  abcd = {};
+  const tweets = document.querySelectorAll('.discussion__container')
+  tweets.forEach(function(tweet){
+     tweet.remove();
+  })
+  const ul = document.querySelector("ul.discussions__container");
+  render(ul);
+}
+
 const convertToDiscussion = (obj) => {
   const li = document.createElement("li"); // li 요소 생성
   li.className = "discussion__container"; // 클래스 이름 지정
 
   const avatarWrapper = document.createElement("div");
   avatarWrapper.className = "discussion__avatar--wrapper";
+  const avatarImg = document.createElement("img");
+  avatarImg.className = "discussion__avatar--image";
+  avatarImg.src = agoraStatesDiscussions[i].avatarUrl;
+  avatarImg.width = "64"
+  avatarImg.height = "64"
+  avatarWrapper.append(avatarImg);
   const discussionContent = document.createElement("div");
   discussionContent.className = "discussion__content";
+  const discussionTitle = document.createElement("h5");
+  discussionTitle.className = "discussion__title";
+  const discussionTitleAtag = document.createElement("a");
+  discussionTitleAtag.href = agoraStatesDiscussions[i].url;
+  discussionTitle.append(discussionTitleAtag);
+  discussionTitleAtag.textContent = agoraStatesDiscussions[i].title;
+  const discussionImpormaiton = document.createElement("div");
+  discussionImpormaiton.className = "discussion__information";
+  discussionImpormaiton.textContent = agoraStatesDiscussions[i].author + " / " + agoraStatesDiscussions[i].createdAt
+  discussionContent.append(discussionTitle, discussionImpormaiton)
   const discussionAnswered = document.createElement("div");
   discussionAnswered.className = "discussion__answered";
-
-  // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
-
-
-
+  const checkBox = document.createElement("input");
+  checkBox.className = "abc";
+  checkBox.type = "checkbox";
+  discussionAnswered.append(checkBox);
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
 };
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
-  for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
+  for (i = 0; i < agoraStatesDiscussions.length; i += 1) {
     element.append(convertToDiscussion(agoraStatesDiscussions[i]));
   }
   return;
