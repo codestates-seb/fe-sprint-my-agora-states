11279ccdfda9512ad11283a8d10bd3c1a88bbe21
homile <ssanho2@naver.com>
2022-05-20 21:31:20 +0900

    Local Storage 유지 및 상단 출력완료

--- script.js
+++ script.js
@@ -48,9 +48,11 @@ const convertToDiscussion = (obj) => {
   discussionAnswered.append(answerP);
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
+
   return li;
 };
 
+const lenAgoraData = agoraStatesDiscussions.length;
 let localDiscussions = JSON.parse(localStorage.getItem("agoraStatesDiscussions"));
 if (!localDiscussions){
   localDiscussions = agoraStatesDiscussions;
@@ -60,11 +62,17 @@ if (localStorage.length !== 0){
   agoraStatesDiscussions = localDiscussions;
 }
 
+console.log(lenAgoraData);
+
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
   console.log(element);
-  for (let i = 0; i < localDiscussions.length; i += 1) {
+  for (let i = 0; i < lenAgoraData; i += 1) {
     element.append(convertToDiscussion(localDiscussions[i]));
+  }
+  // 질문 추가시 작동
+  for (let j = lenAgoraData; j < localDiscussions.length; j++){
+    element.prepend(convertToDiscussion(localDiscussions[j]));
   }
   return;
 };
