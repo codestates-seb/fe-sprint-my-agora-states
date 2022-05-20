3c31cab69b085bfb28cd69ccf7f4293d60dd811b
Inhoob <inhoob@gmail.com>
2022-05-20 16:46:59 +0900

    myAgroa

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# My Agora States
+## 개요
+
+---
+
+게시판 만들기
+
+## 새롭게 알게된 것
+
+1. list-style-type:none; 을 해서 리스트 앞의 점을 없앰.
+2. centering
+   display:flex;
+   justify-content:center;
+   align-items: center;
+   flex = 0 0 0;
+   padding = 1rem;
+3. HTML 에서 form action을 통해 데이터를 js파일로 받아서 사용할 수 있다.
+4. DOM 요소를 조작하여 list를 갱신할 수 있음.
+5. 'submit'버튼을 누르면 바로 새로고침이 일어나서 정보를 저장할 수 없는데 preventDefault()를 통해 방금 벌어진 일들의 정보를 얻을 수 있다.
+
+## 어려움을 겪은 부분
+
+1. ~~html 중첩구조일때 querySelector로 내가 원하는 데이터를 어떻게 뽑는지~~ 이 부분은 그냥 querySelector를 쓰지 않아도 되는 부분이였다.
+2. ~~submit 버튼을 누를 때 왜 addEventListner로 등록한 게 잘 적용이 안되는지? 'click'으로 하면 잘만 적용된다..preventDefault()를 써도 잘 안됨.~~ submitBtn.~~addEventListner 함수를 만들 때 post라는 객체에 추가할 내용을 addEventLister 밖에(맨처음부분) 선언해주니까 해결됨~~
+   나중에 다시 생겨서 다시 click으로 바꿈. 아직 원인불명
+3. ~~unshift로 새로 생성한 게시물 post를 push로 밀어넣고 agorastatesdiscussions[0]엔 해당 post가 생성되었지만 그 후에 변화가 없음.~~
+
+## 추후 구현해볼만 한 것
+
+1. localstorage 를 이용해서 새로고침을 하더라도 데이터가 날아가지 않도록.
--- script.js
+++ script.js
@@ -1,25 +1,125 @@
-// index.html을 열어서 agoraStatesDiscussions 배열 요소를 확인하세요.
-console.log(agoraStatesDiscussions);
+//이 부분은 아이콘의 색깔 조정을 위해 미리 클래스의 쿼리셀렉터지정
+let elFaComment = document.querySelector('.fa-comment')
+let elFaAnswered = document.querySelector('.fa-circle-check')
 
-// convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
+let now = new Date();
+const timediff = function(createdTime){
+  let old = new Date (createdTime);
+  
+  let gap = now.getTime() - old.getTime(); //1970/1/1 12:00 기준 경과한밀리초
+  let sec_gap = gap / 1000;
+  let min_gap = gap / 1000 /60;
+
+  return min_gap;
+}
+//submit 버튼이 눌렸을 때 일어나는 일들
+const elAuthor = document.querySelector('input#author');
+const elTitle = document.querySelector('input#title');
+const elBody = document.querySelector('#story');
+const submitBtn = document.querySelector("#btn__submit"); //원래 #btn__submit
+const elCategory = document.querySelector('#category');
+
 const convertToDiscussion = (obj) => {
+
   const li = document.createElement("li"); // li 요소 생성
   li.className = "discussion__container"; // 클래스 이름 지정
 
   const avatarWrapper = document.createElement("div");
   avatarWrapper.className = "discussion__avatar--wrapper";
-  const discussionContent = document.createElement("div");
+
+  const discussionContent = document.createElement("div"); //1번째 :discussion content 
   discussionContent.className = "discussion__content";
-  const discussionAnswered = document.createElement("div");
-  discussionAnswered.className = "discussion__answered";
+  const discussionCategory = document.createElement("span"); //카테고리 적어줄 span태그
+  discussionCategory.className = "discussion__category";
+  
 
-  // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
+  const discussionTitle = document.createElement("h2"); //h2태그의 title클래스 생성
+  discussionTitle.className = "discussion__title";
+  const discussionTitleReference = document.createElement("a");
+  discussionTitleReference.href = obj.url;
+  discussionTitle.append(discussionTitleReference)
+  discussionCategory.textContent = obj.category;
+  discussionTitleReference.textContent = obj.title;
+  
+  
+  discussionContent.append(discussionCategory,discussionTitle);
+  
 
 
+  const discussionIcons = document.createElement("div");
+  discussionIcons.className = "discussion__icons";
+  const discussionIconsComment = document.createElement("div");
+  discussionIconsComment.className = "discussion__icons--comment";
+  const faComment = document.createElement("i");
+  faComment.className = "fa-solid fa-comment";
+  const faCircle = document.createElement("i");
+  const discussionIconsAnswered = document.createElement("div");
+  if (obj.answer!==null){  
+    faCircle.className = "fa-solid fa-circle-check";
+    faComment.className = "fa-solid fa-comment";
+  }else{
+    faCircle.className = "fa-solid fa-circle-check hide";
+    faComment.className = "fa-solid fa-comment hide";
+  }
+  
+  discussionIconsComment.append(faComment);
+  discussionIconsAnswered.append(faCircle);
+  discussionIcons.append(discussionIconsComment,discussionIconsAnswered)
+  
+  const avatarImg = document.createElement('img');
+  avatarImg.src = obj.avatarUrl;
+  avatarImg.alt = 'avatar of '+obj.author;
+  
 
-  li.append(avatarWrapper, discussionContent, discussionAnswered);
+  const discussionInformation = document.createElement('div');
+  const discussionNickname = document.createElement('div');
+  discussionNickname.className = "discussion__nickname";
+  const discussionDate = document.createElement('div');
+  discussionDate.className = "discussion__date";
+
+  //여기서부턴 위에서 지정해준 timediff를 몇분전으로 표기되도록 시간환산
+  discussionNickname.textContent = obj.author;
+  if(timediff(obj.createdAt)<60){
+    discussionDate.textContent = `${Math.floor(timediff(obj.createdAt))}분 전`
+  }else if(timediff(obj.createdAt)<1440){
+    discussionDate.textContent = `${Math.floor(timediff(obj.createdAt)/60)}시간 전`
+  }else if(timediff(obj.createdAt)<43200){
+    discussionDate.textContent = `${Math.floor(timediff(obj.createdAt)/1440)}일 전`
+  }else if(timediff(obj.createdAt)<518400){
+    discussionDate.textContent = `${Math.floor(timediff(obj.createdAt)/43200)}달 전`
+  }else{
+    discussionDate.textContent = `${Math.floor(timediff(obj.createdAt)/518400)}년 전`
+  }
+  discussionInformation.append(discussionNickname,discussionDate);
+  avatarWrapper.append(avatarImg,discussionInformation);
+
+  
+  li.append(discussionContent,discussionIcons,avatarWrapper);
+  ul.prepend(li)//prepend에 대해 잘 이해하지 못했지만 post를 밀어넣어주는역할
   return li;
 };
+//submit버튼을 눌렀을 때
+submitBtn.addEventListener("click",function(event){
+  let post={};
+  event.preventDefault();
+  console.log(elAuthor.value);
+  console.log(elTitle.value);
+  console.log(elBody.value);
+  alert('글이 등록되었습니다!');
+   //submit버튼을 누를때 그 값을 post에 저장
+  post['author']=elAuthor.value;
+  post['title']=elTitle.value;
+  post['createdAt'] = now;
+  post['answer'] = null;
+  post['avatarUrl']="https://cdn.pixabay.com/photo/2020/03/25/16/01/children-4967808__340.jpg";
+  post['category']=elCategory.value;
+  //document.select.category.value
+  convertToDiscussion(post);
+  elAuthor.value = ''
+  elTitle.value=''
+  elBody.value = ''
+  
+})
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
