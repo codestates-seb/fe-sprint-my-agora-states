f8367fb8bc755909ca5376be2fe4a3fd8b6fec74
LastCarol <lastcarol0315@gmail.com>
2022-05-20 19:04:35 +0900

    bareminium까지 완성

--- script.js
+++ script.js
@@ -71,6 +71,7 @@ const convertToDiscussion = (obj) => {
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
   element.innerHTML="";
+
   for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
     element.append(convertToDiscussion(agoraStatesDiscussions[i]));
   }
@@ -81,3 +82,30 @@ const render = (element) => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+
+const submitBtn = document.querySelector(".form__submit");
+submitBtn.addEventListener('click', function(event){
+  event.preventDefault();
+
+const UseName = document.getElementById("name");
+const USeTitle = document.querySelector("#title");
+const today = new Date().toLocaleDateString();
+
+const newdis = {
+  id: "",
+  createdAt: today,
+  title:UseTitle.value,
+  url: null,
+  author: UseName.value,
+  answer: null,
+  bodyHTML: "",
+  avatarUrl: ""
+}
+
+agoraStatesDiscussions.unshift(newdis);
+render(ul);
+
+});
+
+
