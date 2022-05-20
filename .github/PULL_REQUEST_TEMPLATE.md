4bf04a8194306f0fc61ecc74d1096b1c73f9b726
JiYJ725 <wldudwns07@naver.com>
2022-05-20 12:56:51 +0900

    Feat: 페이지네이션 추가 및 페이지네이션 / 스크롤 모드 전환 기능 추가

--- .github/PULL_REQUEST_TEMPLATE.md
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -7,6 +7,7 @@ Section1에서 배운 내용을 총 동원하여 나만의 멋진 아고라 스�
 ## 배포 링크
 
 Github Page 배포 링크를 아래 작성합니다.
+배포링크 : (https://jiyj725.github.io/fe-sprint-my-agora-states/)
 
 ## Bare Minimum Requirement Self Checklist
 
@@ -25,9 +26,9 @@ Github Page 배포 링크를 아래 작성합니다.
     - [V] `agoraStatesDiscussions` 배열에 추가한 데이터가 실제 쌓여야 합니다.
 - [V] Github Page 배포
   - [V] Github Page 배포 기능을 이용하여 누구나 볼 수 있게 배포합니다.
-- [ ] [코드스테이츠 fe-sprint-my-agora-states 리포지토리](https://github.com/codestates-seb/fe-sprint-my-agora-states)로 Pull Request
-  - [ ] 나만의 아고라 스테이츠를 코드스테이츠 깃허브에 Pull request합니다.
-  - [ ] 주어진 Pull request 형식에 따라주세요.
+- [V] [코드스테이츠 fe-sprint-my-agora-states 리포지토리](https://github.com/codestates-seb/fe-sprint-my-agora-states)로 Pull Request
+  - [V] 나만의 아고라 스테이츠를 코드스테이츠 깃허브에 Pull request합니다.
+  - [V] 주어진 Pull request 형식에 따라주세요.
 
 ## Advanced Challenge Self Checklist
 
@@ -35,12 +36,12 @@ Github Page 배포 링크를 아래 작성합니다.
 
 - [V] 현지 시간 적용
     - [V] 샘플 시간을 잘 변형하여, 현지 시간에 맞게 표현합니다. (ex. 오전 10:02:17)
-- [ ] 페이지네이션 기능
-    - [ ] 페이지네이션에 대해서 스스로 학습합니다.
-    - [ ] 한 페이지에 10개씩 디스커션이 보여야 합니다.
-    - [ ] 다음 페이지로 넘어갈 수 있어야 합니다.
-    - [ ] 이전 페이지로 돌아올 수 있어야 합니다.
-    - [ ] 다음 페이지가 없거나, 이전 페이지가 없는 경우 페이지를 유지해야 합니다.
+- [V] 페이지네이션 기능
+    - [V] 페이지네이션에 대해서 스스로 학습합니다.
+    - [V] 한 페이지에 10개씩 디스커션이 보여야 합니다.
+    - [V] 다음 페이지로 넘어갈 수 있어야 합니다.
+    - [V] 이전 페이지로 돌아올 수 있어야 합니다.
+    - [V] 다음 페이지가 없거나, 이전 페이지가 없는 경우 페이지를 유지해야 합니다.
 - [V] 디스커션 유지 기능
     - [V] LocalStorage에 대해서 스스로 학습하고, 새롭게 추가하는 Discussion이 페이지를 새로고침해도 유지되도록 제작합니다.
 
@@ -50,10 +51,19 @@ Github Page 배포 링크를 아래 작성합니다.
 
 - [V] form__container영역 고정 및 discussion__wrapper영역 자체 스크롤 구현
 - [V] 질문 리스트 hover시 확대기능 구현
-- []
+- [V] 페이지네이션 / Scroll Mode전환 기능
 
 ### 배포 시연 화면
 
 아래 예시를 지우고, 자신의 과제 시연 화면을 추가합니다.
 
- ![image](https://s3.ap-northeast-2.amazonaws.com/urclass-images/NB0JkuHQnLg8X1woSRS84-1652915757557.gif)
+블로그 (https://velog.io/@zer0jun/%EC%BD%94%EB%93%9C%EC%8A%A4%ED%85%8C%EC%9D%B4%EC%B8%A0-Section1%EA%B3%BC%EC%A0%9C)
+
+1. 등록 기능
+ ![image](https://velog.velcdn.com/images/zer0jun/post/fe36d6e5-9bd2-4041-825e-e40f923596e3/image.gif)
+
+2. 스크롤 기능
+ ![image](https://velog.velcdn.com/images/zer0jun/post/89f7629d-4c83-4830-9616-ca8de89dc1df/image.gif)
+
+3. 페이지네이션 기능
+ ![image](https://velog.velcdn.com/images/zer0jun/post/555130e3-9f5f-4530-b79b-cf1f038727bf/image.gif)
--- script.js
+++ script.js
@@ -67,7 +67,7 @@ const addLocalStorageData = () => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 addLocalStorageData(); // 로컬스토리지 데이터를 agoraStatesDiscussions배열에 추가
-render(ul);
+
 
 // 디스커션 추가 기능 submit버튼 누르면 실제 디스커션이 추가되어야 한다.
 const submit = document.querySelector(".submit"); // submit버튼
@@ -91,8 +91,123 @@ const addDiscussions = () => {
   localStorage.setItem(cutDate, objString);
 
   agoraStatesDiscussions.push(obj);
+  console.log(agoraStatesDiscussions);
   ul.prepend(convertToDiscussion(agoraStatesDiscussions[agoraStatesDiscussions.length - 1]));
   alert("Discusstion 등록 완료");
 }
 
 submit.addEventListener('click', addDiscussions)
+
+
+
+//// 페이지네이션
+// 필요 변수 선언
+const maxContent = 10;
+const maxButton = 3;
+const contents = document.querySelector(".contents");
+const buttons = document.querySelector(".buttons");
+const prev = document.createElement("button");
+const next = document.createElement("button");
+const numOfContent = agoraStatesDiscussions.length;
+const maxPage = Math.ceil(numOfContent / maxContent);
+let page = 1;
+
+// 글 목록 & 버튼 생성 함수 구현
+const makeContent = (id) => {
+  const content = document.createElement("li");
+  content.classList.add("content");
+  content.innerHTML = ` 
+  <span class="content__id">${id}</span> 
+  <span class="content__title">게시물 제목</span> 
+  <span class="content__author">작성자</span> 
+  <span class="content__date">2022.01.01</span> 
+  `;
+  return content;
+};
+
+const makeButton = (id) => {
+  const button = document.createElement("button");
+  button.classList.add("button");
+  button.dataset.num = id;
+  button.innerText = id;
+  button.addEventListener("click", (e) => {
+    Array.prototype.forEach.call(buttons.children, (button) => {
+      if (button.dataset.num) button.classList.remove("active");
+    });
+    e.target.classList.add("active");
+    console.log(parseInt(e.target.dataset.num));
+    renderContent(parseInt(e.target.dataset.num));
+  });
+  return button;
+};
+
+// 렌더링 함수 구현
+const renderContent = (page) => {
+  // 목록 리스트 초기화 
+  while (contents.hasChildNodes()) { contents.removeChild(contents.lastChild); }
+  // 글의 최대 개수를 넘지 않는 선에서, 화면에 최대 10개의 글 생성 
+  for (let id = (page - 1) * maxContent + 1; id <= page * maxContent && id <= numOfContent; id++) {
+    //contents.appendChild(makeContent(id)); 
+    contents.appendChild(convertToDiscussion(agoraStatesDiscussions[id - 1]));
+  }
+};
+const renderButton = (page) => {
+  // 버튼 리스트 초기화 
+  while (buttons.hasChildNodes()) {
+    buttons.removeChild(buttons.lastChild);
+  }
+  // 화면에 최대 5개의 페이지 버튼 생성 
+  for (let id = page; id < page + maxButton && id <= maxPage; id++) {
+    buttons.appendChild(makeButton(id));
+  }
+  // 첫 버튼 활성화(class="active") 
+  buttons.children[0].classList.add("active");
+  buttons.prepend(prev);
+  buttons.append(next);
+  // 이전, 다음 페이지 버튼이 필요한지 체크 
+  if (page - maxButton < 1) buttons.removeChild(prev); if (page + maxButton > maxPage) buttons.removeChild(next);
+};
+const render2 = (page) => {
+  renderContent(page);
+  renderButton(page);
+};
+
+const goPrevPage = () => {
+  page -= maxButton; render2(page);
+};
+const goNextPage = () => {
+  page += maxButton; render2(page);
+};
+prev.classList.add("button", "prev");
+prev.innerHTML = '<ion-icon name="chevron-back-outline"></ion-icon>';
+prev.addEventListener("click", goPrevPage);
+next.classList.add("button", "next");
+next.innerHTML = '<ion-icon name="chevron-forward-outline"></ion-icon>';
+next.addEventListener("click", goNextPage);
+
+// 스크롤 / 페이징 모드 전환
+const discussionWrapper = document.querySelector(".discussion__wrapper");
+const article = document.querySelector(".article");
+const switchBtn = document.querySelector(".switch-button input");
+const convertText = document.querySelector(".convertText");
+render(ul); // 스크롤 모드
+var toggle = (function () {
+  var isShow = false;
+  return function () {
+    // box.style.display = isShow ? 'block' : 'none';
+    if (!isShow) {
+      discussionWrapper.style.display = 'none';
+      article.style.display = 'block';
+      convertText.textContent = "Pagination Mode";
+      render2(page); // 페이징 모드
+    }
+    else {
+      discussionWrapper.style.display = 'block';
+      article.style.display = 'none';
+      convertText.textContent = "Scroll Mode";
+    }
+    isShow = !isShow;
+    console.log("확인");
+  };
+})();
+switchBtn.onclick = toggle;
