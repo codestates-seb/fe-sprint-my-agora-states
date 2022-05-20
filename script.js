0bb7bbf3327cb1aa12667c2f1d2976720ea91bee
jungyunhan <hanjy2008@naver.com>
2022-05-20 14:25:40 +0900

    Bare Minimum Requirement 완료

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ const convertToDiscussion = (obj) => {
   avatarImg.alt = "avatar of" + obj.author;
   avatarWrapper.append(avatarImg);
 
-  const discussionTitle = document.createElement("h2");
+  const discussionTitle = document.createElement("h3");
   discussionTitle.className = "discussion__title";
 
   const discussionLink = document.createElement("a");
@@ -57,3 +57,52 @@ const render = (element) => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+//form에서 적은 내용 ul에 추가하기
+const questionForm = document.querySelector(".form");
+
+const formToDiscussion = (obj) => {
+  const li = document.createElement("li"); // li 요소 생성
+  li.className = "discussion__container"; // 클래스 이름 지정
+
+  const avatarWrapper = document.createElement("div");
+  avatarWrapper.className = "discussion__avatar--wrapper";
+  const discussionContent = document.createElement("div");
+  discussionContent.className = "discussion__content";
+  const discussionAnswered = document.createElement("div");
+  discussionAnswered.className = "discussion__answered";
+
+  const avatarImg = document.createElement("img");
+  avatarImg.className = "discussion__avatar--image";
+  avatarImg.src =
+    "https://imagescdn.gettyimagesbank.com/171/201607/a10519973.jpg";
+  avatarWrapper.append(avatarImg);
+
+  const discussionTitle = document.createElement("h3");
+  discussionTitle.className = "discussion__title";
+
+  const discussionLink = document.createElement("a");
+  discussionLink.href = "";
+  discussionLink.textContent = obj[1].value;
+  discussionTitle.append(discussionLink);
+  discussionContent.append(discussionTitle);
+
+  const discussionInformation = document.createElement("div");
+  discussionInformation.className = "discussion__information";
+  discussionInformation.textContent = obj[0].value;
+  discussionContent.append(discussionInformation);
+
+  const discussionAnswer = document.createElement("p");
+  discussionAnswer.innerText = "☒";
+  discussionAnswered.append(discussionAnswer);
+
+  li.append(avatarWrapper, discussionContent, discussionAnswered);
+  return li;
+};
+
+function onSubmit(event) {
+  event.preventDefault();
+  ul.prepend(formToDiscussion(questionForm));
+}
+
+questionForm.addEventListener("submit", onSubmit);
