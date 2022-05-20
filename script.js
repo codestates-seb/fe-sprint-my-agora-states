c5e1555ffd8159c9928d8d94f16335adfe5997da
SANGBIN <mosangbin94@gmail.com>
2022-05-20 11:43:45 +0900

    local storage

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// index.html을 열어서 agoraStatesDiscussions 배열 요소를 확인하세요.
-console.log(agoraStatesDiscussions);
-
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
 const convertToDiscussion = (obj) => {
   const li = document.createElement("li"); // li 요소 생성
@@ -62,10 +59,11 @@ const render = (element, num1, num2) => {
 };
 
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
-let MIN = 0;
-let MAX = 10;
+let startIdx = 0;
+let endIdx = 10;
+
 const ul = document.querySelector("ul.discussions__container");
-render(ul, MIN, MAX);
+// render(ul, startIdx, endIdx);
 
 // form
 const inputName = document.querySelector("#name");
@@ -98,23 +96,45 @@ newDiscussionBtn.addEventListener("click", () => {
 
 // local storage
 
-// const DISCUSSIONS_KEY = "discussions";
+const DISCUSSIONS_KEY = "discussions";
 
-// const saveDiscussion = () => {
-//   localStorage.setItem(DISCUSSIONS_KEY, JSON.stringify(agoraStatesDiscussions));
-// };
-// const savedDiscussions = localStorage.getItem(DISCUSSIONS_KEY);
-// const parsedDiscussions = JSON.parse(savedDiscussions);
+const saveDiscussion = () => {
+  localStorage.setItem(DISCUSSIONS_KEY, JSON.stringify(agoraStatesDiscussions));
+};
 
-// const renderLocalStorage = (element, num1, num2) => {
-//   for (let i = num1; i < num2; i += 1) {
-//     element.append(convertToDiscussion(parsedDiscussions[i]));
-//   }
-//   return;
-// };
+const saveLocalStorage = () => {
+  localStorage.setItem(DISCUSSIONS_KEY, JSON.stringify(parsedDiscussions));
+}
+
+const savedDiscussions = localStorage.getItem(DISCUSSIONS_KEY);
+const parsedDiscussions = JSON.parse(savedDiscussions);
+
+const renderLocalStorage = (element, num1, num2) => {
+  for (let i = num1; i < num2; i += 1) {
+    element.append(convertToDiscussion(parsedDiscussions[i]));
+  }
+  return;
+};
+
+console.log(parsedDiscussions);
+
+const formReset = () => {
+  inputName.value = "";
+  inputTitle.value = "";
+  inputStory.value = "";
+  formContainer.classList.add("hide");
+  submitBtn.disabled = true;
+};
+
+const removeChildes = (el) => {
+  while (el.firstChild) {
+    el.firstChild.remove();
+  }
+};
 
 submitBtn.addEventListener("click", (e) => {
   e.preventDefault();
+
   const newDiscussion = {
     id: Date.now(),
     createdAt: new Date(),
@@ -127,23 +147,30 @@ submitBtn.addEventListener("click", (e) => {
       "https://avatars.githubusercontent.com/u/95295766?s=64&u=85d493e0be0d2ca55965efd9f6c5b268c9dca168&v=4",
   };
 
-  inputName.value = "";
-  inputTitle.value = "";
-  inputStory.value = "";
-  formContainer.classList.add("hide");
-  submitBtn.disabled = true;
+  formReset();
 
-  while (ul.firstChild) {
-    ul.firstChild.remove();
+  removeChildes(ul);
+
+  if (!savedDiscussions) {
+    agoraStatesDiscussions.unshift(newDiscussion);
+    saveDiscussion();
+  } else {
+    parsedDiscussions.unshift(newDiscussion);
+    saveLocalStorage();
   }
 
-  agoraStatesDiscussions.unshift(newDiscussion);
+  // agoraStatesDiscussions.unshift(newDiscussion);
   // ul.insertBefore(convertToDiscussion(newDiscussion), ul.firstChild);
 
   // add to Local Storage
   // saveDiscussion();
 
-  render(ul, MIN, MAX);
+  // render(ul, startIdx, endIdx);
+  if (!savedDiscussions) {
+    render(ul, startIdx, endIdx);
+  } else {
+    renderLocalStorage(ul, startIdx, endIdx);
+  }
 });
 
 // pagination
@@ -151,23 +178,37 @@ const nextPageBtn = document.querySelector("#next__page");
 const prevPageBtn = document.querySelector("#prev__page");
 
 nextPageBtn.addEventListener("click", () => {
-  if (MAX > agoraStatesDiscussions.length) return;
-  while (ul.firstChild) {
-    ul.firstChild.remove();
-  }
-  MIN += 10;
-  MAX += 10;
+  if (endIdx > agoraStatesDiscussions.length) return;
+  removeChildes(ul);
 
-  render(ul, MIN, MAX);
+  startIdx += 10;
+  endIdx += 10;
+
+  // render(ul, startIdx, endIdx);
+  if (!savedDiscussions) {
+    render(ul, startIdx, endIdx);
+  } else {
+    renderLocalStorage(ul, startIdx, endIdx);
+  }
 });
 
 prevPageBtn.addEventListener("click", () => {
-  if (MIN <= 0) return;
-  while (ul.firstChild) {
-    ul.firstChild.remove();
-  }
-  MIN -= 10;
-  MAX -= 10;
+  if (startIdx <= 0) return;
+  removeChildes(ul);
 
-  render(ul, MIN, MAX);
+  startIdx -= 10;
+  endIdx -= 10;
+
+  // render(ul, startIdx, endIdx);
+  if (!savedDiscussions) {
+    render(ul, startIdx, endIdx);
+  } else {
+    renderLocalStorage(ul, startIdx, endIdx);
+  }
 });
+
+if (!savedDiscussions) {
+  render(ul, startIdx, endIdx);
+} else {
+  renderLocalStorage(ul, startIdx, endIdx);
+}
