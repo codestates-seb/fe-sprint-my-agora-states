427a0a13a858dead683d8a1ae6baaa4ebdc405eb
NR0617 <nara20210617@gmail.com>
2022-05-20 14:34:26 +0900

    배열에 더하기

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 // index.html을 열어서 agoraStatesDiscussions 배열 요소를 확인하세요.
 console.log(agoraStatesDiscussions);
 
+const addContainer = () => {
+  let authorName = document.getElementById("name").value;
+  let authorTitle = document.getElementById("title").value;
+  let authorStory = document.getElementById("story").value;
+  console.log(authorName, authorTitle)
+
+  let authorQuestion = {
+    title: authorTitle,
+    author: authorName,
+    answer: null,
+    avatarUrl: agoraStatesDiscussions[0].avatarUrl
+  }
+
+  agoraStatesDiscussions.unshift(authorQuestion)
+
+}
+
+document.getElementById("btn-submit").addEventListener('click', addContainer())
+
+
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
 const convertToDiscussion = (obj) => {
   const li = document.createElement("li"); // li 요소 생성
@@ -44,6 +64,7 @@ const convertToDiscussion = (obj) => {
   return li;
 };
 
+
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
   for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
@@ -52,6 +73,11 @@ const render = (element) => {
   return;
 };
 
+
+
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
+
 render(ul);
+
+//이벤트를 받아서 배열에 추가해보자
