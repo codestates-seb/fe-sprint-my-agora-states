1551d7f00e35b3a9219a7d856a9b06362fdfe639
YHJ96 <yhj878812@naver.com>
2022-05-20 12:25:27 +0900

    feat: 이전, 다음 버튼 기능 추가

--- src/javascript/components/pages.js
+++ src/javascript/components/pages.js
@@ -1,20 +1,35 @@
 export default function Pages() {
-    this.$pagesContainer = document.querySelector(".pages__container");
+    this.$pagesWrapper = document.querySelector(".pages__wrapper");
     // 페이지 넘버 생성
     this.createPages = (pageCount) => {
         const $div = document.createElement("div");
+        $div.className = "pages__container";
+        $div.appendChild(createPreviousBtn());
         for (let i = 1; i <= pageCount; i += 1) {
             const $span = document.createElement("span");
             $span.textContent = i;
-            $span.className = "pages";
+            $span.className = "pages__item";
             $span.setAttribute("data-index", i);
-            $span.style.padding = "0.7rem";
             $div.appendChild($span);
         }
+        $div.appendChild(createNextBtn());
         return $div;
     }
     this.deletePages = () => {
-        const nodes = [...this.$pagesContainer.childNodes];
+        const nodes = [...this.$pagesWrapper.childNodes];
         nodes.forEach((node) => node.remove());
+    }
+
+    const createPreviousBtn = () => {
+        const $span = document.createElement("span");
+        $span.textContent = "< Previous";
+        $span.className = "pages__item previous";
+        return $span;
+        }
+    const createNextBtn = () => {
+        const $span = document.createElement("span");
+        $span.textContent = "> Next";
+        $span.className = "pages__item next";
+        return $span;
     }
 }
