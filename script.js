b04ca67a5df0145d91eca27c3101fbf5eb09e5c4
seonnn <ssni0105@gmail.com>
2022-05-20 14:46:52 +0900

    디스커션 나열, css, 디스커션 추가, 현지시간 적용 기능 업데이트

--- script.js
+++ script.js
@@ -1,5 +1,51 @@
 // index.html을 열어서 agoraStatesDiscussions 배열 요소를 확인하세요.
 console.log(agoraStatesDiscussions);
+const inputName = document.querySelector("#name");
+const inputTitle = document.querySelector("#title");
+const inputStory = document.querySelector("#story");
+const submitForm = document.querySelector("#submit");
+
+submitForm.onclick = function (e) {
+  e.preventDefault();
+  const date = () =>
+    `${new Date().getFullYear()}-${
+      String(new Date().getMonth()).length === 1
+        ? "0" + String(new Date().getMonth() + 1)
+        : new Date().getMonth() + 1
+    }-${
+      String(new Date().getDate()).length === 1
+        ? "0" + String(new Date().getDate())
+        : new Date().getDate()
+    }T${
+      String(new Date().getMinutes()).length === 1
+        ? "0" + String(new Date().getHours())
+        : new Date().getHours()
+    }:${
+      String(new Date().getMinutes()).length === 1
+        ? "0" + String(new Date().getMinutes())
+        : new Date().getMinutes()
+    }:${
+      String(new Date().getSeconds()).length === 1
+        ? "0" + String(new Date().getSeconds())
+        : new Date().getSeconds()
+    }Z`;
+  agoraStatesDiscussions.unshift({
+    author: inputName.value,
+    title: inputTitle.value,
+    createdAt: date(),
+    avatarUrl: "https://avatars.githubusercontent.com/u/90498108?s=96&v=4",
+  });
+  let li = convertToDiscussion({
+    author: inputName.value,
+    title: inputTitle.value,
+    createdAt: date(),
+    avatarUrl: "https://avatars.githubusercontent.com/u/90498108?s=96&v=4",
+  });
+  ul.prepend(li);
+  inputName.value = "";
+  inputTitle.value = "";
+  inputStory.value = "";
+};
 
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
 const convertToDiscussion = (obj) => {
@@ -8,14 +54,45 @@ const convertToDiscussion = (obj) => {
 
   const avatarWrapper = document.createElement("div");
   avatarWrapper.className = "discussion__avatar--wrapper";
+
+  const avatarImg = document.createElement("img");
+  avatarImg.src = obj.avatarUrl;
+  avatarImg.alt = "avatar of" + obj.author;
+  avatarWrapper.append(avatarImg);
+
   const discussionContent = document.createElement("div");
   discussionContent.className = "discussion__content";
+
+  const discussionContentTitle = document.createElement("div");
+  discussionContentTitle.className = "discussion__title";
+  discussionContent.append(discussionContentTitle);
+
+  const discussionContentLink = document.createElement("a");
+  discussionContentLink.append(obj.title);
+  discussionContentLink.href = obj.url;
+  discussionContentTitle.append(discussionContentLink);
+
+  const discusstionInformation = document.createElement("div");
+  discusstionInformation.className = "discussion__information";
+  discusstionInformation.append(
+    `${obj.author} / ${obj.createdAt.slice(0, 10)} ${
+      Number(obj.createdAt.slice(11, 13)) > 12
+        ? "오후 " +
+          String(Number(obj.createdAt.slice(11, 13)) - 12) +
+          ":" +
+          obj.createdAt.slice(14, obj.createdAt.length - 1)
+        : "오전 " + obj.createdAt.slice(11, obj.createdAt.length - 1)
+    }`
+  );
+  discussionContent.append(discusstionInformation);
+
   const discussionAnswered = document.createElement("div");
   discussionAnswered.className = "discussion__answered";
+  obj.answer
+    ? discussionAnswered.append("✅")
+    : discussionAnswered.append("❎");
 
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
-
-
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
