f1f254d1641d8fedc3b9637c06928f3b17720f33
cjsgmail <cjs0097@gmail.com>
2022-05-20 16:12:55 +0900

    css 완성

--- script.js
+++ script.js
@@ -38,22 +38,37 @@ const convertToDiscussion = (obj) => {
   information.textContent = `${obj.author} / ${obj.createdAt}`;
   discussionContent.append(information);
   
-  
+  if (obj.answer !== null){
   const answerd = document.createElement("div");
   const answerdP = document.createElement("p");
   answerd.className = "discussion__answered";
-  answerdP.textContent = "☑";
+  answerdP.textContent = "✓";
   answerd.append(answerdP);
+  discussionAnswered.append(answerd);
+} else {
+  const answerd = document.createElement("div");
+  const answerdP = document.createElement("p");
+  answerd.className = "discussion__answered";
+  answerdP.textContent = "✗";
+  answerd.append(answerdP);
+  discussionAnswered.append(answerd);
   
+  
+}
 
-  
 
-  // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
-  
-  
-  
-  li.append(avatarWrapper, discussionContent, discussionAnswered);
-  return li;
+const inputButton = document.querySelector('#button');
+const nameValue = document.querySelector('#name1');
+const tilteValue = document.querySelector('#name2');
+
+//버튼 클릭시 agoraStatesDiscussions배열 맨 앞에 {id title createdAt} 객체가 추가 된다
+inputButton.onclick = function(){
+  agoraStatesDiscussions.unshift({author: nameValue.textContent, title: tilteValue.textContent, createdAt: new Date()});
+}
+
+
+li.append(avatarWrapper, discussionContent, discussionAnswered);
+return li;
 };
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
