37b769e7d60082da31eb32e08f60de691c031b6a
moondoyeon <mmmdo21@gmail.com>
2022-05-20 15:56:36 +0900

    commit

--- script.js
+++ script.js
@@ -13,10 +13,30 @@ const convertToDiscussion = (obj) => {
   const discussionAnswered = document.createElement("div");
   discussionAnswered.className = "discussion__answered";
 
+  const avatarImg = document.createElement('img');
+  avatarImg.src = obj.avatarUrl;
+  avatarImg.alt = "avatar of" + obj.author;
+  avatarImg.className = "discussion__avartar--image";
+  avatarWrapper.append(avatarImg);
+
+  const discussionTitle = document.createElement("h3");
+  discussionTitle.className = "discussion__title";
+
+  const titleLink = document.createElement("a");
+  titleLink.href = obj.url;
+  titleLink.textContent = obj.title;
+  discussionTitle.append(titleLink);
+
+  const discussionInfo = document.createElement("div");
+  discussionInfo.className = "discussion__information";
+  discussionInfo.textContent = obj.author + " / " +obj.createdAt;
+  discussionContent.append(discussionTitle, discussionInfo);
+
+  const answeredIcon = document.createElement("p");
+  answeredIcon.textContent = obj.answer ? "☑︎" : "◻︎";
+  discussionAnswered.append(answeredIcon);
+
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
-
-
-
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
 };
@@ -29,6 +49,12 @@ const render = (element) => {
   return;
 };
 
+// console.log(obj); 41번 찍힘
+// render라는 함수를 한번호출하면 반복문이 돌면서 계속찍히기 때문임.
+// 반복문을 통해서 정보들을 접근할수있고 
+
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+
