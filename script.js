5170dd1dd28f1433e3b72eb6846511da52f124a6
GH <gwanghyunjeon@gmail.com>
2022-05-20 09:25:47 +0900

    질문 추가 기능 빼고 진행완료

--- script.js
+++ script.js
@@ -57,3 +57,21 @@ const render = (element) => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+// 질문 접수
+const submit = document.querySelector('input[type="submit"]');
+const name = document.querySelector('#name');
+const title = document.querySelector('#title');
+const story = document.querySelector('#story');
+
+const submitYourQuestion = () => {
+  if (name.value === '') {
+    return alert('이름을 입력하세요')
+  } else if (title.value === '') {
+    return alert('제목을 입력하세요')
+  } else if (story.value === '') {
+    return alert('질문을 작성하세요')
+  } else {
+
+  }
+}
