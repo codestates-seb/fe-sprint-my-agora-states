ed8a8a5ec33972c9bc03d294e32c6660eef18b83
TaeHyung KIM <th1122kim@gmail.com>
2022-05-20 15:31:09 +0900

    베어 미니멈 수정

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
 // index.html을 열어서 agoraStatesDiscussions 배열 요소를 확인하세요.
 console.log(agoraStatesDiscussions);
 
+const elBtn = document.querySelector('.btn');
+const NewObj = {};
+const elName = document.querySelector('#name');
+const elTitle = document.querySelector('#title');
+
+
+elBtn.onclick = function() {
+    Add();
+    console.log(agoraStatesDiscussions);
+}
+
+function Add () {
+  NewObj.id = "D_kwDOHOApLM4APXTN";
+  const date = new Date().toLocaleDateString();
+  NewObj.createdAt = date;
+  NewObj.title = document.querySelector('#title').value;
+  NewObj.author = document.querySelector('#name').value;
+  NewObj.answer = null;
+  NewObj.bodyHTML = null;
+  NewObj.avatarUrl = "https://avatars.githubusercontent.com/u/12145019?s=64&u=5c97f25ee02d87898457e23c0e61b884241838e3&v=4";
+  agoraStatesDiscussions.unshift(NewObj);
+  console.log(agoraStatesDiscussions)
+}
+
+
+
+
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
-const convertToDiscussion = (obj) => {
+const convertToDiscussion = (obj, i) => {  //, i추가
   const li = document.createElement("li"); // li 요소 생성
   li.className = "discussion__container"; // 클래스 이름 지정
 
@@ -15,7 +42,55 @@ const convertToDiscussion = (obj) => {
 
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
 
+  const avatarImg = document.createElement('img');
+  avatarImg.className = "discussion__avatar--image"  // 추가함!!!
+  avatarImg.src = agoraStatesDiscussions[i].avatarUrl;  // 0>i로 변경
+  avatarImg.alt = 'avatar of ' + agoraStatesDiscussions[i].author;  // 0>i로 변경
+  avatarWrapper.append(avatarImg);
 
+  // 여기서 부터 추가
+  const contentTitle = document.createElement('h2');
+  contentTitle.className = "discussion__title";
+  const contentTitleLink = document.createElement('a');
+  contentTitleLink.href = agoraStatesDiscussions[i].url;
+  contentTitleLink.textContent = agoraStatesDiscussions[i].title;
+
+  const contentInformation = document.createElement('div');
+  contentInformation.className = 'discussion__information';
+  const contentInfoName = document.createElement('div');
+  contentInfoName.textContent = agoraStatesDiscussions[i].author;
+  const contentInfoDelimiter = document.createElement('div');
+  contentInfoDelimiter.textContent = ' / '
+  const contentInfoTime = document.createElement('div');
+  const newDate = new Date(agoraStatesDiscussions[i].createdAt);
+  contentInfoTime.textContent = newDate.toLocaleString();
+
+  discussionAnswered.className = "discussion__answered";
+  const AnsweredOk = document.createElement('p');
+  AnsweredOk.textContent = '☑';
+  const AnsweredNotOk = document.createElement('p');
+  AnsweredNotOk.textContent = '☒';
+  
+  
+
+  discussionContent.append(contentTitle);
+  contentTitle.append(contentTitleLink);
+  
+  discussionContent.append(contentInformation);
+  contentInformation.append(contentInfoName);
+  contentInformation.append(contentInfoDelimiter);
+  contentInformation.append(contentInfoTime);
+
+  discussionContent.append(discussionAnswered);
+
+  if(agoraStatesDiscussions[i].answer === null) {
+    discussionAnswered.append(AnsweredNotOk);
+  }
+  else {
+    discussionAnswered.append(AnsweredOk);
+  }
+
+  // 여기까지 추가내용
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
@@ -23,8 +98,9 @@ const convertToDiscussion = (obj) => {
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
+  element.innerHTML="";
   for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
-    element.append(convertToDiscussion(agoraStatesDiscussions[i]));
+    element.append(convertToDiscussion(agoraStatesDiscussions[i], i));
   }
   return;
 };
@@ -32,3 +108,6 @@ const render = (element) => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+
+
