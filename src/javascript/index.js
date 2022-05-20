8c69c6b33ce7147c913315235e95c306aa2424e3
YHJ96 <yhj878812@naver.com>
2022-05-20 10:15:26 +0900

    feat: 랜더링 함수 구현

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,24 +1,56 @@
-import Discussion from "./components/discussion.js";
+import Discussions from "./components/discussions.js";
 import Pages from "./components/pages.js";
+import { createArrayPrototypeDivide } from "./utils/divide.js";
 
-const discussion = new Discussion();
-const pages = new Pages();
-pages.init(discussion.$ul, discussion.createDiscussion, discussion.deleteDiscussion);
+createArrayPrototypeDivide();
 
-// 함수 수정
-const $form = document.querySelector("form");
-$form.addEventListener("submit", e => {
-    e.preventDefault();
-    const $textName = document.querySelector("#textName");
-    const $textTitle = document.querySelector("#textTitle");
-    // const $story = document.querySelector("#story");
-    const date = new Date();
-    const userData = {
-        title: $textTitle.value,
-        avatarUrl: null,
-        url: null,
-        answer: null,
-        author: $textName.value,
-        createdAt: date
+function App() {
+    const discussion = new Discussions();
+    const pages = new Pages();
+    const $form = document.querySelector("form");
+    const data = agoraStatesDiscussions.divide(10);
+    let currentPage = 0;
+    // 데이터 생성 함수
+    const createUserData = (author, title) => {
+        const date = new Date();
+        const userData = {
+            title,
+            author,
+            avatarUrl: null,
+            url: null,
+            answer: null,
+            createdAt: date
+        }
+        return userData;
     }
-});
+    // 랜더링 함수
+    const render = () => {
+        discussion.deleteDiscussions();
+        pages.deletePages();
+        pages.$pagesContainer.appendChild(pages.createPages(data.length));
+        discussion.$discussionsContainer.appendChild(discussion.createDiscussions(data[currentPage]));
+    }
+    this.init = () => {
+        render();
+        // 데이터 추가 함수
+        $form.addEventListener("submit", (e) => {
+            e.preventDefault();
+            const dataLastIndex = data.length - 1;
+            const $textName = document.querySelector("#textName");
+            const $textTitle = document.querySelector("#textTitle");
+            const userData = createUserData($textName.value, $textTitle.value);
+            data[dataLastIndex].push(userData);
+            render();
+        });
+        // 페이징네이션 네비게이터 함수
+        pages.$pagesContainer.addEventListener("click", (e) => {
+            const pageNumber = Number(e.target.dataset.index);
+            console.log(pageNumber);
+            if (isNaN(pageNumber)) return;
+            currentPage = pageNumber - 1;
+            render();
+        })
+    }
+}
+
+new App().init();
