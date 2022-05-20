35a79d3cd3c843a3a61beb4eacc36123eb4ae933
pjhyeok <vndepddl04@naver.com>
2022-05-20 16:17:24 +0900

    bare 절반정도하고 css layout까지만 했음

--- script.js
+++ script.js
@@ -14,8 +14,29 @@ const convertToDiscussion = (obj) => {
   discussionAnswered.className = "discussion__answered";
 
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
+  //avartarWrapper 채우기
+  const avatarImg = document.createElement("img"); //이미지 태그 생성.
+  avatarImg.className = "discussion__avatar--image";
+  avatarImg.src = obj.avatarUrl; //이미지 소스 가져오기 ,obj로 하는이유: 매개변수 안에 42번째줄 보면 data의 i번째가 들어가고 있음.
+  avatarImg.alt = "avatar of" + obj.author;
+  avatarWrapper.append(avatarImg);
 
+  //discussionContent 채우기
+  const discussionTitle = document.createElement("h2"); //h2 요소 생성.
+  discussionTitle.className = "discussion__title";
+  const discussionTitle__a = document.createElement("a"); //a 요소 생성.
+  discussionTitle__a.href = obj.url;
+  discussionTitle__a.textContent = obj.title;
+  discussionTitle.append(discussionTitle__a); //a요소 h2요소에 어펜드.
+  const discussionInfor = document.createElement("div"); //작성자 정보,시간 div생성.
+  discussionInfor.className = "discussion__information";
+  discussionInfor.textContent = obj.author + " / " + obj.createdAt;
+  discussionContent.append(discussionTitle, discussionInfor);
 
+  //discussionAnswered 채우기
+  const discussionAnswered__p = document.createElement("p");
+  discussionAnswered__p.textContent = "☑";
+  discussionAnswered.append(discussionAnswered__p);
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
@@ -24,7 +45,7 @@ const convertToDiscussion = (obj) => {
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
   for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
-    element.append(convertToDiscussion(agoraStatesDiscussions[i]));
+    element.append(convertToDiscussion(agoraStatesDiscussions[i])); //여기서 데이터가 계속 순회중.
   }
   return;
 };
