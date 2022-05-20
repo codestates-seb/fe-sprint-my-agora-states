59f6927c6b3dc2d8a88f67e57eb96cecb77a219b
RyuJiChang <104156381+RyuJiChang@users.noreply.github.com>
2022-05-20 16:28:21 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -14,17 +14,43 @@ const convertToDiscussion = (obj) => {
   discussionAnswered.className = "discussion__answered";
 
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
+  const avatarImg = document.createElement('img');
+  avatarImg.src = obj.avatarUrl;
+  avatarImg.alt = "avatar of " + obj.author; //??
+  avatarWrapper.append(avatarImg);
 
+const discussionTitle = document.createElement("h2");
+const titleAnchor = document.createElement("a");
+titleAnchor.href = obj.url;
+titleAnchor.textContent = obj.title;
+discussionTitle.append(titleAnchor);
 
+const discussionInformation = document.createElement("div");
+discussionInformation.className = "discussion__information";
+discussionInformation.textContent = `${obj.author} / ${new Date(obj.createdAt).toLocaleTimeString()}`;
+discussionContent.append(discussionTitle, discussionInformation);
+
+const checked = document.createElement("p");
+checked.textContent = obj.answer ? "✅" : "❎" ;
+discussionAnswered.append(checked);
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
 };
 
+//데이터값 받아오기 안됨
+// const newInputButton = document.querySelector("#button");
+// const newAuthor = document.querySelector("#newAuthor"); //name
+// const newTitle = document.querySelector("#newTitle");   //title
+// const time = new Date()
+// const newObj ={author : newAuthor, createdAt : time, title : newTitle, avatarUrl: "https://avatars.githubusercontent.com/u/79903256?s=64&v=4"};
+// Li.append(avatarWrapper, discussionContent, discussionAnswered)
+
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
+  element.innerHTML="" 
   for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
-    element.append(convertToDiscussion(agoraStatesDiscussions[i]));
+   element.append(convertToDiscussion(agoraStatesDiscussions[i]));
   }
   return;
 };
