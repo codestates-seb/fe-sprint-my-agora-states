4cb28e0019915bb99c2823b858b25ee389bbbaad
kanghyew0n <2018314001@tukorea.ac.kr>
2022-05-20 16:07:43 +0900

    2022-05-20-1차 수정

--- script.js
+++ script.js
@@ -15,7 +15,67 @@ const convertToDiscussion = (obj) => {
 
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
 
+  // avatar
+  const avatarImg = document.createElement("img");
+  avatarImg.src = obj.avatarUrl;
+  avatarImg.alt = "avatar of " + obj.author;
+  avatarWrapper.append(avatarImg);
 
+  //title => a
+  const discussionTitle = document.createElement("h2");
+  discussionTitle.className = "discussion__title";
+  discussionContent.append(discussionTitle);
+  const discussionTitleLink = document.createElement("a");
+  discussionTitleLink.href = obj.url;
+  discussionTitleLink.textContent = obj.title;
+  discussionTitle.append(discussionTitleLink);
+
+  //information
+  const discussionInformation = document.createElement("div");
+  discussionInformation.className = "discussion__information";
+  discussionInformation.textContent = obj.author + " / " + obj.createdAt;
+  discussionContent.append(discussionInformation);
+
+  //answered
+  const answered = document.createElement("p");
+  const answeredIcon = document.createElement("i");
+  answeredIcon.className = "fa-solid fa-circle-check";
+  answered.append(answeredIcon);
+  //const redIcon = (answered.style.color = "red");
+  //const greenIcon = (answered.style.color = "green");
+
+  if (obj.answer === null) {
+    answered.style.color = "red";
+    discussionAnswered.append(answered);
+  } else {
+    answered.style.color = "green";
+    discussionAnswered.append(answered);
+  }
+
+  // //submit
+  // const form = document.querySelector(".form");
+  // const name = document.querySelector("#name");
+  // const title = document.querySelector("#title");
+  // const story = document.querySelector("#story");
+
+  // const getInputValue = (event) => {
+  //   alert("실패~!!!!!!!");
+  //   event.preventDefault();
+  //   let nameValue = name.value;
+  //   let titleValue = title.value;
+
+  //   discussionInformation.textContent = nameValue + " / " + date;
+  //   discussionContent.append(discussionInformation);
+
+  //   discussionTitleLink.textContent = titleValue;
+  //   discussionTitle.append(discussionTitleLink);
+  // };
+
+  // const init = () => {
+  //   form.addEventListener("submit", getInputValue);
+  // };
+
+  // init();
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
@@ -28,6 +88,45 @@ const render = (element) => {
   }
   return;
 };
+
+// const form = document.querySelector(".form");
+// const username = document.querySelector("#name");
+// const title = document.querySelector("#title");
+// //const story = document.querySelector("#story");
+
+// function addList(event) {
+//   alert("실패~!!!!!!!");
+//   event.preventDefault();
+//   const newTitle = title.value;
+//   const newInfo = username.value;
+// }
+
+// form.addEventListener("submit", addList);
+
+//submit
+const form = document.querySelector(".form");
+const name = document.querySelector("#name");
+const title = document.querySelector("#title");
+const story = document.querySelector("#story");
+
+const getInputValue = (event) => {
+  alert("실패~!!!!!!!");
+  event.preventDefault();
+  let nameValue = name.value;
+  let titleValue = title.value;
+
+  discussionInformation.textContent = nameValue;
+  discussionContent.append(discussionInformation);
+
+  discussionTitleLink.textContent = titleValue;
+  discussionTitle.append(discussionTitleLink);
+};
+
+const init = () => {
+  form.addEventListener("submit", getInputValue);
+};
+
+init();
 
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
