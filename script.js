f9910db18e194cb4a087978c38c53b16205ef6dc
tahupapa <xognl3164@gmail.com>
2022-05-20 16:30:53 +0900

    1차 완료본

--- script.js
+++ script.js
@@ -13,10 +13,35 @@ const convertToDiscussion = (obj) => {
   const discussionAnswered = document.createElement("div");
   discussionAnswered.className = "discussion__answered";
 
-  // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
+  // 유저 이미지
+  const avatarImg = document.createElement('img');
+  avatarImg.src = obj.avatarUrl;
+  avatarImg.alt = 'avatar of ' + obj.author;
+  avatarImg.className = 'discussion__avatar--image'
+  avatarWrapper.append(avatarImg);
 
+  // 질문 내용
+  const dicussionTitle = document.createElement('h2');
+  const titleAnchor = document.createElement('a');
+  const titleInfo = document.createElement('div');
+  titleAnchor.href = obj.url;
+  titleAnchor.textContent = obj.title;
+  titleInfo.textContent = obj.author + ' / ' + obj.createdAt;
+  titleInfo.className = 'discussion__information';
+  dicussionTitle.append(titleAnchor);
+  discussionContent.append(dicussionTitle);
+  discussionContent.append(titleInfo);
 
+  // 답변 여부
+  const answerInfo = document.createElement('img');
+  if (obj.answer === null) {
+    answerInfo.src = (src="./img/x.png");
+  } else {
+    answerInfo.src = (src="./img/o.png");
+  }
+  discussionAnswered.append(answerInfo);
 
+  // li 요소에 추가
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
 };
@@ -32,3 +57,27 @@ const render = (element) => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+// 디스커션 추가기능
+const formName = document.querySelector('#nick');
+const formTitle = document.querySelector('#title');
+const btn = document.querySelector('#subBtn');
+const newObj = {
+  createdAt: '...',
+  title: formTitle.value,
+  author: formName.value,
+  avatarUrl: 'https://avatars.githubusercontent.com/u/99703878?s=400&u=0f4fe50ead7be329042c38f0ef1e6a6c2ac49448&v=4',
+  answer: null
+};
+
+const onBtn = (e) => {
+  newObj.title = formTitle.value;
+  newObj.author = formName.value;
+
+  agoraStatesDiscussions.unshift(newObj);
+  let li = convertToDiscussion(newObj);
+  ul.prepend(li);
+  return render(ul);
+}
+
+btn.addEventListener('click',onBtn);
