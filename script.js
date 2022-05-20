eb1174d3af0d12230bae8700e556dfaabbab31f1
Swan <99741801+adeline0618@users.noreply.github.com>
2022-05-21 01:28:58 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 'use strict';
+//처음에 바로 저장.
+localStorage.setItem('lists', JSON.stringify(agoraStatesDiscussions));
+//로컬에서 가져오기 //takedata[i]는 객체 takedata는 배열
+const existingLists = JSON.parse(localStorage.getItem('lists'));
+let newLists =
+  JSON.parse(localStorage.getItem('newlist')) !== null
+    ? JSON.parse(localStorage.getItem('newlist'))
+    : [];
+//로컬에 새로운 리스트 저장하는 함수
+const storeData = obj => {
+  localStorage.setItem('newlist', JSON.stringify(obj));
+};
+
 const makeEl = el => {
   return document.createElement(el);
 };
-
 const getEl = el => {
   return document.querySelector(el);
 };
 
-// //새로운 질문 등록 //
-const inputName = getEl('.input__name');
-const inputTitle = getEl('.input__title');
-const inputQuestion = getEl('.text__question');
-const submitBtn = getEl('.form__submit');
-
-// convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
+//데이터를 DOM으로 바꿔
 const convertToDiscussion = obj => {
-  const li = makeEl('li'); // li 요소 생성
+  const li = makeEl('li'); //li 요소 생성
   li.className = 'discussion__container'; // 클래스 이름 지정
 
   const avatarWrapper = makeEl('div');
@@ -25,15 +31,11 @@ const convertToDiscussion = obj => {
   const discussionAnswered = makeEl('div');
   discussionAnswered.className = 'discussion__answered';
 
-  // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
-
-  //img
   const avatarImg = makeEl('img');
   avatarImg.src = obj.avatarUrl;
   avatarImg.alt = 'avatar of ' + obj.author;
   avatarWrapper.append(avatarImg);
 
-  //title
   const discussionTitle = makeEl('h2');
   const titleAnchor = makeEl('a');
   discussionTitle.className = 'discussion__title';
@@ -41,7 +43,6 @@ const convertToDiscussion = obj => {
   titleAnchor.textContent = obj.title;
   discussionTitle.append(titleAnchor);
 
-  //
   const discussionInformation = makeEl('div');
   discussionInformation.className = 'discussion__information';
   discussionInformation.textContent = `${obj.author} / ${new Date(
@@ -59,21 +60,7 @@ const convertToDiscussion = obj => {
   return li;
 };
 
-//처음에 바로 저장.
-localStorage.setItem('lists', JSON.stringify(agoraStatesDiscussions));
-//로컬에서 가져오기 //takedata[i]는 객체 takedata는 배열
-const existingLists = JSON.parse(localStorage.getItem('lists'));
-let newLists =
-  JSON.parse(localStorage.getItem('newlist')) !== null
-    ? JSON.parse(localStorage.getItem('newlist'))
-    : [];
-
-//로컬에 새로운 리스트 저장하는 함수
-const storeData = obj => {
-  localStorage.setItem('newlist', JSON.stringify(obj));
-};
-
-// agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
+//모든 데이터를 화면에 렌더링하는 함수
 const render = (element, listArray) => {
   for (let i = 0; i < listArray.length; i += 1) {
     element.append(convertToDiscussion(listArray[i]));
@@ -81,37 +68,57 @@ const render = (element, listArray) => {
   return;
 };
 
-// const render2 = element => {
-//   if (newLists === null) newLists = [];
-//   for (let i = 0; i < newLists.length; i += 1) {
-//     element.append(convertToDiscussion(newLists[i]));
-//   }
-//   return;
-// };
-
-// ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
+// ul 요소에 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector('ul.discussions__container');
-// render(ul, newLists);
-// render(ul, existingLists);
 
+//pasination
 const pasing = (num, page = 1) => {
   let allList = [...newLists, ...existingLists];
-  // if (newLists === []) {
-  //   render(ul, existingLists.slice(0, num));
-  // }
-  // if (newLists.length > num) {
-  //   render(ul, newLists.slice(0, num));
-  // } else if (newLists.length < num) {
-  //   render(ul, newLists);
-  //   render(ul, existingLists.slice(0, num - newLists.length));
-  // } else {
-  //   render(ul, newLists);
-  //   render(ul, existingLists);
-  // }
   ul.innerHTML = '';
   render(ul, allList.slice((page - 1) * num, page * num));
 };
 pasing(5, 1);
+let currentPage = 1;
+
+const pageUl = document.querySelector('.pageUl');
+const pagePrev = document.querySelector('.btnPrev');
+const pageNext = document.querySelector('.btnNext');
+// const pageNums = document.querySelectorAll('.number');
+console.log(pageUl);
+console.log(pagePrev);
+console.log(pageNext);
+
+const buttons = document.querySelectorAll('.number');
+buttons.forEach(function (button) {
+  button.addEventListener('click', e => {
+    let page = e.target.textContent;
+    pasing(5, page);
+  });
+});
+
+// pageUl.addEventListener('click', e => {
+//   let key = e.target;
+//   console.log(key);
+// console.log(pageNums);
+// {
+//   currentPage = key.textContent;
+//   render(5, currentPage);
+// }
+// Num(e.target.textContent);
+// });
+
+// const buttons = document.querySelectorAll('.number');
+// 이전 다음으로
+// const before = document.querySelectorAll('.btn');
+// window.addEventListener('click', e => {
+//    if(e.target.innerHTML==="<i class="fas fa-angle-left"></i>")
+// });
+
+//새로운 질문 등록
+const inputName = getEl('.input__name');
+const inputTitle = getEl('.input__title');
+const inputQuestion = getEl('.text__question');
+const submitBtn = getEl('.form__submit');
 
 //초기화
 const resetInput = () => {
@@ -120,22 +127,14 @@ const resetInput = () => {
   inputQuestion.value = '';
 };
 
-function addStorage(newObj) {
-  localStorage.setItem('newObj', JSON.stringify(newObj));
-  existingLists.unshift(newObj);
-  localStorage.setItem('lists', JSON.stringify(existingLists));
-}
-
 //제출하기.
 let newlist = [];
-const submitEl = function (event) {
+const submitEl = function (e) {
   if (inputName.value === '' || inputTitle.value === '' || inputQuestion === '')
     return;
   ul.innerHTML = '';
-  event.preventDefault();
+  e.preventDefault();
 
-  //배열 만들어
-  //새로운 객체를 만든다.
   let newObj = {};
   let currentDay = new Date();
   newObj.id = 'D_kwDOHOApLM4APXTN';
@@ -157,106 +156,9 @@ const submitEl = function (event) {
   //newlist 키에 로컬에 저장.
   localStorage.setItem('newlist', JSON.stringify(newlist));
   newLists = JSON.parse(localStorage.getItem('newlist'));
+  //처음화면으로 돌아감.
   pasing(5, 1);
-  // render(ul, newLists);
-  // render(ul, existingLists);
   resetInput();
   return newLists;
 };
 submitBtn.addEventListener('click', submitEl);
-submitBtn.onclick = submitEl;
-
-const buttons = document.querySelectorAll('.number');
-buttons.forEach(function (button) {
-  button.addEventListener('click', e => {
-    let page = e.target.textContent;
-    pasing(5, page);
-  });
-});
-
-//이전 다음으로 가는건 다음시간에...
-// const before=getEl('.fas fa-angle-left')
-// const after=getEl('.fas fa-angle-right')
-// before.addEventListener('click',()=>{
-//   pa
-// })
-
-// Requirements
-
-// Discussion 컴포넌트
-// 모든 요소에 border-box를 적용합니다.
-// <li> 요소의 기본 스타일인 점을 보이지 않게 합니다.
-// Discussion 컴포넌트 전체의 폭을 540px로 만드세요.
-
-// 왼쪽 아바타
-// 아바타 사진을 인스타그램 프로필 사진처럼 동그랗게 표현하세요.
-// 아바타 사진의 너비를 48px로 설정하세요.
-// 아바타 사진이 Discussion 컴포넌트 높이의 중간에 오도록 정렬합니다.
-
-// 중간 Discussion 콘텐츠
-// Discussion 제목과 저자, 생성일간에 간격을 충분히 줍니다.
-// 저자, 생성일을 함께 작성하고, 오른쪽 정렬하여 오른쪽에 붙입니다.
-
-// 오른쪽 체크 표시
-// 체크 표시가 Discussion 컴포넌트 높이의 중간에 오도록 정렬합니다.
-
-// Bare Minimum Requirement
-// 디스커션 나열 기능
-// script.js를 수정하여 agoraStatesDiscussions 배열의 데이터를 나열할 수 있게 구현합니다.
-
-// CSS
-// 아고라 스테이츠 질문 리스트가 중앙으로 와야 합니다.
-// style.css를 수정하여 멋지고 아름답게 나만의 아고라 스테이츠를 꾸밉니다.
-// colorhunt, dribbble에서 적절한 색 조합, 디자인을 참고합니다.
-
-// 디스커션 추가 기능
-// script.js를 수정하여 디스커션 추가 기능을 구현합니다.
-// section.form__container 요소에 새로운 아고라 스테이츠 질문을 추가할 수 있는 입력 폼을 제작합니다. 형식은 자유입니다.
-// 아이디, 본문을 입력하고 버튼을 누르면 실제 화면에 디스커션이 추가되어야 합니다.
-// agoraStatesDiscussions 배열에 추가한 데이터가 실제 쌓여야 합니다.
-
-// Github Page 배포
-// Github Page 배포 기능을 이용하여 누구나 볼 수 있게 배포합니다.
-
-// 코드스테이츠 fe-sprint-my-agora-states 리포지토리로 Pull Request
-// 나만의 아고라 스테이츠를 코드스테이츠 깃허브에 Pull request합니다.
-// 주어진 Pull request 형식에 따라주세요.
-// Advanced Challenge
-
-// 현지 시간 적용
-// 샘플 시간을 잘 변형하여, 현지 시간에 맞게 표현합니다. (ex. 오전 10:02:17)
-
-// 페이지네이션 기능
-// 페이지네이션에 대해서 스스로 학습합니다.
-// 한 페이지에 10개씩 디스커션이 보여야 합니다.
-// 다음 페이지로 넘어갈 수 있어야 합니다.
-// 이전 페이지로 돌아올 수 있어야 합니다.
-// 다음 페이지가 없거나, 이전 페이지가 없는 경우 페이지를 유지해야 합니다.
-
-const pageUl = document.querySelector('.pageUl');
-
-// function elem(allPages, page) {
-//   let li = '';
-
-//   let beforePages = page - 1;
-//   let afterPages = page + 1;
-//   let liActive;
-
-//   if (page > 1) {
-//     li += `<li class="btn "><i class="fas fa-angle-left"></i></li>`;
-//   }
-//   for (let pageLength = beforePages; pageLength <= afterPages; pageLength++) {
-//     if (page == pageLength) {
-//       liActive = 'active';
-//     } else liActive = '';
-//     li += `<li class="number "><span>${pageLength}</span></li>`;
-//   }
-
-//   if (page < allPages)
-//     li += `<li class="btn "><i class="fas fa-angle-right"></i></li>`;
-//   ul.innerHTML = li;
-// }
-
-// elem(15, 2);
-
-// let currentPage = 1;
