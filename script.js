f3b048f4358df91a853a8b3cf703d1ecc76f1fdb
hxezin <leehxejin@gmail.com>
2022-05-20 16:14:03 +0900

    나만의 아고라 스테이츠 만들기 과제

--- script.js
+++ script.js
@@ -14,8 +14,34 @@ const convertToDiscussion = (obj) => {
   discussionAnswered.className = "discussion__answered";
 
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
+  // 아바타
+  const avatarImg = document.createElement("img");
+  avatarImg.src = obj.avatarUrl;
+  avatarImg.alt = "avatar of " + obj.author;
+  avatarWrapper.append(avatarImg);
 
+  // discussion 콘텐츠
+  const contentTitle = document.createElement("h2");
+  const titleUrl = document.createElement("a");
+  contentTitle.className = "discussion__title";
+  titleUrl.href = obj.url;
+  titleUrl.textContent = obj.title;
+  contentTitle.append(titleUrl);
 
+  const contentInfo = document.createElement("div");
+  const createDate = new Date(obj.createdAt).toLocaleTimeString();
+  contentInfo.className = "discussion__information";
+  contentInfo.textContent = `${obj.author} / ${createDate}`;
+  discussionContent.append(contentTitle, contentInfo);
+
+  // answered
+  const answeredMark = document.createElement("p");
+  if (obj.answer === null) {
+    answeredMark.textContent = "🅇";
+  } else {
+    answeredMark.textContent = "🅅";
+  }
+  discussionAnswered.append(answeredMark);
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
@@ -23,8 +49,11 @@ const convertToDiscussion = (obj) => {
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
-  for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
-    element.append(convertToDiscussion(agoraStatesDiscussions[i]));
+  element.innerHTML = "";
+
+  const savedDiscussions = JSON.parse(localStorage.getItem("data"));
+  for (let i = 0; i < savedDiscussions.length; i += 1) {
+    element.append(convertToDiscussion(savedDiscussions[i]));
   }
   return;
 };
@@ -32,3 +61,32 @@ const render = (element) => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+// 질문 폼
+const questionForm = document.querySelector(".form__container form");
+const formName = document.querySelector(".form__input--name input");
+const formTitle = document.querySelector("div.form__input--title input");
+const formContent = document.querySelector(".form__textbox #story");
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const writeDate = new Date();
+
+  agoraStatesDiscussions.unshift({
+    createdAt: writeDate,
+    title: formTitle.value,
+    url: "www.google.com",
+    author: formName.value,
+    bodyHTML: formContent.value,
+    avatarUrl:
+      "https://avatars.githubusercontent.com/u/97888923?s=64&u=12b18768cdeebcf358b70051283a3ef57be6a20f&v=4",
+  });
+
+  console.log(agoraStatesDiscussions);
+  // localStorage 부분 해결해야함 submit 누르면 새로고침됨ㅠ
+  localStorage.setItem("data", JSON.stringify(agoraStatesDiscussions));
+  render(ul);
+}
+
+questionForm.addEventListener("submit", onFormSubmit);
