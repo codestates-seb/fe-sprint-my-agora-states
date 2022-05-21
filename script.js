1067489a4ae1ebd9c63f04a8a81057becd223720
namwonjae <96907839+namwonjae@users.noreply.github.com>
2022-05-21 20:17:25 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,75 +1,116 @@
 const convertToDiscussion = (obj) => {
-  const li = document.createElement("li"); // li 요소 생성
-  li.className = "discussion__container"; // 클래스 이름 지정
-  const avatarWrapper = document.createElement("div");
-  avatarWrapper.className = "discussion__avatar--wrapper";
-  const discussionContent = document.createElement("div");
-  discussionContent.className = "discussion__content";
-  const discussionAnswered = document.createElement("div");
-  discussionAnswered.className = "discussion__answered";
+const li = document.createElement("li"); // li 요소 생성
+      li.className = "discussion__container"; // 클래스 이름 지정
+
+const avatarWrapper = document.createElement("div");
+      avatarWrapper.className = "discussion__avatar--wrapper";
+const discussionContent = document.createElement("div");
+      discussionContent.className = "discussion__content";
+const discussionAnswered = document.createElement("div");
+      discussionAnswered.className = "discussion__answered";
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
-  // avatar 작성
-  const avatarImg = document.createElement("img");
-  avatarImg.className = "discussion__avatar--image";
-	avatarImg.src = obj.avatarUrl;
-  avatarImg.setAttribute("alt", `avatar of ${obj.author}`);
-  // avatar 추가
-  avatarWrapper.append(avatarImg);
-  // title 작성
-  const discussionTitle = document.createElement("h2");
-  discussionTitle.className = "discussion__title";
-  const discussionLink = document.createElement("a");
-  discussionLink.setAttribute("href", obj.url)
-  discussionLink.textContent = obj.title;
-  discussionTitle.append(discussionLink);
-  // information 작성
-  const discussionInformation = document.createElement("div");
-  discussionInformation.className = "discussion__information";
-  discussionInformation.textContent = `${obj.author} / ${obj.createdAt}`
-  // content 추가
-  discussionContent.append(discussionTitle, discussionInformation);
-  // answer 작성
-  const checkIcon = document.createElement("i");
-  if(obj.answer === null){
-    checkIcon.className = "fa-regular fa-circle-check";
-  } else {
-checkIcon.className = "fa-solid fa-circle-check active";
-  }
-  // answer 추가
-  discussionAnswered.append(checkIcon);
+  // 이미지 파일 내용
+const avatarImg = document.createElement('img');
+      avatarImg.src = obj.avatarUrl;
+      avatarImg.alt = "avatar of " + obj.author; //??
+      avatarWrapper.append(avatarImg);
 
+  // 디스커션 타이틀 내용    
+const contentTitle = document.createElement("h2");
+      contentTitle.className = "discussion__title";
+      discussionContent.append(contentTitle);
+const contentLink = document.createElement("a");
+      contentLink.href = obj.url;
+      contentLink.textContent = obj.title;
+      contentTitle.append(contentLink);
+
+// 디스커션 내용
+const contentInformation = document.createElement("div");
+      contentInformation.className = "discussion__information";
+      contentInformation.textContent = `${obj.author} / ${obj.createdAt}`;
+      discussionContent.append(contentInformation);
+
+// 디스커션 답변 받았는지 체크
+const checkAnswered = document.createElement("p");
+      discussionAnswered.append(checkAnswered);
+      obj.answer
+      ? (checkAnswered.textContent = "☑")
+      : (checkAnswered.textContent = "☒");
   li.append(avatarWrapper, discussionContent, discussionAnswered);
-
   return li;
 };
-
+  
+// TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
+// 아고라스테이츠디스커션의 길이 만큼 converToDiscussion에 
+// agoraStatesDiscussions의 0번째 인덱스 부터 넣어주는 함수
 const render = (element) => {
+  for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
+    element.append(convertToDiscussion(agoraStatesDiscussions[i]));
+  }
+  return;
+};
+
+// ul에서 ul.discussions__container를 찾아서 랜더 ul을 해주는 작업
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
-document.querySelector("#submit").addEventListener("click", () => {
-  const name = document.querySelector("#name");
-  const title = document.querySelector("#title");
-  const content = document.querySelector("#story");
-agoraStatesDiscussions.unshift({
-    id: "D_kwDOHOApLM4APjJi",
-    createdAt: currentTime,
-    title: title.value,
-    url: "https://github.com/codestates-seb/agora-states-fe/discussions/45",
-    author: name.value,
-    answer: {
-    },
-    bodyHTML:
-      '<p dir="auto">--------------- 여기서부터 복사하세요 ---------------</p>\n<p dir="auto">운영 체제: 예) macOS</p>\n<p dir="auto">현재 어떤 챕터/연습문제/과제를 진행 중이고, 어떤 문제에 부딪혔나요?<br>\nPair 과제 / JavaScript Koans</p>\n<p dir="auto">npm install 명령어 입력 시 env: node: No such file or directory 라고 뜹니다</p>\n<p dir="auto">에러 발생하여 아래 명령어 실행 했는데도 불구하고 똑같은 에러가 발생했습니다<br>\nnpm cache clean --force</p>\n<p dir="auto">rm package-lock.json</p>\n<p dir="auto">rm -rf ./node_modules/</p>\n<p dir="auto">npm --verbose install</p>\n<p dir="auto">폴더 자체가 문제가 있다고 생각하여 github에서 다시 fork 후 진행했는데도 같은 에러가 발생했습니다<br>\n리눅스 기초 챕터 때 npm 설치해서 마지막 submit까지는 잘 됐는데 현재 짝수 생성기 폴더도 똑같이 npm install 시 no such file or directory가 발생합니다</p>\n<p dir="auto">에러가 출력된 곳에서, 이유라고 생각하는 부분을 열 줄 이내로 붙여넣기 해 주세요. (잘 모르겠으면 에러라고 생각하는 곳을 넣어주세요)</p>\n<div class="highlight highlight-source-js position-relative overflow-auto" data-snippet-clipboard-copy-content="minjun@dubi fe-sprint-javascript-koans-main % pwd \n/Users/minjun/Documents/fe_frontand_39/fe-sprint-javascript-koans-main\nminjun@dubi fe-sprint-javascript-koans-main % npm install \nenv: node: No such file or directory"><pre><span class="pl-s1">minjun</span>@<span class="pl-s1">dubi</span> <span class="pl-s1">fe</span><span class="pl-c1">-</span><span class="pl-s1">sprint</span><span class="pl-c1">-</span><span class="pl-s1">javascript</span><span class="pl-c1">-</span><span class="pl-s1">koans</span><span class="pl-c1">-</span><span class="pl-s1">main</span> <span class="pl-c1">%</span> <span class="pl-s1">pwd</span> \n<span class="pl-c1">/</span><span class="pl-v">Users</span><span class="pl-c1">/</span><span class="pl-s1">minjun</span><span class="pl-c1">/</span><span class="pl-v">Documents</span><span class="pl-c1">/</span><span class="pl-s1">fe_frontand_39</span><span class="pl-c1">/</span><span class="pl-s1">fe</span><span class="pl-c1">-</span><span class="pl-s1">sprint</span><span class="pl-c1">-</span><span class="pl-s1">javascript</span><span class="pl-c1">-</span><span class="pl-s1">koans</span><span class="pl-c1">-</span><span class="pl-s1">main</span>\n<span class="pl-s1">minjun</span><span class="pl-kos"></span>@<span class="pl-s1">dubi</span> <span class="pl-s1">fe</span><span class="pl-c1">-</span><span class="pl-s1">sprint</span><span class="pl-c1">-</span><span class="pl-s1">javascript</span><span class="pl-c1">-</span><span class="pl-s1">koans</span><span class="pl-c1">-</span><span class="pl-s1">main</span> <span class="pl-c1">%</span> <span class="pl-s1">npm</span> <span class="pl-s1">install</span> \nenv: node: <span class="pl-v">No</span> <span class="pl-s1">such</span> <span class="pl-s1">file</span> <span class="pl-s1">or</span> <span class="pl-s1">directory</span></pre></div>\n<p dir="auto">검색했던 링크가 있다면 첨부해 주세요.<br>\n<a href="https://mia-dahae.tistory.com/89" rel="nofollow">https://mia-dahae.tistory.com/89</a></p>\n<p dir="auto"><a href="https://stackoverflow.com/questions/38143558/npm-install-resulting-in-enoent-no-such-file-or-directory" rel="nofollow">https://stackoverflow.com/questions/38143558/npm-install-resulting-in-enoent-no-such-file-or-directory</a></p>\n<p dir="auto"><a href="https://velog.io/@hn04147/npm-install-%ED%95%A0-%EB%95%8C-tar-ENOENT-no-such-file-or-directory-lstat-%EC%97%90%EB%9F%AC%EB%82%A0-%EA%B2%BD%EC%9A%B0" rel="nofollow">https://velog.io/@hn04147/npm-install-%ED%95%A0-%EB%95%8C-tar-ENOENT-no-such-file-or-directory-lstat-%EC%97%90%EB%9F%AC%EB%82%A0-%EA%B2%BD%EC%9A%B0</a></p>\n<p dir="auto"><a href="https://m.blog.naver.com/PostView.naver?isHttpsRedirect=true&amp;blogId=chandong83&amp;logNo=221064506346" rel="nofollow">https://m.blog.naver.com/PostView.naver?isHttpsRedirect=true&amp;blogId=chandong83&amp;logNo=221064506346</a></p>\n<p dir="auto"><a href="https://webisfree.com/2021-07-15/npm-install-%EC%97%90%EB%9F%AC-%EB%B0%9C%EC%83%9D-rename-no-such-file-or-directory-%ED%95%B4%EA%B2%B0%ED%95%98%EA%B0%80" rel="nofollow">https://webisfree.com/2021-07-15/npm-install-%EC%97%90%EB%9F%AC-%EB%B0%9C%EC%83%9D-rename-no-such-file-or-directory-%ED%95%B4%EA%B2%B0%ED%95%98%EA%B0%80</a></p>\n<p dir="auto"><a href="https://hellowworlds.tistory.com/57" rel="nofollow">https://hellowworlds.tistory.com/57</a></p>',
-    avatarUrl:
-      "https://avatars.githubusercontent.com/u/104333720?s=40&v=4",
-  });
 
-  // 화면에 출력
-  ul.prepend(convertToDiscussion(agoraStatesDiscussions[0]));
-  // Discussins에 데이터 추가
-  addDiscussion();
+// 이름, 타이들, 이야기 등을 선언하고 찾아오는 선언
+const inputName = document.querySelector("#name");
+const inputTitle = document.querySelector("#title");
+const inputStory = document.querySelector("#story");
+const submitBtn = document.querySelector("#submit__btn");
+const newDiscussionBtn = document.querySelector('.new__discussion');
+const formContainer = document.querySelector('.form__container');
+
+// formValidate를 선언 후  이름 , 타이틀, 스토리 값이 다 있으면
+// 버튼 디스에이블드가 에이블드로 나오고 아니면 그대로 디스에이블드
+const formValidate = () => {
+  if(inputName.value && inputTitle.value && inputStory.value) {
+    submitBtn.disabled = false;
+  } else {
+    submitBtn.disabled = true;
+  }
+}
+
+// 아래의 인풋이 키업 되면 위에서 조건문에서 체크
+inputName.addEventListener('keyup', () => {
+  formValidate();
 })
-const addDiscussion = () =>{
-  window.localStorage.setItem("agoraStatesDiscussions", JSON.stringify(agoraStatesDiscussions));
-  agoraStatesDiscussions = JSON.parse(window.localStorage.getItem("agoraStatesDiscussions"));
-}}
+
+inputTitle.addEventListener('keyup', () => {
+  formValidate();
+})
+
+inputStory.addEventListener('keyup', () => {
+  formValidate();
+})
+// newDiscussionBtn이 클릭되면 formContainer 클래스 리스트에서 하이드된
+// 토글이 나타남
+newDiscussionBtn.addEventListener('click', () => {
+  formContainer.classList.toggle('hide');
+  inputName.focus();
+})
+
+// 버튼의 조건이 완성되고 버튼이 에이블드 되었을 때 클릭하면
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const newDiscussion = {
+    id: Date.now(),
+    createdAt: new Date().toString(),
+    title: inputTitle.value,
+    url: "https://github.com/codestates-seb/agora-states-fe/discussions/45",
+    author: inputName.value,
+    answer: null,
+    bodyHTML: inputStory.value,
+    avatarUrl:
+      "https://avatars.githubusercontent.com/u/95295766?s=64&u=85d493e0be0d2ca55965efd9f6c5b268c9dca168&v=4",
+  };
+
+  inputName.value = '';
+  inputTitle.value = '';
+  inputStory.value = '';
+  formContainer.classList.add('hide');
+
+  agoraStatesDiscussions.unshift(newDiscussion);
+  ul.insertBefore(convertToDiscussion(newDiscussion), ul.firstChild);
+});
