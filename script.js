336ab1588547d51a813f1799a34b839958e7237a
CLOONEY92 <104333957+CLOONEY92@users.noreply.github.com>
2022-05-21 20:12:04 +0900

    1차 수정

--- script.js
+++ script.js
@@ -19,25 +19,35 @@ const convertToDiscussion = (obj) => {
   // alt="avatar of kimploo"></img>
   const avatarImg = document.createElement('img');
   avatarImg.src = obj.avatarUrl;
-  avatarImg.alt = " avatar of " + obj.author;
+  avatarImg.alt = "avatar of " + obj.author;
+  avatarImg.className = "discussion__avatar--image";
+
   avatarWrapper.append(avatarImg);
 
   const discussionTitle = document.createElement("h2");
+  discussionTitle.className = "discussion__title";
+
   const titleAnchor = document.createElement("a");
+  
   titleAnchor.href = obj.url;
   titleAnchor.textContent = obj.title;
+
   discussionTitle.append(titleAnchor);
 
   const discussionInformation = document.createElement("div");
-  discussionInformation.className = "discussion_information";
-  discussionInformation.textContent = `${obj.author} / ${new Date(obj.createAt).toLocaleTimeString()}`;
+  discussionInformation.className = "discussion__information";
+
+  const createNewDate = new Date(obj.createdAt).toLocaleString();
+
+  discussionInformation.textContent = `${obj.author} / ${createNewDate}`;
+
   discussionContent.append(discussionTitle, discussionInformation);
 
   const checked = document.createElement("p");
   checked.textContent = obj.answer ? "☑" : "☒"
   discussionAnswered.append(checked);
 
-  li.append(avatarWrapper, discussionContent, discussionAnswered, avatarImg);
+  li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
 };
 
@@ -53,3 +63,33 @@ const render = (element) => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+document.querySelector('.form').onsubmit = function(){
+  //파트1. 여기서 this는 'form'을 의미합니다.
+  var nameValue = this.name.value
+  var titleValue = this.title.value
+  //var storyValue = this.story.value
+
+
+  let newArray = {
+    author:nameValue, 
+    title:titleValue,
+    answer:null,
+    createdAt:new Date(),
+    avatarUrl:"https://i.pinimg.com/564x/ce/40/63/ce406399a419ef8cd669811951942321.jpg"}; 
+
+  console.log(newArray)
+  agoraStatesDiscussions.unshift(newArray);
+  console.log(agoraStatesDiscussions) 
+
+  ul.prepend(convertToDiscussion(newArray));
+
+  //파트3. input에 쓰여있던 모든 데이터를 없애줍니다(초기화).
+  this.name.value = ""
+  this.title.value = ""
+  this.story.value = ""
+
+  //파트4. 제출 이벤트로 인한 새로고침 방지
+  return false;
+}
+
