ab43a278988cc73c6b68e481f9bec0dd7b0d124e
couchSol <sollee922@gmail.com>
2022-05-22 13:49:26 +0900

    ✨ localstorage 이용해 새 discussion 저장

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // index.html을 열어서 agoraStatesDiscussions 배열 요소를 확인하세요.
 console.log(agoraStatesDiscussions);
 
+// 새 입력값을 저장하는 newList가 존재하는지 확인하고 없으면 새로 만든다.
+const checkNewList = localStorage.getItem('newList')
+if(checkNewList === null){
+  localStorage.setItem('newList', '[]')
+}else{
+  console.log('newList exists')
+}
+const newList = JSON.parse(localStorage.getItem('newList'))
+
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
 const convertToDiscussion = (obj) => {
   const li = document.createElement("li"); // li 요소 생성
@@ -13,8 +22,6 @@ const convertToDiscussion = (obj) => {
   const discussionAnswered = document.createElement("div");
   discussionAnswered.className = "discussion__answered";
 
-  // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
-  
   //img 정보 불러와서 avataWrapper에 붙이기
   const avatarImg = document.createElement('img');
     avatarImg.className = 'discussion__avatar--image';
@@ -44,11 +51,12 @@ const convertToDiscussion = (obj) => {
 
 };
 
-// agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
+// agoraStatesDiscussions와 newLIst 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
   element.innerHTML = '';
-  for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
-    element.append(convertToDiscussion(agoraStatesDiscussions[i]));
+  const updatedList = [...newList, ...agoraStatesDiscussions]
+  for (let i = 0; i < updatedList.length; i += 1) {
+    element.append(convertToDiscussion(updatedList[i]));
   }
   return;
 };
@@ -58,17 +66,9 @@ const ul = document.querySelector("ul.discussions__container");
 render(ul);
 
 
-//글쓰기 폼이 업데이트되면 해당 내용을 data.js에 넣어준다
-//submit.onclick = update(e) 내용을 받아온다
-// {배열 안에 형식 맞춰 적어준다}
-//agoraStatesDiscussions에 push한다
-//다시 render한다
+//글쓰기 폼이 업데이트되면 해당 내용을 newList에 업데이트한다
 let form = document.querySelector('.form');
 form.addEventListener('submit', update);
-
-
-let submit = document.querySelector('.form');
-submit.addEventListener('submit', update);
 
 function update(event) {
   event.preventDefault();
@@ -95,37 +95,8 @@ function update(event) {
     "https://avatars.githubusercontent.com/u/90553688?s=64&u=3c4e4dc2053d4977ac12b9cfc2667582f986d3d8&v=4"
   }
   console.log(newObj)
-  agoraStatesDiscussions.unshift(newObj);
-  console.log(agoraStatesDiscussions);
+  newList.unshift(newObj);
+  localStorage.setItem('newList', JSON.stringify(newList))
+  console.log(newList);
   render(ul);
 }
-
-// function update(e) {
-//   e.preventaDefault();
-
-//   const today = new Date();
-//   const year = today.getFullYear();
-//   const month = ('0' + (today.getMonth() +1)).slice(-2);
-//   const day = ('0' + today.getDate()).slice(-2);
-//   const hours = ('0' + today.getHours()).slice(-2);
-//   const minutes = ('0' + today.getMinutes()).slice(-2);
-//   const seconds = ('0' + today.getSeconds()).slice(-2);
-//   const timestamp = `${year}-${month}-${day}T${hours}:${minutes}:${seconds}Z`
-
-//   const newObj = {
-//     id: '',
-//     createdAt: timestamp,
-//     title: document.querySelector('.input--title').value,
-//     url: '/',
-//     author: document.querySelector('.input--name').value,
-//     answer: null,
-//     bodyHTML: document.querySelector('.input--story').value,
-//     bodyHTML: '',
-//     avatarUrl:
-//     "https://avatars.githubusercontent.com/u/90553688?s=64&u=3c4e4dc2053d4977ac12b9cfc2667582f986d3d8&v=4"
-//   }
-//   console.log(newObj)
-//   agoraStatesDiscussions.unshift(newObj);
-//   console.log(agoraStatesDiscussions);
-//   render(ul);
-// }
