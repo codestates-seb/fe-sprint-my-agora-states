746c0c4f515c9aaa1e6ef0ea91915da46baa838f
random-olive <ahnssy7@gmail.com>
2022-05-23 02:55:16 +0900

    third

---
+++ fe-sprint-my-agora-states-main/test/script.js
@@ -0,0 +1,9 @@
+const parse = document.createElement('p');
+parse.className='love';
+
+const div = document.createElement('div');
+div.className='hello';
+
+parse.textContent = 'world';
+
+div.append(parse)
