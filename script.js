7f1eb2d4e989f616005ba068a18562f75ed88b76
TAETAEMO <pjh7179200@gmail.com>
2022-05-23 03:40:37 +0900

    과제제출

--- script.js
+++ script.js
@@ -2,20 +2,54 @@
 console.log(agoraStatesDiscussions);
 
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
+
+//const section = document.createElement("section");
+//section.className = "form__container";
+
+
+
+
+
+
+
+
 const convertToDiscussion = (obj) => {
   const li = document.createElement("li"); // li 요소 생성
   li.className = "discussion__container"; // 클래스 이름 지정
 
-  const avatarWrapper = document.createElement("div");
-  avatarWrapper.className = "discussion__avatar--wrapper";
-  const discussionContent = document.createElement("div");
+  
+  const avatarWrapper = document.createElement("div"); //div 요소 생성
+  avatarWrapper.className = "discussion__avatar--wrapper"; //클래스 이름 지정
+  const discussionContent = document.createElement("div"); 
   discussionContent.className = "discussion__content";
+    const idCreatedAt = document.createElement("div");
+    idCreatedAt.className = "discussion__information";
+    idCreatedAt.textContent = obj.id + " / " + obj.createdAt;
+    discussionContent.append(idCreatedAt);
+  
+  
   const discussionAnswered = document.createElement("div");
   discussionAnswered.className = "discussion__answered";
+    const check = document.createElement("input");
+    check.id = "keep-checkbox";
+    check.type = "checkbox";
+    discussionAnswered.append(check);
 
+  
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
 
+  const avatarImg = document.createElement('img');
+  avatarImg.src = obj.avatarUrl;
+  avatarImg.alt = "avatar of " + obj.author;
+  avatarImg.className = "discussion__avatar--image";
+  avatarWrapper.append(avatarImg);
 
+  const discussionTitle = document.createElement("h2");
+  const titleAnchor = document.createElement("a");
+  titleAnchor.href = obj.url;
+  titleAnchor.textContent = obj.title;
+  discussionTitle.append(titleAnchor);
+  discussionContent.append(discussionTitle);
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
@@ -23,6 +57,7 @@ const convertToDiscussion = (obj) => {
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
+  //element.innerHTML="" 
   for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
     element.append(convertToDiscussion(agoraStatesDiscussions[i]));
   }
@@ -32,3 +67,4 @@ const render = (element) => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
