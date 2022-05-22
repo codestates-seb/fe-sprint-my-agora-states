e1a284579a06eb48c44956f5c1f363ca2a2f8f02
조영민 <0miiii@naver.com>
2022-05-22 21:29:29 +0900

    보완

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+'use strict'
 // index.html을 열어서 agoraStatesDiscussions 배열 요소를 확인하세요.
 console.log(agoraStatesDiscussions);
 
@@ -24,11 +25,11 @@ const convertToDiscussion = (obj) => {
 
   // 컨텐츠
   const contentTitle = document.createElement('h3');
-  const contentTitleLink = document.createElement('a');
-  const contentInfo = document.createElement('div');
   contentTitle.className = 'discussion__title';
+  const contentTitleLink = document.createElement('a');
   contentTitleLink.href = obj.url;
   contentTitleLink.textContent = obj.title;
+  const contentInfo = document.createElement('div');
   contentTitle.append(contentTitleLink);
   discussionContent.append(contentTitle);
 
@@ -36,40 +37,60 @@ const convertToDiscussion = (obj) => {
   contentInfo.textContent = `${obj.author} / ${obj.createdAt}`;
   discussionContent.append(contentInfo);
   
-  // 답변확인
+  // 답변 확인 체크박스
   const chkbox = document.createElement('input');
   chkbox.setAttribute('type', 'checkbox')
   chkbox.setAttribute('disabled', 'disabled')
   discussionAnswered.append(chkbox);
 
+  // 답변 유무에 따라 체크표시
   if( obj.answer !== null ) {
     chkbox.setAttribute('checked', 'checked');
   }
-
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
 };
 
+
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
-  // element.innerHTML = "";
   for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
     element.append(convertToDiscussion(agoraStatesDiscussions[i]));
   }
   return;
 };
-
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
 
-// 입력한 데이터 출력
-const SUBMIT = document.querySelector(".form__submit");
 
-SUBMIT.addEventListener('click', function(event){
+
+// 입력한 데이터 출력
+// submit 버튼을 클릭했을 때
+const submitBtn = document.querySelector(".form__submit");
+submitBtn.addEventListener('click', function(event){
+  
+  // 입력 받은 값
+  const inputName = document.querySelector(".form__input--name input");
+  const inputTitle = document.querySelector(".form__input--title input");
+  const inputQuestion = document.querySelector(".form__textbox textarea");
+
+  // 입력값이 하나라도 없으면 알림 
+  if( inputName.value === '' || inputTitle.value === '' || inputQuestion.value === '' ) {
+    alert('값을 입력하세요');
+    return
+  }
   event.preventDefault();
 
+  // ul 초기화
+  while(ul.firstChild) {
+    ul.firstChild.remove();
+  }
+  // 참고) ul.innerHTML = '';
+  // innerHTML은 사용을 지양하므로 ul.firstChild 사용 
+
+  // 시간
   let today = new Date();
   let year = today.getFullYear();
   let month = String(today.getMonth() + 1).padStart(2,'0');
@@ -79,28 +100,39 @@ SUBMIT.addEventListener('click', function(event){
   let hour = String(today.getHours()).padStart(2,'0');
   let minutes = String(today.getMinutes()).padStart(2,'0');
   let seconds = String(today.getSeconds()).padStart(2,'0');
-  
   let current = `${year}-${month}-${date} ${week[day]} ${hour}:${minutes}:${seconds}`;
 
-  const inputName = document.querySelector(".form__input--name input");
-  const inputTitle = document.querySelector(".form__input--title input");
   const addDiscussion = {
-    id: "작성해야함",
+    id: Date.now(),
     createdAt: current,
     title: inputTitle.value,
     url: "#",
     author: inputName.value,
     answer: null,
-    bodyHTML: '',
-    avatarUrl: "#",
+    bodyHTML: inputQuestion.value,
+    avatarUrl: "https://avatars.githubusercontent.com/u/12145019?s=64&u=5c97f25ee02d87898457e23c0e61b884241838e3&v=4",
   }
+
+  // 데이터 추가
   agoraStatesDiscussions.unshift(addDiscussion);
+
+  // 로컬 스토리지에 저장
+  localStorage.setItem('addDiscussion', JSON.stringify(addDiscussion));
   
-  const discussion__container = document.querySelectorAll(".discussion__container");
-  discussion__container.forEach(function(i) {
-    i.remove();
-  })
+  
+  // form 초기화
+  inputTitle.value = '';
+  inputName.value = '';
+  inputQuestion.value = '';
+
   render(ul);
-  
 })
 
+// 페이지만들기
+// 한 화면에 데이터 7개까지 출력
+// 만약에 데이터가 7개가 넘어간다면 다음 페이지 생성
+// 
+// 로컬스토리지에 있는 데이터와 data.js에 있는 데이터를 합쳐서 렌더링
+
+// 페이지 버튼은 4개씩화면에 보임
+// 데이터가 7개가 넘어갈 때마다 1페이지씩 늘어남
