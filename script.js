427c61d3b0118632de1a8ecb47616ea669015aab
eunyoung <shineeol20@gmail.com>
2022-06-21 20:11:03 +0900

    fetch

--- script.js
+++ script.js
@@ -58,19 +58,11 @@ async function getPromise() {
 */
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
-  fetch("http://localhost:3001/discussions")
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-      for (let i = 0; i < data.length; i += 1) {
-        element.append(convertToDiscussion(data[i]));
-      }
-    });
-
-  // for (let i = 0; i < agoraData.length; i += 1) {
-  //   element.append(convertToDiscussion(agoraData[i]));
-  // }
-  // return;
+  element.innerHTML = ""; // 초기화하는 코드
+  for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
+    element.append(convertToDiscussion(agoraStatesDiscussions[i]));
+  }
+  return;
 };
 
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
@@ -106,3 +98,11 @@ submit.onclick = function (e) {
   agoraStatesDiscussions.unshift(newObj);
   render(ul);
 };
+
+fetch("http://localhost:3001/discussions")
+    .then((res) => res.json())
+    .then((data) => {
+      agoraStatesDiscussions = data;
+      const ul = document.querySelector("ul.discussions__container");
+      render(ul);
+    });
