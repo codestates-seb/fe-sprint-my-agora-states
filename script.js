f92b47a03905967eb350fa6af2758d55833d04d4
dukjjang <jhd7292@naver.com>
2022-06-21 15:19:16 +0900

    서버와 연결

--- script.js
+++ script.js
@@ -83,7 +83,6 @@ function onLoginSubmit(event) {
   discussionAdd.bodyHTML = event.srcElement[2].value;
   agoraStatesDiscussions.unshift(discussionAdd);
 
-  console.log(agoraStatesDiscussions);
   while (ul.hasChildNodes()) {
     ul.removeChild(ul.firstChild);
   }
@@ -93,7 +92,6 @@ submitForm.addEventListener("submit", onLoginSubmit);
 submitForm.addEventListener("submit", toggle);
 
 // index.html을 열어서 agoraStatesDiscussions 배열 요소를 확인하세요.
-console.log(agoraStatesDiscussions);
 
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
 const convertToDiscussion = (obj) => {
@@ -142,10 +140,19 @@ const convertToDiscussion = (obj) => {
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
-  for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
-    element.append(convertToDiscussion(agoraStatesDiscussions[i]));
-  }
-  return;
+  fetch("http://localhost:3001/discussions")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      for (let i = 0; i < data.length; i += 1) {
+        element.append(convertToDiscussion(data[i]));
+      }
+    });
+
+  // for (let i = 0; i < agoraData.length; i += 1) {
+  //   element.append(convertToDiscussion(agoraData[i]));
+  // }
+  // return;
 };
 
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
