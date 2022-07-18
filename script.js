559f36d977862d0b8c37fc0c054d33502914364f
GohDongwook <104189107+nada1221@users.noreply.github.com>
2022-07-18 20:53:56 +0900

    수정중
    수정중

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // index.html을 열어서 agoraStatesDiscussions 배열 요소를 확인하세요.
 console.log(agoraStatesDiscussions);
 
+
+
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
 const convertToDiscussion = (obj) => {
   const li = document.createElement("li"); // li 요소 생성
@@ -14,6 +16,31 @@ const convertToDiscussion = (obj) => {
   discussionAnswered.className = "discussion__answered";
 
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
+
+  const avatarImg = document.createElement("img");
+  avatarImg.classList = "discussion__avatar--image";
+  avatarImg.setAttribute("src", obj.avatarUrl);
+  avatarImg.setAttribute("alt", `avartar of ${obj.author}`);
+
+  avatarWrapper.append(avatarImg);
+
+  const contentTitle = document.createElement("h2");
+  contentTitle.classList = "discussion__title";
+  const contentTitleLink = document.createElement("a");
+  contentTitleLink.setAttribute("href",obj.url);
+  contentTitleLink.textContent = obj.title;
+  contentTitle.append(contentTitleLink);
+
+  const contentInfo = document.createElement("div")
+  contentInfo.classList = "discussion__information";
+  contentInfo.textContent = obj.author/obj.createdAt;
+
+discussionContent.append(contentTitle, contentInfo);
+
+const answerCheck = document.createElement("p");
+  answerCheck.textContent = obj.answer !== null ? "☑" : "☒";
+
+  discussionAnswered.append(answerCheck);
 
 
 
