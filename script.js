d45a2361ffebdc8a9e3fde169901fba16ca2ffb9
HyeonWooGa <yeonwoopark22@gmail.com>
2022-07-19 00:37:24 +0900

    애로우 펑션에서 함수선언식으로 변경

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const form = document.querySelector(".form");
 form.addEventListener("submit", handleFormSubmit);
 
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
-const convertToDiscussion = (obj) => {
+function convertToDiscussion(obj) {
   const li = document.createElement("li"); // li 요소 생성
   li.className = "discussion__container"; // 클래스 이름 지정
 
@@ -83,7 +83,7 @@ const convertToDiscussion = (obj) => {
   li.append(discussionBox);
 
   return li;
-};
+}
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
