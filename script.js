51c63cdd3c58e40b6454b357e08a6de19f429fe9
ethanJcoding <ethan.95j@gmail.com>
2022-07-18 16:30:21 +0900

    아고라 기능구현 완료

--- script.js
+++ script.js
@@ -15,11 +15,51 @@ const convertToDiscussion = (obj) => {
 
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
 
+  //TITLE DOM 지정
+  const title = document.createElement('h2')
+  title.className = "discussion__title"
+
+  //TITLE URL a 태그 지정
+  const titleUrl = document.createElement('a')
+  titleUrl.href = obj.url
+
+  //TITLE 데이터 삽입
+  titleUrl.textContent = obj.title
+
+  //TITLE HTML 표현
+  title.append(titleUrl)
+  discussionContent.append(title)
+
+  //AVATAR DOM 지정
+  const avatar = document.createElement('img')
+  avatar.className = 'discussion__avatar--wrapper'
+
+  //AVATAR 데이터 삽입
+  avatar.src = obj.avatarUrl
+
+  //AVATAR HTML 표현
+  avatarWrapper.append(avatar)
+
+  //DISCUSSION INFO (author, createdAT) 태그 지정
+  const discussInfo = document.createElement('div')
+  discussInfo.className = 'discussion__information'
+
+  //DISCUSSION 데이터 삽입
+  discussInfo.textContent = `${obj.author} / ${obj.createdAt}`
+
+  //DISCUSSION 표현
+  discussionContent.append(discussInfo)
+  
 
 
+  
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
 };
+
+
+
+
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
