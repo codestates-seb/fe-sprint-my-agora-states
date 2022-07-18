e085edf6f8f7c45f31701379ae4b426852489456
jwo0o0 <jwo0o0358@gmail.com>
2022-07-18 17:32:39 +0900

    all

---
+++ setting.js
@@ -0,0 +1,5 @@
+const existLi = document.querySelector("li");
+const existAnswer = existLi.lastChild;
+existLi.removeChild(existAnswer);
+
+const randomAvatarImg = ["images/avatar_1.JPG", "./images/avatar_2.jpeg", "images/avatar_3.jpg", "images/avatar_4.JPG", "images/avatar_5.JPG"];
