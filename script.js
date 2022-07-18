3bb6978a22a47fe46afb25342a8284653cc73385
z1zonemoi <zizonemoi@gmail.com>
2022-07-19 08:33:10 +0900

    submit만 눌렀을때 시간 출력 구현

--- script.js
+++ script.js
@@ -55,6 +55,20 @@ const render = (element) => {
   return;
 };
 
+const submit = document.querySelector('#submit');
+submit.onclick = () => {
+  const inputName = document.querySelector('#name');
+  const inputTitle = document.querySelector('#title');
+  const newObj = {};
+  newObj.author = inputName.value;
+  newObj.title = inputTitle.value;
+  newObj.avatarUrl = 'mandarin.jpg';
+  newObj.url = "#";
+  newObj.createdAt = new Date();
+  newObj.answer = null;
+  ul.prepend(convertToDiscussion(newObj));
+  return;
+};
 
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
