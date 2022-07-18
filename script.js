2253418f6987e97a8d3709bb7efa55e6ed1f8862
rosenfence <rosenfence@gmail.com>
2022-07-18 21:14:29 +0900

    [feat] List load on Browser

--- script.js
+++ script.js
@@ -11,9 +11,84 @@ const convertToDiscussion = (obj) => {
   const discussionContent = document.createElement("div");
   discussionContent.className = "discussion__content";
 
-  // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
+
+  //avatarWrapper에 프로필 사진 넣기
+  const avatarImage = document.createElement("img");
+    avatarImage.className = "discussion__upper--image";
+    avatarImage.setAttribute("src", obj.avatarUrl)
+    avatarImage.setAttribute("alt", `avatar of ${obj.author}`);
+
+  avatarWrapper.append(avatarImage);
+
+  //avatarWrapper에 프로필 텍스트 감싸는거 넣기
+  const profileText = document.createElement("span");
+  profileText.className = "discussion__upper--profile";
+    //profileText에 사용자 닉네임 넣기
+    const profileAuthor = document.createElement("div");
+    profileAuthor.className = "discussion__upper--profile--author";
+    profileAuthor.innerText = String(obj.author);
+    profileText.append(profileAuthor);
+    //profileText에 작성 시간 넣기
+    const profileWrittenTime = document.createElement("div");
+    profileWrittenTime.className = "discussion__upper--profile--writtentime";
+      const timeText = `${obj.createdAt.slice(5,7)}.${obj.createdAt.slice(8,10)} ${obj.createdAt.slice(11,19)}`;
+    profileWrittenTime.innerText = String(timeText);
+    profileText.append(profileWrittenTime);
+    //profileText avatarWrapper에 넣기
+  avatarWrapper.append(profileText);
+    
+    //avatarWrapper에 answersheet 넣기
+  const profileAnswer = document.createElement("span");
+  profileAnswer.className = "discussion__upper--answersheet";
+    //profileAnswer에 답변됨 박스 넣기
+    const answered = document.createElement("div");
+    answered.className = "discussion__upper--answersheet-answered";
+      //answered에 초록 체크 넣기
+      const checkbox = document.createElement("div");
+      answered.append(checkbox);
+
+    const notanswered = document.createElement("div");
+    notanswered.className = "discussion__upper--answersheet-notanswered";
+      //notanswered에 빨강 엑스 넣기
+      const xbox = document.createElement("div");
+      notanswered.append(xbox);
+
+      //안보이게 할 박스 정하기
+      console.log(obj.answer);
+      obj.answer === null ? xbox.innerText = "❎" : checkbox.innerText = "✅";
+    profileAnswer.append(answered);
+    profileAnswer.append(notanswered);
+  
+  avatarWrapper.append(profileAnswer);
 
 
+
+  const discussionLink = document.createElement("h2");
+  discussionLink.className = "discussion__title";
+    const discussionLinkText = document.createElement("a");
+    discussionLinkText.setAttribute("href", obj.url);
+    discussionLinkText.setAttribute("target", "_blank");
+    discussionLinkText.className = "discussion__title--text";
+    discussionLinkText.innerText = obj.title;
+    discussionLink.append(discussionLinkText);
+
+
+  const discussionWantedbox = document.createElement("div");
+  discussionWantedbox.className = "discussion__wantedbox";
+    const discussionWantedboxText = document.createElement("div")
+    discussionWantedboxText.className = "discussion__wantedbox--text";
+    discussionWantedboxText.innerText = "궁금해요!"
+    discussionWantedbox.append(discussionWantedboxText);
+
+    const discussionWantedButton = document.createElement("button");
+    discussionWantedButton.className = "discussion__wantedbox--button";
+    discussionWantedButton.innerText = "0"
+    discussionWantedbox.append(discussionWantedButton);
+
+
+  discussionContent.append(discussionLink);
+  discussionContent.append(discussionWantedbox);
+  
 
   li.append(avatarWrapper, discussionContent);
   return li;
