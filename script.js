a5ffbc9f86cf094be1ab6b1abb7b8848eee19037
Jongwon <jwp7073@gmail.com>
2022-07-18 18:22:27 +0900

    feat: 디스커션 나열

--- script.js
+++ script.js
@@ -8,15 +8,31 @@ const convertToDiscussion = (obj) => {
 
   const avatarWrapper = document.createElement("div");
   avatarWrapper.className = "discussion__avatar--wrapper";
+  const avatarImage = document.createElement("img");
+  avatarImage.className = "discussion__avatar--image";
   const discussionContent = document.createElement("div");
   discussionContent.className = "discussion__content";
+  const discussionTitle = document.createElement("h2");
+  discussionTitle.className = "discussion__title";
+  const discussionTitleLink = document.createElement("a");
+  const discussionInformation = document.createElement("div");
+  discussionInformation.className = "discussion__information";
   const discussionAnswered = document.createElement("div");
   discussionAnswered.className = "discussion__answered";
+  const discussionAnsweredParagraph = document.createElement("p");
 
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
+  avatarImage.src = obj.avatarUrl;
+  avatarImage.alt = `avatar of ${obj.author}`;
+  discussionTitleLink.textContent = obj.title;
+  discussionTitleLink.href = obj.url;
+  discussionInformation.textContent = `${obj.author} / ${obj.createdAt}`;
+  discussionAnsweredParagraph.textContent = obj.answer ? "☑" : "☒";
 
-
-
+  avatarWrapper.append(avatarImage);
+  discussionTitle.append(discussionTitleLink);
+  discussionContent.append(discussionTitle, discussionInformation);
+  discussionAnswered.append(discussionAnsweredParagraph);
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
 };
