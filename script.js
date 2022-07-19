9b6bc82436c92682081aa7ec5118df9eed1a3486
youthmn <banghmin@hanmail.net>
2022-07-19 19:53:51 +0900

    업로드 진짜 시작

--- script.js
+++ script.js
@@ -14,12 +14,49 @@ const convertToDiscussion = (obj) => {
   discussionAnswered.className = "discussion__answered";
 
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
+  const avatarImg = document.createElement('img')
+  avatarImg.classList.add('discussion__avatar--image')
+  avatarImg.src = obj.avatarUrl
+  avatarImg.alt = "avatar of " + obj.author;
+  avatarWrapper.append(avatarImg)
 
+  const discussionTitle = document.createElement('img')
+  const titleAncor = document.createElement('a')
+  discussionContent.append(titleAncor)
+  titleAncor.textContent = obj.title;
+
+  const discussionInfo = document.createElement('div')
+  discussionInfo.textContent = `${obj.author} / ${new Date(obj.createdAt).toLocaleString()}`
+
+  discussionContent.append(discussionTitle, discussionInfo)
 
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
 };
+
+const form = document.querySelector('.form')
+const title = document.querySelector('.form__input--title').value
+const nameInput = document.querySelector('.form__input--name').value
+const textbox = document.querySelector('.form__textbox').value
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault()
+  const obj = {
+      id: 'unique.id',
+      createdAt: new Date().toLocaleString(),
+      title: title,
+      url: "https://github.com/codestates-seb/agora-states-fe/discussions/45",
+      author: nameInput,
+      answer: null,
+      bodyHTML: textbox,
+      avatarUrl:
+        "https://avatars.githubusercontent.com/u/97888923?s=64&u=12b18768cdeebcf358b70051283a3ef57be6a20f&v=4",
+    }
+  agoraStatesDiscussions.unshift(obj);
+  const newDiscussion = convertToDiscussion(obj)
+  ul.prepend(newDiscussion)
+})
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
@@ -32,3 +69,4 @@ const render = (element) => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
