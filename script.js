0961440114dd525fa57d281570aa669b52bbff42
Hyejung Park <hyejj19@naver.com>
2022-07-19 15:26:12 +0900

    feat: 로컬스토리지 저장 / 수정 요망 : 새로고침 후 input 작성시 기존 작성 데이터 사라짐 현상

--- script.js
+++ script.js
@@ -4,6 +4,23 @@ const btnSubmit = document.querySelector('.form__submit');
 const form = document.querySelector('.form');
 const btns = document.querySelector('.btn__page--wrapper');
 
+// 페이지네이션 구현에 필요한 데이터
+const numOfContent = agoraStatesDiscussions.length;
+const maxContent = 5;
+const maxButton = 5;
+const maxPage = Math.ceil(numOfContent / maxContent);
+let page = 1;
+
+const dataSaveLocalStorage = function (obj) {
+  // 로컬스토리지 저장
+  agoraStatesDiscussions.forEach((el, idx) => {
+    const objString = JSON.stringify(el);
+    window.localStorage.setItem(idx, objString);
+  });
+
+  render(page);
+};
+
 // 데이터 => DOM 생성 함수
 const convertToDiscussion = obj => {
   const li = document.createElement('li'); // li 요소 생성
@@ -93,13 +110,6 @@ next.classList.add('btn', 'next');
 next.textContent = '다음 >';
 next.addEventListener('click', goNextPage);
 
-// 페이지네이션 구현에 필요한 데이터
-const numOfContent = agoraStatesDiscussions.length;
-const maxContent = 5;
-const maxButton = 5;
-const maxPage = Math.ceil(numOfContent / maxContent);
-let page = 1;
-
 // 배열의 데이터 렌더링 함수
 const renderContent = (element, page) => {
   // 글 목록 초기화
@@ -110,8 +120,10 @@ const renderContent = (element, page) => {
   // 현재 페이지부터 표시될 최대 컨텐츠까지 반복하여 리스트 생성 함수 실행 후 컨텐츠 표시
   // ... 직전 페이지로부터 넘어온 현재 i값을 한 페이지의 최대 컨텐츠 그리고 전체 컨텐츠 이내의 범위에서 증감시키며, 배열의 i번째 요소로 DOM 생성함수를 호출하여 element(ul)에 추가.
   for (let i = (page - 1) * maxContent; i < page * maxContent && i <= numOfContent; i++) {
-    if (agoraStatesDiscussions[i]) {
-      element.append(convertToDiscussion(agoraStatesDiscussions[i]));
+    // 로컬 스토리지에 저장된 item을 불러온다.
+    const item = window.localStorage.getItem(i);
+    if (item) {
+      element.append(convertToDiscussion(JSON.parse(item)));
     }
   }
   return;
@@ -148,7 +160,6 @@ function render(page) {
   renderContent(ul, page);
   renderButton(page);
 }
-render(page);
 
 // 사용자 입력 데이터 저장 후 렌더
 const makeInputToQuestion = function (e) {
@@ -168,10 +179,10 @@ const makeInputToQuestion = function (e) {
     bodyHTML: null,
     avatarUrl: 'https://lh3.googleusercontent.com/a-/AFdZucpSPHlMgNuNOQUqhzdWeMA4yWl4vexd730dVTXllQ=s96-c-rg-br100',
   };
-
   // data배열에 새롭게 생성한 obj를 추가한다.
   agoraStatesDiscussions.unshift(obj);
-
+  // 로컬 스토리지 불러오기
+  dataSaveLocalStorage();
   // 리스트의 상단에 만들어진 li를 붙인다.
   render(page);
 };
@@ -179,6 +190,18 @@ const makeInputToQuestion = function (e) {
 // form 이벤트 리스너
 form.addEventListener('submit', makeInputToQuestion);
 
+// 첫 실행 시 화면에 데이터 표시
+if (window.localStorage.length === 0) {
+  dataSaveLocalStorage();
+}
+render(page);
+
 // TODOs
 // 2. 로컬 스토리지에 저장하고, submit시 새로고침하며 데이터 로드
 // 3. prev 버튼 눌렀을 때 바로 직전 페이지 렌더링 되도록 표시
+
+// 로컬스토리지 사용
+// 1. 현재 더미데이터를 로컬 스토리지에 저장한다.
+// 2. 로컬 스토리지에 저장된 데이터로 화면에 렌더링한다.
+// 3. 사용자가 입력한 데이터로 객체를 만들어 로컬 스토리지에 저장한다. (앞쪽으로)
+// 4. submit을 누르면 새로고침 후 로컬 스토리지의 데이터를 불러와 렌더링한다.
