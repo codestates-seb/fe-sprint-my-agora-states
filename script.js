cd9270b81952d362ef14182d3f835b0afd972f11
tlsrb100 <tlsrb100@gmail.com>
2022-07-19 11:24:48 +0900

    디스커션 submit + localStorage 기능 추가

--- script.js
+++ script.js
@@ -37,7 +37,6 @@ const convertToDiscussion = (obj) => {
 
   discussionAnswered.append(answeredCheckBox);// discussionAnswered div요소안에 answeredCheckBox p요소 추가
 
-
   li.append(avatarWrapper, discussionContent, discussionAnswered); //li에 div요소 붙임
   return li;
 };
@@ -52,41 +51,6 @@ const isAnswered = function(obj){ // 객체 answer 속성에 따른 체크 표�
     return '☑';
   }
 };
-
-
-
-// submit 버튼을 누를때 디스커션 추가하는 함수입니다.
-//submit을 누를때 값 읽어오기
-const formInputName = document.querySelector('.form__input--name > input');
-const formInputTitle = document.querySelector('.form__input--title > input');
-const formInputTextbox = document.querySelector('.form__textbox > textarea');
-const formSubmit = document.querySelector('.form');
-
-const readSubmit = () =>{ // submit 이벤트 발생하면 각각의 value 출력하는 함수.
-  console.log(formInputName.value);
-  console.log(formInputTitle.value);
-  console.log(formInputTextbox.value);
-};
-
-
-const addSubmittedDiscussion = function(){ //submit 이벤트 발생시 agoraStatesDiscussions에 객체타입으로 데이터 넣는 함수.
-  agoraStatesDiscussions.push({ // 배열에 데이터 추가
-    id: null,
-    createdAt: returnNowtime(),
-    title: formInputTitle.value,
-    url: null,
-    author: formInputName.value,
-    answer: null,
-    bodyHTML: null,
-    avatarUrl: 'https://avatars.githubusercontent.com/u/12145019?s=64&u=5c97f25ee02d87898457e23c0e61b884241838e3&v=4',
-  }
-  )
-  render(ul);
-  console.log(agoraStatesDiscussions);
-};
-formSubmit.addEventListener('submit', addSubmittedDiscussion); //  submit 제출 이벤트 발생시 작동하는 함수 (submit 이벤트는 form 요소에서만 잡힌다.)
-
-
 
 const returnNowtime = function(){ //현재 시간 출력하는 함수.
   let today = new Date();
@@ -104,19 +68,78 @@ const returnNowtime = function(){ //현재 시간 출력하는 함수.
   minutes = ('00'+minutes.toString()).slice(-2);
   seconds = ('00'+seconds.toString()).slice(-2);
 
-  // return `${year}-${month}-${date}T${hours}:${minutes}:${seconds}T`;
   return `${year}.${month}.${date} - ${isAmPm} ${hours}:${minutes}:${seconds}`;
 };
 
 
+const formInputName = document.querySelector('.form__input--name > input');
+const formInputTitle = document.querySelector('.form__input--title > input');
+const formInputTextbox = document.querySelector('.form__textbox > textarea');
+const formSubmit = document.querySelector('.form');
+let tempSpace;
+let insertDataForm = {
+  id: null,
+  createdAt: null,
+  title: null,
+  url: null,
+  author: null,
+  answer: null,
+  bodyHTML: null,
+  avatarUrl: 'https://avatars.githubusercontent.com/u/12145019?s=64&u=5c97f25ee02d87898457e23c0e61b884241838e3&v=4',
+  };
 
+//submit 이벤트 발생시 agoraStatesDiscussions에 객체타입으로 데이터 넣는 함수.
+const addSubmittedDiscussion = function(){
 
+  if(Object.keys(localStorage).length>0){
+    console.log('오브젝트키'+Object.keys(localStorage));
+    tempSpace = arrayFromLocalStorage();
+    initInsertDataForm();
+    tempSpace.push(insertDataForm);
+  }else{
+    console.log('아무것도 없는쪽진입')
+    initInsertDataForm();
+    tempSpace = [insertDataForm]
+  }
+  //submit으로 추가한 값은 localStorage에 따로 저장한다.
+  setToLocalStorage();
+  render(ul);
+  console.log(agoraStatesDiscussions);
+  return;
+};
+formSubmit.addEventListener('submit', addSubmittedDiscussion); //  submit 제출 이벤트 발생시 작동하는 함수 (submit 이벤트는 form 요소에서만 잡힌다.)
 
+//event.preventDefault();넣어줘야한다.
+
+//submit할때의 데이터로 초기화시켜주는 함수
+const initInsertDataForm = function() { 
+  insertDataForm.createdAt = returnNowtime();
+  insertDataForm.title = formInputTitle.value;
+  insertDataForm.author = formInputName.value;
+  insertDataForm.bodyHTML = formInputTextbox.value;
+};
+
+// agoraStatesDiscussions배열값을 localStorage 에 그대로 넣어주는 함수
+const setToLocalStorage = function() {
+  localStorage.setItem("userData", JSON.stringify(tempSpace)); //로컬에 데이터값 저장
+  return;
+};
+// localstorage에 저장되어있는 값을 원 배열의 형태로 리턴하는 함수
+const arrayFromLocalStorage = function() {
+  return JSON.parse(localStorage.getItem("userData")); // agoraStatesDiscussions에 로컬저장값을 원배열로 파싱해서 할당
+};
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
-  for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
-    element.prepend(convertToDiscussion(agoraStatesDiscussions[i]));
+  for (let i = 0; i < agoraStatesDiscussions.length; i++) {
+    element.append(convertToDiscussion(agoraStatesDiscussions[i]));
+  }
+  console.log(arrayFromLocalStorage());
+  console.log(arrayFromLocalStorage().length);
+  if(Object.keys(localStorage.length >0)){
+    for(let j =0; j< arrayFromLocalStorage().length; j++){
+      element.prepend(convertToDiscussion(arrayFromLocalStorage()[j] ));
+    }
   }
   return;
 };
@@ -124,3 +147,20 @@ const render = (element) => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+
+
+
+
+/*제일 처음 화면을 켠다. 
+-> 처음 아고라의 배열값을 출력해야한다
+-> submit으로 추가를 해주면 추가한 배열이 출력이 되야한다
+
+agora 배열을 기준으로 출력한다
+그래야지 local 데이터가 없더라도 기본 배열값 41개는 출력된다
+그러면 어떻게?
+서밋 되는 부분만 따로 local데이터에 저장한다
+서밋하는부분에선 새로운데이터를 local에 저장한다.
+render 함수에서 기본 원배열을 다 출력해주고 if문으로 localStorage안에 값이 있으면 있는만큼 출력해준다.
+*/
+
