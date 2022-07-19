a8fa77465724aead7a15fb0fdd4e8f90e07689e1
waymokorea <taw0207@naver.com>
2022-07-19 12:50:56 +0900

    form 입력 discussion 추가 구현

--- script.js
+++ script.js
@@ -14,12 +14,15 @@ const convertToDiscussion = (obj) => {
   discussionAnswered.className = "discussion__answered";
 
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
+
+  // 프로필 이미지
   const avatarImg = document.createElement('img');
   avatarImg.src = obj.avatarUrl;
   avatarImg.alt = 'avatar of ' + obj.author;
   avatarImg.className = "discussion__avatar--image";
   avatarWrapper.append(avatarImg);
 
+  // content 제목
   const discussionTitle = document.createElement('h2');
   discussionTitle.className = "discussion__title";
 
@@ -29,6 +32,7 @@ const convertToDiscussion = (obj) => {
 
   discussionTitle.append(discussionUrl);
 
+  //content 정보표시
   const discussionInformation = document.createElement('div');
   discussionInformation.className = "discussion__information";
 
@@ -63,3 +67,35 @@ const render = (element) => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+
+//new discussion submit
+
+
+const form = document.querySelector('.form')
+const title = document.querySelector('div.form__input--title > input')
+const nameInput = document.querySelector('div.form__input--name > input')
+const textbox = document.querySelector('div.form__textbox > textarea')
+
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  // 새로운 객체를 만들어야한다
+  // input에 입력된값 밸류를 넣은 새로운 객체
+  // 새로운 객체를 ul요소 아래로 넣어준다
+  // 더미 데이터(agoraStatesDiscussions) 에도 추가해준다 
+  const obj = {
+    id: "unique id",
+    createdAt: new Date(),
+    title: title.value,
+    url: null,
+    author: nameInput.value,
+    answer: null,
+    bodyHTML: textbox.value,
+    avatarUrl:
+      "https://avatars.githubusercontent.com/u/97888923?s=64&u=12b18768cdeebcf358b70051283a3ef57be6a20f&v=4",
+  }
+  agoraStatesDiscussions.unshift(obj);
+  const newDiscussion = convertToDiscussion(obj)
+  ul.prepend(newDiscussion)
+})
