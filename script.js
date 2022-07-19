c7f993fbfe3746da3566b6111ebd2083d89835ab
x-xuhxun <hiihii0402@gmail.com>
2022-07-19 15:11:11 +0900

    디스커션 추가

--- script.js
+++ script.js
@@ -28,8 +28,7 @@ const convertToDiscussion = (obj) => {
   discussionContent.append(discussionTitle);
   // 디스커션 인포메이션 가져오기
   const disscussionInformation = document.createElement("div");
-  disscussionInformation.className = "discussion__information";
-  disscussionInformation.textContent = obj.author + ' / ' + obj.createdAt;
+  disscussionInformation.textContent = obj.author + ' / ' + new Date(obj.createdAt).toLocaleString();
   discussionContent.append(disscussionInformation);
     
   // 체크표시
@@ -37,12 +36,33 @@ const convertToDiscussion = (obj) => {
   discussionAnswer.textContent = '☑';
   discussionAnswered.append(discussionAnswer);
 
-  const ul = document.querySelector('ul.discussions__container');
-  ul.append(li);
-
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
 };
+
+const form = document.querySelector('form.form');
+const title = document.querySelector('div.form__input--title > input')
+const nameInput  = document.querySelector('div.form__input--name > input')
+const textbox = document.querySelector('div.form__textbox > textarea')
+form.addEventListener("submit", (event) => {
+  //submit 됨과 동시에 창이 다시 실행됨 -> 초기화면으로 그것을 막기위해사용
+  event.preventDefault();
+  // 새로운 개체 만들기 , 더미 데이터에도 추가
+  const obj = {
+      id: "id",
+      createdAt: new Date().toLocaleString(),
+      title: title.value,
+      url: "https://github.com/codestates-seb/agora-states-fe/discussions/45",
+      author: nameInput.value,
+      answer: null,
+      bodyHTML:textbox.value,
+        avatarUrl: "https://avatars.githubusercontent.com/u/79903256?s=64&v=4",
+    }
+  // input에 받은 값을 넣은 새로운 객체 만들기
+  agoraStatesDiscussions.unshift(obj);
+  // 새로운 객체를 ul 요소로 넣어준다
+  ul.prepend(convertToDiscussion(obj));
+})
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
