4faf992b3474eb5b724ffd9d3ea834e79135d25b
Hyejung Park <hyejj19@naver.com>
2022-07-19 19:44:28 +0900

    Feat : 로컬스토리지 저장 + answered 아이콘 수정

--- script.js
+++ script.js
@@ -4,22 +4,48 @@ const btnSubmit = document.querySelector('.form__submit');
 const form = document.querySelector('.form');
 const btns = document.querySelector('.btn__page--wrapper');
 
+let data;
+const dataFromLocalStorage = localStorage.getItem('agoraStatesDiscussions');
+if (dataFromLocalStorage) {
+  data = JSON.parse(dataFromLocalStorage);
+} else {
+  data = agoraStatesDiscussions.slice();
+}
+
+/////////////////////////////////////////////////////////////////////
+//페이지네이션
+
 // 페이지네이션 구현에 필요한 데이터
 const numOfContent = agoraStatesDiscussions.length;
-const maxContent = 10;
+const maxContent = 8;
 const maxButton = 5;
 const maxPage = Math.ceil(numOfContent / maxContent);
 let page = 1;
 
-const dataSaveLocalStorage = function (obj) {
-  // 로컬스토리지 저장
-  agoraStatesDiscussions.forEach((el, idx) => {
-    const objString = JSON.stringify(el);
-    window.localStorage.setItem(idx, objString);
-  });
-
+// 페이지 이동 함수
+const goPrevPage = () => {
+  page -= maxButton;
   render(page);
 };
+
+const goNextPage = () => {
+  page += maxButton;
+  render(page);
+};
+
+// prev 버튼 생성
+const prev = document.createElement('button');
+prev.classList.add('btn', 'prev');
+prev.textContent = '< 이전';
+prev.addEventListener('click', goPrevPage);
+
+const next = document.createElement('button');
+next.classList.add('btn', 'next');
+next.textContent = '다음 >';
+next.addEventListener('click', goNextPage);
+
+/////////////////////////////////////////////////////////////////////
+// DATA => DOM 생성 함수
 
 // 데이터 => DOM 생성 함수
 const convertToDiscussion = obj => {
@@ -60,7 +86,7 @@ const convertToDiscussion = obj => {
   const descussionCheckbox = document.createElement('p');
   // title에 [notice]가 붙지 않은 경우에만 답변 여부 표시
   if (!obj.title.includes('[notice]')) {
-    descussionCheckbox.textContent = obj.answer !== null ? '✔️' : '❌';
+    descussionCheckbox.textContent = obj.answer !== null ? '✅' : '❎';
   }
   discussionAnswered.append(descussionCheckbox);
 
@@ -88,29 +114,10 @@ const makeButton = id => {
   return btn;
 };
 
-// 페이지 이동 함수
-const goPrevPage = () => {
-  page -= maxButton;
-  render(page);
-};
+/////////////////////////////////////////////////////////////////////
+// DOM => 화면 렌더링 함수
 
-const goNextPage = () => {
-  page += maxButton;
-  render(page);
-};
-
-// prev 버튼 생성
-const prev = document.createElement('button');
-prev.classList.add('btn', 'prev');
-prev.textContent = '< 이전';
-prev.addEventListener('click', goPrevPage);
-
-const next = document.createElement('button');
-next.classList.add('btn', 'next');
-next.textContent = '다음 >';
-next.addEventListener('click', goNextPage);
-
-// 배열의 데이터 렌더링 함수
+// 데이터 렌더링 함수
 const renderContent = (element, page) => {
   // 글 목록 초기화
   while (element.hasChildNodes()) {
@@ -121,9 +128,8 @@ const renderContent = (element, page) => {
   // ... 직전 페이지로부터 넘어온 현재 i값을 한 페이지의 최대 컨텐츠 그리고 전체 컨텐츠 이내의 범위에서 증감시키며, 배열의 i번째 요소로 DOM 생성함수를 호출하여 element(ul)에 추가.
   for (let i = (page - 1) * maxContent; i < page * maxContent && i <= numOfContent; i++) {
     // 로컬 스토리지에 저장된 item을 불러온다.
-    const item = window.localStorage.getItem(i);
-    if (item) {
-      element.append(convertToDiscussion(JSON.parse(item)));
+    if (data[i]) {
+      element.append(convertToDiscussion(data[i]));
     }
   }
   return;
@@ -155,7 +161,7 @@ const renderButton = page => {
   if (maxPage - page < page && page <= maxPage) btns.removeChild(next);
 };
 
-// 화면 처음 띄울 때 컨텐츠, 버튼 렌더링
+// 컨텐츠, 버튼 렌더링 실행 함수
 function render(page) {
   renderContent(ul, page);
   renderButton(page);
@@ -180,9 +186,10 @@ const makeInputToQuestion = function (e) {
     avatarUrl: 'https://lh3.googleusercontent.com/a-/AFdZucpSPHlMgNuNOQUqhzdWeMA4yWl4vexd730dVTXllQ=s96-c-rg-br100',
   };
   // data배열에 새롭게 생성한 obj를 추가한다.
-  agoraStatesDiscussions.unshift(obj);
-  // 로컬 스토리지 불러오기
-  dataSaveLocalStorage();
+  data.unshift(obj);
+
+  localStorage.setItem('agoraStatesDiscussions', JSON.stringify(data));
+
   // 리스트의 상단에 만들어진 li를 붙인다.
   render(page);
 };
@@ -191,17 +198,4 @@ const makeInputToQuestion = function (e) {
 form.addEventListener('submit', makeInputToQuestion);
 
 // 첫 실행 시 화면에 데이터 표시
-if (window.localStorage.length === 0) {
-  dataSaveLocalStorage();
-}
 render(page);
-
-// TODOs
-// 2. 로컬 스토리지에 저장하고, submit시 새로고침하며 데이터 로드
-// 3. prev 버튼 눌렀을 때 바로 직전 페이지 렌더링 되도록 표시
-
-// 로컬스토리지 사용
-// 1. 현재 더미데이터를 로컬 스토리지에 저장한다.
-// 2. 로컬 스토리지에 저장된 데이터로 화면에 렌더링한다.
-// 3. 사용자가 입력한 데이터로 객체를 만들어 로컬 스토리지에 저장한다. (앞쪽으로)
-// 4. submit을 누르면 새로고침 후 로컬 스토리지의 데이터를 불러와 렌더링한다.
