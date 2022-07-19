e74dca1bd84d671116254e87bcc8b628837b5e3b
skynolimit <sky221b9@gmail.com>
2022-07-19 15:09:18 +0900

    1차 제출

--- script.js
+++ script.js
@@ -7,14 +7,27 @@ const convertToDiscussion = (obj) => {
   li.className = "discussion__container"; // 클래스 이름 지정
 
   const avatarWrapper = document.createElement("div");
-  avatarWrapper.className = "discussion__avatar--wrapper";
+  avatarWrapper.className = "discussion__avatar--wrapper";//아직 empty
   const discussionContent = document.createElement("div");
-  discussionContent.className = "discussion__content";
+  discussionContent.className = "discussion__content";//아직 empty
   const discussionAnswered = document.createElement("div");
-  discussionAnswered.className = "discussion__answered";
+  discussionAnswered.className = "discussion__answered";//아직 empty
 
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
+  //1번 div내용 - img (src + alt)
+const avatarImg = document.createElement('img');
+avatarImg.src = obj.avatarUrl;
+avatarImg.alt ="avatar of" + obj.author
+avatarWrapper.append(avatarImg)
 
+const discussionTitle = document.createElement('h2')
+const titleAnchor = document.createElement('a')
+discussionTitle.append(titleAnchor);
+titleAnchor.textContent = obj.title
+
+const discussionInfo = document.createElement('div')
+discussionInfo.textContent = `${obj.author}/${new Date(obj.createAt).toLocaleString()}`
+discussionContent.append(discussionTitle,discussionInfo)
 
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
@@ -22,13 +35,13 @@ const convertToDiscussion = (obj) => {
 };
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
-const render = (element) => {
+const render = (ul) => {
   for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
-    element.append(convertToDiscussion(agoraStatesDiscussions[i]));
+    ul.append(convertToDiscussion(agoraStatesDiscussions[i]));
   }
   return;
 };
 
-// ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
+// ul(in html) 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
