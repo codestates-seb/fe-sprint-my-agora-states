6b0ea3c129c84794f7c7f17c03f28187cd5b6c05
leejehoon92 <2eejehoon@gmail.com>
2022-07-19 16:00:52 +0900

    renderButton 수정

--- script.js
+++ script.js
@@ -73,7 +73,6 @@ const render = (element) => {
   for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
     element.append(convertToDiscussion(agoraStatesDiscussions[i]));
   }
-  renderButton(element);
   return;
 };
 
