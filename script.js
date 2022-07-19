e415aa86275723cc9785e965bd5f22d397daacb2
Pikabi-LOVU <pikabi.lovu@gmail.com>
2022-07-19 15:51:16 +0900

    eventlistener null error

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ console.log(agoraStatesDiscussions);
 const convertToDiscussion = (obj) => {
   const li = document.createElement("li"); // li 요소 생성
   li.className = "discussion__container"; // 클래스 이름 지정
-//이 함수의 목적: li 뭉치를 새롭게 만든다 (왜 여기서 새로 만드는가...?)
+  //이 함수의 목적: li 뭉치를 새롭게 만든다 (왜 여기서 새로 만드는가...?)
   const avatarWrapper = document.createElement("div"); //아바타, 아이콘
   avatarWrapper.className = "discussion__avatar--wrapper";
   const discussionContent = document.createElement("div");
@@ -15,59 +15,38 @@ const convertToDiscussion = (obj) => {
 
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
 
-//이미지를 넣자 
-const avatarImg = document.createElement('img')
-avatarImg.src = obj.avatarUrl
-avatarImg.alt = "avatar of " + obj.author
+  //이미지, 대체 택스트
+  const avatarImg = document.createElement("img");
+  avatarImg.setAttribute("src", obj.avatarUrl);
+  avatarImg.setAttribute("alt", obj.author);
+  avatarWrapper.appendChild(avatarImg);
 
-avatarWrapper.append(avatarImg)
+  //제목 내용 + 하이퍼링크
+  const discussionTitle = document.createElement("h2");
+  const titleAnchor = document.createElement("a");
+  titleAnchor.setAttribute("href", obj.url);
+  titleAnchor.textContent = obj.title;
 
-//제목을 넣자 
-const discussionTitle = document.createElement('h2')
-const titleAnchor = document.createElement('a')
-discussionTitle.append(titleAnchor)
+  discussionTitle.appendChild(titleAnchor);
+  discussionContent.appendChild(titleAnchor);
 
-discussionContent.append(titleAnchor)
-titleAnchor.textContent = obj.title
+  //아이디, 작성시간을 추가 정보로 넣자
+  const discussionInfo = document.createElement("div");
+  discussionInfo.className = "discussion__information";
+  discussionInfo.textContent = `${obj.author} / ${new Date(
+    obj.createdAt
+  ).toLocaleString("ko-KR")}`;
 
-//아이디, 작성시간을 넣자 
-const discussionInfo = document.createElement('div')
-discussionInfo.textContent = `${obj.author} / ${new Date(obj.createdAt).toLocaleString()}`
-discussionContent.append(discussionInfo)
+  discussionContent.appendChild(discussionInfo);
 
+  //답변완료 체크박스를 넣자
+  const answeredMark = document.createElement("p");
+  answeredMark.textContent = obj.anwer ? "☑" : "⊠";
+  discussionAnswered.appendChild(answeredMark);
 
-li.append(avatarWrapper, discussionContent, discussionAnswered);
-  return li;
+  li.append(avatarWrapper, discussionContent, discussionAnswered);
+  return li; //이건 왜 하는 걸까?
 };
-
-//form 
-const form = document.querySelector('form.form')
-const title = document.querySelector('div.form__input--title > input')
-const name = document.querySelector('div.form__input--name > input')
-const textbox = document.querySelector('div.form__textbox--name > textarea')
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  // 새로운 객체를 만들어야 한다. 
-  // input에 입력된 값(value)를 넣은 새로운 객체다.
-  // 새로운 객체를 ul요소 아래로 넣어준다 
-  // 더미 데이터 (agoraStatesDiscussions)에도 추가해준다 
-  const obj = {
-    id: "unique id",
-    createdAt: new Date().toLocaleString(),
-    title: title.value,
-    url: "https://github.com/codestates-seb/agora-states-fe/discussions/45",
-    author: "dubipy",
-    answer: null,
-    bodyHTML: textbox.value,
-    avatarUrl:
-      "https://avatars.githubusercontent.com/u/97888923?s=64&u=12b18768cdeebcf358b70051283a3ef57be6a20f&v=4",
-  }
-
-  agoraStatesDiscussions.unshift(obj);
-  const newDiscussion = convertToDiscussion(obj);
-  ul.prepend();
-})
-
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
@@ -81,3 +60,36 @@ const render = (element) => {
 const ul = document.querySelector("ul.discussions__container");
 render(ul); //rendering: 브라우저에 우리가 그린 코드를 실행
 
+//새로운 질문 올리기
+const inputName = document.getElementById("name");
+const inputTitle = document.getElementById("title");
+const inputStory = document.getElementById("story");
+const sumbit = document.getElementById("submit");
+
+sumbit.addEventListener("click", (event) => {
+
+  if (inputName.value && inputTitle.value && inputStory.value) {
+    let confirmSubmit = window.confirm("새로운 질문을 제출합니다.");
+    if (confirmSubmit === true) {
+      event.preventDefault();
+      const inputObj = {
+        id: "unique id",
+        author: inputName.value,
+        title: inputTitle.value,
+        bodyHTML: inputStory.value,
+        createdAt: new Date().toLocaleString("ko-KR"),
+        avatarUrl:
+          "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+        answer: null,
+        url: "#",
+      };
+      agoraStatesDiscussions.unshift(inputObj);
+      // const newDiscussion = convertToDiscussion(inputObj);
+      ul.prepend(convertToDiscussion(agoraStatesDiscussions[0]));
+    } else {
+      alert("제출을 그만둡니다.");
+    }
+  } else {
+    window.alert("칸을 전부 채워주세요.");
+  }
+});
