46d063c92be2e173175754a6ff0da06dabf1a980
tlsrb100 <tlsrb100@gmail.com>
2022-07-19 15:19:32 +0900

    삭제 기능 미완성 보완필요

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 console.log(agoraStatesDiscussions);
 
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
-const convertToDiscussion = (obj) => {
+const convertToDiscussion = (obj, j) => {
   const li = document.createElement("li"); // li 요소 생성
   li.className = "discussion__container"; // 클래스 이름 지정
 
@@ -27,7 +27,8 @@ const convertToDiscussion = (obj) => {
   
   const disccusionQuestion = document.createElement('div'); //질문내용 div 요소 생성
   disccusionQuestion.classList  = 'discussion__question';
-  disccusionQuestion.textContent = obj.bodyHTML;
+  // disccusionQuestion.textContent = obj.bodyHTML;
+  disccusionQuestion.textContent = obj.bodyHTML.replace(/(<([^>]+)>)/ig,"");
 
   const discussionInformation = document.createElement('div'); //정보 div 요소 생성
   discussionInformation.className = 'discussion__information'; //discussionInformation요소 class 이름 초기화
@@ -36,8 +37,19 @@ const convertToDiscussion = (obj) => {
   
   const answeredCheckBox = document.createElement('p'); // answered 여부 확인 표시 요소 생성
   answeredCheckBox.textContent = isAnswered(obj);//answered에 체크여부 데이터 할당
-  discussionAnswered.append(answeredCheckBox);// discussionAnswered div요소안에 answeredCheckBox p요소 추가
-  li.append(avatarWrapper, discussionContent, discussionAnswered); //li에 div요소 붙임
+  const deleteBox = document.createElement('div');// delete box 요소 생성
+  deleteBox.className = 'discussion__delete'; //클래스 네임 정의
+  const buttonBox = document.createElement('button'); //버튼 박스 생성
+  buttonBox.textContent = '삭제';
+  deleteBox.append(buttonBox);
+
+  //렌더링하면서 인덱스를 만들어줌
+  const index = document.createElement('div'); // 인덱스 붙일 요소 생성
+  index.className = 'hidden';
+  index.textContent = j; //순서대로 인덱스 할당
+
+  discussionAnswered.append(answeredCheckBox );// discussionAnswered div요소안에 answeredCheckBox p요소 추가
+  li.append(avatarWrapper, discussionContent, discussionAnswered, deleteBox, index); //li에 div요소 붙임
   return li;
 };
 
@@ -88,27 +100,48 @@ let insertDataForm = {
   avatarUrl: 'https://avatars.githubusercontent.com/u/12145019?s=64&u=5c97f25ee02d87898457e23c0e61b884241838e3&v=4',
   };
 
+
+
+
+
 //submit 이벤트 발생시 agoraStatesDiscussions에 객체타입으로 데이터 넣는 함수.
 const addSubmittedDiscussion = function(){
-
   if(Object.keys(localStorage).length>0){
     console.log('오브젝트키'+Object.keys(localStorage));
     tempSpace = arrayFromLocalStorage();
     initInsertDataForm();
     tempSpace.push(insertDataForm);
   }else{
-    console.log('아무것도 없는쪽진입')
     initInsertDataForm();
     tempSpace = [insertDataForm]
   }
-  //submit으로 추가한 값은 localStorage에 따로 저장한다.
-  setToLocalStorage();
+
+  setToLocalStorage();//submit으로 추가한 값은 localStorage에 따로 저장한다.
   render(ul);
   console.log(agoraStatesDiscussions);
   return;
 };
 formSubmit.addEventListener('submit', addSubmittedDiscussion); //  submit 제출 이벤트 발생시 작동하는 함수 (submit 이벤트는 form 요소에서만 잡힌다.)
 //event.preventDefault();넣어줘야한다.
+
+
+//delete 이벤트 발생시 li요소삭제하는 함수.
+const discussionList = document.querySelector('.discussions__container');// ul요소
+const removeSubmittedDiscussion = function(event){
+  let eventEl = event.target; //이벤트 타켓 요소 (버튼요소)
+  eventEl.parentNode.parentNode.remove(); // li요소 삭제
+  removeLocalStorage();
+};
+discussionList.addEventListener('click', removeSubmittedDiscussion); //remove 버튼 클릭 이벤트 발생시 작동
+
+
+//삭제되는 li요소 인덱스에 맞춰 localstorage 해당 인덱스 배열데이터도 삭제하는 함수
+const removeLocalStorage = function(delete_index){
+  tempSpace = arrayFromLocalStorage();
+  tempSpace.splice(delete_index-1, 1);
+  console.log(tempSpace);
+  setToLocalStorage();
+};
 
 
 //submit할때의 데이터로 초기화시켜주는 함수
@@ -125,10 +158,17 @@ const setToLocalStorage = function() {
   localStorage.setItem("userData", JSON.stringify(tempSpace)); //로컬에 데이터값 저장
   return;
 };
+
 // localstorage에 저장되어있는 값을 원 배열의 형태로 리턴하는 함수
 const arrayFromLocalStorage = function() {
   return JSON.parse(localStorage.getItem("userData")); // agoraStatesDiscussions에 로컬저장값을 원배열로 파싱해서 할당
 };
+
+
+
+
+
+
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
@@ -137,9 +177,10 @@ const render = (element) => {
   }
   console.log(arrayFromLocalStorage());
   console.log(arrayFromLocalStorage().length);
-  if(Object.keys(localStorage.length >0)){
+  if(Object.keys(localStorage.length >0)){ //localstorage에 있는 데이터 렌더링
     for(let j =0; j< arrayFromLocalStorage().length; j++){
-      element.prepend(convertToDiscussion(arrayFromLocalStorage()[j] ));
+      element.prepend(convertToDiscussion(arrayFromLocalStorage()[j], j));
+
     }
   }
   return;
@@ -148,20 +189,3 @@ const render = (element) => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
-
-
-
-
-
-/*제일 처음 화면을 켠다. (새로고침마다 script.js 재실행)
--> 처음 아고라의 배열값을 출력해야한다
--> submit으로 추가를 해주면 추가한 배열이 출력이 되야한다
-
-agora 배열을 기준으로 출력한다
-그래야지 local 데이터가 없더라도 기본 배열값 41개는 출력된다
-그러면 어떻게?
-서밋 되는 부분만 따로 local데이터에 저장한다
-서밋하는부분에선 새로운데이터를 local에 저장한다.
-render 함수에서 기본 원배열을 다 출력해주고 if문으로 localStorage안에 값이 있으면 있는만큼 출력해준다.
-*/
-
