1cfbe7983bc344b43309bae89b769357fa721d5e
rosenfence <rosenfence@gmail.com>
2022-07-19 10:24:04 +0900

    [feat] new question submit

--- script.js
+++ script.js
@@ -8,60 +8,60 @@ const convertToDiscussion = (obj) => {
 
   const avatarWrapper = document.createElement("div");
   avatarWrapper.className = "discussion__upper--wrapper";
-  const discussionContent = document.createElement("div");
-  discussionContent.className = "discussion__content";
-
-
+    
   //avatarWrapper에 프로필 사진 넣기
   const avatarImage = document.createElement("img");
-    avatarImage.className = "discussion__upper--image";
-    avatarImage.setAttribute("src", obj.avatarUrl)
-    avatarImage.setAttribute("alt", `avatar of ${obj.author}`);
-
+  avatarImage.className = "discussion__upper--image";
+  avatarImage.setAttribute("src", obj.avatarUrl)
+  avatarImage.setAttribute("alt", `avatar of ${obj.author}`);
+  
   avatarWrapper.append(avatarImage);
-
+  
   //avatarWrapper에 프로필 텍스트 감싸는거 넣기
   const profileText = document.createElement("span");
   profileText.className = "discussion__upper--profile";
-    //profileText에 사용자 닉네임 넣기
-    const profileAuthor = document.createElement("div");
-    profileAuthor.className = "discussion__upper--profile--author";
-    profileAuthor.innerText = String(obj.author);
-    profileText.append(profileAuthor);
-    //profileText에 작성 시간 넣기
-    const profileWrittenTime = document.createElement("div");
-    profileWrittenTime.className = "discussion__upper--profile--writtentime";
-      const timeText = `${obj.createdAt.slice(5,7)}.${obj.createdAt.slice(8,10)} ${obj.createdAt.slice(11,19)}`;
-    profileWrittenTime.innerText = String(timeText);
-    profileText.append(profileWrittenTime);
-    //profileText avatarWrapper에 넣기
+  //profileText에 사용자 닉네임 넣기
+  const profileAuthor = document.createElement("div");
+  profileAuthor.className = "discussion__upper--profile--author";
+  profileAuthor.innerText = String(obj.author);
+  profileText.append(profileAuthor);
+  //profileText에 작성 시간 넣기
+  const profileWrittenTime = document.createElement("div");
+  profileWrittenTime.className = "discussion__upper--profile--writtentime";
+  const timeText = `${obj.createdAt.slice(5,7)}.${obj.createdAt.slice(8,10)} ${obj.createdAt.slice(11,19)}`;
+  profileWrittenTime.innerText = String(timeText);
+  profileText.append(profileWrittenTime);
+  //profileText avatarWrapper에 넣기
   avatarWrapper.append(profileText);
-    
-    //avatarWrapper에 answersheet 넣기
+  
+  //avatarWrapper에 answersheet 넣기
   const profileAnswer = document.createElement("span");
   profileAnswer.className = "discussion__upper--answersheet";
-    //profileAnswer에 답변됨 박스 넣기
-    const answered = document.createElement("div");
-    answered.className = "discussion__upper--answersheet-answered";
-      //answered에 초록 체크 넣기
-      const checkbox = document.createElement("div");
-      answered.append(checkbox);
-
-    const notanswered = document.createElement("div");
-    notanswered.className = "discussion__upper--answersheet-notanswered";
-      //notanswered에 빨강 엑스 넣기
-      const xbox = document.createElement("div");
-      notanswered.append(xbox);
-
-      //안보이게 할 박스 정하기
-      console.log(obj.answer);
-      obj.answer === null ? xbox.innerText = "❎" : checkbox.innerText = "✅";
-    profileAnswer.append(answered);
-    profileAnswer.append(notanswered);
+  //profileAnswer에 답변됨 박스 넣기
+  const answered = document.createElement("div");
+  answered.className = "discussion__upper--answersheet-answered";
+  //answered에 초록 체크 넣기
+  const checkbox = document.createElement("div");
+  answered.append(checkbox);
+  
+  const notanswered = document.createElement("div");
+  notanswered.className = "discussion__upper--answersheet-notanswered";
+  //notanswered에 빨강 엑스 넣기
+  const xbox = document.createElement("div");
+  notanswered.append(xbox);
+  
+  //안보이게 할 박스 정하기
+  obj.answer === null ? xbox.innerText = "❎" : checkbox.innerText = "✅";
+  profileAnswer.append(answered);
+  profileAnswer.append(notanswered);
   
   avatarWrapper.append(profileAnswer);
+  
 
 
+  const discussionContent = document.createElement("div");
+  discussionContent.className = "discussion__content";
+  
 
   const discussionLink = document.createElement("h2");
   discussionLink.className = "discussion__title";
@@ -105,3 +105,42 @@ const render = (element) => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+// submit을 누르면 id, 지금시간, 제목을 ul 밑에 li로 추가함
+const form = document.querySelector("form.form");
+const inputID = document.querySelector("#name");
+const inputTitle = document.querySelector("#title");
+const inputText = document.querySelector("#story");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  //시간 만들기
+  const nowATimes = new Date();
+  const year = nowATimes.getFullYear();
+  const month = ('0' + (nowATimes.getMonth() + 1)).slice(-2);
+  const day = ('0' + (nowATimes.getDate() + 1)).slice(-2);
+  const hours = ('0' + nowATimes.getHours()).slice(-2);
+  const minutes = ('0' + nowATimes.getMinutes()).slice(-2);
+  const seconds = ('0' + nowATimes.getSeconds()).slice(-2);
+  const inputCreatedAt = `${year}-${month}-${day}T${hours}:${minutes}:${seconds}Z`;
+
+
+  const obj = {
+    id: "unique ID",
+    createdAt: inputCreatedAt,//! 시간 양식에 맞게 변경 필요
+    title: inputTitle.value,
+    url: "",
+    author: inputID.value,
+    answer: null,
+    bodyHTML: inputText.value,
+    avatarUrl:
+      "https://noticon-static.tammolo.com/dgggcrkxq/image/upload/v1586271210/noticon/sageach1qrmmyfufwli1.gif",
+  
+  }
+  agoraStatesDiscussions.unshift(obj);
+  console.log(agoraStatesDiscussions)
+  const newDiscussion = convertToDiscussion(obj);
+  ul.prepend(newDiscussion);
+})
+
