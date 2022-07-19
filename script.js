204d662412333e8bd9ed8163422af6f2ff1ee779
callmejeje <choije401@naver.com>
2022-07-19 14:00:00 +0900

    bare minimum requirement

--- script.js
+++ script.js
@@ -13,10 +13,40 @@ const convertToDiscussion = (obj) => {
   const discussionAnswered = document.createElement("div");
   discussionAnswered.className = "discussion__answered";
 
+
+  const avatarImg = document.createElement('img');
+  avatarImg.className = "discussion__avatar--image";
+
+  const discussionTitle = document.createElement("h2");
+  discussionTitle.className = "discussion__title";
+
+  discussionTitle.appendChild(document.createElement("a"));
+
+  const discussionInformation = document.createElement("div");
+  discussionInformation.className = "discussion__information";
+
+
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
 
+  discussionTitle.firstChild.href = obj.url;
+  discussionTitle.firstChild.textContent = obj.title;
+  avatarImg.src = obj.avatarUrl;
+  avatarImg.alt = "avatar of " + obj.author;
+  discussionInformation.textContent = `${obj.author} / ${obj.createdAt}`;
 
 
+  if (obj.answer) {
+    discussionAnswered.textContent = "☑";
+  }
+  else {
+    discussionAnswered.textContent = "☒";
+  }
+
+  discussionContent.append(discussionTitle);
+  avatarWrapper.append(avatarImg);
+  discussionContent.append(discussionInformation);
+
+  
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
 };
@@ -28,6 +58,7 @@ const render = (element) => {
   }
   return;
 };
+
 
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
