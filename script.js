f3f5dedb7bee6972261b0ddec779e04454171800
JungInHa <juudyy99@gmail.com>
2022-07-19 15:14:45 +0900

    Bare Minimum Requirement

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // index.html을 열어서 agoraStatesDiscussions 배열 요소를 확인하세요.
-console.log(agoraStatesDiscussions);
+// console.log(agoraStatesDiscussions);
 
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
 const convertToDiscussion = (obj) => {
@@ -8,15 +8,41 @@ const convertToDiscussion = (obj) => {
 
   const avatarWrapper = document.createElement("div");
   avatarWrapper.className = "discussion__avatar--wrapper";
+  const avatarImg = document.createElement('img');
+  avatarImg.className = "discussion__avatar--image";
+
   const discussionContent = document.createElement("div");
   discussionContent.className = "discussion__content";
+  const discussionTitle = document.createElement("h2");
+  discussionTitle.className = "discussion__title";
+  const discussionTitleAnchor = document.createElement("a");
+  const discussionInfo = document.createElement("div");
+  discussionInfo.className = "discussion__information";
+
   const discussionAnswered = document.createElement("div");
   discussionAnswered.className = "discussion__answered";
+  // const discussionCheckbox = document.createElement("p");
+  const discussionCheckbox = document.createElement("img");
 
-  // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
+  // 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
+  avatarImg.src = obj.avatarUrl;
+  avatarImg.alt = 'avatar of ' + obj.author;
+  discussionTitleAnchor.textContent = obj.title;
+  discussionTitleAnchor.href = obj.url;
+  discussionInfo.textContent =  `${obj.author} • ${new Date(obj.createdAt).toLocaleString('ko-KR')}`;
 
-
-
+  if (obj.answer) {
+    discussionCheckbox.src = "./img/answered.png";
+    discussionCheckbox.alt = "answered"
+  } else {
+    discussionCheckbox.src = "./img/unanswered.png";
+    discussionCheckbox.alt = "unanswered"
+  }
+  
+  avatarWrapper.append(avatarImg);
+  discussionTitle.append(discussionTitleAnchor);
+  discussionContent.append(discussionTitle, discussionInfo);
+  discussionAnswered.append(discussionCheckbox);
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
 };
@@ -32,3 +58,38 @@ const render = (element) => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+// agoraStatesDiscussions 배열의 모든 데이터를 화면에서 지우는 함수입니다. 
+// const erase = (container) => {
+//   while (container.firstChild) {
+//     container.removeChild(container.firstChild);
+//   } 
+// }
+
+const form = document.querySelector('form.form');
+const formName = document.querySelector('div.form__input--name > input');
+const formTitle = document.querySelector('div.form__input--title > input');
+const formStory = document.querySelector('div.form__textbox > textarea');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const obj = {
+    id: "null id",
+    createdAt: new Date(),
+    title: formTitle.value,
+    url: "https://github.com/codestates-seb/agora-states-fe/discussions/",
+    author: formName.value,
+    answer: null,
+    bodyHTML: formStory.value,
+    avatarUrl: "./img/avatar.png",
+  };
+
+  agoraStatesDiscussions.unshift(obj);
+  const newDiscussion = convertToDiscussion(obj);
+  ul.prepend(newDiscussion);
+
+  formName.value = "";
+  formTitle.value = "";
+  formStory.value = "";
+});
