31c45d579db989be81b197f7dd39847db75ae296
js-ha <jshaha0911@gmail.com>
2022-07-19 19:39:54 +0900

    Bare Minimum Requirment

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 // index.html을 열어서 agoraStatesDiscussions 배열 요소를 확인하세요.
 console.log(agoraStatesDiscussions);
 
+
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
-const convertToDiscussion = (obj) => {
+const convertToDiscussion = (obj) => { 
   const li = document.createElement("li"); // li 요소 생성
   li.className = "discussion__container"; // 클래스 이름 지정
+  // 이 함수의 목적 -> li 요소 안에 있는 자식 요소들의 뭉치를 만든다
 
   const avatarWrapper = document.createElement("div");
   avatarWrapper.className = "discussion__avatar--wrapper";
@@ -12,8 +14,40 @@ const convertToDiscussion = (obj) => {
   discussionContent.className = "discussion__content";
   const discussionAnswered = document.createElement("div");
   discussionAnswered.className = "discussion__answered";
+  
+  
+  
+
+ 
+  
 
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
+  const avatarImg = document.createElement('img');
+  avatarImg.src = obj.avatarUrl;
+  avatarImg.alt = 'avatar of ' + agoraStatesDiscussions[0].author;
+  avatarWrapper.append(avatarImg);
+  
+  
+  const discussiontitle = document.createElement("h2");
+  discussiontitle.className = "discussion__title"
+  const titleAnchor = document.createElement("a");
+  titleAnchor.href = obj.url;
+  titleAnchor.textContent = obj.title;
+  discussiontitle.append(titleAnchor);
+
+  const discussionInfo = document.createElement("div");
+  discussionInfo.textContent = `${obj.author}/${obj.createdAt}`
+  discussionContent.append(discussiontitle, discussionInfo);
+ 
+  const checked = document.createElement("p");
+  checked.className = "same";
+  if(obj.answer === null){
+    checked.textContent = "❎ "
+  } else {
+    checked.textContent = "✅ "
+  }
+
+  discussionAnswered.append(checked); 
 
 
 
@@ -21,8 +55,37 @@ const convertToDiscussion = (obj) => {
   return li;
 };
 
+
+const form = document.querySelector("form.form")
+const title = document.querySelector("div.form__input--title > input")
+const nameInput = document.querySelector("div.form__input--name > input")
+const textbox = document.querySelector("div.form__textbox > textarea")
+form.addEventListener("submit",(event)=> {
+  // event.preventDefault(); 해야된다는데 어떻게?
+
+  // 새로운 객체를 만들어야 한다.
+  // Input에 입력된 값(value)을 넣은 새로운 객체.
+  // 새로운 객체를 ul요소 아래로 넣어준다.
+  // 더미 데이터(agoraStatesDiscussions)에도 추가해준다.
+  const obj = {
+    id: "unique.id",
+    createdAt: new Date().toLocaleString(),
+    title: title.value,
+    url: "https://github.com/codestates-seb/agora-states-fe/discussions/45",
+    author: nameInput.value,
+    answer: null,
+    bodyHTML: textbox.value,
+    avatarUrl:
+      "https://avatars.githubusercontent.com/u/97888923?s=64&u=12b18768cdeebcf358b70051283a3ef57be6a20f&v=4",
+  }
+  agoraStatesDiscussions.unshift(obj);
+  const newDiscussion = convertToDiscussion(obj)
+  ul.prepend(newDiscussion);
+})
+
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
+  // element에는 ul이 들어옴
   for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
     element.append(convertToDiscussion(agoraStatesDiscussions[i]));
   }
@@ -31,4 +94,7 @@ const render = (element) => {
 
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
-render(ul);
+render(ul); // render라는 이 함수를 실행시키면서 끝남
+
+
+
