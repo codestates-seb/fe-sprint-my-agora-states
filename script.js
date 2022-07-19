af4c07e63ca5a9f43f59ea85259e6b3d9b32d4a9
callmejeje <choije401@naver.com>
2022-07-19 22:48:03 +0900

    bare minimum requirement complete

--- script.js
+++ script.js
@@ -8,12 +8,6 @@ const buttonSubmit = document.querySelector('.form__submit > input');
 
 const form = document.querySelector('.form');
 
-// console.log(inputName);
-// console.log(inputTitle);
-// console.log(textareaStory);
-// console.log(buttonSubmit);
-
-
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
 const convertToDiscussion = (obj) => {
   const li = document.createElement("li"); // li 요소 생성
@@ -38,9 +32,6 @@ const convertToDiscussion = (obj) => {
   const discussionInformation = document.createElement("div");
   discussionInformation.className = "discussion__information";
 
-
-  // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
-
   discussionTitle.firstChild.href = obj.url;
   discussionTitle.firstChild.textContent = obj.title;
   avatarImg.src = obj.avatarUrl;
@@ -58,7 +49,6 @@ const convertToDiscussion = (obj) => {
   discussionContent.append(discussionTitle);
   avatarWrapper.append(avatarImg);
   discussionContent.append(discussionInformation);
-
   
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
@@ -66,21 +56,45 @@ const convertToDiscussion = (obj) => {
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
+  // 다 지우고 배열에 있는 내용 다 보여주기
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+  
   for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
     element.append(convertToDiscussion(agoraStatesDiscussions[i]));
   }
   return;
 };
 
-form.addEventListener('submit', function (event) {
-  // console.log('submit');
-  const newDiscussion = {
-    id: inputName.value,
-  }
-  agoraStatesDiscussions.unshift();
-})
-
-
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+form.addEventListener('submit', function (event) {
+  // console.log('submit');
+  event.preventDefault();
+  
+  const newDiscussion = {
+    id: inputName.value,
+    createdAt: new Date(),
+    title: inputTitle.value,
+    url: "https://github.com/codestates-seb/agora-states-fe/discussions",
+    author: inputName.value,
+    bodyHTML: textareaStory.value,
+    avatarUrl:
+      "https://avatars.githubusercontent.com/u/97888923?s=64&u=12b18768cdeebcf358b70051283a3ef57be6a20f&v=4",
+  }
+
+  // console.log(newDiscussion);
+
+  agoraStatesDiscussions.unshift(newDiscussion);
+  // const discussion = convertToDiscussion(newDiscussion);
+
+  
+  // while (ul.firstChild) {
+  //   ul.removeChild(ul.firstChild);
+  // }
+
+  render(ul);
+})
