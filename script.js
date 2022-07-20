10020d50f1a26237e212979018d59aaa1c321750
starplanter93 <starplanter93@gmail.com>
2022-07-20 19:21:16 +0900

    html에 버튼요소 추가, 스크립트에 reder함수 수정 및 newRender함수 비활성화, 스크립트에 페이지네이션 뒤로가기 앞으로가기 로컬스토리지 지우기 추가

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // index.html을 열어서 agoraStatesDiscussions 배열 요소를 확인하세요.
 console.log(agoraStatesDiscussions);
+//로컬스토리지
 let data;
 const dataFromLocalStorage = localStorage.getItem('agoraStatesDiscussions')
 if (dataFromLocalStorage) {
@@ -54,25 +55,99 @@ const convertToDiscussion = (obj) => {
 };
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
-const render = (element) => {
-  for (let i = 0; i < data.length; i += 1) {
+const render = (element, from, to) => {
+  console.log(from, to)
+  if (!from && !to) {
+    from = 0; to = data.length - 1
+  }
+  // 다 지우고 배열에 있는 내용 다 보여주기
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+  for (let i = from; i < to; i += 1) {
     element.append(convertToDiscussion(data[i]));
   }
   return;
 };
 
+// 페이지네이션을 위한 변수
+let limit = 10, page = 1
+
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
-render(ul, 10, 1);
+render(ul, 0, limit);
+
+//로컬스토리지 데이터 배열의 몇번째 요소부터 몇번째 요소까지 출력할 것인지 정하기
+const getPageStartEnd = (limit, page) => {
+  const len = data.length - 1
+  let pageStart = Number(page - 1) * Number(limit);
+  let pageEnd = Number(pageStart) + Number(limit);
+  if (page <= 0) {
+    pageStart = 0;
+  }
+  if (pageEnd >= len) {
+    // pageStart = Number(page - 2) * Number(limit)
+    pageEnd = len
+  }
+  return { pageStart, pageEnd };
+};
+
+//버튼 구현
+const buttons = document.querySelector('.buttons');
+buttons.children[0].addEventListener('click', (event) => {
+  if (page > 1) {
+    page = page - 1;
+  }
+  const { pageStart, pageEnd } = getPageStartEnd(limit, page)
+  render(ul, pageStart, pageEnd);
+})
+
+buttons.children[1].addEventListener('click', () => {
+  if ((limit * page) < data.length - 1) {
+    page = page + 1;
+  }
+  const { pageStart, pageEnd } = getPageStartEnd(limit, page)
+  render(ul, pageStart, pageEnd);
+})
+
+buttons.children[2].addEventListener('click', () => {
+  localStorage.removeItem('agoraStatesDiscussions');
+  data = agoraStatesDiscussions.slice();
+  limit = 10; page = 1;
+  render(ul, 0, limit)
+})
 
 /* 새 디스커션 추가 */
-let newDiscussion = document.querySelector("form");
+const newDiscussion = document.querySelector("form");
 const inputName = document.querySelector("#name");
 const inputTitle = document.querySelector("#title");
 const inputText = document.querySelector("#story");
-const inputSubmit = document.querySelector("#submit");
 
-const addDiscussion = (event) => {
+newDiscussion.addEventListener('submit', (event) => {
+  const obj = {
+    id: 'unknown',
+    createdAt: new Date().toISOString(),
+    title: inputTitle.value,
+    url: null,
+    author: inputName.value,
+    answer: null,
+    bodyHTML: inputText.value,
+    avatarUrl: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+  }
+  data.unshift(obj);
+
+  //로컬스토리지에 저장
+  localStorage.setItem('agoraStatesDiscussions', JSON.stringify(data))
+
+  // 렌더링
+  render(ul, 0, limit);
+})
+
+// const newRender = (element) => {
+//   element.prepend(convertToDiscussion(data[0]))
+//   return;
+// }
+/* const addDiscussion = (event) => {
   event.preventDefault();
   data.unshift({
     id: 'unknown',
@@ -91,9 +166,5 @@ const addDiscussion = (event) => {
   newRender(ul);
 }
 
-newDiscussion = addEventListener('submit', addDiscussion);
+newDiscussion = addEventListener('submit', addDiscussion); */
 
-const newRender = (element) => {
-  element.prepend(convertToDiscussion(data[0]))
-  return;
-}
