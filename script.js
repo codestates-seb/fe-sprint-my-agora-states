af41bc6087a5afaf1819a10cecf00049cf8da9f3
yeowool <qyoong3579@gmail.com>
2022-07-22 03:51:40 +0900

    1차 솔로프로젝트 수정

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ discussionAnswered.className = 'discussion__answered';
 
 
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
+
   const face = document.createElement("img") // 프로필 사진
   face.src = obj.avatarUrl;
   face.alt = "avatar of" + obj.author;
@@ -32,7 +33,7 @@ discussionAnswered.className = 'discussion__answered';
   
 
   const discussionInfo = document.createElement("div");
-  discussionInfo.textContent = `${obj.author} / ${new Date(obj.createAt).toLocaleTimeString()}` // 날짜 표현 형식이 여러개가 있는데 이걸 제일 많이  쓴다
+  discussionInfo.textContent = `${obj.author} / ${new Date(obj.createdAt).toLocaleTimeString()}` // 날짜 표현 형식이 여러개가 있는데 이걸 제일 많이  쓴다
   discussionContent.append(discussionTitle, discussionInfo);
 
 
@@ -45,53 +46,8 @@ discussionAnswered.className = 'discussion__answered';
   return li; // il요소를 위의 함수를 통해 가공해서 append
 };
 
-// 폼요소 가져오기
-
-
-// const formTitle = document.querySelector("div.form__input--title >input")
-// const nameInput = document.querySelector("form__input--name >input")
-// const textBox = document.querySelector("form__textbox > textarea ")
-
-// form.addEventListener("submit", (event) => {
-//   // 새로운 객체 만들어야 한다
-//   // Input에 입력된 값을 넣은 새로운 객체가 필요
-//   // 새로운 객체를 ul 아래로 넣어준다
-//   // 더미데이터를 추가해준다.
-//   const newObj = {
-//     id: "id",
-//     createdAt: new Date().toLocaleTimeString(),
-//     title: formTitle.value,
-//     author: nameInput.value,
-//     answer: null,
-//     };
-//   agoraStatesDiscussions.unshift(newObj);
-
-//   ul.prepend(li)
-// })
-
-
-
-
-// 새로운 글 생성
-const newDiscussion = document.querySelector('form');
-
-newDiscussion.addEventListener('submit', (event) => {
-  event.preventDefault();
-  const yourName = document.querySelector('#name').value; 
-  const yourTitle = document.querySelector('#title').value;
-  const addNewDiscussion = {
-    createdAt: new Date().toLocaleString(),
-    title: yourTitle,
-    author: yourName
-  }
-  agoraStatesDiscussions.unshift(addNewDiscussion);
-
-  while(ul.children.length > 1) {
-    ul.removeChild(ul.lastChild);
-  }
-
-  render(ul);
-}); 
+  // form 은 데이터를 깔끔하게 받기 위해서 받는것
+  // 
 
 
 
@@ -108,3 +64,40 @@ const render = (element) => { // 여기 엘리먼트는 ul.discussions__containe
 const ul = document.querySelector("ul.discussions__container");
 render(ul); 
 // 화면에 그리는 작업을 렌더링이라고 하는데 지금 이 함수는 렌더링함수 이렇게 호출하면서 끝난다.
+
+// 디스커션 추가 구현
+
+// 문서 내용 가져오기.
+const form = document.querySelector("form.form");
+
+// submit을 클릭하면 자료를 가져온다
+
+form.addEventListener("submit", 
+(event) => {
+  event.preventDefault(); //서브밋 이벤트로 사용시 꼭 함께 사용해주어야함
+  
+  const author = form.querySelector("div.form__input--name > input").value;
+  const title = form.querySelector("div.form__input--title > input").value;
+  const textbox = form.querySelector("div.form__textbox > textarea").value;
+
+  const newObj = {
+    id: "new id",
+    createdAt: new Date().toISOString(),
+    title: title,
+    url: "https://github.com/codestates-seb/agora-states-fe/discussions",
+    author: author,
+    bodyHTML: textbox,
+    avatarUrl: "https://avatars.githubusercontent.com/u/97888923?s=64&u=12b18768cdeebcf358b70051283a3ef57be6a20f&v=4"
+  }
+
+  agoraStatesDiscussions.unshift(newObj);
+
+  const discussion = convertToDiscussion(newObj);
+  
+  ul.prepend(discussion);
+
+  form.querySelector("div.form__input--name > input").value = "";
+  form.querySelector("div.form__input--title > input").value = "";
+  form.querySelector("div.form__textbox > textarea").value = "";
+}
+)
