b8e133059e7967b40ea2ac1269f59e7374136882
seonga <girl0930h@naver.com>
2022-07-26 01:08:02 +0900

    번들링 완료

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
+require('./style.css');
+
 console.log(agoraStatesDiscussions);
+
+
 
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
 const convertToDiscussion = (obj) => {
@@ -100,6 +104,8 @@ const render = (element) => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+
 
 
 
