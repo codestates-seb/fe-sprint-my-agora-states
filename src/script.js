46022d72bfda319ff01a26be2638110b5a943ba6
hazel <yhj2756@naver.com>
2022-07-25 16:29:59 +0900

    webpack 적용

--- src/script.js
+++ src/script.js
@@ -1,5 +1,7 @@
+const agoraStatesDiscussions = require('./data.js');
+require('./style.css');
 // index.html을 열어서 agoraStatesDiscussions 배열 요소를 확인하세요.
-console.log(agoraStatesDiscussions);
+// console.log(agoraStatesDiscussions);
 
 // 현지 시간 변환 함수
 const getLocalDate = function(date) {
