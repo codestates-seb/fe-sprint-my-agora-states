b3b622c7153d4d370749c7a81588edd21418c6a8
JungInHa <juudyy99@gmail.com>
2022-08-17 15:34:41 +0900

    my-agora-states-server의 discussionsData와 연결

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-// index.html을 열어서 agoraStatesDiscussions 배열 요소를 확인하세요.
-// console.log(agoraStatesDiscussions);
+const ul = document.querySelector("ul.discussions__container");
+
+fetch(`http://localhost:4000/discussions/`)
+  .then((response) => response.json())
+  .then((data) => render(data));
+
+function render(response) {
+  for (let i = 0; i < response.length; i += 1) {
+    ul.append(convertToDiscussion(response[i]));
+  }
+  return;
+}
 
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
 const convertToDiscussion = (obj) => {
-  const li = document.createElement("li"); // li 요소 생성
-  li.className = "discussion__container"; // 클래스 이름 지정
+  const li = document.createElement("li"); 
+  li.className = "discussion__container"; 
 
   const avatarWrapper = document.createElement("div");
   avatarWrapper.className = "discussion__avatar--wrapper";
@@ -46,25 +56,6 @@ const convertToDiscussion = (obj) => {
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
 };
-
-// agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
-const render = (element) => {
-  for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
-    element.append(convertToDiscussion(agoraStatesDiscussions[i]));
-  }
-  return;
-};
-
-// ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
-const ul = document.querySelector("ul.discussions__container");
-render(ul);
-
-// agoraStatesDiscussions 배열의 모든 데이터를 화면에서 지우는 함수입니다. 
-// const erase = (container) => {
-//   while (container.firstChild) {
-//     container.removeChild(container.firstChild);
-//   } 
-// }
 
 const form = document.querySelector('form.form');
 const formName = document.querySelector('div.form__input--name > input');
