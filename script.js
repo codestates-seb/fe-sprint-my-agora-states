1e527da00d9e56357b2adb00cbc618b55ca4facd
kimhyojung <rlgywnd123@gmail.com>
2022-08-17 16:59:33 +0900

    SEB FE 40기 김효중 과제

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // index.html을 열어서 agoraStatesDiscussions 배열 요소를 확인하세요.
 console.log(agoraStatesDiscussions);
 
+
+
+
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
 const convertToDiscussion = (obj) => {
   const li = document.createElement("li"); // li 요소 생성
@@ -82,6 +85,7 @@ form.addEventListener('submit', (event) => {
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
+  element.innerHTML = '';
   for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
     element.append(convertToDiscussion(agoraStatesDiscussions[i]));
     
@@ -90,5 +94,16 @@ const render = (element) => {
 };
 
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
-const ul = document.querySelector("ul.discussions__container");
-render(ul); // render() 함수의 인자로 ul을 넣어준후 실행되고 끝
+// const ul = document.querySelector("ul.discussions__container");
+
+
+fetch('http://localhost:4000/discussions')
+.then((res) =>  res.json())
+.then((data) => { 
+  agoraStatesDiscussions = data;
+  const ul = document.querySelector('ul.discussions__container');
+  render(ul);
+})
+
+
+// render(ul); // render() 함수의 인자로 ul을 넣어준후 실행되고 끝
