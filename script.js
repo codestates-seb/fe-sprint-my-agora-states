179de0b9c43b367c1290ca3e90c97c26f3c50afd
kongalabear <110971394+kongalabear@users.noreply.github.com>
2022-08-31 15:53:03 +0900

    SEF 41기 서범길 calculator 과제 제출합니다.

--- script.js
+++ script.js
@@ -1,34 +1,103 @@
-// index.html을 열어서 agoraStatesDiscussions 배열 요소를 확인하세요.
-console.log(agoraStatesDiscussions);
+const calculator = document.querySelector('.calculator'); // calculator 엘리먼트와, 그 자식 엘리먼트의 정보를 모두 담고 있습니다.
+const buttons = calculator.querySelector('.calculator__buttons'); // calculator__keys 엘리먼트와, 그 자식 엘리먼트의 정보를 모두 담고 있습니다.
 
-// convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
-const convertToDiscussion = (obj) => {
-  const li = document.createElement("li"); // li 요소 생성
-  li.className = "discussion__container"; // 클래스 이름 지정
+const firstOperend = document.querySelector('.calculator__operend--left'); // calculator__operend--left 엘리먼트와, 그 자식 엘리먼트의 정보를 모두 담고 있습니다.
+const operator = document.querySelector('.calculator__operator'); // calculator__operator 엘리먼트와, 그 자식 엘리먼트의 정보를 모두 담고 있습니다.
+const secondOperend = document.querySelector('.calculator__operend--right'); // calculator__operend--right 엘리먼트와, 그 자식 엘리먼트의 정보를 모두 담고 있습니다.
+const calculatedResult = document.querySelector('.calculator__result'); // calculator__result 엘리먼트와, 그 자식 엘리먼트의 정보를 모두 담고 있습니다.
 
-  const avatarWrapper = document.createElement("div");
-  avatarWrapper.className = "discussion__avatar--wrapper";
-  const discussionContent = document.createElement("div");
-  discussionContent.className = "discussion__content";
-  const discussionAnswered = document.createElement("div");
-  discussionAnswered.className = "discussion__answered";
+function calculate(n1, operator, n2) {
+  let result = 0;
+  n1 = +n1;
+  n2 = +n2;
+  if  (operator === '+')
+  result = n1 + n2;
+  if  (operator === '-')
+  result = n1 - n2;
+  if  (operator === '/')
+  result = n1 / n2;
+  if  (operator === '*')
+  result = n1 * n2;
 
-  // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
+  // TODO : n1과 n2를 operator에 따라 계산하는 함수를 만드세요.
+  // ex) 입력값이 n1 : '1', operator : '+', n2 : '2' 인 경우, 3이 리턴됩니다.
+  return String(result);
+}
 
+buttons.addEventListener('click', function (event) {
+  // 버튼을 눌렀을 때 작동하는 함수입니다.
 
+  const target = event.target; // 클릭된 HTML 엘리먼트의 정보가 저장되어 있습니다.
+  const action = target.classList[0]; // 클릭된 HTML 엘리먼트에 클레스 정보를 가져옵니다.
+  const buttonContent = target.textContent; // 클릭된 HTML 엘리먼트의 텍스트 정보를 가져옵니다.
+  // ! 위 코드(Line 19 - 21)는 수정하지 마세요.
 
-  li.append(avatarWrapper, discussionContent, discussionAnswered);
-  return li;
-};
+  if (target.matches('button')) {
+    // TODO : 계산기가 작동할 수 있도록 아래 코드를 수정하세요. 작성되어 있는 조건문과 console.log를 활용하시면 쉽게 문제를 풀 수 있습니다.
+    // 클릭된 HTML 엘리먼트가 button이면
+    if (action === 'number') {
+      if(firstOperend.textContent === '0'){
+      firstOperend.textContent = buttonContent;
+    } else if (firstOperend.textContent !=='0'){
+      secondOperend.textContent=buttonContent;
+    }
+      console.log('숫자 ' + buttonContent + ' 버튼');
+    }
 
-// agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
-const render = (element) => {
-  for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
-    element.append(convertToDiscussion(agoraStatesDiscussions[i]));
+    if (action === 'operator') {
+      operator.textContent = buttonContent
+      console.log('연산자 ' + buttonContent + ' 버튼');
+    }
+
+    if (action === 'decimal') {
+      // console.log('소수점 버튼');
+    }
+
+    if (action === 'clear') {
+      firstOperend.textContent = '0'
+      secondOperend.textContent = '0'
+      calculatedResult.textContent = '0'
+      console.log('초기화 버튼');
+    }
+
+    if (action === 'calculate') {
+    calculatedResult.textContent = calculate (firstOperend.textContent , operator.textContent , secondOperend.textContent);
+      console.log('계산 버튼');
+    }
   }
-  return;
-};
+});
 
-// ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
-const ul = document.querySelector("ul.discussions__container");
-render(ul);
+
+// ! Advanced Challenge test와 Nightmare test를 위해서는 아래 주석을 해제하세요.
+
+const display = document.querySelector('.calculator__display--for-advanced'); // calculator__display 엘리먼트와, 그 자식 엘리먼트의 정보를 모두 담고 있습니다.
+let firstNum, operatorForAdvanced, previousKey, previousNum;
+
+buttons.addEventListener('click', function (event) {
+  // 버튼을 눌렀을 때 작동하는 함수입니다.
+
+  const target = event.target; // 클릭된 HTML 엘리먼트의 정보가 저장되어 있습니다.
+  const action = target.classList[0]; // 클릭된 HTML 엘리먼트에 클레스 정보를 가져옵니다.
+  const buttonContent = target.textContent; // 클릭된 HTML 엘리먼트의 텍스트 정보를 가져옵니다.
+  // ! 위 코드는 수정하지 마세요.
+
+  // ! 여기서부터 Advanced Challenge & Nightmare 과제룰 풀어주세요.
+  if (target.matches('button')) {
+    if (action === 'number') {
+      if (display.textContent === '0'){ display.textContent = buttonContent;
+        firstNum = display.textContent
+        }
+          else if (previousKey === 'operator') display.textContent = buttonContent;
+          else if (display.textContent !== '0') display.textContent = display.textContent + buttonContent;
+    }
+    if (action === 'operator') {
+      operatorForAdvanced = buttonContent;
+      previousKey = 'operator';
+      previousNum = display.textContent;
+    }
+    if (action === 'decimal') {}
+    if (action === 'clear') {}
+    if (action === 'calculate') {}
+  }
+
+});
