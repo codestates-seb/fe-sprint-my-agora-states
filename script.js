ff7849fc09600994980860ebd942b7b511ee027e
bakjonghyo <cutitout52@gmail.com>
2022-09-16 02:14:49 +0900

    DB에 사진, 버튼 연동

--- script.js
+++ script.js
@@ -8,18 +8,30 @@ const convertToDiscussion = (obj) => {
 
   const avatarWrapper = document.createElement("div");
   avatarWrapper.className = "discussion__avatar--wrapper";
+
+  const avatarImage = document.createElement("img");
+  avatarImage.className ='discussion__avatar--image'
+  avatarImage.src = obj["avatarUrl"];
+
+  
   const discussionContent = document.createElement("div");
   discussionContent.className = "discussion__content";
+  discussionContent.textContent = obj['title'];
+  
   const discussionAnswered = document.createElement("div");
   discussionAnswered.className = "discussion__answered";
+  
+  const answeredButton = document.createElement('button');
+  answeredButton.textContent = "☑";
+  
 
-  // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
-
-
-
+  
+  avatarWrapper.append(avatarImage);
+  discussionAnswered.append(answeredButton);
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
 };
+
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
@@ -29,6 +41,14 @@ const render = (element) => {
   return;
 };
 
+// const render = function(document.querySelector("ul.discussions__container")) {
+//   for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
+//     document.querySelector("ul.discussions__container").append(convertToDiscussion(agoraStatesDiscussions[i]));
+//   }
+//   return;
+// }
+
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
+
 render(ul);
