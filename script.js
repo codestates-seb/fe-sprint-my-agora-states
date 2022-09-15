8a705e5aad74e392973b053f9f84525915a9f1f6
lmimoh <dlalsgns9039@gmail.com>
2022-09-16 02:10:13 +0900

    paging 함수 개발

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // index.html을 열어서 agoraStatesDiscussions 배열 요소를 확인하세요.
 console.log(agoraStatesDiscussions);
 
+//===========================================================//
+//==================== 렌더링에 필요한 함수 =====================//
+//===========================================================//
+
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
 const convertToDiscussion = (obj) => {
   const li = document.createElement("li"); // li 요소 생성
@@ -41,7 +45,15 @@ const convertToDiscussion = (obj) => {
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
-  for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
+  // 주소의 parameter에서 현재 페이지와 조회 게시물을 불러온다.
+  const curPage = (location.href).split('?page=')[1];
+  const startContents = (curPage - 1 ) * 10;
+  const range = agoraStatesDiscussions.length - curPage * 10;
+  const countContents = range < 0 ? curPage * 10 + range : curPage * 10;
+
+  console.log(`게시물 시작 : ${startContents + 1} | 게시물 종료 : ${countContents}`)
+
+  for (let i = startContents; i < countContents; i += 1) {
     element.append(convertToDiscussion(agoraStatesDiscussions[i]));
   }
   return;
@@ -50,3 +62,22 @@ const render = (element) => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+//===========================================================//
+//==================== paging에 필요한 함수 ====================//
+//===========================================================//
+
+function pageBtn() {
+  const paging = document.querySelector("#paging_area");
+  const length = agoraStatesDiscussions.length;
+  const page = length % 10 === 0 ? length / 10 : length / 10 + 1;
+
+  for(let i = 1; i < page; i++) {
+    const btn = document.createElement('a');
+    btn.href = `?page=${i}`;
+    btn.textContent = i;
+    paging.append(btn);
+  }
+}
+
+pageBtn();
