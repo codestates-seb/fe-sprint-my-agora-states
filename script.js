89d7ac2f72dc7ab68c9629316e457d95bb421d4e
noah-eunsoo <noah.eunsoo0504@gmail.com>
2022-09-15 17:32:53 +0900

    이전, 다음 네비게이션 버튼 기능 추가

--- script.js
+++ script.js
@@ -1,39 +1,32 @@
 // index.html을 열어서 agoraStatesDiscussions 배열 요소를 확인하세요.
 //console.log(agoraStatesDiscussions);
 
+// local storage data 확인
 let localStorageObj = localStorage.getItem('obj');
 if (localStorageObj) {
   localStorageObj = JSON.parse(localStorage.getItem('obj'));
 } else {
   localStorageObj = agoraStatesDiscussions.slice();
-  localStorage.setItem('obj', JSON.stringify(localStorageObj));
 }
-
 localStorage.setItem('obj', JSON.stringify(localStorageObj));
 
-console.log(localStorageObj);
-
+// 총 페이지 수
 let allPagingNum = Math.ceil(localStorageObj.length / 10);
-
-console.log(allPagingNum);
+// 현재 페이지
+let nowPage = 1;
 
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
 const convertToDiscussion = (obj) => {
   const li = document.createElement('li'); // li 요소 생성
-
   li.className = 'discussion__container'; // 클래스 이름 지정
-
   const avatarWrapper = document.createElement('div');
   avatarWrapper.className = 'discussion__avatar--wrapper';
-
   const discussionContent = document.createElement('div');
   discussionContent.className = 'discussion__content';
-
   const discussionAnswered = document.createElement('div');
   discussionAnswered.className = 'discussion__answered';
 
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
-
   li.append(avatarWrapper, discussionContent, discussionAnswered);
 
   const avataImage = document.createElement('img');
@@ -41,7 +34,6 @@ const convertToDiscussion = (obj) => {
   avataImage.alt = 'avatar of ' + obj.author;
   avataImage.className = 'discussion__avatar--image';
   avatarWrapper.append(avataImage);
-
   const discussionTitle = document.createElement('h2');
   discussionTitle.className = 'discussion__title';
   const discussionTitleLink = document.createElement('a');
@@ -49,12 +41,10 @@ const convertToDiscussion = (obj) => {
   discussionTitleLink.textContent = obj.title;
   discussionTitleLink.target = '_blank';
   discussionTitle.append(discussionTitleLink);
-
   const discussionInformation = document.createElement('div');
   discussionInformation.className = 'discussion__information';
   discussionInformation.textContent = `${obj.author} / ${obj.createdAt}`;
   discussionContent.append(discussionTitle, discussionInformation);
-
   const discussionAnsweredCheck = document.createElement('span');
 
   if (obj.answer !== null) {
@@ -68,19 +58,6 @@ const convertToDiscussion = (obj) => {
 
   return li;
 };
-
-const btnClStorage = document.querySelector('.clear_local_storage');
-
-btnClStorage.addEventListener('click', function () {
-  localStorage.removeItem('obj');
-  localStorageObj = agoraStatesDiscussions.slice();
-  ul.textContent = '';
-  render(ul, 0);
-
-  allPagingNum = Math.ceil(localStorageObj.length / 10);
-  discussionPaging.textContent = '';
-  renderPaging(allPagingNum);
-});
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element, itemStartNum) => {
@@ -108,12 +85,38 @@ const convertToPaging = (pagingNum) => {
   return btnPaging;
 };
 const renderPaging = (allPagingNum) => {
+  const btnPrev = document.createElement('button');
+  btnPrev.textContent = '<';
+  btnPrev.type = 'button';
+  btnPrev.className = 'btn_nav btn_prev';
+
+  const btnNext = document.createElement('button');
+  btnNext.textContent = '>';
+  btnNext.type = 'button';
+  btnNext.className = 'btn_nav btn_next';
+
+  discussionPaging.append(btnPrev);
   for (let i = 0; i < allPagingNum; i += 1) {
     discussionPaging.append(convertToPaging(i + 1));
   }
+  discussionPaging.append(btnNext);
   return;
 };
 renderPaging(allPagingNum);
+
+// local storage 삭제
+const btnClStorage = document.querySelector('.clear_local_storage');
+btnClStorage.addEventListener('click', function () {
+  localStorage.removeItem('obj');
+  localStorageObj = agoraStatesDiscussions.slice();
+  ul.textContent = '';
+  const itemStartNum = 0;
+  render(ul, itemStartNum);
+
+  allPagingNum = Math.ceil(localStorageObj.length / 10);
+  discussionPaging.textContent = '';
+  renderPaging(allPagingNum);
+});
 
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector('ul.discussions__container');
@@ -160,25 +163,62 @@ agoForm.addEventListener('submit', function (e) {
   allPagingNum = Math.ceil(localStorageObj.length / 10);
   discussionPaging.textContent = '';
   renderPaging(allPagingNum);
+  nowPage = 1;
 });
 
-let ArrBtnNav = document.querySelectorAll('.btn_nav');
+let arrBtnNav = document.querySelectorAll('.btn_nav');
 
 const onClickNav = (e) => {
   let itemStartNum = e.target.textContent * 10 - 10;
-  console.log(itemStartNum);
   ul.textContent = '';
   render(ul, itemStartNum);
   e.target.classList.add('on');
+  nowPage = +e.target.textContent;
+};
+
+const onClickPrev = (arrBtnNav, nowPageNum) => {
+  let itemStartNum = nowPageNum * 10 - 20;
+  ul.textContent = '';
+  render(ul, itemStartNum);
+  console.log(arrBtnNav[nowPageNum]);
+  arrBtnNav.forEach((btnNav) => {
+    btnNav.classList.remove('on');
+  });
+  nowPage = nowPageNum - 1;
+  arrBtnNav[nowPage].classList.add('on');
+};
+
+const onClickNext = (arrBtnNav, nowPageNum) => {
+  let itemStartNum = nowPageNum * 10;
+  ul.textContent = '';
+  render(ul, itemStartNum);
+  console.log(arrBtnNav[nowPageNum]);
+  arrBtnNav.forEach((btnNav) => {
+    btnNav.classList.remove('on');
+  });
+  nowPage = nowPageNum + 1;
+  arrBtnNav[nowPage].classList.add('on');
 };
 
 document.addEventListener('click', function (e) {
-  console.log(e.target.className);
-  ArrBtnNav = document.querySelectorAll('.btn_nav');
+  arrBtnNav = document.querySelectorAll('.btn_nav');
   if (e.target && e.target.className === 'btn_nav') {
-    ArrBtnNav.forEach((btnNav) => {
+    arrBtnNav.forEach((btnNav) => {
       btnNav.classList.remove('on');
     });
     onClickNav(e);
+  }
+
+  if (e.target && e.target.className === 'btn_nav btn_prev') {
+    if (nowPage !== 1) {
+      onClickPrev(arrBtnNav, nowPage);
+    }
+  }
+
+  if (e.target && e.target.className === 'btn_nav btn_next') {
+    console.log(nowPage);
+    if (nowPage < arrBtnNav.length - 2) {
+      onClickNext(arrBtnNav, nowPage);
+    }
   }
 });
