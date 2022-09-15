319138d8d60608d842ce8a12d28e0cc14cc267ae
Atia <67799687+AtiaKR@users.noreply.github.com>
2022-09-16 07:38:58 +0900

    로컬 스토리지 저장,  아코디언 탭 기능  추가

---
+++ saveload.js
@@ -0,0 +1,56 @@
+/** localStorage 에 데이터를 저장함 */
+function saveLocalstorageData (){
+  const _name = document.querySelector("#name").value;
+  const _title = document.querySelector("#title").value;
+  const _story = document.querySelector("#story").value;
+  const _date = new Date();
+
+  const _object = {
+    id: null,
+    createdAt: _date,
+    title: _title,
+    url: null,
+    author: _name,
+    answer: null,
+    bodyHTML:_story,
+    avatarUrl:"https://cdn.discordapp.com/attachments/1017286837376655413/1020086428291579945/wa3XJg-b_400x400.jpg",
+  }
+  
+  // 작성한 object 를 json 으로 변환
+  const _objJSON = JSON.stringify(_object);
+
+  // 랜덤키 생성 대신 현재시간을 키로 사용함
+  window.localStorage.setItem(Date.parse(_date), _objJSON);
+}
+
+/** localStorage 에서 데이터를 불러옴 */
+function loadLocalstoragedata() {
+  //window.localStorage.clear();
+  const _objarr = {};
+  for ( let i = 0; i < window.localStorage.length; i ++ ) {
+    
+    // localStorage 에서 데이터를 가져옴
+    const _key    = window.localStorage.key(i);
+    const _value  = window.localStorage.getItem(_key);
+
+    // 가져온 JSON 을 다시 Object 로 변환한뒤 키값 그대로 임시객체에 저장
+    const _object = JSON.parse(_value);
+    _objarr[_key] = _object;
+  }
+
+  // 임시객체를 키 값기준으로 정렬한뒤 새로운 객체로 반환
+  const _newObject = Object.keys(_objarr).sort().reduce(
+    (newObj, key) => {
+      newObj[key] = _objarr[key];
+      return newObj;
+    },
+    {}
+  );
+
+    // 정렬된 객체의 키값을 배열로 받은 다음, 디스커션 객체에 차례대로 unshift
+  const _newObjectKeys = Object.keys(_newObject);
+
+  for ( let i = 0; i < _newObjectKeys.length; i ++ ) {
+    agoraStatesDiscussions.unshift(_newObject[_newObjectKeys[i]]);
+  }
+}
--- script.js
+++ script.js
@@ -26,16 +26,27 @@ const convertToDiscussion = (obj) => {
   discussionTitle.className = "discussion__title";
   
   const discussionTitleTagA = document.createElement("a");
-  discussionTitleTagA.setAttribute("href", obj["url"]);
+  discussionTitleTagA.setAttribute("href", '#');
+  discussionTitleTagA.setAttribute("onclick", 'return false');
   discussionTitleTagA.textContent = obj["title"];
 
   discussionTitle.append(discussionTitleTagA);
   discussionContent.append(discussionTitle);
 
+  // 아티클 생성후 넣기
+  const discussionArticle = document.createElement("p");
+  discussionArticle.className = "discussion__article";
+  discussionArticle.innerHTML = obj["bodyHTML"];
+
+  discussionContent.append(discussionArticle);
+
+
   // 닉네임, 작성일자 생성후 discussionContent 에 넣기
   const discussionInformation = document.createElement("div");
   discussionInformation.className = "discussion__information";
-  discussionInformation.textContent = `${obj["author"]} / ${obj["createdAt"]}`;
+
+  const discussionCreatedtime = new Date(obj["createdAt"]).toLocaleString('ko-KR');
+  discussionInformation.textContent = `${obj["author"]} / ${discussionCreatedtime}`;
 
   discussionContent.append(discussionInformation);
 
@@ -43,9 +54,9 @@ const convertToDiscussion = (obj) => {
   const answeredFlag = obj["answer"];
   const disscusionAnsweredPtag = document.createElement("p");
   if (answeredFlag !== null ) {
-    disscusionAnsweredPtag.textContent = "☑";
+    disscusionAnsweredPtag.innerHTML = `<a href="${obj['url']}"><i class="fa-brands fa-github"></i></a><i class="fa-regular fa-square-check"></i>`;
   } else {
-    disscusionAnsweredPtag.textContent = "🔲";
+    disscusionAnsweredPtag.innerHTML = `<a href="${obj['url']}"><i class="fa-brands fa-github"></i></a><i class="fa-regular fa-square"></i>`;
   }
   
   discussionAnswered.append(disscusionAnsweredPtag);
@@ -55,22 +66,104 @@ const convertToDiscussion = (obj) => {
   return li;
 };
 
+
+const convertToAnswer = (obj) => {
+  const li = document.createElement("li"); // li 요소 생성
+  li.className = "discussion__container"; // 클래스 이름 지정
+  li.classList.add("discussion__answer"); // answer 지정
+
+  const avatarWrapper = document.createElement("div");
+  avatarWrapper.className = "discussion__avatar--wrapper";
+  const discussionContent = document.createElement("div");
+  discussionContent.className = "discussion__content";
+  const discussionAnswered = document.createElement("div");
+  discussionAnswered.className = "discussion__answered";
+
+  // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
+
+  // 프로필 이미지 생성하고 넣기
+  const avatarImage = document.createElement("img");
+  avatarImage.className = "discussion__avatar--image";
+  avatarImage.setAttribute("src", obj["answer"]["avatarUrl"]);
+  avatarWrapper.append(avatarImage);
+
+  // 타이틀 생성하고 discussionContent 에 넣기
+  const discussionTitle = document.createElement("h2");
+  discussionTitle.className = "discussion__title";
+  
+  const discussionTitleTagA = document.createElement("a");
+  discussionTitleTagA.innerHTML = obj["answer"]["bodyHTML"];
+
+  discussionTitle.append(discussionTitleTagA);
+  discussionContent.append(discussionTitle);
+
+  // 닉네임, 작성일자 생성후 discussionContent 에 넣기
+  const discussionInformation = document.createElement("div");
+  discussionInformation.className = "discussion__information";
+
+  const discussionCreatedtime = new Date(obj["answer"]["createdAt"]).toLocaleString('ko-KR');
+  discussionInformation.textContent = `${obj["answer"]["author"]} / ${discussionCreatedtime}`;
+
+  discussionContent.append(discussionInformation);
+
+  // 디스커션에 object 가 있는지 확인하고 없으면 스킵, 있으면 넣기
+  // const answeredFlag = obj["answer"]["answer"];
+  // const disscusionAnsweredPtag = document.createElement("p");
+  // if (answeredFlag !== null ) {
+  //   disscusionAnsweredPtag.textContent = "☑";
+  // } else {
+  //   disscusionAnsweredPtag.textContent = "🔲";
+  // }
+  // 
+  // discussionAnswered.append(disscusionAnsweredPtag);
+
+  li.append(avatarWrapper, discussionContent);
+  
+  return li;
+}
+
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
-  for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
+  // for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
+  for (let i = 0; i < 10; i += 1) {
     element.append(convertToDiscussion(agoraStatesDiscussions[i]));
+
+    // 만약 answer 가 null 이 아니라면 다음에 바로 두번째 클래스가 discussion__answer 인 디스커션 추가
+    if ( agoraStatesDiscussions[i]["answer"] !== null ){
+      element.append(convertToAnswer(agoraStatesDiscussions[i]));
+    }
   }
   return;
 };
 
 
-// textarea 의 내용이 늘어나면 자동으로 크기를 늘려주는 함수
-const renameHeader = (e) => {
-  const _textarea = document.querySelector("h1");
-  _textarea.textContent = 'MyAgoraStates';
-}
+// 처음에 localStorage 에서 데이터 불러오기
+loadLocalstoragedata();
 
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
-renameHeader();
+
+
+
+const submitBtn = document.querySelector("#saveLs");
+submitBtn.addEventListener('click', () => {
+  saveLocalstorageData();
+});
+
+
+// 모든 타이틀에 클릭 이벤트 작성
+const btn = document.querySelectorAll(".discussion__title");
+for (let i = 0; i < btn.length; i ++ ){
+  btn[i].addEventListener('click', function (){
+    const _answer = btn[i].parentElement.parentElement.nextElementSibling;
+
+    // 타이틀 클릭시 내용과 답변열기
+    const _article = btn[i].nextElementSibling;
+    _article.classList.toggle("discussion__article__active");
+    
+    if (_answer.classList[1] === 'discussion__answer'){
+      _answer.classList.toggle('accordion__active');
+    }
+  });
+}
