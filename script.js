861d49b57d75d6eff039a69a3c1e9036893a12ad
ashleysheo <ashleysyheo@gmail.com>
2022-09-15 13:57:37 +0900

    feat: 페이지네이션

--- script.js
+++ script.js
@@ -6,14 +6,20 @@ const convertToDiscussion = (obj) => {
     const li = document.createElement("li"); // li 요소 생성
     li.className = "discussion__container"; // 클래스 이름 지정
 
+    const discussionContentContainer = document.createElement('div');
+    discussionContentContainer.className = 'discussion__content--container';
+
     const avatarWrapper = document.createElement("div");
     avatarWrapper.className = "discussion__avatar--wrapper";
+    discussionContentContainer.append(avatarWrapper);
 
     const discussionContent = document.createElement("div");
     discussionContent.className = "discussion__content";
+    discussionContentContainer.append(discussionContent);
 
     const discussionAnswered = document.createElement("div");
     discussionAnswered.className = "discussion__answered";
+    discussionContentContainer.append(discussionAnswered);
 
     // profile image
     const avatarImg = document.createElement('img');
@@ -26,7 +32,7 @@ const convertToDiscussion = (obj) => {
     const discussionTitle = document.createElement('h2');
     discussionTitle.classList.add('discussion__title');
     discussionContent.append(discussionTitle);
-    
+
     // title -> url
     const discussionURL = document.createElement('a');
     discussionURL.href = obj.url;
@@ -44,7 +50,7 @@ const convertToDiscussion = (obj) => {
     discussionInfoAuthor.textContent = obj.author;
     discussionInfo.append(discussionInfoAuthor);
 
-   // author 
+    // author 
     const discussionInfoDate = document.createElement('span');
     discussionInfoDate.classList.add('discussion__information--date');
     const createdDateTime = new Date(obj.createdAt).toLocaleString('en-US', {
@@ -63,10 +69,12 @@ const convertToDiscussion = (obj) => {
         discussionAnsweredSign.classList.add('fa-solid', 'fa-circle-check');
     }
 
-    // discussionAnsweredSign.classList.add = obj.answer ? '☑' : '☐';
     discussionAnswered.append(discussionAnsweredSign);
 
-    li.append(avatarWrapper, discussionContent, discussionAnswered);
+    const hr = document.createElement('div');
+    hr.classList.add('hr');
+
+    li.append(discussionContentContainer, hr);
     return li;
 };
 
@@ -74,10 +82,6 @@ const convertToDiscussion = (obj) => {
 const render = (element) => {
     for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
         element.append(convertToDiscussion(agoraStatesDiscussions[i]));
-        const hr = document.createElement('div');
-        hr.classList.add('hr');
-        element.append(hr);
-        
     }
     return;
 };
@@ -85,6 +89,17 @@ const render = (element) => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+// declare/set pagination variables
+const paginationNumbers = document.querySelector("#pagination-numbers");
+const paginatedList = document.querySelector("#paginated-list");
+let listItems = paginatedList.querySelectorAll(".discussion__container");
+const nextButton = document.querySelector("#next-button");
+const prevButton = document.querySelector("#prev-button");
+
+const paginationLimit = 7;
+let pageCount = Math.ceil(listItems.length / paginationLimit);
+let currentPage = 1;
 
 // add discussion to list on submit 
 let form = document.querySelector('.form');
@@ -102,9 +117,114 @@ form.onsubmit = function (e) {
         author: author,
         url: '',
         title: title,
-        createdAt: createdDateTime, 
+        createdAt: createdDateTime,
         answer: null,
     };
 
     ul.prepend(convertToDiscussion(inputData));
+
+    // update pagination on discussion post submission 
+    listItems = paginatedList.querySelectorAll(".discussion__container");
+    pageCount = Math.ceil(listItems.length / paginationLimit);
+
+    document.querySelector("#pagination-numbers").textContent = '';
+    getPaginationNumbers();
+    setCurrentPage(1);
+    addEventToPaginationNumber();
 };
+
+// pagination
+const disableButton = (button) => {
+    button.classList.add("disabled");
+    button.setAttribute("disabled", true);
+};
+
+const enableButton = (button) => {
+    button.classList.remove("disabled");
+    button.removeAttribute("disabled");
+};
+
+const handlePageButtonsStatus = () => {
+    if (currentPage === 1) {
+        disableButton(prevButton);
+    } else {
+        enableButton(prevButton);
+    }
+
+    if (pageCount === currentPage) {
+        disableButton(nextButton);
+    } else {
+        enableButton(nextButton);
+    }
+};
+
+const handleActivePageNumber = () => {
+    document.querySelectorAll(".pagination-number").forEach((button) => {
+        button.classList.remove("active");
+        const pageIndex = Number(button.getAttribute("page-index"));
+        if (pageIndex == currentPage) {
+            button.classList.add("active");
+        }
+    });
+};
+
+const appendPageNumber = (index) => {
+    const pageNumber = document.createElement("button");
+    pageNumber.className = "pagination-number";
+    pageNumber.innerHTML = index;
+    pageNumber.setAttribute("page-index", index);
+    pageNumber.setAttribute("aria-label", "Page " + index);
+
+    paginationNumbers.appendChild(pageNumber);
+};
+
+const getPaginationNumbers = () => {
+    for (let i = 1; i <= pageCount; i++) {
+        appendPageNumber(i);
+    }
+};
+
+const setCurrentPage = (pageNum) => {
+    currentPage = pageNum;
+
+    handleActivePageNumber();
+    handlePageButtonsStatus();
+
+    const prevRange = (pageNum - 1) * paginationLimit;
+    const currRange = pageNum * paginationLimit;
+
+    listItems.forEach((item, index) => {
+        item.classList.add("hidden");
+        if (index >= prevRange && index < currRange) {
+            item.classList.remove("hidden");
+        }
+    });
+};
+
+const addEventToPaginationNumber = function () {
+    document.querySelectorAll(".pagination-number").forEach((button) => {
+        const pageIndex = Number(button.getAttribute("page-index"));
+
+        if (pageIndex) {
+            button.addEventListener("click", () => {
+                console.log("h")
+                setCurrentPage(pageIndex);
+            });
+        }
+    });
+};
+
+window.addEventListener("load", () => {
+    getPaginationNumbers();
+    setCurrentPage(1);
+
+    prevButton.addEventListener("click", () => {
+        setCurrentPage(currentPage - 1);
+    });
+
+    nextButton.addEventListener("click", () => {
+        setCurrentPage(currentPage + 1);
+    });
+
+    addEventToPaginationNumber();
+});
