370481aa87fea056ad50134cd4268a84b1a829ef
seungheonkim <seungheon818@naver.com>
2022-09-15 15:14:31 +0900

    discussion_amend

--- script.js
+++ script.js
@@ -3,30 +3,29 @@ console.log(agoraStatesDiscussions);
 
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
 const convertToDiscussion = (obj) => {
-  const li = document.createElement("li"); // li 요소 생성
-  li.className = "discussion__container"; // 클래스 이름 지정
+    const li = document.createElement("li"); // li 요소 생성
+    li.className = "discussion__container"; // 클래스 이름 지정
 
-  const avatarWrapper = document.createElement("div");
-  avatarWrapper.className = "discussion__avatar--wrapper";
-  const discussionContent = document.createElement("div");
-  discussionContent.className = "discussion__content";
-  const discussionAnswered = document.createElement("div");
-  discussionAnswered.className = "discussion__answered";
+    const avatarWrapper = document.createElement("div");
+    avatarWrapper.className = "discussion__avatar--wrapper";
+    const discussionContent = document.createElement("div");
+    discussionContent.className = "discussion__content";
+    const discussionAnswered = document.createElement("div");
+    discussionAnswered.className = "discussion__answered";
 
-  // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
+    // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
 
 
-
-  li.append(avatarWrapper, discussionContent, discussionAnswered);
-  return li;
+    li.append(avatarWrapper, discussionContent, discussionAnswered);
+    return li;
 };
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
-  for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
-    element.append(convertToDiscussion(agoraStatesDiscussions[i]));
-  }
-  return;
+    for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
+        element.append(convertToDiscussion(agoraStatesDiscussions[i]));
+    }
+    return;
 };
 
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
