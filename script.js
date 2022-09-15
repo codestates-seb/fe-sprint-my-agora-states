92db60502aeece98894087e2908fa8cbe916061b
Hyunji0911 <hyunji.lee0911@gmail.com>
2022-09-15 17:23:10 +0900

    이현지_41_아고라스테이츠 bare minimum 과제 제출

--- script.js
+++ script.js
@@ -6,20 +6,54 @@ const convertToDiscussion = (obj) => {
   const li = document.createElement("li"); // li 요소 생성
   li.className = "discussion__container"; // 클래스 이름 지정
 
+
+  //avatar
   const avatarWrapper = document.createElement("div");
   avatarWrapper.className = "discussion__avatar--wrapper";
+  const avatarImg = document.createElement('img');
+  avatarImg.className = 'discussion__avatar--image';
+  avatarImg.src = obj.avatarUrl;
+  avatarImg.alt = `avatar of ${obj.author}`;
+  avatarWrapper.append(avatarImg);
+
+
+  //discussion content
   const discussionContent = document.createElement("div");
   discussionContent.className = "discussion__content";
+
+  const discussionTitle = document.createElement('h2');
+  discussionTitle.className = 'discussion__title';
+
+  const discussionTitleUrl = document.createElement('a');
+  discussionTitleUrl.href = obj.url;
+  discussionTitleUrl.textContent = obj.title;
+  discussionTitle.append(discussionTitleUrl);  
+
+  const discussionInfo = document.createElement('div');
+  discussionInfo.className = 'discussion__information';
+  discussionInfo.textContent = `${obj.author} / ${obj.createdAt}`;
+  
+  discussionContent.append(discussionTitle, discussionInfo);
+
+
+  //discussion answered
   const discussionAnswered = document.createElement("div");
   discussionAnswered.className = "discussion__answered";
-
-  // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
-
-
-
+  discussionAnswered.textContent = isAnswered(obj);
+  
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
 };
+
+
+function isAnswered (obj) {
+  if (obj.answer === null) {
+    return '☒';
+  } else {
+    return '☑';
+  }
+}
+
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
