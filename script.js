b53fcefea7a8fc39a6ba0c650e57de56a5202ab1
“PIkadev1771” <pikadev1771@gmail.com>
2022-09-16 02:55:39 +0900

    feat: submit 버튼에 click 이벤트 추가, 디스커션 나열 기능의 이벤트 핸들러 함수 구현

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 console.log(agoraStatesDiscussions);
 
 const submitBtn = document.querySelector(".form__submit__btn")
+const ul = document.querySelector("ul.discussions__container");
 
 const userId = document.querySelector("#name")
 const title = document.querySelector("#title")
@@ -18,6 +19,7 @@ let seconds = ('0' + today.getSeconds()).slice(-2);
 
 let currentTime = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`
 
+
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
 const convertToDiscussion = (obj) => {
   const li = document.createElement("li"); // li 요소 생성
@@ -31,15 +33,47 @@ const convertToDiscussion = (obj) => {
   discussionAnswered.className = "discussion__answered";
 
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
+// 프로필
+const avatarImage = document.createElement("img") //  img 태그 요소 만들고
+avatarImage.className = "discussion__avatar--image"; // 클래스네임 지정
+avatarImage.setAttribute("src", obj.avatarUrl) // src 속성
+avatarImage.setAttribute("alt", `avatar of ${obj.author}`) // alt 속성
+avatarWrapper.append(avatarImage) // avatarWrapper 요소에 append
 
+// 질문 
+const discussionTitle = document.createElement("h2")
+discussionTitle.className = "discussion__title"
+const discussionUrl = document.createElement("a")
+discussionUrl.setAttribute("href", obj.url)
+discussionUrl.textContent = obj.title;
+discussionTitle.append(discussionUrl)
+discussionContent.append(discussionTitle)
 
+// 작성자 및 날짜
+const discussionInformationContainer =  document.createElement("div")
+discussionInformationContainer.className = "discussion__information__container"
+const discussionInformation =  document.createElement("div")
+discussionInformation.className = "discussion__information"
+discussionInformation.textContent = `${obj.author} / ${obj.createdAt}`;
+discussionInformationContainer.append(discussionInformation)
+discussionContent.append(discussionInformationContainer)
 
-  li.append(avatarWrapper, discussionContent, discussionAnswered);
-  return li;
+// 체크 박스
+const discussionAnsweredP =  document.createElement("p")
+if (obj.answer === null) {
+  discussionAnswered.textContent = `X`;
+} else {
+  discussionAnswered.textContent = `O`;
+}
+discussionAnswered.append(discussionAnsweredP)
+
+li.append(avatarWrapper, discussionContent, discussionAnswered);
+return li;
 };
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
+  element.innerHTML = ""; 
   for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
     element.append(convertToDiscussion(agoraStatesDiscussions[i]));
   }
@@ -47,5 +81,29 @@ const render = (element) => {
 };
 
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
-const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+// 이벤트 핸들러 : input의 밸류들을 모아 데이터에 추가
+const submitQuestion = (e) => {
+  e.preventDefault(); 
+console.log('하하')
+
+let newObj= {
+  id: "",
+createdAt: currentTime,
+title: title.value,
+url: "https://github.com/codestates-seb/agora-states-fe/discussions/3",
+author : userId.value,
+answer: null,
+bodyHTML: "",
+avatarUrl: "https://avatars.githubusercontent.com/u/87750478?s=64&v=4"
+};
+
+
+console.log(newObj)
+agoraStatesDiscussions.unshift(newObj)
+render(ul);
+console.log(agoraStatesDiscussions)
+}
+
+submitBtn.addEventListener( 'click', submitQuestion )
