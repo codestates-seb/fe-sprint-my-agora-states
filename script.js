bc062f39c725ebbf8d9093a9e304eb4002a6e3bf
yejin32 <vhdekddl3@naver.com>
2022-09-16 15:35:22 +0900

    나만의 아고라스테이츠 html, css 1차 완성

--- script.js
+++ script.js
@@ -14,8 +14,32 @@ const convertToDiscussion = (obj) => {
   discussionAnswered.className = "discussion__answered";
 
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
+  // 1. 이미지
+  const avatarImg = document.createElement("img");
+  avatarImg.src = obj.avatarUrl;
+  avatarImg.alt = "avatar of " + obj.author;
+  avatarWrapper.append(avatarImg);
 
+  // 2. 제목
+  const contentTitle = document.createElement("h2");
+  const contentLink = document.createElement("a");
+  contentLink.href = obj.url;
+  contentLink.textContent = obj.title;
+  contentTitle.append(contentLink);
+  discussionContent.append(contentLink);
 
+  // 3. 정보
+  const contentInformation = document.createElement("div");
+  contentInformation.className = "discussion__information";
+  contentInformation.textContent = `${obj.author} / ${new Date(
+    obj.createdAt
+  ).toLocaleString()}`;
+  discussionContent.append(contentInformation);
+
+  // 4. 체크박스
+  const checked = document.createElement("p");
+  checked.textContent = obj.answer ? "☑︎" : "☒";
+  discussionAnswered.append(checked);
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
@@ -32,3 +56,30 @@ const render = (element) => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+// 이벤트 리스너
+const form = document.querySelector(".form");
+const userName = document.querySelector(".form__input--name >input");
+const newTitle = document.querySelector(".form__input--title > input");
+const newTextbox = document.querySelector(".form__textbox > textarea");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const obj = {
+    id: "unique number",
+    createdAt: new Date(),
+    title: newTitle.value,
+    url: "https://github.com/codestates-seb/agora-states-fe/discussions/45",
+    author: userName.value,
+    answer: null,
+    bodyHTML: newTextbox.value,
+    avatarUrl: "https://avatars.githubusercontent.com/u/79903256?s=64&v=4",
+  };
+  agoraStatesDiscussions.unshift(obj);
+  ul.prepend(convertToDiscussion(obj));
+
+  userName.value = "";
+  newTitle.value = "";
+  newTextbox.value = "";
+});
