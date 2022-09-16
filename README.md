54b0d2a9fcd5797a2594fad5545350126ee5726e
kimtank <sycork@gmail.com>
2022-09-16 20:19:20 +0900

    init으로 localstorage 구현

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ fe-sprint-my-agora-states
 > ㅏㅡ index.html   
 > ㅏㅡ script.js   
 > ㅏㅡ style.css   
+> ㅏㅡ index_init.js &rightarrow; init 파일생성
 
 # 나만의 아고라 스테이츠 만들기 개요
 
@@ -15,8 +16,8 @@ fe-sprint-my-agora-states
 
 ## Bare Minimum Requirement
 
-- 디스커션 나열 기능
-  - 나만의 Discussion 컴포넌트 만들기
+- ~~디스커션 나열 기능~~
+  - ~~나만의 Discussion 컴포넌트 만들기~~
     - ### ~~Bare Minimum Requirement~~
       1. ~~Discussion 컴포넌트~~
          - ~~모든 요소에 border-box를 적용합니다.~~
@@ -43,19 +44,19 @@ fe-sprint-my-agora-states
   - ~~agoraStatesDiscussions 배열에 추가한 데이터가 실제 쌓여야 합니다.~~
 - ~~Github Page 배포~~
   - ~~Github Page 배포 기능을 이용하여 누구나 볼 수 있게 배포합니다.~~
-- 코드스테이츠 fe-sprint-my-agora-states 리포지토리로 Pull Request
-  - 나만의 아고라 스테이츠를 코드스테이츠 깃허브에 Pull request합니다.
-  - 주어진 Pull request 형식에 따라주세요.
+- ~~코드스테이츠 fe-sprint-my-agora-states 리포지토리로 Pull Request~~
+  - ~~나만의 아고라 스테이츠를 코드스테이츠 깃허브에 Pull request합니다.~~
+  - ~~주어진 Pull request 형식에 따라주세요.~~
 
 ## Advanced Challenge
 
-- 현지 시간 적용
-  - 샘플 시간을 잘 변형하여, 현지 시간에 맞게 표현합니다. (ex. 오전 10:02:17)
+- ~~현지 시간 적용~~
+  - ~~샘플 시간을 잘 변형하여, 현지 시간에 맞게 표현합니다. (ex. 오전 10:02:17)~~
 - 페이지네이션 기능
   - 페이지네이션에 대해서 스스로 학습합니다.
   - 한 페이지에 10개씩 디스커션이 보여야 합니다.
   - 다음 페이지로 넘어갈 수 있어야 합니다.
   - 이전 페이지로 돌아올 수 있어야 합니다.
   - 다음 페이지가 없거나, 이전 페이지가 없는 경우 페이지를 유지해야 합니다.
-- 디스커션 유지 기능
-  - LocalStorage에 대해서 스스로 학습하고, 새롭게 추가하는 Discussion이 페이지를 새로고침해도 유지되도록 제작합니다.
+- ~~디스커션 유지 기능~~
+  - ~~LocalStorage에 대해서 스스로 학습하고, 새롭게 추가하는 Discussion이 페이지를 새로고침해도 유지되도록 제작합니다.~~
--- script.js
+++ script.js
@@ -86,9 +86,12 @@ const convertToDiscussion = (obj) => {
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
-  for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
-    element.append(convertToDiscussion(agoraStatesDiscussions[i]));
-  }
+  //로컬스토리지 READ
+  const dummyArrInLocalStorage = JSON.parse(localStorage.getItem(DATA_KEY));
+
+  for (let i = 0; i < dummyArrInLocalStorage.length; i += 1) 
+    element.append(convertToDiscussion(dummyArrInLocalStorage[i]));
+  
   return;
 };
 
@@ -97,27 +100,43 @@ const ul = document.querySelector("ul.discussions__container");
 render(ul);
 
 const containerForm = document.querySelector(".form");
+
+
+/**
+ * dummyData insert
+ * @param {form data submit action} event 
+ */
 containerForm.onsubmit = (event) => {
   event.preventDefault();
   //console.log(agoraStatesDiscussions.length);
-
+  //로컬 스토리지 READ
+  const dummyArrInLocalStorage = JSON.parse(localStorage.getItem(DATA_KEY));
   const randomObj =
-    agoraStatesDiscussions[
-      getRandomIndexFromDummyArr(agoraStatesDiscussions.length)
+    dummyArrInLocalStorage[
+      getRandomIndexFromDummyArr(dummyArrInLocalStorage.length)
     ];
 
   const copyObj = Object.assign({}, randomObj);
+
   copyObj.author = this.id.value;
   copyObj.title = this.title.value;
   copyObj.createdAt = new Date();
-  copyObj.answer = Math.random() > 0.5 ? null : copyObj.answer;
+  if(Math.random() > 0.5)
+    copyObj.answer = null;
 
-  agoraStatesDiscussions.unshift(copyObj);
+  dummyArrInLocalStorage.unshift(copyObj);
 
   //console.log(agoraStatesDiscussions.length);
   //추가문
   ul.prepend(convertToDiscussion(copyObj));
+
+  //로컬 스토리지 UPDATE
+  LOCAL_STORAGE.setItem(DATA_KEY, JSON.stringify(dummyArrInLocalStorage));
+
+  console.log(JSON.parse(LOCAL_STORAGE.getItem(DATA_KEY)));
 };
 
 const getRandomIndexFromDummyArr = (arrLength) =>
   Math.floor(Math.random() * arrLength);
+
+
