3c9302094c52516d5af55519f93413f639343b65
LeeWang <99003659+Leewang31@users.noreply.github.com>
2022-09-16 16:54:24 +0900

    gif

--- openQuestion.js
+++ openQuestion.js
@@ -2,6 +2,6 @@ function new_window() {
     window.open(
         "question.html",
         "Child",
-        "width=400, height=300, top=100, left=700",
+        "width=400, height=300, top=100, left=400",
     );
 }
