2715c425d2dda4cc8ae4e98257b2bb5fdfe329ec
FE012 <twte96@gmail.com>
2022-09-16 14:02:23 +0900

    나만의 아고라 스테이츠1

--- script.js
+++ script.js
@@ -12,10 +12,30 @@ const convertToDiscussion = (obj) => {
   discussionContent.className = "discussion__content";
   const discussionAnswered = document.createElement("div");
   discussionAnswered.className = "discussion__answered";
-
+  
+  // convertToDiscussion 함수의 목적은 값을 추출해서 새로운 li를
+  //만들기 위해
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
+  
+  const face = document.createElement("img") 
+  face.src = obj.avatarUrl;
+  face.alt = "avatar of" + obj.author;
+  avatarWrapper.append(face);
 
+  const discussionTitle = document.createElement("h2");
+  const titleAnchor = document.createElement("a");
+  titleAnchor.href = obj.url;
+  titleAnchor.textContent = obj.title;
+  discussionTitle.append(titleAnchor);
+  discussionContent.append(discussionTitle);
+  
+  const discussionInfo = document.createElement("div");
+  discussionInfo.textContent = `${obj.author} / ${new Date(obj.createdAt).toLocaleTimeString()}`
+  discussionContent.append(discussionTitle, discussionInfo);
 
+  const checked = document.createElement("p");
+  checked.textContent = obj.answer ? "☑︎" : "☒";
+  discussionAnswered.append(checked);
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
@@ -32,3 +52,27 @@ const render = (element) => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+
+const form = document.querySelector("form.form");
+const author = form.querySelector("div.form__input--name > input");
+const title = form.querySelector("div.form__input--title > input");
+const textbox = form.querySelector("div.form__itextbox > textarea");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const obj = {
+    id: "new id",
+    createdAt: new Date().toISOString(),
+    title: title.value ,
+    url: "https://github.com/codestates-seb/agora-states-fe/discussions",
+    author: author.value,
+    avatarUrl: "https://avatars.githubusercontent.com/u/97888923?s=64&u=12b18768cdeebcf358b70051283a3ef57be6a20f&v=4"
+  }
+})  
+
+agoraStatesDiscussions.unshift(obj);
+
+const discussion = convertToDiscussion(obj);
+
+ul.prepend(discussion);
