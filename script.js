d6be9c86a6caf720fa25d7de2b61399b66fe7e49
JEONGMIN-LE97 <std4432@gmail.com>
2022-09-16 14:12:39 +0900

    페이지네이션 js추가

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
 
-// 로컬 스토리지 체크
-// 로컬 자료가 없을때 저장
-if (localStorage.length === 0) {
-  localStorage.setItem('local', JSON.stringify(agoraStatesDiscussions));
-};
-// 로컬 에서 불러오기
-let getLocal = JSON.parse(localStorage.getItem('local'));
-
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
 const convertToDiscussion = (obj) => {
   const li = document.createElement("li"); // li 요소 생성
@@ -37,7 +29,7 @@ const convertToDiscussion = (obj) => {
   // information
   const contentInformation = document.createElement('div');
   contentInformation.className = 'discussion__information';
-  contentInformation.textContent = new Date(obj.createdAt).toLocaleString();
+  contentInformation.textContent = `${obj.author} / ${new Date(obj.createdAt).toLocaleString()}` 
   // new Date(obj.createdAt).toLocaleString()
   discussionContent.append(contentInformation);
 
@@ -84,7 +76,6 @@ form.addEventListener('submit', (event) => {
   textbox.value = '';
 })
 
-
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
   for (let i = 0; i < agoraStatesDiscussions.length; i++) {
@@ -93,8 +84,170 @@ const render = (element) => {
   return;
 };
 
-
-
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+
+
+
+
+/*
+let 총 페이지수 = Math.ceil(총갯수 / 1개화면에서 보여줄 갯수)
+
+let 나머지 = 현재나의페이지 % 하단부크기
+let 하단부첫번째숫자 = 현재나의페이지 - 현재나의페이지%하단부크기 + 1
+
+ex) 
+현재나의페이지 = 11
+하단부크기 = 10
+하단부첫번째숫자 = 11 - 11%10 + 1 // 11
+
+현재나의페이지 = 3
+하단부크기 = 10
+하단부첫번째숫자 = 3 - 3%10 + 1 // 1
+
+let 하단부마지막숫자 = 현재나의페이지 - 현재나의페이지%하단부크기 + 페이지하단부크기
+현재나의페이지 = 3
+하단부크기 = 10
+하단부마지막숫자 = 3 + 3%10 + 10 // 10
+
+하단부의 마지막 숫자는 총 갯수를 넘어갈 수 없어야 한다.
+if(하단부마지막숫자 > 총페이지수){
+    하단부마지막숫자 = 총페이지수
+}
+
+
+function pageAlgo(total, bottomSize, listSize, cursor ){
+    //total = 총 갯수
+    //bottomSize = 하단크기
+    //listSize = 화면에서 보여줄 크기
+    //cursor = 현재 나의 페이지
+
+    let totalPageSize = Math.ceil(total / listSize)  //한 화면에 보여줄 갯수에서 구한 하단 총 갯수 
+
+    let firstBottomNumber = cursor - cursor % bottomSize + 1;  //하단 최초 숫자
+    let lastBottomNumber = cursor - cursor % bottomSize + bottomSize;  //하단 마지막 숫자
+
+    if(lastBottomNumber > totalPageSize) lastBottomNumber = totalPageSize  //총 갯수보다 큰 경우 방지
+
+    return {
+        firstBottomNumber,
+        lastBottomNumber,
+        totalPageSize,
+        total,
+        bottomSize,
+        listSize,
+        cursor
+    }
+}
+
+//280개의 데이터, 하단에는 20개씩, 1개화면에는 10개, 지금 나의페이지는 21
+let info = pageAlgo(50, 5, 5, 6)  
+
+
+//실제 출력하는 방법 샘플
+for(let i = info.firstBottomNumber ; i <= info.lastBottomNumber; i++){
+    i == info.cursor ? console.log(`<span>cur : ${i}</span>`) : console.log(`<span>${i}</span>`)
+}
+*/
+
+
+
+
+const pagination_element = document.querySelector('#pagination');
+
+let current_page = 1;
+const rows = 10;
+
+const DisplayList = (items, wrapper, rows_per_page, page)=>{
+    wrapper.innerHTML = "";
+    page--;
+
+    const start = rows_per_page * page;
+    const end = start + rows_per_page;
+    const paginatedItems = items.slice(start, end);
+
+    for(let i=0; i < paginatedItems.length; i++){
+        
+        const item = paginatedItems[i];
+
+        const item_element = convertToDiscussion(item);
+        
+        wrapper.appendChild(item_element);
+
+    }
+}
+
+const setupPagination = (items, wrapper, rows_per_page) => {
+    wrapper.innerHTML = "";
+    const page_count = Math.ceil(items.length / rows_per_page);
+    
+    for(let i=1; i < page_count +1; i++){
+        const btn = paginationButton(i, items);
+        wrapper.appendChild(btn);
+    }
+}
+
+const paginationButton = (page, items) => {
+    const button = document.createElement('button');
+    button.textContent = page;
+    
+    if(current_page === page) button.classList.add('active');
+
+    button.addEventListener('click', function(){
+        current_page = page;
+        DisplayList(items, ul, rows, current_page);
+
+        const current_btn = document.querySelector('#pagination button.active');
+        current_btn.classList.remove('active');
+
+        button.classList.add('active');
+    });
+
+    return button;
+}
+
+const leftBtn = document.querySelector('#left__btn');
+const rightBtn = document.querySelector('#right__btn');
+
+leftBtn.addEventListener('click', () => {
+    current_page--;
+    if(current_page < 1){
+        current_page = 1;
+    }
+    DisplayList(agoraStatesDiscussions, ul, rows, current_page);
+
+
+    const current_btn = document.querySelectorAll('#pagination button');
+    for(let i=0; i<current_btn.length; i++){
+        current_btn[i].classList.remove('active');
+      }
+
+    current_btn[current_page-1].classList.add('active');
+
+
+})
+
+rightBtn.addEventListener('click', () => {
+    const current_btn = document.querySelectorAll('#pagination button');
+
+    current_page++;
+    if(current_page > current_btn.length){
+        current_page = current_btn.length;
+    }
+    DisplayList(agoraStatesDiscussions, ul, rows, current_page);
+
+
+    for(let i=0; i<current_btn.length; i++){
+        current_btn[i].classList.remove('active');
+      }
+
+    current_btn[current_page-1].classList.add('active');
+})
+
+
+DisplayList(agoraStatesDiscussions, ul, rows, current_page);
+setupPagination(agoraStatesDiscussions, pagination_element, rows);
+
+
