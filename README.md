69de134907312001e880040c7b5dc0c33f8e865d
kimtank <sycork@gmail.com>
2022-09-16 16:42:03 +0900

    디자인못했음.

--- README.md
+++ README.md
@@ -36,13 +36,13 @@ fe-sprint-my-agora-states
   - ~~아고라 스테이츠 질문 리스트가 중앙으로 와야 합니다.~~
   - style.css를 수정하여 멋지고 아름답게 나만의 아고라 스테이츠를 꾸밉니다.
   - colorhunt, dribbble에서 적절한 색 조합, 디자인을 참고합니다.
-- ~~디스커션 추가 기능~~
+- 디스커션 추가 기능
   - ~~script.js를 수정하여 디스커션 추가 기능을 구현합니다.~~
   - section.form__container 요소에 새로운 아고라 스테이츠 질문을 추가할 수 있는 입력 폼을 제작합니다. 형식은 자유입니다.
-  - 아이디, 본문을 입력하고 버튼을 누르면 실제 화면에 디스커션이 추가되어야 합니다.
-  - agoraStatesDiscussions 배열에 추가한 데이터가 실제 쌓여야 합니다.
-- Github Page 배포
-  - Github Page 배포 기능을 이용하여 누구나 볼 수 있게 배포합니다.
+  - ~~아이디, 본문을 입력하고 버튼을 누르면 실제 화면에 디스커션이 추가되어야 합니다.~~
+  - ~~agoraStatesDiscussions 배열에 추가한 데이터가 실제 쌓여야 합니다.~~
+- ~~Github Page 배포~~
+  - ~~Github Page 배포 기능을 이용하여 누구나 볼 수 있게 배포합니다.~~
 - 코드스테이츠 fe-sprint-my-agora-states 리포지토리로 Pull Request
   - 나만의 아고라 스테이츠를 코드스테이츠 깃허브에 Pull request합니다.
   - 주어진 Pull request 형식에 따라주세요.
--- script.js
+++ script.js
@@ -26,7 +26,7 @@ const convertToDiscussion = (obj) => {
   discussionTitle.append(discussionLink);
   discussionContent.append(discussionInformation);
 
-/*   좋은 삽질이였다.
+  /*   좋은 삽질이였다.
     letsMappingDatas(
     obj,
     discussionAvatarImage,
@@ -40,14 +40,17 @@ const convertToDiscussion = (obj) => {
     "https://faopharmacy.unc.edu/wp-content/uploads/sites/200/2022/04/noimage.png";
   discussionLink.textContent = obj.title;
   discussionLink.href = obj.url;
-  discussionInformation.textContent = `${obj.author} / ${new Date(obj.createdAt).toLocaleString()}`;
+  discussionInformation.textContent = `${obj.author} / ${new Date(
+    obj.createdAt
+  ).toLocaleString()}`;
+  discussionAnswered.textContent = obj.answer === null ? "X" : "O";
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
 };
 
 // /**
-//  * 아아아앙대 
+//  * 아아아앙대
 //  * @param {*} obj, arguments
 //  * @returns boolean
 //  */
@@ -92,3 +95,29 @@ const render = (element) => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+const containerForm = document.querySelector(".form");
+containerForm.onsubmit = (event) => {
+  event.preventDefault();
+  //console.log(agoraStatesDiscussions.length);
+
+  const randomObj =
+    agoraStatesDiscussions[
+      getRandomIndexFromDummyArr(agoraStatesDiscussions.length)
+    ];
+
+  const copyObj = Object.assign({}, randomObj);
+  copyObj.author = this.id.value;
+  copyObj.title = this.title.value;
+  copyObj.createdAt = new Date().toUTCString;
+  copyObj.answer = Math.random > 0.5 ? null : copyObj.answer;
+
+  agoraStatesDiscussions.unshift(copyObj);
+
+  //console.log(agoraStatesDiscussions.length);
+  //추가문
+  ul.prepend(convertToDiscussion(copyObj));
+};
+
+const getRandomIndexFromDummyArr = (arrLength) =>
+  Math.floor(Math.random() * arrLength);
