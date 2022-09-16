58f06a94ae447b8241bf2d57ca21bc8d4938bffa
kimhw7 <khw786678@gmail.com>
2022-09-16 12:51:21 +0900

    form DOM 가져오기

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ const convertToDiscussion = (obj) => {
   titleUrl.textContent = obj.title;
   titleUrl.href = obj.url;
   // 작성자, 작성일 추가
-  discussionInfomation.textContent = `${obj.author} / ${obj.createdAt}`;
+  discussionInfomation.textContent = `${obj.author} / ${new Date(obj.createdAt).toLocaleString()}`;
   discussionTitle.appendChild(titleUrl);
   discussionContent.append(discussionTitle, discussionInfomation);
   // 답변 체크박스 추가
@@ -44,6 +44,15 @@ const convertToDiscussion = (obj) => {
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
 };
+
+// 2.form 입력을 받아 객체에 추가하여 새로운 discussion 생성
+// 2-1. form DOM 으로 가져오기
+const inputWrapper = document.querySelector('div.form__input--wrapper');
+const inputName = document.querySelector('div.form__input--name > input#name');
+const inputTitle = document.querySelector('div.form__input--title > input#name');
+const formTextbox = document.querySelector('div.form__textbox > textarea#story');
+
+// 2-2. event 연결하기
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
