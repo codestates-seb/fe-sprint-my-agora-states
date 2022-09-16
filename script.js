dfc22795b830c29b1b1fd2a93469b523a2668dcd
kimdong-hun <kimdonghun426@gmail.com>
2022-09-16 11:53:33 +0900

    feat: 작성 시간 표시 수정

--- script.js
+++ script.js
@@ -13,8 +13,6 @@ const convertToDiscussion = (obj) => {
   const discussionAnswered = document.createElement("div");
   discussionAnswered.className = "discussion__answered";
 
-  // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
-
   // 아바타 이미지 렌더링 기능 구현
   const avatarImg = document.createElement("img");
 
@@ -34,7 +32,7 @@ const convertToDiscussion = (obj) => {
   titleLink.textContent = obj.title;
   title.append(titleLink);
   information.className = "discussion__information";
-  information.textContent = `${obj.author} / ${obj.createdAt}`;
+  information.textContent = `${obj.author} / ${new Date(obj.createdAt).toLocaleString('ko-KR')}`;
 
   discussionContent.append(title);
   discussionContent.append(information);
@@ -44,9 +42,9 @@ const convertToDiscussion = (obj) => {
   const answeredCheck = document.createElement("p");
 
   if(obj.answer !== null) {
-    answeredCheck.textContent = 'O';
+    answeredCheck.textContent = '☑︎';
   } else {
-    answeredCheck.textContent = 'X';
+    answeredCheck.textContent = '☒';
   }  
 
   answered.className = "discussion__answered";
@@ -69,3 +67,4 @@ const render = (element) => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
