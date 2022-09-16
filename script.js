6e51a39e04e4156043ee9ec218283343be99b55f
CHOGANGYEOL <loeygnak@gmail.com>
2022-09-16 10:56:31 +0900

    bare minimum 코드 수정

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const convertToDiscussion = (obj) => {
   const avatarImg = document.createElement("img")
   avatarImg.className = "discussion__avatar--image";
   avatarImg.src = obj.avatarUrl;
-  avatarImg.alt = 'avatar of' + obj.author;
+  avatarImg.alt = 'avatar of ' + obj.author;
 
   // 질문 코드
   const discussionContent = document.createElement("div");
@@ -25,13 +25,21 @@ const convertToDiscussion = (obj) => {
   // discussionTitleAnchor.setAttribute("href", obj.url);
   const discussionInformation = document.createElement("div")
   discussionInformation.className = "discussion__information"
-  discussionInformation.textContent = `${obj.id} / ${obj.createdAt}`
+  discussionInformation.textContent = `${obj.id} ${obj.createdAt}`
+  // discussionInformation.textContent = `${obj.id} ${new Date(obj.createdAt).toLocaleString()}`
   
   // checkbox
   const discussionAnswered = document.createElement("div");
   discussionAnswered.className = "discussion__answered";
-  const discussionAnsweredP = document.createElement("p");
-  discussionAnsweredP.textContent = "☑"
+  const discussionAnsweredP = document.createElement("i");
+  // 삼항연상자
+  discussionAnsweredP.className = obj.answer ? "xi-check-circle" : "xi-check-circle-o";
+  
+  // if (obj.answer !== null) {
+  //   discussionAnsweredP.className = "xi-check-circle"
+  // } else {
+  //   discussionAnsweredP.className = "xi-check-circle-o"
+  // }
   
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   avatarWrapper.append(avatarImg);
@@ -42,7 +50,9 @@ const convertToDiscussion = (obj) => {
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
+  // dummy data의 길이만큼, 탐색
   for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
+    // i번째 요소를 convertToDiscussion에 전달하여 결과를 ul에 append
     element.append(convertToDiscussion(agoraStatesDiscussions[i]));
   }
   return;
@@ -53,34 +63,40 @@ const ul = document.querySelector("ul.discussions__container");
 render(ul);
 
 // submit을 눌렀을때
-const submit = document.querySelector('.form__submit input')
-submit.addEventListener('click', function() {
+const form = document.querySelector('.form')
+form.addEventListener('submit', (e) => {
   // console.log('submit 버튼 누름')
-  
-  const name = document.querySelector('.form__input--name input').value;
-  const title = document.querySelector('.form__input--title input').value;
-  const question = document.querySelector('.form__textbox textarea').value;
+  // 페이지 새로 고침을 방지
+  e.preventDefault();
+
+  const name = document.querySelector('#name');
+  const title = document.querySelector('#title');
+  const question = document.querySelector('.form__textbox textarea');
   let today = new Date();
   let year = today.getFullYear(); //년도
   let month = today.getMonth() + 1;  // 월
   let date = today.getDate();  // 날짜
-  const eng = ['S', 'M', 'T', 'W', 'T', 'F', 'S']
+  const eng = ['S', 'M', 'T', 'W', 'T', 'F', 'S'] // 요일 영어로 바꾸기용 배열
   let day = eng[today.getDay()];  // 요일
   let hours = today.getHours(); // 시
   let minutes = today.getMinutes();  // 분
   let seconds = today.getSeconds();  // 초
 
-  let newForm = {
-    id : name,
-    title : title,
-    bodyHTML : question,
-    createdAt: `${year}-${month}-${date}${day}${hours}:${minutes}:${seconds}`,
-    url: "#",
+  const newForm = {
+    id : "unique number",
+    title : title.value,
+    bodyHTML : question.value,
+    createdAt: `${year}/${month}/${date} ${hours}:${minutes}:${seconds}`,
+    url: "",
     avatarUrl:
     "https://pbs.twimg.com/media/EjDeXNOU4AA_lE1.jpg",
+    author : name.value,
+    answer: null,
   }
-  if (name !== '' && title !== '' && question !== '') {
     agoraStatesDiscussions.unshift(newForm);
-    ul.before(convertToDiscussion(agoraStatesDiscussions[0]));
-  }
+    // ul.before(convertToDiscussion(agoraStatesDiscussions[0]));
+    ul.prepend(convertToDiscussion(newForm))
+    name.value = "";
+    title.value = "";
+    question.value = "";
 });
