41b3da91102a959d2f9fdaaa4ed472216074b4d3
Zoe Park <mary990t@gmail.com>
2022-09-17 11:52:47 +0900

    feat: 자바스크립트 완성

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 // index.html을 열어서 agoraStatesDiscussions 배열 요소를 확인하세요.
-console.log(agoraStatesDiscussions);
+
 
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
 const convertToDiscussion = (obj) => {
+  
   const li = document.createElement("li"); // li 요소 생성
   li.className = "discussion__container"; // 클래스 이름 지정
 
@@ -14,12 +15,92 @@ const convertToDiscussion = (obj) => {
   discussionAnswered.className = "discussion__answered";
 
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
+  // 요소를 선택한다.☑ 문제점 해결
+  // const elDiscussionAvatarWrapper = document.querySelector('.discussion__avatar--wrapper');
+  // const elDiscussionContent = document.querySelector('.discussion__content');
+  // const elDiscussionAnswered = document.querySelector('.discussion__answered');
+  
+  // 선택한 요소들의 자식태그를 만들어준다.
+  // 1. avatarwrapper 안에는 img 자식이 있고, 
+  // 2. discussioncontent 안에는 h2, div 자식이 있고,
+  // 3. discussionanswered 안에는 p 자식이 있다.
+  // 자식태그의 클래스명과 속성들을 추가해준다.
+  const avatarImg = document.createElement("img");
+  avatarImg.className = "discussion__avatar--image"
+  avatarImg.src = obj.avatarUrl; // data.js의 키값 바로 가져와도 되나..?
+  avatarImg.alt = "avatar of " + obj.author;
+  
+  const discussionTitle = document.createElement("h2");
+  discussionTitle.className = "discussion__title";
+  // h2에는 href 속성을 가지는 <a> 자식이 있다.
+  const titleAnchor = document.createElement("a");
+  titleAnchor.href = obj.url;
+  discussionTitle.append(titleAnchor);
+
+  // const discussionTitleUrl = document.createElement("a");
+  // discussionTitleUrl.href = obj.url;
+  
+  const discussionInformation = document.createElement("div");
+  discussionInformation.className = "discussion__information";
+  
+  const discussionAnsweredCheckbox = document.createElement("p");
+
+  // 요소 안에 적절한 자식요소를 넣어준다.☑ 문제점 해결
+  // elDiscussionAvatarWrapper.append(avatarImg);
+  // elDiscussionContent.append(discussionTitle, discussionInformation);
+  // elDiscussionAnswered.append(discussionAnsweredCheckbox);
+  avatarWrapper.append(avatarImg);
+  discussionContent.append(discussionTitle, discussionInformation);
+  discussionAnswered.append(discussionAnsweredCheckbox);
+
+
+  // 자식요소들에 들어갈 값을 넣어준다.
+  // 1. 질문 제목
+  // 2. 작성자와 작성날짜
+  // 3. 체크박스
+
+  titleAnchor.textContent = obj.title;
+  discussionInformation.textContent = `${obj.author} / ${new Date(obj.createdAt).toLocaleString()}`;
+  discussionAnsweredCheckbox.textContent = obj.answer ? "☑": "☒";
+  
 
 
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
 };
+
+// <form에 작성후, submit을 누르면 ul에 li요소로 들어가게 하기>
+// note: submit 누르면 새로고침 되는 기능이 브라우저에 기본적으로 들어있다. -> prevent.Default 사용하여 새로고침 없애기
+const form = document.querySelector(".form");
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const inputName = document.querySelector("#name");
+  const inputTitle = document.querySelector("#title");
+  const obj = {
+    id: "D_kwDOHOApLM4APjJi",
+    createdAt: `${new Date()}`,
+    title: inputTitle.value,
+    url: "https://github.com/codestates-seb/agora-states-fe/discussions/45",
+    author: inputName.value,
+    answer: null,
+    bodyHTML:
+      '<p dir="auto">--------------- 여기서부터 복사하세요 ---------------</p>\n<p dir="auto">운영 체제: 예) macOS</p>\n<p dir="auto">현재 어떤 챕터/연습문제/과제를 진행 중이고, 어떤 문제에 부딪혔나요?<br>\nPair 과제 / JavaScript Koans</p>\n<p dir="auto">npm install 명령어 입력 시 env: node: No such file or directory 라고 뜹니다</p>\n<p dir="auto">에러 발생하여 아래 명령어 실행 했는데도 불구하고 똑같은 에러가 발생했습니다<br>\nnpm cache clean --force</p>\n<p dir="auto">rm package-lock.json</p>\n<p dir="auto">rm -rf ./node_modules/</p>\n<p dir="auto">npm --verbose install</p>\n<p dir="auto">폴더 자체가 문제가 있다고 생각하여 github에서 다시 fork 후 진행했는데도 같은 에러가 발생했습니다<br>\n리눅스 기초 챕터 때 npm 설치해서 마지막 submit까지는 잘 됐는데 현재 짝수 생성기 폴더도 똑같이 npm install 시 no such file or directory가 발생합니다</p>\n<p dir="auto">에러가 출력된 곳에서, 이유라고 생각하는 부분을 열 줄 이내로 붙여넣기 해 주세요. (잘 모르겠으면 에러라고 생각하는 곳을 넣어주세요)</p>\n<div class="highlight highlight-source-js position-relative overflow-auto" data-snippet-clipboard-copy-content="minjun@dubi fe-sprint-javascript-koans-main % pwd \n/Users/minjun/Documents/fe_frontand_39/fe-sprint-javascript-koans-main\nminjun@dubi fe-sprint-javascript-koans-main % npm install \nenv: node: No such file or directory"><pre><span class="pl-s1">minjun</span>@<span class="pl-s1">dubi</span> <span class="pl-s1">fe</span><span class="pl-c1">-</span><span class="pl-s1">sprint</span><span class="pl-c1">-</span><span class="pl-s1">javascript</span><span class="pl-c1">-</span><span class="pl-s1">koans</span><span class="pl-c1">-</span><span class="pl-s1">main</span> <span class="pl-c1">%</span> <span class="pl-s1">pwd</span> \n<span class="pl-c1">/</span><span class="pl-v">Users</span><span class="pl-c1">/</span><span class="pl-s1">minjun</span><span class="pl-c1">/</span><span class="pl-v">Documents</span><span class="pl-c1">/</span><span class="pl-s1">fe_frontand_39</span><span class="pl-c1">/</span><span class="pl-s1">fe</span><span class="pl-c1">-</span><span class="pl-s1">sprint</span><span class="pl-c1">-</span><span class="pl-s1">javascript</span><span class="pl-c1">-</span><span class="pl-s1">koans</span><span class="pl-c1">-</span><span class="pl-s1">main</span>\n<span class="pl-s1">minjun</span><span class="pl-kos"></span>@<span class="pl-s1">dubi</span> <span class="pl-s1">fe</span><span class="pl-c1">-</span><span class="pl-s1">sprint</span><span class="pl-c1">-</span><span class="pl-s1">javascript</span><span class="pl-c1">-</span><span class="pl-s1">koans</span><span class="pl-c1">-</span><span class="pl-s1">main</span> <span class="pl-c1">%</span> <span class="pl-s1">npm</span> <span class="pl-s1">install</span> \nenv: node: <span class="pl-v">No</span> <span class="pl-s1">such</span> <span class="pl-s1">file</span> <span class="pl-s1">or</span> <span class="pl-s1">directory</span></pre></div>\n<p dir="auto">검색했던 링크가 있다면 첨부해 주세요.<br>\n<a href="https://mia-dahae.tistory.com/89" rel="nofollow">https://mia-dahae.tistory.com/89</a></p>\n<p dir="auto"><a href="https://stackoverflow.com/questions/38143558/npm-install-resulting-in-enoent-no-such-file-or-directory" rel="nofollow">https://stackoverflow.com/questions/38143558/npm-install-resulting-in-enoent-no-such-file-or-directory</a></p>\n<p dir="auto"><a href="https://velog.io/@hn04147/npm-install-%ED%95%A0-%EB%95%8C-tar-ENOENT-no-such-file-or-directory-lstat-%EC%97%90%EB%9F%AC%EB%82%A0-%EA%B2%BD%EC%9A%B0" rel="nofollow">https://velog.io/@hn04147/npm-install-%ED%95%A0-%EB%95%8C-tar-ENOENT-no-such-file-or-directory-lstat-%EC%97%90%EB%9F%AC%EB%82%A0-%EA%B2%BD%EC%9A%B0</a></p>\n<p dir="auto"><a href="https://m.blog.naver.com/PostView.naver?isHttpsRedirect=true&amp;blogId=chandong83&amp;logNo=221064506346" rel="nofollow">https://m.blog.naver.com/PostView.naver?isHttpsRedirect=true&amp;blogId=chandong83&amp;logNo=221064506346</a></p>\n<p dir="auto"><a href="https://webisfree.com/2021-07-15/npm-install-%EC%97%90%EB%9F%AC-%EB%B0%9C%EC%83%9D-rename-no-such-file-or-directory-%ED%95%B4%EA%B2%B0%ED%95%98%EA%B0%80" rel="nofollow">https://webisfree.com/2021-07-15/npm-install-%EC%97%90%EB%9F%AC-%EB%B0%9C%EC%83%9D-rename-no-such-file-or-directory-%ED%95%B4%EA%B2%B0%ED%95%98%EA%B0%80</a></p>\n<p dir="auto"><a href="https://hellowworlds.tistory.com/57" rel="nofollow">https://hellowworlds.tistory.com/57</a></p>',
+    avatarUrl:
+      "https://avatars.githubusercontent.com/u/97888923?s=64&u=12b18768cdeebcf358b70051283a3ef57be6a20f&v=4",
+  }
+  // submit을 하면, 객체가 convertToDiscussion으로 전달되어 dom으로(li로)) 바뀌고,
+  // 그 결과를 render 함수에 전달하여 화면에 표시한다. (ul의 맨 앞으로)
+  ul.prepend(convertToDiscussion(obj));
+  agoraStatesDiscussions.unshift(obj);
+  console.log(agoraStatesDiscussions.length);
+})
+
+
+
+
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
@@ -28,6 +109,7 @@ const render = (element) => {
   }
   return;
 };
+
 
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
