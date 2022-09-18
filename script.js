fd35a2b8e78c4949dcf6bf7b742c2eb93a3944b0
sweetyr928 <031694@naver.com>
2022-09-18 15:17:37 +0900

    localStorage 구현

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 // index.html을 열어서 agoraStatesDiscussions 배열 요소를 확인하세요.
 //console.log(agoraStatesDiscussions);
 
-let submitBtn = document.querySelector('.form__submit');
+let data;
+/* 로컬 스토리지 */
+// 로컬 스토리지에 저장된 데이터를 가져온다(이 때, 데이터는 JSON 문자열로 되어있음)
+const dataFromLocalStorage = localStorage.getItem("agoraStatesDiscussions");
+if (dataFromLocalStorage) {
+  // 가져온 JSON 문자열을 객체로 변환한다.
+  data = JSON.parse(dataFromLocalStorage);
+} else {
+  // 만약 로컬스토리지가 비어있으면 data에 agoraStatesDiscussions 배열을 저장해준다.
+  data = agoraStatesDiscussions.slice();
+}
 
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
 const convertToDiscussion = (obj) => {
@@ -34,7 +44,7 @@ const convertToDiscussion = (obj) => {
 
   const information = document.createElement('div');
   information.className = "discussion__information";
-  information.textContent = `${obj.author} / ${new Date(obj.createdAt).toLocaleString()}`;
+  information.textContent = `${obj.author} / ${new Date(obj.createdAt).toLocaleTimeString()}`;
   discussionContent.append(titleName, information);
 
   // 체크박스 영역
@@ -48,16 +58,29 @@ const convertToDiscussion = (obj) => {
 };
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
-const render = (element) => {
-  for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
-    element.append(convertToDiscussion(agoraStatesDiscussions[i]));
+const render = (element, from, to) => {
+  console.log(from, to);
+  if (!from && !to) {
+    from = 0;
+    to = data.length - 1;
+  }
+  // 다 지우고 배열에 있는 내용 다 보여주기
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+  for (let i = from; i < to; i += 1) {
+    element.append(convertToDiscussion(data[i]));
   }
   return;
 };
 
+// 페이지네이션을 위한 변수
+let limit = 10,
+  page = 1;
+
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
-render(ul);
+render(ul, 0, limit);
 
 const form = document.querySelector('.form');
 let elName =  document.querySelector('.form__input--name > input');
@@ -71,12 +94,15 @@ form.addEventListener('submit', (event) =>{
   newDisscussion.id = "unique number";
   newDisscussion.author = elName.value;
   newDisscussion.title = elTitle.value;
-  newDisscussion.createdAt = new Date();
+  newDisscussion.createdAt = new Date().toISOString();
   newDisscussion.answer = null;
   newDisscussion.avatarUrl = 'https://avatars.githubusercontent.com/u/12145019?s=64&u=5c97f25ee02d87898457e23c0e61b884241838e3&v=4';
-  agoraStatesDiscussions.unshift(newDisscussion);
+  data.unshift(newDisscussion);
+
+  // 로컬스토리지에 저장(data를 문자열로 바꿔줌)
+  localStorage.setItem("agoraStatesDiscussions", JSON.stringify(data));
+
   ul.prepend(convertToDiscussion(newDisscussion));
-  console.log(agoraStatesDiscussions);
   elTitle.value = "";
   elStory.value = "";
   elName.value = "";
