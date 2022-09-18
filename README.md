247bf527939d9550f88ef5a2bc324da678a08abd
“PIkadev1771” <pikadev1771@gmail.com>
2022-09-18 19:19:52 +0900

    refactor: script.js 주석 정리, README 파일 수정

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ https://pikadev1771.github.io/fe-sprint-my-agora-states/
 * 디스커션 추가 기능
 * Github Page 배포
 * 현지 시간 적용
+* Local Storage로 데이터 관리
 
 ## 배포 시연 화면
 ![아고라 스테이츠_박경현](https://user-images.githubusercontent.com/111509842/190858427-9f40ba5c-2322-4327-b258-3b781647b042.gif)
--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-// index.html을 열어서 agoraStatesDiscussions 배열 요소를 확인하세요.
 console.log(agoraStatesDiscussions);
 
+// 로컬 스토리지
 let data; 
-
-// 로컬 스토리지에 데이터 있으면 => 업데이트 / 없으면 => agoraStatesDiscussions 복사(최초 1번, 처음 1번만 쓸 것이기 때문에 그냥 얕은 복사로. 주소값 같아도 상관없어서?)
 const localStorageData = localStorage.getItem("discussionData");
 if(localStorageData) {
   data = JSON.parse(localStorageData) // 문자열을 자바스크립트 객체로 변환
@@ -11,6 +9,7 @@ if(localStorageData) {
   data = agoraStatesDiscussions.slice(); 
 }
 
+// 요소 지정
 const formSubmit = document.querySelector(".form")
 const ul = document.querySelector("ul.discussions__container");
 
@@ -30,7 +29,7 @@ let seconds = ('0' + today.getSeconds()).slice(-2);
 let currentTime = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`
 
 
-// convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
+// 하나의 디스커션 데이터(객체)를 DOM(하나의 li)으로 변환
 const convertToDiscussion = (obj) => {
   const li = document.createElement("li"); // li 요소 생성
   li.className = "discussion__container"; // 클래스 이름 지정
@@ -42,7 +41,6 @@ const convertToDiscussion = (obj) => {
   const discussionAnswered = document.createElement("div");
   discussionAnswered.className = "discussion__answered";
 
-  // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
 // 프로필
 const avatarImage = document.createElement("img") //  img 태그 요소 만들고
 avatarImage.className = "discussion__avatar--image"; // 클래스네임 지정
@@ -50,7 +48,7 @@ avatarImage.setAttribute("src", obj.avatarUrl) // src 속성
 avatarImage.setAttribute("alt", `avatar of ${obj.author}`) // alt 속성
 avatarWrapper.append(avatarImage) // avatarWrapper 요소에 append
 
-// 질문 
+// 디스커션 
 const discussionTitle = document.createElement("h2")
 discussionTitle.className = "discussion__title"
 const discussionUrl = document.createElement("a")
@@ -77,7 +75,7 @@ li.append(avatarWrapper, discussionContent, discussionAnswered);
 return li;
 };
 
-// agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
+// 1. 인자 안의 요소를 모두 지우고 -> 2. DOM으로 변환한 배열 형태의 데이터(data)를 인자에 하나씩 요소로 붙여서 렌더링하는 함수 
 const render = (element) => {
   // 일단 ul 안의 내용 다 지우기
   while (element.firstChild) {
@@ -90,13 +88,13 @@ const render = (element) => {
   return 
 };
 
-// ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
+// 렌더링 실행
 render(ul);
 
-// 이벤트 핸들러 : input의 밸류들을 모아 데이터에 추가
+// 이벤트 핸들러 : 새 디스커션 객체(newObj)를 만들고 -> 데이터(data)에 추가 ->  로컬스토리지에 저장 -> 새 데이터로 렌더링
 const submitQuestion = (e) => {
-  e.preventDefault(); 
-
+  e.preventDefault(); // submit과 동시에 다시 render가 되는 것을 막아준다. 
+  
   let newObj= {
   id: "",
   createdAt: currentTime,
@@ -108,13 +106,14 @@ const submitQuestion = (e) => {
   avatarUrl: "https://avatars.githubusercontent.com/u/87750478?s=64&v=4"
 };
 
-// ul.prepend(convertToDiscussion(newObj)) // 새 디스커션 요소 추가  (로컬 스토리지 없이 할 떄)
+
 data.unshift(newObj) // 데이터 추가 (업데이트))
 
 localStorage.setItem("discussionData", JSON.stringify(data)); // 추가된 원본 데이터 받아서 로컬 스토리지에 다시 저장
 
-render(ul);
+render(ul); // 새 데이터로 렌더링
 
+// input 창의 value들 초기화 (빈칸으로))
 userId.value = ""
 title.value = ""
 question.value = ""
