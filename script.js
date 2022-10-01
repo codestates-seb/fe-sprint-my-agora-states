dd691f73e78614ffd1fce7867ce5a6542f380cb5
Yoontaeyeon <okko8522@gmail.com>
2022-10-01 15:41:29 +0900

    Refactor(scripts.js): getLocalDates의 쓸데없는 for문 주석처리

--- script.js
+++ script.js
@@ -87,9 +87,9 @@ form.addEventListener("submit", (event) => {
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
-  for (let i = 0; i < getLocalDates.length; i++) {
-    element.append(convertToDiscussion(getLocalDates));
-  }
+  // for (let i = 0; i < getLocalDates.length; i++) {
+  //   element.append(convertToDiscussion(getLocalDates));
+  // }
   for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
     element.append(convertToDiscussion(agoraStatesDiscussions[i]));
   }
