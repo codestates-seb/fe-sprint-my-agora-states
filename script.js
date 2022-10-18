ab69434c5e8b96395e2f119c77d7b539350db8a6
nayul34 <dagg762@gmail.com>
2022-10-18 17:53:56 +0900

    서버 연동

--- script.js
+++ script.js
@@ -99,5 +99,15 @@ textArea.value="";
 })
 
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
-const ul = document.querySelector("ul.discussions__container");
-render(ul);
+
+fetch('http://localhost:4000/discussions')
+.then(res => res.json())
+.then(json => {
+  agoraStatesDiscussions = json;
+  const ul = document.querySelector("ul.discussions__container");
+  render(ul);
+  console.log( agoraStatesDiscussions);
+})
+
+//const ul = document.querySelector("ul.discussions__container");
+//render(ul);
