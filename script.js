c35885d064a9ca6889762d5a0e63e66aa1c6f28b
uk11 <dlckddnrr@gmail.com>
2022-10-18 17:14:18 +0900

    fetch

--- script.js
+++ script.js
@@ -82,7 +82,7 @@ form.addEventListener('submit', (event) =>  {
   textArea.value='';
 })
 
-// agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
+// agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수
 const render = (element) => {
   for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
     // i번째 요소를 convertToDiscussion에 전달해서 결과를 ul에 append
@@ -91,6 +91,16 @@ const render = (element) => {
   return;
 };
 
+let discussions = [];  // 서버에서 받은 데이터를 담을 변수를 준비
+fetch('http://localhost:4000/discussions/')
+.then(res => res.json())
+.then(json => {
+  discussions = json;  // 받아온 데이터를 담아주기
+  const ul = document.querySelector("ul.discussions__container");
+  render(ul);
+})
+
+
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
-const ul = document.querySelector("ul.discussions__container");
-render(ul);
+// const ul = document.querySelector("ul.discussions__container");
+// render(ul);
