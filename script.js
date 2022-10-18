96d05a6e74bb3775b78f3666201843260531d2ff
Yoontaeyeon <okko8522@gmail.com>
2022-10-18 16:59:24 +0900

    Feat: my-agora-states-server 연동 - index.html: data.js 코드 삭제 - script.js: fetch API 사용(fetch로 불러오고 렌더링), 로컬스토리지와 충돌로 로컬스토리지 코드 삭제링

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
+// let agoraStatesDiscussions;
+// fetch("http://localhost:4000/discussions")
+//   .then((response) => response.json())
+//   .then((json) => {
+//     agoraStatesDiscussions = json;
+//   });
+// 실행하고 렌더링 되면 agoraStatesDiscussions이 data로 들어감
 // 제출시 로컬스토리지를 불러오는 변수를 전역에서 선언하여 렌더링 함수에도 쓸 수 있게
-let getLocalDates = JSON.parse(localStorage.getItem("agoraLocalData"));
-agoraStatesDiscussions.unshift(getLocalDates);
+// let data;
+// const dataFromLocalStorage = localStorage.getItem("agoraStatesDiscussions");
+// dataFromLocalStorage ? (data = JSON.parse(dataFromLocalStorage)) : (data = agoraStatesDiscussions.slice());
 
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
 const convertToDiscussion = (obj) => {
@@ -51,33 +59,34 @@ const convertToDiscussion = (obj) => {
 
 // 제출
 const form = document.querySelector("form");
+const author = form.querySelector("div.form__input--name > input").value;
+const title = form.querySelector("div.form__input--title > input").value;
+const textbox = form.querySelector("div.form__textbox > textarea").value;
+// 제출된 내용(Value) 변수화
+// render에 넣어서 렌더링 되게 하기
+// 들어갈 임시 객체 그릇 만들기
+// 입력되면 객체 그릇에 내용 담기
+// data.js에 옮기고 convertToDiscussion로 DOM 변환
 
 form.addEventListener("submit", (event) => {
   // submit 이벤트 필수, 새로고침 안되게
   // 로컬스토리지화 되면서 새로고침돼도 안없어지니 필요없어짐
   // event.preventDefault();
-  // 제출된 내용(Value) 변수화
-  const author = form.querySelector("div.form__input--name > input").value;
-  const title = form.querySelector("div.form__input--title > input").value;
-  const textbox = form.querySelector("div.form__textbox > textarea").value;
-  // 들어갈 임시 객체 그릇 만들기
-  // 입력되면 객체 그릇에 내용 담기
-  // data.js에 옮기고 convertToDiscussion로 DOM 변환
-  // render에 넣어서 렌더링 되게 하기
 
   const newObj = {
-    id: "new id",
-    createdAt: new Date(),
-    title: title,
+    id: "unique id",
+    createdAt: new Date().toISOString(),
+    title: title.value,
     url: "https://github.com/codestates-seb/agora-states-fe/discussions",
-    author: author,
+    author: author.value,
     answer: null,
-    bodyHTML: textbox,
-    // 랜덤 이미지 출력
+    bodyHTML: textbox.value,
     avatarUrl: "https://i.pravatar.cc/300",
   };
+  data.unshift(obj);
+
   // 로컬스토리지에 저장, getItem으로 불러와야함
-  localStorage.setItem("agoraLocalData", JSON.stringify(newObj));
+  localStorage.setItem("agoraStatesDiscussions", JSON.stringify(data));
 
   // submit 후 빈칸으로 리셋
   form.querySelector("div.form__input--name > input").value = "";
@@ -85,7 +94,8 @@ form.addEventListener("submit", (event) => {
   form.querySelector("div.form__textbox > textarea").value = "";
 });
 
-// agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
+// agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수
+// 로컬, 페이지네이션 빼고
 const render = (element) => {
   // for (let i = 0; i < getLocalDates.length; i++) {
   //   element.append(convertToDiscussion(getLocalDates));
@@ -95,10 +105,29 @@ const render = (element) => {
   }
   return;
 };
-
-// ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
-const ul = document.querySelector("ul.discussions__container");
-render(ul);
+// 로컬스토리지 까지
+// const render = (element) => {
+//   for (let i = 0; i < data.length; i += 1) {
+//     element.append(convertToDiscussion(data[i]));
+//   }
+//   return;
+// };
+// from to 는 페이지네이션때 씀
+// const render = (element, from, to) => {
+//   // console.log(from, to);
+//   if (!from && !to) {
+//     from = 0;
+//     to = data.length - 1;
+//   }
+//   // 다 지우고 배열에 있는 내용 다 보여주기
+//   while (element.firstChild) {
+//     element.removeChild(element.firstChild);
+//   }
+//   for (i = from; i < to; i += 1) {
+//     element.append(convertToDiscussion(data[i]));
+//   }
+//   return;
+// };
 
 // 시계
 const clock = document.getElementById("clock");
@@ -151,3 +180,15 @@ toTopEl.addEventListener("click", function () {
     scrollTo: 0, // 회면의 위치를 0으로
   });
 });
+
+// ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
+// const ul = document.querySelector("ul.discussions__container");
+// render(ul);
+fetch("http://localhost:4000/discussions")
+  .then((response) => response.json())
+  .then((json) => {
+    console.log(json);
+    agoraStatesDiscussions = json;
+    const ul = document.querySelector("ul.discussions__container");
+    render(ul);
+  });
