bd92ec3e5a58840d656080e0e21e7e476a6c9bd8
pchan191 <pchan191@gmail.com>
2022-10-18 16:14:54 +0900

    fetch 설정

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // index.html을 열어서 agoraStatesDiscussions 배열 요소를 확인하세요.
 // console.log(agoraStatesDiscussions);
-
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
 const convertToDiscussion = (obj) => {
   const li = document.createElement("li"); // li 요소 생성
@@ -88,9 +87,14 @@ form.addEventListener('submit', (e) => {
     text.value = "";
   });
 
+fetch('http://localhost:4000/discussions')
+.then(res => res.json())
+.then(json => {
+  let agoraStatesDiscussions = json;
+  // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
+  const ul = document.querySelector("ul.discussions__container");
+  render(ul);
+})
 
-// ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
-const ul = document.querySelector("ul.discussions__container");
-render(ul);
 
 
