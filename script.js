2be6e68b6d6c93231f8843fef27cd0d6fa02daae
operat04 <operat04@naver.com>
2022-10-18 17:00:48 +0900

    server연동

--- script.js
+++ script.js
@@ -54,8 +54,13 @@ const render = (element) => {
 
 
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
-const ul = document.querySelector("ul.discussions__container");
-render(ul);
+fetch('http://localhost:4000/discussions')
+.then(res=>res.json())
+.then(json=>{
+  agoraStatesDiscussions = json;
+  const ul = document.querySelector("ul.discussions__container");
+  render(ul);
+})
 
 const Inputname = document.querySelector('.form__input--name > input')
 const Inputtitle = document.querySelector('.form__input--title > input')
