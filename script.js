98aaa28fd2b7dccc5840c33f883883773d25b597
12Ahn22 <12ayo22@gmail.com>
2022-11-14 15:22:14 +0900

    [feat] 디스커션 추가기능 구현

--- script.js
+++ script.js
@@ -18,7 +18,8 @@ const convertToDiscussion = (obj) => {
   const $avatarImg = document.createElement("img");
   $avatarImg.className = "discussion__avatar--image"; // img 태그
   $avatarImg.setAttribute("alt", `avatar of ${obj.author || ""}`); // alt 속성
-  $avatarImg.src = obj.avatarUrl;
+  $avatarImg.src =
+    obj.avatarUrl || "https://github.com/identicons/jasonlong.png";
   avatarWrapper.appendChild($avatarImg);
 
   // discussionContent
@@ -54,3 +55,32 @@ const render = (element) => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+// 질문 작성하는 로직
+const $form = document.getElementById("form");
+const $formName = document.getElementById("name");
+const $formTitle = document.getElementById("title");
+
+// submit 이벤트 캐치
+$form.addEventListener("submit", (e) => {
+  // 기존 submit 기능 중지시키기
+  e.preventDefault();
+
+  // 배열에 입력한 데이터를 추가하기
+  const newDiscussion = {
+    id: $formName.value,
+    createdAt: new Date().toLocaleString(),
+    title: $formTitle.value,
+    url: "",
+    author: $formName.value,
+    answer: null,
+    bodyHTML: "",
+    avatarUrl: "",
+  };
+  // 배열 가장 앞에 넣어주기
+  agoraStatesDiscussions.unshift(newDiscussion);
+  console.log("배열 추가 확인", agoraStatesDiscussions);
+  // 하나만 렌더링 해주기
+  const li = convertToDiscussion(newDiscussion);
+  ul.prepend(li); // 앞에 추가할 때는 prepend를 써준다.
+});
