2aafbc966ec360d0454d76ac8fe0991b4c7ee56c
qwerty00ui88 <qwerty0.0ui88@gmail.com>
2022-11-14 17:09:17 +0900

    Bare Minimum Requirement 완료

--- script.js
+++ script.js
@@ -3,21 +3,51 @@ console.log(agoraStatesDiscussions);
 
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
 const convertToDiscussion = (obj) => {
-  const li = document.createElement("li"); // li 요소 생성
-  li.className = "discussion__container"; // 클래스 이름 지정
+  const li = document.createElement('li'); // li 요소 생성
+  li.className = 'discussion__container'; // 클래스 이름 지정
 
-  const avatarWrapper = document.createElement("div");
-  avatarWrapper.className = "discussion__avatar--wrapper";
-  const discussionContent = document.createElement("div");
-  discussionContent.className = "discussion__content";
-  const discussionAnswered = document.createElement("div");
-  discussionAnswered.className = "discussion__answered";
+  const avatarWrapper = document.createElement('div');
+  avatarWrapper.className = 'discussion__avatar--wrapper';
+  const discussionContent = document.createElement('div');
+  discussionContent.className = 'discussion__content';
+  const discussionAnswered = document.createElement('div');
+  discussionAnswered.className = 'discussion__answered';
+  li.append(avatarWrapper, discussionContent, discussionAnswered);
+
+  //image
+  const avatarImg = document.createElement('img');
+  avatarImg.className = 'discussion__avatar--image';
+  avatarImg.src = obj.avatarUrl;
+  avatarImg.alt = 'avatar of ' + obj.author;
+  avatarWrapper.append(avatarImg);
+
+  //discussion__content
+  const discussionTitle = document.createElement('h2');
+  discussionTitle.className = 'discussion__title';
+  discussionContent.append(discussionTitle);
+
+  const discussionTitleLink = document.createElement('a');
+  discussionTitleLink.href = obj.url;
+  discussionTitleLink.textContent = obj.title;
+  discussionTitle.append(discussionTitleLink);
+
+  const discussionInformation = document.createElement('div');
+  discussionInformation.className = 'discussion__information';
+  discussionInformation.textContent = obj.id + ' / ' + obj.createdAt;
+  discussionContent.append(discussionInformation);
+
+  const discussionAnsweredCheck = document.createElement('p');
+  if (obj.answer === null) {
+    discussionAnsweredCheck.textContent = '☒';
+  } else {
+    discussionAnsweredCheck.textContent = '☑';
+  }
+
+  discussionAnswered.append(discussionAnsweredCheck);
 
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
+  // body.append(li);
 
-
-
-  li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
 };
 
@@ -30,5 +60,5 @@ const render = (element) => {
 };
 
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
-const ul = document.querySelector("ul.discussions__container");
+const ul = document.querySelector('ul.discussions__container');
 render(ul);
