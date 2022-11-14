a9604e5f32874ed2211dd39227efca5e55414d05
12Ahn22 <12ayo22@gmail.com>
2022-11-14 20:59:51 +0900

    [feat] 페이지네이션 숫자 이동 기능 추가

--- script.js
+++ script.js
@@ -71,8 +71,11 @@ const totalPage = Math.ceil(agoraStatesDiscussions.length / PAGE_ITEM_NUMBERS); 
  * @param maxShowPage 한 번에 보여줄 페이지네이션 개수
  */
 const renderPagination = (totalPage, maxShowPage) => {
-  const $discussion__pagination = document.querySelector(".pagination__btns");
+  const $discussionPagination = document.querySelector(".pagination__btns");
+  $discussionPagination.textContent = "";
   const $pageFragment = document.createDocumentFragment(); // fragment로 한번만 DOM 변경하기
+
+  // 첫 렌더링
   // 총 페이지가 한 번에 보여줄 페이지 개수보다 작거나 같은 경우
   if (totalPage <= maxShowPage) {
     for (let i = 1; i <= totalPage; i++) {
@@ -83,23 +86,54 @@ const renderPagination = (totalPage, maxShowPage) => {
     }
   } else {
     // 총 페이지가 한 번에 보여줄 페이지 개수보다 많은 경우
-    for (let i = 1; i <= maxShowPage; i++) {
+    // 만약 현재 페이지가 중간 이상인 경우 앞에 prevTrack 추가..
+    if (page >= Math.ceil(maxShowPage / 2)) {
+      const $firstPage = document.createElement("button");
+      $firstPage.className = "pagination__btn";
+      $firstPage.textContent = "1";
+      const $pagePrevTrack = document.createElement("span");
+      $pagePrevTrack.textContent = "...";
+      $pageFragment.appendChild($firstPage);
+      $pageFragment.appendChild($pagePrevTrack);
+    }
+    for (
+      let i = parseInt(page) === 1 ? 1 : parseInt(page) - 1;
+      i < parseInt(page) + maxShowPage - 1 && i <= totalPage;
+      i++
+    ) {
       const $pageBtn = document.createElement("button");
-      $pageBtn.className = `pagination__btn ${i === 1 ? "active" : ""}`; // i===1이면 active 클래스 추가
+      $pageBtn.className = `pagination__btn ${
+        parseInt(page) === i ? "active" : ""
+      }`; // page이면 active 클래스 추가
       $pageBtn.textContent = i;
       $pageFragment.appendChild($pageBtn);
     }
     // ...와 마지막 페이지 숫자 붙여주기
     const $pageNextTrack = document.createElement("span");
     $pageNextTrack.textContent = "...";
+    $pageNextTrack.className = "pagination__next-track";
     const $lastPage = document.createElement("button");
     $lastPage.textContent = totalPage;
     $lastPage.className = "pagination__btn pagination__last";
     $pageFragment.append($pageNextTrack, $lastPage);
   }
 
+  // 만약 자식 중의 contenteText 중에 total이 있다면 마지막 제거
+  for (const pageBtn of [...$pageFragment.children]) {
+    if (
+      parseInt(pageBtn.textContent) === totalPage &&
+      !pageBtn.classList.contains("pagination__last")
+    ) {
+      console.log("pageBtn", pageBtn);
+      const nextTrack = $pageFragment.querySelector(".pagination__next-track");
+      const lastPage = $pageFragment.querySelector(".pagination__last");
+      $pageFragment.removeChild(nextTrack);
+      $pageFragment.removeChild(lastPage);
+    }
+  }
+
   // DOM에 붙이기
-  $discussion__pagination.appendChild($pageFragment);
+  $discussionPagination.appendChild($pageFragment);
 };
 
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
@@ -107,7 +141,7 @@ const ul = document.querySelector("ul.discussions__container");
 render(ul, 0, PAGE_ITEM_NUMBERS); // 초기 화면 렌더링
 
 // 페이지네이션 렌더링 하기
-renderPagination(totalPage, PAGE_ITEM_NUMBERS);
+renderPagination(totalPage, 5);
 
 // 질문 작성하는 로직
 const $form = document.getElementById("form");
@@ -168,6 +202,9 @@ const changePage = (e) => {
     parseInt(page - 1) * PAGE_ITEM_NUMBERS,
     parseInt(page - 1) * PAGE_ITEM_NUMBERS + parseInt(PAGE_ITEM_NUMBERS)
   ); // 초기 화면 렌더링
+
+  // 페이지네이션 다시 렌더링
+  renderPagination(totalPage, 5);
 };
 $pagination.addEventListener("click", changePage);
 
@@ -205,3 +242,7 @@ $paginationNextBtn.addEventListener("click", () => {
     }
   }
 });
+
+// 버튼을 이동할 때,
+// 만약 총 페이지가 5보다 큰 경우
+// 3페이지(5/2) 부터 맨 앞은 무조곤 1번 페이지와 prev-stack를 추가해준다.
