751d6fb2d8ec34c28ac050c437a98be2a86d5050
kimdonggu42 <kdonggoo44@gmail.com>
2022-11-14 16:23:57 +0900

    avatar img, title, information 부분 스크립트 추가

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// index.html을 열어서 agoraStatesDiscussions 배열 요소를 확인하세요.
-console.log(agoraStatesDiscussions);
-
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
 const convertToDiscussion = (obj) => {
   const li = document.createElement("li"); // li 요소 생성
@@ -8,12 +5,51 @@ const convertToDiscussion = (obj) => {
 
   const avatarWrapper = document.createElement("div");
   avatarWrapper.className = "discussion__avatar--wrapper";
+
   const discussionContent = document.createElement("div");
   discussionContent.className = "discussion__content";
+
   const discussionAnswered = document.createElement("div");
   discussionAnswered.className = "discussion__answered";
 
-  // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
+// <img> 태그 생성 후 avatarImg 변수에 할당
+  const avatarImg = document.createElement('img');
+// 이미지 주소 설정
+  avatarImg.src = obj.avatarUrl;
+// 대체 이미지 설정
+  avatarImg.alt = 'avatar of ' + obj.author;
+// <div class = discussion__avatar--wrapper> 태그 마지막에 <img> 태그 추가
+  avatarWrapper.append(avatarImg);
+
+// <h2> 태그 생성 후 discussionTitle 변수에 할당
+  const discussionTitle = document.createElement("h2");
+// <h2 class = "discussion__title">로 클래스명 변경
+  discussionTitle.className = "discussion__title";
+// <a> 태그 생성 후 discussionTitleLink 변수에 할당
+  const discussionTitleLink = document.createElement('a');
+// title에 url연결
+  discussionTitleLink.href = obj.url;
+// title에 텍스트 추가
+  discussionTitleLink.textContent = obj.title;
+// <div class = discussion__title> 태그 마지막에 <a href> 태그 추가
+  discussionTitle.append(discussionTitleLink);
+// <div class = discussion__content> 태그 마지막에 <h2 class = "discussion__title"> 태그 추가
+  discussionContent.append(discussionTitle);
+
+// <div> 태그 생성 후 discussionInfo 변수에 할당
+  const discussionInfo = document.createElement("div");
+// <div class = "discussion__information">로 클래스명 변경
+  discussionInfo.className = "discussion__information";
+// div에 author, createdAt value값 텍스트로 추가
+  discussionInfo.textContent = `${obj.author} / ${obj.createdAt}`;
+// <div class = discussion__content> 태그 마지막에 <div class = "discussion__information"> 태그 추가
+  discussionContent.append(discussionInfo);
+
+
+
+
+
+
 
 
 
