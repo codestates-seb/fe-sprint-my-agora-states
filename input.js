6f4b8df0faaa5a3cfb12b0cccdde384e65156a20
plast <notplastic2@gmail.com>
2022-11-15 01:01:20 +0900

    새로고침으로 페이징 문제 해결

--- input.js
+++ input.js
@@ -23,7 +23,8 @@ if(saveDiscussions !== null){
 // submit 클릭시 폼에 입력된 내용을 변수에 저장하고 폼 비우기
 // prepend로 맨 앞에 출력하기
 function handleSubmit(event){
-  event.preventDefault(); // submit 클릭시 페이지 리로드 방지
+  // submit과 동시에 페이징 적용하기 위해 그냥 리로드 적용함
+ // event.preventDefault(); // submit 클릭시 페이지 리로드 방지
   const newDiscussion = {};
   newDiscussion.title = inputTitle.value;
   newDiscussion.author = inputName.value;
