14cad8e19fc94857ba02ec1c72f0c9942e6cbe9e
es39 <esj10025@gmail.com>
2022-11-14 20:51:18 +0900

    title link modify

--- script.js
+++ script.js
@@ -16,21 +16,30 @@ const convertToDiscussion = (obj) => {
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
   // 아바타, 제목, 컨텐트(타이틀, 정보), 체크
 
+  // 1. 이미지
   const avatarImg = document.createElement('img');
   avatarImg.src = obj.avatarUrl;
   avatarImg.alt = 'avatar of ' + obj.author;
   avatarWrapper.append(avatarImg);
 
-  const discussionTitle = document.createElement('a');
-  //const discussTitle = document.createElement('h2');
-  discussionTitle.textContent = obj.title;
-  discussionTitle.href = obj.url;
-  discussionContent.append(discussionTitle);
+  // 2. 제목 
+  // 2-1. 제목 <- 링크 붙이기
+  const discussionTitlelink = document.createElement('a');
+  discussionTitlelink.textContent = obj.title;
+  discussionTitlelink.href = obj.url;
+  
+  const discussTitle = document.createElement('h2');
+  discussTitle.className = "discussion__title";
+  
+  discussionContent.append(discussTitle);
+  discussTitle.append(discussionTitlelink);
 
+  // 3. 작성자 및 날짜
   const discussionCreateAt = document.createElement('div');
   discussionCreateAt.textContent = `${obj.author} / ${obj.createdAt}`;
   discussionContent.append(discussionCreateAt);
 
+  // 4. 답변 여부
   const discussionAnswer = document.createElement('p');
   discussionAnswer.textContent = obj.answer ? '☑' : 'x' ;
   discussionContent.append(discussionAnswer);
@@ -51,3 +60,5 @@ const render = (element) => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+
