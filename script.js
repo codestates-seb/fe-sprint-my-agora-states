6fed55123318b3110420d4a7334193ba0e1d5f7d
myungju <myungju030@gmail.com>
2022-11-14 18:18:01 +0900

    add: 아고라 스테이츠 질문,답변리스트 화면에 보여주기  구현

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ const convertToDiscussion = (obj) => {
   const li = document.createElement("li"); // li 요소 생성
   li.className = "discussion__container"; // 클래스 이름 지정
 
+  const discussionWrapper = document.createElement("div");
+  discussionWrapper.className = "discussion__container--wrapper";
+
   const avatarWrapper = document.createElement("div");
   avatarWrapper.className = "discussion__avatar--wrapper";
   const discussionContent = document.createElement("div");
@@ -13,11 +16,70 @@ const convertToDiscussion = (obj) => {
   const discussionAnswered = document.createElement("div");
   discussionAnswered.className = "discussion__answered";
 
-  // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
+
+  const avatarImg = document.createElement('img');
+  avatarImg.src = obj.avatarUrl;
+  avatarImg.alt = 'avatar of ' + obj.author;
+  avatarWrapper.append(avatarImg);
+
+  const contentTitle = document.createElement('h2');
+  contentTitle.className = "discussion__title";
+  const contentTitleUrl =document.createElement('a');
+  contentTitleUrl.href =obj.url;
+  contentTitleUrl.textContent = obj.title;
+  contentTitleUrl.target ='_blank';
+  contentTitle.appendChild(contentTitleUrl);
+  
+  const contentInformation = document.createElement('div');
+  contentInformation.className = 'discussion__information';
+  contentInformation.textContent = obj.createdAt;
+  discussionContent.append(contentTitle, contentInformation);
+
+  // 답변
+  const answerCheckbox =  document.createElement('input');
+  answerCheckbox.type = 'checkbox';
+  
+  const answerWrapper = document.createElement("div"); 
+  answerWrapper.className = "answer__container--wrapper"; 
 
 
+  const answerAvatarWrapper = document.createElement("div");
+  answerAvatarWrapper.className = "answer__avatar--wrapper";
+  const answerContent = document.createElement("div");
+  answerContent.className = "answer__content";
+  const answeranswered = document.createElement("div");
+  answeranswered.className = "answer__answered";
 
-  li.append(avatarWrapper, discussionContent, discussionAnswered);
+  if(obj.answer){
+    answerCheckbox.checked = true;
+
+    const answerAvatarImg = document.createElement('img');
+    answerAvatarImg.src = obj.answer.avatarUrl;
+    answerAvatarImg.alt = 'avatar of ' + obj.answer.author;
+    answerAvatarWrapper.append(answerAvatarImg);
+
+
+    const answercontentTitle = document.createElement('h2');
+    answercontentTitle.className = "answer__title";
+    const answercontentTitleUrl =document.createElement('a');
+    answercontentTitleUrl.href =obj.answer.url;
+    answercontentTitleUrl.textContent = obj.answer.author + '님의 답변보기';
+    answercontentTitleUrl.target ='_blank';
+    answercontentTitle.appendChild(answercontentTitleUrl);
+    
+    const answercontentInformation = document.createElement('div');
+    answercontentInformation.className = 'answer__information';
+    answercontentInformation.textContent = obj.answer.createdAt;
+    answerContent.append(answercontentTitle, answercontentInformation);
+
+  }
+  else{answerCheckbox.checked  = false;}
+
+  discussionAnswered.append(answerCheckbox);
+  discussionWrapper.append(avatarWrapper, discussionContent, discussionAnswered);
+  answerWrapper.append(answerAvatarWrapper,answerContent, answeranswered);
+  li.append(discussionWrapper,answerWrapper );
+
   return li;
 };
 
