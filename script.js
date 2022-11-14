0ba5bacdde655913eb930eb187acc3796d7bb07a
12Ahn22 <12ayo22@gmail.com>
2022-11-14 17:27:03 +0900

    [feat] 페이지네이션을 렌더링하는 함수 완성

--- script.js
+++ script.js
@@ -56,9 +56,56 @@ const render = (element) => {
   return;
 };
 
+/* 
+  페이지네이션 처리
+*/
+let page = 1; // 현재 페이지 인덱스
+const PAGE_ITEM_NUMBERS = 5; // 한번에 보여줄 개수
+const totalPage = Math.ceil(agoraStatesDiscussions.length / PAGE_ITEM_NUMBERS); // 총 페이지 숫자
+
+/**
+ * 첫 페이지네이션을 렌더링하는 함수
+ * @param totalPage 총 페이지 개수
+ * @param maxShowPage 한 번에 보여줄 페이지네이션 개수
+ */
+const renderPagination = (totalPage, maxShowPage) => {
+  const $discussion__pagination = document.querySelector(".pagination__btns");
+  const $pageFragment = document.createDocumentFragment(); // fragment로 한번만 DOM 변경하기
+  // 총 페이지가 한 번에 보여줄 페이지 개수보다 작거나 같은 경우
+  if (totalPage <= maxShowPage) {
+    for (let i = 1; i <= totalPage; i++) {
+      const $pageBtn = document.createElement("button");
+      $pageBtn.className = `pagination__btn ${i === 1 ? "active" : ""}`; // i===1이면 active 클래스 추가
+      $pageBtn.textContent = i;
+      $pageFragment.appendChild($pageBtn);
+    }
+  } else {
+    // 총 페이지가 한 번에 보여줄 페이지 개수보다 많은 경우
+    for (let i = 1; i <= maxShowPage; i++) {
+      const $pageBtn = document.createElement("button");
+      $pageBtn.className = `pagination__btn ${i === 1 ? "active" : ""}`; // i===1이면 active 클래스 추가
+      $pageBtn.textContent = i;
+      $pageFragment.appendChild($pageBtn);
+    }
+    // ...와 마지막 페이지 숫자 붙여주기
+    const $pageNextTrack = document.createElement("span");
+    $pageNextTrack.textContent = "...";
+    const $lastPage = document.createElement("button");
+    $lastPage.textContent = totalPage;
+    $lastPage.className = "pagination__btn pagination__last";
+    $pageFragment.append($pageNextTrack, $lastPage);
+  }
+
+  // DOM에 붙이기
+  $discussion__pagination.appendChild($pageFragment);
+};
+
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+// 페이지네이션 렌더링 하기
+renderPagination(totalPage, PAGE_ITEM_NUMBERS);
 
 // 질문 작성하는 로직
 const $form = document.getElementById("form");
