82ce40b617eaff8061b164b9b4913392cdbfad35
kimdonggu42 <kdonggoo44@gmail.com>
2022-11-14 18:05:36 +0900

    newDiscussion 기능 추가

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ const convertToDiscussion = (obj) => {
   avatarImg.src = obj.avatarUrl;
 // 대체 이미지 설정
   avatarImg.alt = "avatar of " + obj.author;
-// <div class = discussion__avatar--wrapper> 태그 마지막에 <img> 태그 추가
+// <div class = discussion__avatar--wrapper> 태그 마지막 자식 요소에 <img> 태그 추가
   avatarWrapper.append(avatarImg);
 
 // <h2> 태그 생성 후 discussionTitle 변수에 할당
@@ -31,9 +31,9 @@ const convertToDiscussion = (obj) => {
   discussionTitleLink.href = obj.url;
 // title에 텍스트 추가
   discussionTitleLink.textContent = obj.title;
-// <div class = discussion__title> 태그 마지막에 <a href> 태그 추가
+// <div class = discussion__title> 태그 마지막 자식 요소에 <a href> 태그 추가
   discussionTitle.append(discussionTitleLink);
-// <div class = discussion__content> 태그 마지막에 <h2 class = "discussion__title"> 태그 추가
+// <div class = discussion__content> 태그 마지막 자식 요소에 <h2 class = "discussion__title"> 태그 추가
   discussionContent.append(discussionTitle);
 
 // <div> 태그 생성 후 discussionInfo 변수에 할당
@@ -42,28 +42,58 @@ const convertToDiscussion = (obj) => {
   discussionInfo.className = "discussion__information";
 // div에 author, createdAt value값 텍스트로 추가
   discussionInfo.textContent = `${obj.author} / ${obj.createdAt}`;
-// <div class = discussion__content> 태그 마지막에 <div class = "discussion__information"> 태그 추가
+// <div class = discussion__content> 태그 마지막 자식 요소에 <div class = "discussion__information"> 태그 추가
   discussionContent.append(discussionInfo);
 
-// 답글체크
+// <p> 태그 생성
 const discussionAnsweredCheck = document.createElement("p");
-
+// obj.answer가 null이 아니면 체크표시된 아이콘을 넣고, null이면 체크가 안 된 빈 아이콘을 넣음
   if (obj.answer !== null) {
     discussionAnsweredCheck.innerHTML = '<i class="fa-regular fa-square-check"></i>';
   } else {
     discussionAnsweredCheck.innerHTML = '<i class="fa-regular fa-square"></i>';
   }
-
+// <p> 태그 마지막 자식 요소에 아이콘을 넣음
   discussionAnswered.append(discussionAnsweredCheck);
-
-  
-
-  
-
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
 };
+
+// submit 클릭 시 새로운 discussion 등록 기능 구현
+const form = document.querySelector('.form');
+const formName = document.querySelector('#name');
+const formTitle = document.querySelector('#title');
+const formStory = document.querySelector('#story');
+
+const addDiscussion = function(event) {
+  event.preventDefault();
+  const newObj = {
+    id: Math.random().toString(36).substring(2, 12),
+    createdAt: new Date(),
+    title: formTitle.value,
+    url: "#",
+    author: formName.value,
+    answer: null,
+    bodyHtml: formStory.value,
+    avatarUrl: null,
+  };
+
+  agoraStatesDiscussions.unshift(newObj);
+
+  const newDiscussion = convertToDiscussion(newObj);
+  ul.prepend(newDiscussion);
+
+  formName.value = '';
+  formTitle.value = '';
+  formStory.value = '';
+};
+
+form.addEventListener('submit',addDiscussion);
+
+
+
+
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
