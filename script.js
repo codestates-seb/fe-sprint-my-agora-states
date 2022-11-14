a80f9f798cab35395994c292a72ba851eab02972
BRWEB <52031484+rockbell89@users.noreply.github.com>
2022-11-14 18:48:58 +0900

    아고라스테이츠 CSS 및 입력폼 데이터 추가 함수 작성

--- script.js
+++ script.js
@@ -14,8 +14,28 @@ const convertToDiscussion = (obj) => {
   discussionAnswered.className = "discussion__answered";
 
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
+  const image = document.createElement("img");
+  image.className = "discussion__avatar--image";
+  image.setAttribute(
+    "src",
+    obj.avatarUrl
+      ? obj.avatarUrl
+      : "https://cdn-icons-png.flaticon.com/512/25/25231.png"
+  );
+  image.setAttribute("alt", `avatar of ${obj.author}`);
 
+  avatarWrapper.appendChild(image);
 
+  const title = document.createElement("h2");
+  title.className = "discussion__title";
+  const url = document.createElement("a");
+  url.setAttribute("href", obj.url);
+  url.textContent = obj.title;
+  title.appendChild(url);
+  const info = document.createElement("div");
+  info.classNamm = "discussion__information";
+  info.textContent = `${obj.author} / ${getDate(obj.createdAt)}`;
+  const content = discussionContent.append(title, info);
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
@@ -32,3 +52,26 @@ const render = (element) => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+// TODO : 폼 입력하여 리스트에 추가
+const form = document.querySelector(".form");
+const inputTitle = document.getElementById("title");
+const inputName = document.getElementById("name");
+const inputContent = document.getElementById("story");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const newStory = {};
+  newStory.title = inputTitle.value;
+  newStory.author = inputName.value;
+  newStory.content = inputContent.value;
+  newStory.createdAt = new Date().toISOString();
+  ul.prepend(convertToDiscussion(newStory));
+});
+
+// TODO : 현재 시간 표시
+function getDate(date) {
+  const d = new Date(date).toLocaleString();
+  const convertDate = d.split(". ");
+  return convertDate[3];
+}
