9814d3b1c8f6d94844c722ec0b0333cd35eab103
Mia-Oh <songmiaoh@gmail.com>
2022-11-14 11:18:31 +0900

    디스커션 나열 기능 구현

--- script.js
+++ script.js
@@ -2,20 +2,57 @@
 console.log(agoraStatesDiscussions);
 
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
+
 const convertToDiscussion = (obj) => {
   const li = document.createElement("li"); // li 요소 생성
   li.className = "discussion__container"; // 클래스 이름 지정
 
-  const avatarWrapper = document.createElement("div");
-  avatarWrapper.className = "discussion__avatar--wrapper";
-  const discussionContent = document.createElement("div");
-  discussionContent.className = "discussion__content";
-  const discussionAnswered = document.createElement("div");
-  discussionAnswered.className = "discussion__answered";
+  // const avatarWrapper = document.createElement("div");
+  // avatarWrapper.className = "discussion__avatar--wrapper"; //<div class="discussion__avatar--wrapper"></div>
+
+  // const discussionContent = document.createElement("div");
+  // discussionContent.className = "discussion__content"; //<div class="discussion__content"></div>
+  
+  // const discussionAnswered = document.createElement("div");
+  // discussionAnswered.className = "discussion__answered"; //<div class="discussion__answered"></div>
 
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
 
+  const avatarWrapper = document.createElement("div");
+  avatarWrapper.className = "discussion__avatar--wrapper"; //<div class="discussion__avatar--wrapper"></div>
 
+  const avatarImg = document.createElement("img");
+  avatarImg.className = "discussion__avatar--image";
+  avatarImg.src = obj.avatarUrl;
+  avatarImg.alt = 'avatar of' + obj.author;
+  avatarWrapper.append(avatarImg);
+
+  
+  const discussionContent = document.createElement("div");
+  discussionContent.className = "discussion__content"; //<div class="discussion__content"></div>
+
+  const discussionTitle = document.createElement("h2");
+  discussionTitle.className = "discussion__title"; //<h2 class="discussion__title"></h2>
+
+
+  const discussionAnchor = document.createElement("a");
+  discussionAnchor.href = obj.url;
+  discussionAnchor.textContent = obj.title;
+  discussionTitle.append(discussionAnchor); //<h2 class="discussion__title"><a></a></h2>
+
+  const discussionInformation = document.createElement("div");
+  discussionInformation.className = "discussion__information"; //<div class="discussion__information"></div>
+  discussionInformation.textContent = obj.author + ' / ' +obj.createdAt;
+  
+  discussionContent.append(discussionTitle, discussionInformation);
+
+
+  const discussionAnswered = document.createElement("div");
+  discussionAnswered.className = "discussion__answered"; //<div class="discussion__answered"></div>
+  
+  const discussionAnsweredP =  document.createElement("p");
+  discussionAnsweredP.textContent = obj.answer ? "☑︎" : "☒";
+  discussionAnswered.append(discussionAnsweredP);
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
@@ -32,3 +69,42 @@ const render = (element) => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+
+// TO DO :  아이디, 본문을 입력하고 버튼을 누르면 실제 화면에 디스커션이 추가되어야 합니다.
+
+//0. submit이 클릭되었을 때
+//1. form에서 입력 받은 데이터를 가져온다
+//2. 가져온 데이터를 배열에 앞쪽에 넣어 준다
+//3. 그 배열을 뿌려주면 입력 받은 디스커션이 추가 되지 않을까?
+
+const form = document.querySelector(".form");
+
+form.addEventListener("submit", function(event){
+
+  //submit을 사용해서 새로고침이 되기 때문에 꼭 필수!
+  event.preventDefault();
+
+  const form = document.querySelector(".form");
+
+  const formAuthor = form.querySelector('.form__input--name > input').value;
+  const formTitle = form.querySelector('.form__input--title > input').value;
+  const formTextarea = form.querySelector('.form__textbox > textarea').value;
+
+  const addDiscussion = {
+    id: "new id",
+    createdAt: new Date().toISOString(),
+    title: formTitle,
+    url: "https://github.com/codestates-seb/agora-states-fe/discussions",
+    author: formAuthor,
+    bodyHTML: formTextarea,
+    avatarUrl: "https://avatars.githubusercontent.com/u/97888923?s=64&u=12b18768cdeebcf358b70051283a3ef57be6a20f&v=4",
+
+  }
+
+  agoraStatesDiscussions.unshift(addDiscussion);
+  console.log(agoraStatesDiscussions); 
+  
+  
+
+});
