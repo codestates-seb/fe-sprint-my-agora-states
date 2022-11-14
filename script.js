f354dbad50f1d3d73d46b02dc8a0491d1d049c5d
dding-v <v.dding.v@gmail.com>
2022-11-14 21:16:07 +0900

    페이지네이션 전

--- script.js
+++ script.js
@@ -15,6 +15,28 @@ const convertToDiscussion = (obj) => {
 
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
 
+  const avatarImg = document.createElement('img');
+  avatarImg.src = obj.avatarUrl;
+  avatarImg.alt = 'avatar of ' + obj.author;
+  avatarWrapper.append(avatarImg)
+
+  const discussionTitle = document.createElement('h2');
+  const titleAnchor = document.createElement('a')
+  titleAnchor.href = obj.url;
+  titleAnchor.textContent = obj.title;
+  discussionTitle.append(titleAnchor)
+  discussionContent.append(discussionTitle)
+
+  const discussionInfo = document.createElement('div');
+  discussionInfo.className = "discussion__information";
+  
+  discussionInfo.textContent = `${obj.author} / ${new Date(obj.createdAt).toLocaleTimeString()}`
+  discussionContent.append(discussionInfo)
+
+
+  const checked = document.createElement('p')
+  checked.textContent = obj.answer ? "☑︎" : "◻︎";
+  discussionAnswered.append(checked)
 
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
@@ -32,3 +54,44 @@ const render = (element) => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+
+  // 디스커션 추가 구현
+
+
+// 문서 내용 가져 오기
+
+const form = document.querySelector("form.form")
+
+form.addEventListener ("submit",
+  (event) => {
+    event.preventDefault();
+
+    const author = form.querySelector("div.form__input--name > input").value
+    const title = form.querySelector("div.form__input--title > input").value
+    const textbox = form.querySelector("div.form__textbox > textarea").value
+
+    const newObj = {
+      id: "new id",
+      createdAt: new Date().toISOString(),
+      title: title,
+      url: "https://github.com/codestates-seb/agora-states-fe/discussions",
+      author: author,
+      bodyHTML: textbox,
+      avatarUrl: "https://avatars.githubusercontent.com/u/61141988?s=64&u=92c71910d9f6409d38d40d7d5a0a094d8ec647ed&v=4",
+    }
+
+    agoraStatesDiscussions.unshift(newObj);
+
+    const discussion = convertToDiscussion(newObj)
+
+    ul.prepend(discussion)
+
+    form.querySelector("div.form__input--name > input").value
+    form.querySelector("div.form__input--title > input").value
+    form.querySelector("div.form__textbox > textarea").value
+
+  }
+
+)
+
