a5f4eec501994926ae6a4cd35038214fb54effd7
jellyjw <jiujang356@gmail.com>
2022-11-14 19:44:03 +0900

    feat/전체 질문 렌더링

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // index.html을 열어서 agoraStatesDiscussions 배열 요소를 확인하세요.
 console.log(agoraStatesDiscussions);
-
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
 const convertToDiscussion = (obj) => {
   const li = document.createElement("li"); // li 요소 생성
@@ -15,7 +14,38 @@ const convertToDiscussion = (obj) => {
 
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
 
+  // 이미지 삽입
+  const avatarImg = document.createElement("img");
+  avatarImg.src = obj.avatarUrl;
+  avatarImg.alt = `avatar of ${obj.author}`;
+  avatarImg.className = "discussion__avatar--image";
+  avatarWrapper.append(avatarImg);
 
+  // 질문 title
+  const mainTitle = document.createElement("h2");
+  const subTitle = document.createElement("a");
+  mainTitle.className = "discussion__title";
+  subTitle.href = obj.url;
+  subTitle.textContent = obj.title;
+  subTitle.className = "discussion__information";
+  mainTitle.append(subTitle);
+
+  // 날짜
+  const questionDate = document.createElement("div");
+  questionDate.className = "discussion__information";
+  questionDate.textContent = obj.createdAt;
+  discussionContent.append(mainTitle, questionDate);
+
+  //덥변 여부
+  const isChecked = document.createElement("p");
+  isChecked.textContent = "☑";
+  isChecked.className = "discussion__answered";
+  if (obj.answer !== null) {
+    isChecked.textContent = "☑︎";
+  } else {
+    isChecked.textContent = "□";
+  }
+  discussionAnswered.append(isChecked);
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
