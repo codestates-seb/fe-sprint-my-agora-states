e2ed4c46562f891a33c51757e23252c530ba9906
2realzoo <lunalee00@gmail.com>
2022-11-15 18:22:31 +0900

    LocalStorage 기능

--- script.js
+++ script.js
@@ -91,6 +91,7 @@ const render = (element) => {
 
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 agoraStatesDiscussions = JSON.parse(localStorage.getItem('json'));
+
 const ul = document.querySelector('ul.discussions__container');
 render(ul);
 
