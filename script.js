1d23fc502688ec45adae11d27c89467bbe67dad5
hongjungi94 <92861329+hongjungi94@users.noreply.github.com>
2022-11-15 15:17:48 +0900

    SEB_FE_42_홍준기 과제 제출합니다.

--- script.js
+++ script.js
@@ -14,12 +14,66 @@ const convertToDiscussion = (obj) => {
   discussionAnswered.className = "discussion__answered";
 
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
+  const avatarImg = document.createElement('img')
+  avatarImg.classList.add('discussion__avatar--image')
+  avatarImg.src = obj.avatarUrl
+  avatarImg.alt = "avatar of " + obj.author;
+  avatarWrapper.append(avatarImg)
 
+  const titleAncor = document.createElement('a')
+  titleAncor.classList.add('titleInfo')
+  titleAncor.textContent = obj.title;
+  titleAncor.href = obj.url
+  discussionContent.append(titleAncor)
 
+  const discussionInfo = document.createElement('div')
+  discussionInfo.textContent = "작성자 : " + obj.author + " / " + "작성시간 : " + obj.createdAt;
+  discussionContent.append(discussionInfo)
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
 };
+
+const form = document.querySelector("form.form");
+const author = form.querySelector("div.form__input--name > input");
+const title = form.querySelector("div.form__input--title > input");
+const textbox = form.querySelector("div.form__textbox > textarea");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault()
+
+  let now = new Date();
+  let nowYear = now.getFullYear(); // 년
+  let nowMonth = now.getMonth() + 1; // 월
+  let nowDate = now.getDate(); // 월
+  let nowHour = now.getHours(); // 시간
+  let nowMinutes = ("0" + now.getMinutes()).slice(-2); // 분
+
+  const obj = {
+      id: 'unique.id',
+      createdAt:
+      nowYear +
+      " - " +
+      nowMonth +
+      " - " +
+      nowDate +
+      " " +
+      nowHour +
+      " : " +
+      nowMinutes,
+      title: title.value,
+      url: "https://github.com/codestates-seb/agora-states-fe/discussions/45",
+      author: author.value,
+      answer: null,
+      bodyHTML: textbox.value,
+      avatarUrl:
+        "https://avatars.githubusercontent.com/u/97888923?s=64&u=12b18768cdeebcf358b70051283a3ef57be6a20f&v=4",
+    }
+
+  agoraStatesDiscussions.unshift(obj);
+  const newDiscussion = convertToDiscussion(obj)
+  ul.prepend(newDiscussion)
+})
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
