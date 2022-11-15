d92a45ae2dc3d7f625cda7a4da78149c4f653ca9
kimkisic <rlarltlr123@naver.com>
2022-11-15 16:42:52 +0900

    과제제출

--- script.js
+++ script.js
@@ -14,12 +14,55 @@ const convertToDiscussion = (obj) => {
   discussionAnswered.className = "discussion__answered";
 
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
+  const avatarImg = document.createElement("img");
+  avatarImg.setAttribute('class', 'discussion__avatar--image');
+  avatarImg.src = obj.avatarUrl;
+  avatarImg.alt = `avatar of ${obj.author}`;
+  avatarWrapper.append(avatarImg);
 
-
+  const discussionTitle = document.createElement("h2");
+  discussionTitle.setAttribute('class', 'discussion__title');
+  const title = document.createElement('a');
+  title.href = obj.url;
+  title.textContent = obj.title;
+  const info = document.createElement("div");
+  info.setAttribute('class', 'discussion__information');
+  info.textContent = `${obj.author} / ${new Date(obj.createdAt).toLocaleString()}`;
+  discussionTitle.append(title);
+  discussionContent.append(discussionTitle);
+  discussionContent.append(info);
+  const answer = document.createElement("p");
+  answer.textContent = obj.answer ? '☑' : '☒';
+  discussionAnswered.append(answer);
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
 };
+
+const form = document.querySelector('form.form');
+const inputName = document.getElementById('name');
+const inputTitle = document.getElementById('title');
+const inputQuestion = document.getElementById('story');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const obj = {
+    id: "random999",
+    createdAt: new Date(),
+    title: inputTitle.value,
+    url: "https://github.com/codestates-seb/agora-states-fe/discussions/45",
+    author: inputName.value,
+    bodyHTML: inputQuestion.value,
+    avatarUrl:
+      "https://avatars.githubusercontent.com/u/97888923?s=64&u=12b18768cdeebcf358b70051283a3ef57be6a20f&v=4",
+  }
+  agoraStatesDiscussions.unshift(obj);
+  console.log(obj);
+  console.log(inputName.value);
+  ul.prepend(convertToDiscussion(obj));
+  inputName.value = '';
+  inputTitle.value = '';
+  inputQuestion.value = '';
+})
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
