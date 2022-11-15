8bf928df03986e5cea75ce63752db7282eac7a7d
yeojuyeong <yjy8606@gmail.com>
2022-11-15 22:15:10 +0900

    아고라스테이츠 만들기

--- script.js
+++ script.js
@@ -14,12 +14,35 @@ const convertToDiscussion = (obj) => {
   discussionAnswered.className = "discussion__answered";
 
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
+  const face = document.createElement("img") // 프로필 사진
+  face.src = obj.avatarUrl;
+  face.alt = "avatar of" + "누가 썼는지";
+  avatarWrapper.append(face);
+  
 
+  const discussionTitle = document.createElement("h2");
+  const titleAnchor = document.createElement("a");
+  titleAnchor.href = obj.url;
+  titleAnchor.textContent = obj.title;
+  discussionTitle.append(titleAnchor);
+  // discussionContent.append(discussionTitle);
 
+  const discussionInfo = document.createElement("div");
+  discussionInfo.textContent = `${obj.author} / ${new Date(obj.createAt).toLocaleTimeString()}` // 날짜 표현 형식이 여러개가 있는데 이걸 제일 많이  쓴다
+  discussionContent.append(discussionTitle, discussionInfo);
+
+  const checked = document.createElement("p");
+  checked.textContent = obj.answer ? "☑︎" : "☒";
+  discussionAnswered.append(checked);
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
-  return li;
+  return li; // il요소를 위의 함수를 통해 가공해서 append
 };
+
+  // form 은 데이터를 깔끔하게 받기 위해서 받는것
+  // 
+
+
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
