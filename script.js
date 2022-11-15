3f490a7d1b9a715a634067d3cc5428252e411104
HooniKim <HooniKim@github.com>
2022-11-15 13:12:49 +0900

    submit해결 css레이아웃 해결

--- script.js
+++ script.js
@@ -34,16 +34,46 @@ const convertToDiscussion = (obj) => {
   // 사용자 정보
   const information = document.createElement('div')
   information.className = 'discussion__information'
-  information.textContent = `${obj.author} / ${obj.createdAt}` // 날짜 표현 형식이 여러개가 있는데 이걸 제일 많이  쓴다
+  information.textContent = `${obj.author} / ${new Date(obj.createdAt).toLocaleString()}` 
   discussionContent.append(information)
   // answer p 
   const checked = document.createElement('p')
+  checked.textContent = obj.answer ? "☑︎" : "☒";
   discussionAnswered.append(checked)
   
-
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
 };
+
+// 질문 입력창 
+const form = document.querySelector('form.form'); //form 요소이면서 class가 form인것을 불러냄
+const inputName = document.querySelector('.form__input--name > input');
+const inputTitle = document.querySelector('.form__input--title > input')
+const inputQuestion = document.querySelector('.form__textbox > textarea')
+form.addEventListener('submit', (event) => {
+  event.preventDefault(); // 새로고침을 막음
+  const obj = {
+    id: "999",  //임의의 값 아무거나
+    createdAt: new Date(),
+    title: inputTitle.value,
+    url: "https://github.com/codestates-seb/agora-states-fe/discussions/45",
+    author: inputName.value,
+    answer: null,
+    bodyHTML: inputQuestion.value,
+    avatarUrl:
+      "https://avatars.githubusercontent.com/u/97888923?s=64&u=12b18768cdeebcf358b70051283a3ef57be6a20f&v=4",
+  }
+
+  // 기존의 데이터 => 아고라스테이츠 디스커젼  가장 앞에 추가
+  agoraStatesDiscussions.unshift(obj);
+  ul.prepend(convertToDiscussion(obj)) // 하나의 DOM으로 만들어주고 prepend: 맨앞으로 추가
+
+  inputName.value = '';
+  inputTitle.value = '';
+  inputQuestion.value = '';
+})
+
+
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
@@ -56,3 +86,5 @@ const render = (element) => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+
