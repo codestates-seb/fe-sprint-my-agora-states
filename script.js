6e5387b64b061974d89cb8c8189f11d28395d6d0
EunjiLee1997 <eunjilee1111@gmail>
2022-11-15 13:58:29 +0900

    bare minimum 추가

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ console.log(agoraStatesDiscussions);
 
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
 const convertToDiscussion = (obj) => {
+  //obj === agoraStatesDiscussions[i]
   const li = document.createElement("li"); // li 요소 생성
   li.className = "discussion__container"; // 클래스 이름 지정
 
@@ -15,11 +16,63 @@ const convertToDiscussion = (obj) => {
 
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
 
+  const photo = document.createElement("img");
+  photo.className = "discussion__avatar--image";
+  photo.src = obj.avatarUrl;
+  photo.alt = "avatar of" + obj.author;
+  avatarWrapper.append(photo);
 
+  const discussionTitle = document.createElement("h2");
+  discussionTitle.className = "discussion__title";
+  discussionContent.append(discussionTitle);
+
+  const titleRef = document.createElement("a");
+  titleRef.href = obj.url;
+  titleRef.textContent = obj.title;
+  discussionTitle.append(titleRef);
+
+  const discussionInfo = document.createElement("div");
+  discussionInfo.className = "discussion__information";
+  discussionInfo.textContent = `${obj.author} / ${new Date(obj.createdAt).toLocaleString()}`;
+  discussionContent.append(discussionInfo);
+
+  const isAnswered = document.createElement("p");
+  isAnswered.className = "discussion__answered";
+  isAnswered.textContent = obj.answer ? "☑︎" : "◻︎";
+  discussionAnswered.append(isAnswered);
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
 };
+
+//새로운 게시글 등록
+const form = document.querySelector('form.form');
+const inputName = document.querySelector('.form__input--name > input');
+const inputTitle = document.querySelector('.form__input--title > input');
+const inputContent = document.querySelector('.form__textbox > textarea');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const obj = {
+    id: "new id",
+    createdAt: new Date(),
+    title: inputTitle.value,
+    url: "https://github.com/codestates-seb/agora-states-fe/discussions",
+    author: inputName.value,
+    answer: null,
+    bodyHTML: inputContent.value,
+    avatarUrl: "https://t4.ftcdn.net/jpg/00/65/77/27/240_F_65772719_A1UV5kLi5nCEWI0BNLLiFaBPEkUbv5Fv.jpg", 
+  }
+
+  //기존 배열에 추가
+  agoraStatesDiscussions.unshift(obj);
+  ul.prepend(convertToDiscussion(obj));
+
+  //submit 후 초기화
+  inputName.value = "";
+  inputTitle.value = "";
+  inputContent.value = "";
+});
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
