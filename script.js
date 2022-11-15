85f5ab9a39b27b9ff0052bd1c5d93fa898327699
yjyaang <topbuty97@gmail.com>
2022-11-15 16:02:03 +0900

    script.js 날짜시간 오류 수정

--- script.js
+++ script.js
@@ -71,12 +71,12 @@ function addDiscussion(event) {
   const date = new Date();
 
   const year = date.getFullYear();
-  const month = ('0' + (date.getMonth() + 1));
-  const day = ('0' + date.getDate());
+  const month = ('0' + (date.getMonth() + 1)).slice(-2);
+  const day = ('0' + date.getDate()).slice(-2);
   const dateStr = year + '-' + month + '-' + day;
-  const hours = ('0' + date.getHours());
-  const minutes = ('0' + date.getMinutes());
-  const seconds = ('0' + date.getSeconds());
+  const hours = ('0' + date.getHours()).slice(-2);
+  const minutes = ('0' + date.getMinutes()).slice(-2);
+  const seconds = ('0' + date.getSeconds()).slice(-2);
   const timeStr = hours + ':' + minutes + ':' + seconds;
 
   // 새 객체를 추가 -> 새로 쓰는 내용 담기 위해
