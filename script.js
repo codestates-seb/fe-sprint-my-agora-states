47699f18fe94ebbe762d7591e20e9dc5307670ad
Im Sung Eun <se2id@naver.com>
2022-11-15 13:41:42 +0900

    css최종수정

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ console.log(agoraStatesDiscussions);
   const convertToDiscussion = (obj) => {
   const li = document.createElement("li"); // li 요소 생성
   li.className = "discussion__container"; // 클래스 이름 지정
-
+  const name = document.querySelector("#name");
+  const titleInput = document.querySelector("#title");
   const avatarWrapper = document.createElement("div");
   avatarWrapper.className = "discussion__avatar--wrapper";
   const discussionContent = document.createElement("div");
@@ -14,9 +15,10 @@ console.log(agoraStatesDiscussions);
   discussionAnswered.className = "discussion__answered";
 
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
+
+ 
+
   //이미지 넣기
-
-
 
   const avatarImg = document.createElement('img');
   avatarImg.src = obj.avatarUrl;
@@ -41,12 +43,37 @@ console.log(agoraStatesDiscussions);
 
   //답변체크박스
   const checkBox = document.createElement('p');
-  checkBox.append('☑')
+  checkBox.textContent = obj.answer ? '☑' : '☒';
   discussionAnswered.append(checkBox)
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
 };
+
+const form = document.querySelector('form.form');
+const inputName = document.querySelector('.form__input--name > input')
+const inputTitle = document.querySelector('.form__input--title > input')
+const inputQuestion = document.querySelector('.form__textbox > textarea')
+
+form.addEventListener('submit',(event) => {
+  event.preventDefault(); //새로고침방지
+  const obj = {
+    id: "9",//임의의값
+    createdAt: new Date(),
+    title: inputTitle.value,
+    url: "https://github.com/codestates-seb/agora-states-fe/discussions/45",
+    author: inputName.value,
+    answer:null,
+    bodyHTML:inputQuestion.value, 
+    avatarUrl:
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS51zpgflCyj4qpnHqAqnEf4bL-J8_2-7vyeYsBoUFgSKXtS2rp2-S81R-qeJToqVoldy0&usqp=CAU",
+}
+agoraStatesDiscussions.unshift(obj);
+ul.prepend(convertToDiscussion(obj))
+inputTitle.value=""
+inputName.value=""
+inputQuestion.value=""
+})
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
