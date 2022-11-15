26573a93f81326c956c5ec0910afb9e221e276f8
HMH <aldur930@gmail.com>
2022-11-15 16:10:44 +0900

    commit from Window

---
+++ images/images.js
@@ -0,0 +1,5 @@
+const profileImgArray = [
+    "https://i.pinimg.com/originals/47/dd/96/47dd96bb8e40d74479d7a8195f993e55.jpg",
+    "https://www.crash.fr/wp-content/uploads/2019/08/rodland05-1080x1359.jpg",
+    "http://file3.instiz.net/data/file3/2018/03/09/5/0/8/508e688b39f891d02178abaebddfc2a9.jpg",
+];
--- script.js
+++ script.js
@@ -7,19 +7,89 @@ const convertToDiscussion = (obj) => {
   li.className = "discussion__container"; // 클래스 이름 지정
 
   const avatarWrapper = document.createElement("div");
-  avatarWrapper.className = "discussion__avatar--wrapper";
+  avatarWrapper.className = "discussion__avatar--wrapper"; // <div class="discussion__avatar--wrapper">
   const discussionContent = document.createElement("div");
-  discussionContent.className = "discussion__content";
+  discussionContent.className = "discussion__content"; // <div class="discussion__content">
   const discussionAnswered = document.createElement("div");
-  discussionAnswered.className = "discussion__answered";
+  discussionAnswered.className = "discussion__answered"; // <div class="discussion__answered">
 
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
 
+  // avatarWrapper 조립
+  const discussionAvatarImg = document.createElement("img");
+  discussionAvatarImg.className =
+    obj.id !== "" ? "discussion__avatar--image" : "newImg-circle";
+  discussionAvatarImg.src = obj.avatarUrl;
+  discussionAvatarImg.alt = "avatar of " + obj.author;
+  avatarWrapper.append(discussionAvatarImg);
 
+  // discussionContent 조립
+  const discussionLink = document.createElement("a");
+  discussionLink.href = obj.url;
+  discussionLink.target = "_blank";
+  discussionLink.textContent = obj.title;
+  const discussionTitle = document.createElement("h2");
+  discussionTitle.className = "discussion__title";
+  discussionTitle.append(discussionLink);
+  const discussionInfo = document.createElement("div");
+  discussionInfo.className = "discussion__information";
+  discussionInfo.textContent = obj.author + " / " + obj.createdAt;
+  discussionContent.append(discussionTitle, discussionInfo);
 
+  // discussionAnswered 조립
+  const answerObj = obj.answer;
+  const discussionAnswer = document.createElement("p");
+  discussionAnswer.textContent = answerObj === null ? "x" : "☑";
+  discussionAnswered.append(discussionAnswer);
+
+  // append to li
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
 };
+
+// Session
+const form = document.querySelector("form.form");
+const inputName = document.querySelector("input#name");
+const inputTitle = document.querySelector("input#title");
+const inputContent = document.querySelector("input#story");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let randomIdx = (max) => Math.floor(Math.random() * max);
+  const newQuestion = {
+    id: "",
+    createdAt: new Date().toISOString(),
+    title: inputTitle.value,
+    url: "https://github.com/codestates-seb/agora-states-fe/discussions/45",
+    author: inputName.value,
+    answer: null,
+    bodyHTML: inputContent.value,
+    avatarUrl: profileImgArray[randomIdx(profileImgArray.length)],
+  };
+  agoraStatesDiscussions.unshift(newQuestion);
+  ul.prepend(convertToDiscussion(agoraStatesDiscussions[0]));
+
+  // 초기화
+  inputName.value = "";
+  inputTitle.value = "";
+  inputContent.value = "";
+
+  // 저장
+});
+
+
+// 질문 작성 애니메이션
+// const divInputName = document.querySelector("div.form__input--name");
+// const divInputTitle = document.querySelector("div.form__input--title");
+// inputContent.addEventListener("focus", (e) => {
+//   divInputName.classList.add("display-none");
+//   divInputTitle.classList.add("display-none");
+//   inputContent.classList.add("w100-h100");
+// });
+// inputContent.addEventListener("blur", (e) => {
+//   divInputName.classList.remove("display-none");
+//   divInputTitle.classList.remove("display-none");
+//   inputContent.classList.remove("w100-h100");
+// });
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
