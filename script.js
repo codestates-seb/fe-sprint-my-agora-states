e362bd67f75ce36df9f6f695dae29f50990c2982
hanjunn22 <hanjunn22@gmail.com>
2022-11-15 16:06:59 +0900

    과제 Bare Minimum

--- script.js
+++ script.js
@@ -116,6 +116,9 @@ const render = (element) => {
   return;
 };
 
+
+
+
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
