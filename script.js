a6412f3ad7720d8fa979953ec66ba765d2d7b8b4
wangamy0222 <wangamyy222@gmail.com>
2022-11-15 20:53:23 +0900

    나만의 아고라 스테이츠

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 // index.html을 열어서 agoraStatesDiscussions 배열 요소를 확인하세요.
 console.log(agoraStatesDiscussions);
+let data;
+const dataFromLocalStorage = localStorage.getItem("agoraStatesDiscussions");
+if (dataFromLocalStorage) {
+  data = JSON.parse(dataFromLocalStorage);
+} else {
+  data = agoraStatesDiscussions.slice();
+}
 
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
 const convertToDiscussion = (obj) => {
@@ -311,3 +318,9 @@ audioContainer.addEventListener("click", () => {
 audio.addEventListener("ended", () => {
   clearInterval(interval);
 });
+
+// 로컬스토리지에 저장
+localStorage.setItem("agoraStatesDiscussions", JSON.stringify(data));
+
+// 렌더링
+render(ul, 0, limit);
