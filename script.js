2482956607b14195af65eace01bf9660774f7e58
임경아 <avocadopajamas@gmail.com>
2022-11-15 13:50:09 +0900

    아고라스테이츠배포

--- script.js
+++ script.js
@@ -5,30 +5,79 @@ console.log(agoraStatesDiscussions);
 const convertToDiscussion = (obj) => {
   const li = document.createElement("li"); // li 요소 생성
   li.className = "discussion__container"; // 클래스 이름 지정
-
+  
   const avatarWrapper = document.createElement("div");
   avatarWrapper.className = "discussion__avatar--wrapper";
   const discussionContent = document.createElement("div");
   discussionContent.className = "discussion__content";
   const discussionAnswered = document.createElement("div");
   discussionAnswered.className = "discussion__answered";
+  
+  const avatarImg = document.createElement('img');
+  avatarImg.className = 'discussion__avatar--image'
+  avatarImg.src = obj.avatarUrl;
+  avatarImg.alt = 'avatar of ' + obj.author;
+  avatarWrapper.append(avatarImg);
+  
+  const discusstionTitle = document.createElement("h2");
+  discusstionTitle.className = 'discussion__title'
+  const titleLink = document.createElement('a');
+  titleLink.href = obj.url;
+  titleLink.textContent = obj.title;
+  discusstionTitle.append(titleLink);
+  
+  const discusstionInfo = document.createElement("div");
+  discusstionInfo.className = 'discussion__information';
+  discusstionInfo.textContent = `${obj.author} / ${new Date(obj.createdAt).toLocaleString() }`;
+  
+  discussionContent.append(discusstionTitle,discusstionInfo);
+  
+  const ptag = document.createElement("p");  
+  if(obj.answer){ ptag.textContent = '☑'
+}else{ ptag.textContent = '☒' }
 
-  // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
+discussionAnswered.append(ptag);
 
 
-
-  li.append(avatarWrapper, discussionContent, discussionAnswered);
-  return li;
+li.append(avatarWrapper, discussionContent ,discussionAnswered);
+return li;
 };
+
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
   for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
     element.append(convertToDiscussion(agoraStatesDiscussions[i]));
+
   }
   return;
 };
 
+
+
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+const formEliment = document.querySelector('form.form')
+const inputTitle = document.querySelector('.form__input--title > input')
+const inputName = document.querySelector('#name')
+const inputQuestion = document.querySelector('#story')
+
+formEliment.addEventListener('submit',function(event){
+  event.preventDefault();
+  const obj = {
+    id: "999",
+    createdAt: new Date(),
+    title: inputTitle.value,
+    url: "https://github.com/codestates-seb/agora-states-fe/discussions/45",
+    author: inputName.value,
+    answer: null,
+    bodyHTML: inputQuestion.value,
+    avatarUrl:
+    "https://avatars.githubusercontent.com/u/97888923?s=64&u=12b18768cdeebcf358b70051283a3ef57be6a20f&v=4",
+  }
+  
+  agoraStatesDiscussions.unshift(obj);
+  ul.prepend(convertToDiscussion(obj));
+})
