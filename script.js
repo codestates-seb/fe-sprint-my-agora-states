fb19c19265cff5adc8454795aa1b333e4e29e49b
qwa1822 <asdzxc1822@naver.com>
2022-11-15 16:12:42 +0900

    style수정

--- script.js
+++ script.js
@@ -13,13 +13,66 @@ const convertToDiscussion = (obj) => {
   const discussionAnswered = document.createElement("div");
   discussionAnswered.className = "discussion__answered";
 
+  const face=document.createElement("img");
+  face.className="discussion__avatar--image";
+  face.src=obj.avatarUrl;
+  face.alt="avatar of"+obj.author;
+  avatarWrapper.append(face);
+
+  const discussionTitle =document.createElement("h2");
+
+  const titleAnchor=document.createElement("a");
+  titleAnchor.href=obj.url;
+  titleAnchor.textContent=obj.title;
+  discussionTitle.append(titleAnchor);
+  discussionContent.append(discussionTitle)
+
+  const discusstionInfo=document.createElement('div');
+  discusstionInfo.className='discussion__information';
+  discusstionInfo.textContent=`${obj.author} ${new Date(obj.createdAt).toLocaleTimeString()}`
+  discussionContent.append(discussionTitle,discusstionInfo)
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
 
-
+  const checked = document.createElement("p");
+  checked.textContent = obj.answer ? "☑︎" : "☒"; 
+  discussionAnswered.append(checked);
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
 };
+
+
+const form=document.querySelector('form.form');
+const inputName=document.querySelector(".form__input--name>input");
+const inputTitle=document.querySelector(".form__input--title>input");
+const inputQuestion=document.querySelector(".form__textbox>textarea");
+
+form.addEventListener('submit',(event)=>{
+  event.preventDefault();
+  const obj={
+      id: "1233",
+      createdAt: new Date(),
+      title: inputTitle.value,
+      url: "https://github.com/codestates-seb/agora-states-fe/discussions/45",
+      author: inputName.value,
+      answer:null,
+      bodyHTML:inputQuestion.value,
+      avatarUrl:
+        "https://avatars.githubusercontent.com/u/97888923?s=64&u=12b18768cdeebcf358b70051283a3ef57be6a20f&v=4",
+  }
+
+  // 기존 데이터 가장 앞에 추가.
+
+  agoraStatesDiscussions.unshift(obj);
+  
+  
+  ul.prepend(convertToDiscussion(obj));
+
+  inputName.value="";
+  inputTitle.value="";
+  inputQuestion.value="";
+})
+
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
@@ -30,5 +83,14 @@ const render = (element) => {
 };
 
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
+
+
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+  
+
+
+
+
+
