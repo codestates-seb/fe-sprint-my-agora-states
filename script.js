d440075767f68bea8b5336d6775220f47e73f1f3
JOAAAAAAAAAAA <blueseablueskyblueme@gmail.com>
2022-11-16 02:14:02 +0900

    listenr 설정 위해 submit 에 id  부여

--- script.js
+++ script.js
@@ -6,15 +6,15 @@ const convertToDiscussion = (obj) => {
   const li = document.createElement("li"); // li 요소 생성
   li.className = "discussion__container"; // 클래스 이름 지정
 
-  // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
+  //image
   const avatarWrapper = document.createElement("div");
   avatarWrapper.className = "discussion__avatar--wrapper";
   const avatarImg = document.createElement('img');
   avatarImg.className = "discussion__avatar--image";
-  avatarImg.src = obj.answer.avatarUrl;
+  avatarImg.src = obj.avatarUrl;
   avatarImg.alt = 'avatar of ' + obj.author;
   avatarWrapper.append(avatarImg);
-
+  //discussionContent
   const discussionContent = document.createElement("div");
   discussionContent.className = "discussion__content";
   
@@ -30,7 +30,7 @@ const convertToDiscussion = (obj) => {
   const discussionInformation = document.createElement("div");
   discussionInformation.textContent = obj.author+" "+obj.createdAt;
   discussionContent.append(discussionInformation);
-
+  //answered
   const discussionAnswered = document.createElement("div");
   discussionAnswered.className = "discussion__answered";
   const discussionAnsweredcheckbox = document.createElement("p");
@@ -52,3 +52,15 @@ const render = (element) => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+let inputName = document.querySelector("#name")
+let inputTitle = document.querySelector("#title")
+let inputStory = document.querySelector("#story")
+// 객체 생성 후 새로 입력된 내용 배열에 추가하기
+let newPost = {}
+newPost.id = inputName.value
+newPost.title = inputTitle.value
+  // bodyHTML : 이건 나중에 구현해보자
+
+agoraStatesDiscussions.unshift('newPost')
+
