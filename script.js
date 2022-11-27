e0b9574e888f5ee4750577ab50429bcef1dca9fc
jeongminsang <101001956+jeongminsang@users.noreply.github.com>
2022-11-27 16:07:01 +0900

    페이지네이션 기능 추가

--- script.js
+++ script.js
@@ -54,9 +54,15 @@ const convertToDiscussion = (obj) => {
 };
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
-const render = (element) => {
-  for (let i = 0; i < discussionList.length; i += 1) {
+const render = (element, pageNum) => {
+  for (let i = 0; i < 10; i += 1) {
     element.append(convertToDiscussion(discussionList[i]));
+  }
+  if (pageNum) { // 페이지넘버를 눌렀을 때 이 부분이 없으면, 계속해서 쌓이면서 렌더링됩니다.
+    element.innerHTML = "";
+  }
+  for (let i = pageNum * 10 - 10; i < Math.min(pageNum * 10, discussionList.length); i += 1) {
+      element.append(convertToDiscussion(discussionList[i]));
   }
   return;
 };
@@ -87,14 +93,14 @@ formSubmit.addEventListener('click', (event) => {
           "https://avatars.githubusercontent.com/u/97888923?s=64&u=12b18768cdeebcf358b70051283a3ef57be6a20f&v=4",
       }
       //기존 데이터 가장 앞에 추가
-      discussionList.unshift(Obj);
-      // ul.prepend(convertToDiscussion(obj));
-      console.log(discussionList);
+      // discussionList.unshift(Obj);
+      // ul.prepend(convertToDiscussion(Obj));
+      // console.log(discussionList);
 
-      const notice = document.querySelector(".discussion__container");
-      notice.after(convertToDiscussion(Obj));
+      // const notice = document.querySelector(".discussion__container");
+      // notice.after(convertToDiscussion(Obj));
   
-      jsonLocalStorage.setItem("discussion", discussionList);
+      // jsonLocalStorage.setItem("discussion", discussionList);
 
       inputName.value = '';
       inputTitle.value = '';
@@ -105,25 +111,35 @@ formSubmit.addEventListener('click', (event) => {
     }
 })
 
+//페이지네이션 1. 페이지 개수를 자동으로 만듭니다.
+function paginationMaking() {
+  if (discussionList) {
+      for (let i = 1; i <= Math.ceil(discussionList.length / 10); i++) {
+          const pageNum = document.createElement('span');
+          const pagination = document.querySelector('.pagination__pagenum');
+          pageNum.textContent = i;
+          pageNum.className = `pagination__pagenum--num num${i}`;
+          pagination.append(pageNum);
+      }
+  } else {
+      for (let i = 1; i <= Math.ceil(agoraStatesDiscussions.length / 10); i++) {
+          const pageNum = document.createElement('span');
+          const pagination = document.querySelector('.pagination__pagenum');
+          pageNum.textContent = i;
+          pageNum.className = `pagination__pagenum--num num${i}`;
+          pagination.append(pageNum);
+      }
+  }
+}
+paginationMaking(); //최초 실행
 
-// const avatarImg = document.createElement('img');
-//   avatarImg.className = "discussion__avatar--image";
-//   avatarImg.alt = "avatar of " + obj.author;
-//   avatarImg.src = obj.avatarUrl;
-//   avatarWrapper.append(avatarImg);
-
-//   const contentTitle = document.createElement('h2');
-//   contentTitle.className = "discussion__title";
-//   const titleAnchor = document.createElement('a');
-//   titleAnchor.href = obj.url;
-//   titleAnchor.textContent = obj.title;
-//   contentTitle.append(titleAnchor);
-
-//   const contentInfo = document.createElement('div');
-//   contentInfo.className = "discussion__information";
-//   contentInfo.textContent = `${obj.author} / ${new Date(obj.createdAt).toLocaleString}`;//toISOString 도 사용
-//   discussionContent.append(contentTitle, contentInfo);
-
-//   const checked = document.createElement('p');
-//   checked.textContent = obj.answer ? '☑' : '☒' ;
-//   discussionAnswered.append(checked);
+// 페이지네이션 3. 버튼을 누르면 해당 페이지에 10개씩 배열의 데이터를 화면에 렌더링합니다.
+// querySelectorAll이 배열을 반환한다는 점에서 착안해, 두 개의 클래스를 미리 부여했습니다.
+// 숫자 규칙을 따라서 반복문을 사용해, addEventListener를 동적으로 생성합니다.
+let pageNumObject = {};
+for (let i = 1; i <= document.querySelectorAll('.pagination__pagenum--num').length; i++) {
+  pageNumObject[`num${i}`] = document.querySelector(`.num${i}`);
+  pageNumObject[`num${i}`].addEventListener('click', () => {
+      render(ul, pageNumObject[`num${i}`].textContent);
+  });
+}
