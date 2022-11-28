f04ed449d54260f21dd587078ffd7fe84a753dda
Jung Su <wjdwjdtn92@naver.com>
2022-11-29 00:40:12 +0900

    feat: pageNation 5개씩 출력 구현

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ let agoraStatesDiscussions = getAgoraStatesDiscussions();
 const DEFAULT_AVATOR_IMAGE = "./avator_default.png";
 
 const $ul = document.querySelector("ul.discussions__container");
+const $discussions__area = document.querySelector(".discussions__area");
+
 const $paginationNumbers = document.querySelector("#pagination-numbers");
 const $paginationContainer = document.querySelector("#pagination-container");
 const ITEM_SHOW_LIMIT = 10;
@@ -84,7 +86,7 @@ $paginationNumbers.addEventListener("click", (event) => {
   }
 
   pagenationCurrentNum = parseInt($a.id, 10);
-  render($ul, $a.id);
+  render();
 });
 
 // next / prev button 클릭
@@ -110,15 +112,25 @@ $paginationContainer.addEventListener("click", (event) => {
 
   pagenationCurrentNum = nextPage;
 
-  render($ul, pagenationCurrentNum);
+  render();
 });
 
 // 하단 pagenation 리스트 출력
 const renderPageNations = () => {
-  removeAllchild($paginationNumbers);
-  const pageCount = Math.ceil(agoraStatesDiscussions.length / ITEM_SHOW_LIMIT);
+  $paginationNumbers.textContent = "";
 
-  for (let i = 1; i <= pageCount; i++) {
+  const pageCount = Math.ceil(agoraStatesDiscussions.length / ITEM_SHOW_LIMIT);
+  const divPageNum = pagenationCurrentNum % PAGENATION_SHOW_LIMIT;
+  const addNum = divPageNum === 0 ? -PAGENATION_SHOW_LIMIT + 1 : 1;
+
+  const displayStartPageNum = pagenationCurrentNum - divPageNum + addNum;
+  const displayEndPageNum = displayStartPageNum + PAGENATION_SHOW_LIMIT - 1;
+
+  if (displayEndPageNum > pageCount) {
+    displayEndPageNum = pageCount;
+  }
+
+  for (let i = displayStartPageNum; i <= displayEndPageNum; i++) {
     const $a = document.createElement("a");
     $a.href = "#";
     $a.textContent = i;
@@ -129,23 +141,28 @@ const renderPageNations = () => {
 };
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
-const render = (element) => {
-  renderPageNations();
+const render = () => {
+  const $ul = document.createElement("ul");
+  $ul.className = "discussions__container";
 
-  removeAllchild(element);
+  $discussions__area.textContent = "";
+
   const displayDiscussions = agoraStatesDiscussions.slice(
     (pagenationCurrentNum - 1) * 10,
     pagenationCurrentNum * 10
   );
 
   for (let i = 0; i < displayDiscussions.length; i += 1) {
-    element.append(convertToDiscussion(displayDiscussions[i]));
+    $ul.append(convertToDiscussion(displayDiscussions[i]));
   }
+
+  $discussions__area.append($ul);
+  renderPageNations();
   return;
 };
 
 // $ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
-render($ul);
+render();
 
 //form submit 기능, Discussions 배열에 요소 추가 후 재출력
 const formSubmitButton = document.querySelector(".form__submit > input");
@@ -168,7 +185,7 @@ formSubmitButton.addEventListener("click", (event) => {
 
   agoraStatesDiscussions = [discussionItem, ...agoraStatesDiscussions];
   setLocalStorage("agoraStatesDiscussions", agoraStatesDiscussions);
-  render($ul);
+  render();
 
   name.value = "";
   title.value = "";
