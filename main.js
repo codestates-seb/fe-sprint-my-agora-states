32c47f69c9e00f3d377ac7a131a5382ce52df3fb
LeeHyoGeun-create <dkdkdk99835@gmail.com>
2022-12-13 15:20:37 +0900

    feat: 데이터 불러오기 기능 구현

--- main.js
+++ main.js
@@ -1,33 +1,37 @@
-import agoraStatesDiscussions from "./src/data.js";
+// import agoraStatesDiscussions from "./src/data.js";
 import Discussbox from "./src/DiscussBox.js";
 import FilterBox from "./src/FilterBox.js";
 import { FormBox } from "./src/FormBox.js";
 import Pagenationbox from "./src/PagenationBox.js";
 import SearchBox from "./src/SearchBox.js";
+import getData from "./src/api.js";
 
-const $discussionWrapper = document.querySelector('.discussion__wrapper');
-const $main = document.querySelector('main');
+const $discussionWrapper = document.querySelector(".discussion__wrapper");
+const $main = document.querySelector("main");
 
 // index.html을 열어서 agoraStatesDiscussions 배열 요소를 확인하세요.
-console.log(agoraStatesDiscussions);
-
+// console.log(agoraStatesDiscussions);
 
 function app() {
   this.state = {
     pageNum: 1,
     dropNum: 5,
     isTrue: null,
-    searchClassification: '제목',
-    searchContext: '',
-    agoraStatesDiscussions,
+    searchClassification: "제목",
+    searchContext: "",
+    agoraStatesDiscussions: null,
     filteredArr: [],
-  }
-
+  };
 
   const searchSection = new SearchBox({
-    $discussionWrapper, onClick: (value) => {
-      this.setState({ searchClassification: value.searchClassification, searchContext: value.searchContext, pageNum: 1 });
-    }
+    $discussionWrapper,
+    onClick: (value) => {
+      this.setState({
+        searchClassification: value.searchClassification,
+        searchContext: value.searchContext,
+        pageNum: 1,
+      });
+    },
   });
 
   const filterBox = new FilterBox({
@@ -35,44 +39,50 @@ function app() {
     initialState: this.state.isTrue,
     onClick: (value) => {
       this.setState({ isTrue: value, pageNum: 1 });
-    }
+    },
   });
 
-  const discussBox = new Discussbox({ $discussionWrapper, initialState: this.state });
+  const discussBox = new Discussbox({
+    $discussionWrapper,
+    initialState: this.state,
+  });
 
   const pagenation = new Pagenationbox({
-    $discussionWrapper, initialState: this.state,
+    $discussionWrapper,
+    initialState: this.state,
     onClick: (value, pagenationEndNum) => {
       console.log("value", value);
-      if (value === '<<') {
+      if (value === "<<") {
         this.setState({ pageNum: 1 });
         return;
       }
 
-      if (value === '>>') {
+      if (value === ">>") {
         this.setState({ pageNum: pagenationEndNum });
         return;
       }
       this.setState({ pageNum: Number(value) });
-    }
-  })
+    },
+  });
 
   const formBox = new FormBox({
-    $main, onSubmit: (obj) => {
+    $main,
+    onSubmit: (obj) => {
       console.log(obj);
       agoraStatesDiscussions.unshift(obj);
       this.setState();
-    }
+    },
   });
 
   this.setState = (nextState) => {
     this.state = { ...this.state, ...nextState };
+    console.log(this.state);
     this.state = { ...this.state, filteredArr: getFilterArr() };
-    filterBox.setState({ isTrue: this.state.isTrue })
+
+    filterBox.setState({ isTrue: this.state.isTrue });
     discussBox.setState(this.state);
     pagenation.setState(this.state);
-    console.log(this.state);
-  }
+  };
 
   const answered = (object) => {
     switch (this.state.isTrue) {
@@ -80,85 +90,50 @@ function app() {
         return true;
         break;
       case true:
-        if (object.answer) return true
+        if (object.answer) return true;
         else return false;
         break;
       case false:
-        if (!object.answer) return true
+        if (!object.answer) return true;
         else return false;
         break;
     }
-  }
+  };
 
   const search = (object) => {
     if (this.state.searchContext === null) {
       return true;
     }
     switch (this.state.searchClassification) {
-      case '제목':
+      case "제목":
         return object.title.includes(this.state.searchContext);
         break;
-      case '작성자':
+      case "작성자":
         return object.author.includes(this.state.searchContext);
         break;
     }
-  }
-
-
+  };
 
   const getFilterArr = () => {
     const filteredArr = this.state.agoraStatesDiscussions.filter((object) => {
       return answered(object) && search(object);
-    })
+    });
     return filteredArr;
-  }
+  };
 
-  const init = () => {
-    this.setState();
-  }
+  const init = async () => {
+    try {
+      const agoraStatesDiscussions = await getData();
+      this.setState({ agoraStatesDiscussions });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   init();
-
-
 }
 
 new app();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
 // const convertToDiscussion = (obj) => {
@@ -173,8 +148,6 @@ new app();
 //   discussionAnswered.className = "discussion__answered";
 
 //   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
-
-
 
 //   li.append(avatarWrapper, discussionContent, discussionAnswered);
 //   return li;
@@ -191,5 +164,3 @@ new app();
 // // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 // const ul = document.querySelector("ul.discussions__container");
 // render(ul);
-
-
--- src/api.js
+++ src/api.js
@@ -1,16 +1,20 @@
 const API_END_POINT = "http://localhost:4000/";
 
-export async function getData(id = "") {
+export default async function getData(id = "") {
   // HINT: 가장 마지막 테스트를 통과하기 위해, fetch를 이용합니다. 아래 구현은 완전히 삭제되어도 상관없습니다.
   // TODO: 아래 구현을 REST API 호출로 대체하세요.
   try {
-    const API = `${API_END_POINT}${id}`;
-    const responce = await fetch(API);
+    const API = `${API_END_POINT}discussions/${id}`;
+    const responce = await fetch(API, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
     if (!responce.ok) {
       throw new Error(`서버에 이상이 있습니다 ${responce.status}`);
     }
-    const value = await responce.json();
-    return await value;
+    return await responce.json();
   } catch (e) {
     throw new Error(`코드에 이상이 있습니다. ${e}`);
   }
