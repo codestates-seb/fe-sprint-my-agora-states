c7db07758e8e16c8d9e9e8a2b7496cbd53b4c153
Mmaryyy <marynrvly@gmail.com>
2022-12-13 14:13:17 +0900

    [feat] 서버 연결

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
-// index.html을 열어서 agoraStatesDiscussions 배열 요소를 확인하세요.
-// 한 페이지당 노출될 게시글 갯수 설정
-// 첫 페이지에 노출될 게시글로 초기화
-const pageSize = 5;
-let pageList = agoraStatesDiscussions.slice(0, pageSize);
 
 const ul = document.querySelector("ul.discussions__container");
 const discussionSection = document.querySelector('section.discussion__wrapper');
+// let agoraStatesDiscussions = []
+//  fetch('http://localhost:4000/discussions')
+// .then(res => res.json())
+// .then(json => {
+//   return agoraStatesDiscussions = json
+// })
+async function getData() {
+  const res = await fetch('http://localhost:4000/discussions')
+  let agoraStatesDiscussions = await res.json()
 
-//페이지네이션 이벤트 생성
-//본 객체의 시작 인덱스 ~~ 종료 인덱스까지를 변수로 설정
-const buttonClick = (event, target) => {
-  const startIndex = (event.target.textContent - 1) * pageSize;
-  const lastIndex = startIndex + pageSize;
-  pageList = agoraStatesDiscussions.slice(startIndex,lastIndex);
-  render(ul);
-}
+  console.log(agoraStatesDiscussions)
 
-// convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
+  // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
 const convertToDiscussion = (obj) => {
   const li = document.createElement("li"); // li 요소 생성
   li.className = "discussion__container"; // 클래스 이름 지정
@@ -101,46 +98,47 @@ const render = (element) => {
   while (element.firstChild) {
     element.removeChild(element.lastChild);
   }
-  console.log(pageList);
   for (let i = 0; i < pageList.length; i += 1) {
     element.append(convertToDiscussion(pageList[i]));
   }
   return;
 };
 
+const main = document.querySelector('main')
 
-const renderPage = () => {
-// 페이지네이션 html 추가
-const paginationList = document.createElement("div");
-paginationList.className = "paginationList";
-const firstPage = document.createElement('a');
-firstPage.className = 'pagination';
-firstPage.textContent = '<<';
-
-paginationList.append(firstPage);
-
-const lastPage = document.createElement('a');
-lastPage.className = 'pagination';
-lastPage.textContent = '>>';
-
-// 이전, 다음 버튼 사이에 숫자 칸 생성
-// 페이지당 노출될 게시글의 갯수만큼 렌더링을 반복함
-for (let i = 1; i < Math.ceil(agoraStatesDiscussions.length/5); i++){
-  let pageNumber = document.createElement('a');
-  pageNumber.textContent = `  ${i}  `;
-  pageNumber.role = 'button';
-  pageNumber.className = 'pagination';
-  pageNumber.onclick = buttonClick;
-  paginationList.append(pageNumber);
+//페이지네이션 이벤트 생성
+//본 객체의 시작 인덱스 ~~ 종료 인덱스까지를 변수로 설정
+const buttonClick = (event, target) => {
+  const startIndex = (event.target.textContent - 1) * pageSize;
+  const lastIndex = startIndex + pageSize;
+  pageList = agoraStatesDiscussions.slice(startIndex,lastIndex);
+  render(ul);
 }
 
-// document.querySelector('.pagination').onclick
-paginationList.append(lastPage);
-discussionSection.append(paginationList);
+const pageSize = 5
+let pageList = agoraStatesDiscussions.slice(0, pageSize)
 
+// 페이지네이션 렌더링
+const renderPage = () => {
+// 페이지네이션 html 추가
+  const pagination = document.createElement('footer')
+  pagination.className = 'pagination__container'
+  main.append(pagination)
+  
+  for(let i = 1; i < Math.ceil(agoraStatesDiscussions.length / 5); i++) {
+    const pageNumber = document.createElement('a')
+    pageNumber.className = 'pagination__number'
+    pageNumber.textContent = ` ${i} `
+    pageNumber.onclick = buttonClick
+    pagination.append(pageNumber)
+  }
 }
 
 // start
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 render(ul);
 renderPage();
+
+}
+
+getData()
