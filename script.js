1b84309d90e4eb221de1da9d712c51ad8cf8e685
Kadesti <adesti3527@gmail.com>
2023-01-09 14:17:58 +0900

    my_agora_v1

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 console.log(agoraStatesDiscussions);
 
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
-const convertToDiscussion = (obj) => {
+const convertToDiscussion = (obj,index) => {
   const li = document.createElement("li"); // li 요소 생성
   li.className = "discussion__container"; // 클래스 이름 지정
 
@@ -15,8 +15,37 @@ const convertToDiscussion = (obj) => {
 
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
 
+  // avatarWrapper
+  const avatarImg = document.createElement('img');
+  avatarImg.className = ("discussion__avatar--image");
+  avatarImg.src = agoraStatesDiscussions[index].avatarUrl;
+  avatarImg.alt = 'avatar of ' + agoraStatesDiscussions[index].author;
+  avatarWrapper.append(avatarImg);
 
+  // discussionContent - contentTitle
+  const contentTitle = document.createElement('h2');
+  const contentLink = document.createElement('a');
+  contentTitle.textContent = agoraStatesDiscussions[index].title;
+  contentLink.href = agoraStatesDiscussions[index].url;
+  
+  contentTitle.innerHTML = `<h2 class="discussion__title"><a href="${contentLink.href}">${contentTitle.textContent}</a></h2>`
+  discussionContent.append(contentTitle);
 
+  // discussionContent - contentTitle
+  const informUser = document.createElement('span');
+  const informDate = document.createElement('span');
+  informUser.textContent = agoraStatesDiscussions[index].author;
+  informDate.textContent = agoraStatesDiscussions[index].createdAt;
+  
+  const informText = `${informUser.textContent} / ${informDate.textContent}`;
+  discussionContent.append(informText);
+
+  // discussionAnswered
+  const answerCheck = document.createElement('p');
+  if(!(agoraStatesDiscussions[index].answer)) answerCheck.textContent = '☐';
+  else answerCheck.textContent = '☑';
+
+  discussionAnswered.append(answerCheck);
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
 };
@@ -24,7 +53,7 @@ const convertToDiscussion = (obj) => {
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
   for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
-    element.append(convertToDiscussion(agoraStatesDiscussions[i]));
+    element.append(convertToDiscussion(agoraStatesDiscussions[i],i));
   }
   return;
 };
