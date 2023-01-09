7ba35e2e0c88851a32d2493f4fef00345fe3b89a
Dongholy <dongholy@naver.com>
2023-01-09 16:43:12 +0900

    객체 하나에 담긴 정보를 DOM에 적용

--- script.js
+++ script.js
@@ -14,7 +14,30 @@ const convertToDiscussion = (obj) => {
   discussionAnswered.className = "discussion__answered";
 
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
+  
+  // 아바타 사진
+  const avatarImg = document.createElement("img");
+  avatarImg.src = obj.avatarUrl; // data.js 에 있는 avatarUrl의 Object 주소값
+  avatarImg.alt = 'avatar of ' + obj.author; // 이미지를 대체할 텍스트를 명시 , 이미지가 없을 시 'avatar of xxx' 출력
+  avatarWrapper.append(avatarImg);
 
+  // 제목
+  const questionTitle = document.createElement("h2"); // index.html 에 제목에 h2 값으 주었으니 여기서도 h2 값을 줬습니다.
+  const titlelink = document.createElement("a"); // 링크를 안넣어주니 클릭 효과가 사라져 넣어주었습니다.
+  titlelink.href = obj.url;
+  titlelink.textContent = obj.title; // data.js에 있는 title 를 출력 해줍니다.
+  questionTitle.append(titlelink);
+  discussionContent.append(questionTitle);
+
+  // 닉네임 , 날짜
+  const avatarinfo = document.createElement("div");
+  avatarinfo.textContent = `${obj.author} / ${new Date(obj.createdAt).toLocaleTimeString()}`
+  discussionAnswered.append(avatarinfo);
+
+  //체크 박스
+  const checked = document.createElement("p");
+  checked.textContent = obj.answer ? "" : "☒";
+  discussionAnswered.append(checked);
 
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
@@ -32,3 +55,5 @@ const render = (element) => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+
