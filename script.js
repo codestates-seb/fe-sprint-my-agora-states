30ff465879ef18db820788ead025e2f707b93320
choihaneul <sre130@naver.com>
2023-01-09 22:44:37 +0900

    팝업 js 작성

--- script.js
+++ script.js
@@ -52,3 +52,15 @@ const render = (element) => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+const popup = document.querySelector('#layer_popup')
+const popupExit = document.querySelector('.form-exit')
+const writeForm = document.querySelector('.write_form');
+
+popup.onclick = function() {
+  writeForm.classList.remove('hide')
+}
+
+popupExit.onclick = function() {
+  writeForm.classList.add('hide')
+}
