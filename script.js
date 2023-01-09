f4e7ce6fff9e7936f4ff1ba234e95cce66426aeb
gjy0605 <gjy0605@gmail.com>
2023-01-09 18:02:29 +0900

    js 객채를dom에 담아 배열의 디스커션 화면에 보이게함. CSS 위치잡는 중

--- script.js
+++ script.js
@@ -13,17 +13,52 @@ const convertToDiscussion = (obj) => {
   const discussionAnswered = document.createElement("div");
   discussionAnswered.className = "discussion__answered";
 
-  // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
+  // 프로필 이미지
+  const avatarImg = document.createElement('img');
+  avatarImg.className = "discussion__avatar--image";
+  avatarImg.src = obj.avatarUrl;
+  avatarImg.alt = 'avatar of ' + obj.author;
+  avatarWrapper.append(avatarImg);
+  // 제목
+  const discussionTitle = document.createElement("h2");
+  discussionTitle.className = "discussion__title";
+  discussionContent.append(discussionTitle)
+  // 제목링크
+  const discussionLink = document.createElement("a")
+  discussionLink.textContent = obj.title;
+  discussionLink.src = obj.url;
+  discussionTitle.append(discussionLink);
+  // 유저이름
 
+  // 업로드 시간
+  const discussionInfo = document.createElement("div");
+  discussionInfo.className = "discussion__information";
+  discussionInfo.textContent = `${obj.author} / ${obj.createdAt}`;
+  discussionContent.append(discussionInfo);
+  // 체크박스
+  const discussionCheckbox = document.createElement("p");
+  discussionCheckbox.className = "discussion__answered";
+  discussionContent.append(discussionAnswered);
+  // 체크박스에 답
+  // 
+  
 
+  // 만약 답변이 있다면 체크박스, 없다면 x박스
+  if(obj.answer !== null){
+    discussionAnswered.textContent= "☒";
+  }else{
+    discussionAnswered.textContent= "☑";
+  }
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
+
 };
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
   for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
+
     element.append(convertToDiscussion(agoraStatesDiscussions[i]));
   }
   return;
