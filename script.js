8d29a749e8422c395d557e57fc99a10792f30ec6
Choalstn <choalstn@gmail.com>
2023-01-09 16:37:34 +0900

    모든 디스커션 보이기

--- script.js
+++ script.js
@@ -6,16 +6,43 @@ const convertToDiscussion = (obj) => {
   const li = document.createElement("li"); // li 요소 생성
   li.className = "discussion__container"; // 클래스 이름 지정
 
+  //아바타
   const avatarWrapper = document.createElement("div");
   avatarWrapper.className = "discussion__avatar--wrapper";
+
+  const avatarImg = document.createElement('img');
+  avatarImg.className = "discussion__avatar--image"
+  avatarImg.src = obj.avatarUrl;
+  avatarImg.alt = 'avatar of ' + obj.author;
+  avatarWrapper.append(avatarImg);
+  
+
+  //제목, 작성자, 날짜
   const discussionContent = document.createElement("div");
   discussionContent.className = "discussion__content";
+
+  const title = document.createElement("h2");
+  title.className = 'discussion__title';
+  discussionContent.append(title);
+
+  const link = document.createElement('a');
+  link.innerText = obj.title;
+  link.href = obj.url;
+  title.append(link);
+
+  const info = document.createElement("div");
+  info.className = 'discussion__information';
+  info.textContent = `${obj.author} / ${obj.createdAt} `;
+  discussionContent.append(info);
+
+  // 답변 확인
   const discussionAnswered = document.createElement("div");
   discussionAnswered.className = "discussion__answered";
-
-  // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
-
-
+  
+  const AnsweredParaph = document.createElement("p");
+  AnsweredParaph.textContent = '☑';
+  discussionAnswered.append(AnsweredParaph);
+  discussionContent.append(discussionAnswered);
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
@@ -32,3 +59,4 @@ const render = (element) => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
