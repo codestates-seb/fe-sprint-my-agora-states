273f1a29fa391390ec73cf039821deed0fdba1f6
pizzaYami <cyj9407@naver.com>
2023-01-10 01:28:59 +0900

    반복은되고 undefined를 못 고침

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-// index.html을 열어서 agoraStatesDiscussions 배열 요소를 확인하세요.
-console.log(agoraStatesDiscussions);
 
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
+// obj가 원래 agoraStatesDiscussions[i]였고 for문을 한번더 사용했다.
 const convertToDiscussion = (obj) => {
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
-  for(let i = 0; i < agoraStatesDiscussions.length; i += 1){
   const li = document.createElement("li"); // li 요소 생성
   li.className = "discussion__container"; // 클래스 이름 지정
 
@@ -16,32 +14,33 @@ const convertToDiscussion = (obj) => {
   discussionAnswered.className = "discussion__answered";
 
 
-  // discussion__avatar--wrapper(이미지) 넣기
+  // avatarWrapper에 이미지 넣기
   const avatarImg = document.createElement('img');
   // class를 안지정해줘서 이미지가 뒤죽박죽이였다.
   avatarImg.className = "discussion__avatar--image";
-  avatarImg.src = agoraStatesDiscussions[i].avatarUrl;
-  avatarImg.alt = 'avatar of ' + agoraStatesDiscussions[i].author;
+  avatarImg.src = obj.avatarUrl;
+  avatarImg.alt = 'avatar of ' + obj.author;
   avatarWrapper.append(avatarImg);
 
-  // discussion__content(질문 제목) 넣기
+  // discussionContent에 질문 제목 넣기
   const discussion__title__maker = document.createElement("h2");
   discussion__title__maker.className =  "discussion__title";
   const title_Url = document.createElement("a");
-  title_Url.href = agoraStatesDiscussions[i].url;
-  title_Url.textContent = agoraStatesDiscussions[i].title;
+  title_Url.href = obj.url;
+  title_Url.textContent = obj.title;
   discussion__title__maker.append(title_Url);
   discussionContent.append(discussion__title__maker);
 
-  // discussionAnswered(작성자와 날짜) 넣기
+  // discussionContent에 작성자와 날짜 넣기
   const discussion__information__maker = document.createElement("div");
   discussion__information__maker.className =  "discussion__information"
-  discussion__information__maker.textContent = agoraStatesDiscussions[i].id + ' / ' + agoraStatesDiscussions[i].createdAt;
+  discussion__information__maker.textContent = obj.id + ' / ' + obj.createdAt;
   discussionContent.append(discussion__information__maker);
 
-  // 체크박스 넣기 answer=null이라면 네모칸만 
+  // discussionAnswered에 체크박스 넣기 
+  // answer=null이라면 네모칸만 
   const checkbox__maker = document.createElement("p");
-  if(agoraStatesDiscussions[i].answer === null){
+  if(obj.answer === null){
     checkbox__maker.textContent = "☒";
   }
   else{
@@ -49,14 +48,12 @@ const convertToDiscussion = (obj) => {
   }
   discussionAnswered.append(checkbox__maker)
 
-
-
+  
   li.append(avatarWrapper, discussionContent, discussionAnswered);
 
-  const ul = document.querySelector('ul.discussions__container');
-ul.append(li);
+  const ul = document.querySelector("ul.discussions__container");
+  ul.append(li);
 
-  }
 };
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
@@ -64,9 +61,24 @@ const render = (element) => {
   for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
     element.append(convertToDiscussion(agoraStatesDiscussions[i]));
   }
-  return;
 };
 
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+
+// date.js양식처럼 값을가져와 convertToDiscussion로 돌린다.
+// let으로 변경가능하게 바꾼다.
+// let submitForm = {
+//   name:"1",
+//   createdAt:"2",
+//   titie:"3",
+//   url:"4",
+//   author:"5",
+//   answer:"6",
+//   bodyHTML:"7",
+//   avatarUrl:"https://avatars.githubusercontent.com/u/79903256?s=64&v=4",
+// }
+
+// convertToDiscussion(submitForm)
