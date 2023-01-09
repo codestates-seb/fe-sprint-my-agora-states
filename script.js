b9eebc3ca48050b153c2808ed3ac572c42d7f250
khkh0109 <ist200120@gmail.com>
2023-01-09 17:43:24 +0900

    feat: 현지시간 적용 기능 추가

--- script.js
+++ script.js
@@ -71,7 +71,18 @@ form.addEventListener("submit", (event) => {
   inputTextbox.value = "";
 
   const data = {};
-  data.createdAt = "2022-04-22T14:08:33Z";
+
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+
+  data.createdAt = `
+  ${year}-${month}-${day} 
+  ${hours}:${minutes}:${seconds}`;
   data.title = title;
   data.url = "";
   data.author = name;
