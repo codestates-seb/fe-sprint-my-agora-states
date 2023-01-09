de6b49b7c9ba0e6593674bc2cfe83fb736722bfe
dakyungoh <dakyung.work@gmail.com>
2023-01-09 23:10:33 +0900

    데이터를 dom object로 변경

--- script.js
+++ script.js
@@ -3,19 +3,40 @@ console.log(agoraStatesDiscussions);
 
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
 const convertToDiscussion = (obj) => {
+  console.log("obj: ", obj);
   const li = document.createElement("li"); // li 요소 생성
   li.className = "discussion__container"; // 클래스 이름 지정
 
+  // avatar
   const avatarWrapper = document.createElement("div");
   avatarWrapper.className = "discussion__avatar--wrapper";
+  avatarWrapper.innerHTML = `
+    <img
+      class="discussion__avatar--image"
+      src=${obj.avatarUrl}
+      style="width: 48px; height: 48px ;"
+      alt="avatar of ${obj.author}"
+    />
+  `;
+
+  // content
   const discussionContent = document.createElement("div");
   discussionContent.className = "discussion__content";
+  discussionContent.innerHTML = `
+    <h2 class="discussion__title">
+      <a href="https://github.com/codestates-seb/agora-states-fe/discussions/6">
+        ${obj.title}
+      </a>
+    </h2>
+    <div class="discussion__information">
+      ${obj.author} / ${obj.createdAt}
+    </div>
+  `;
+
+  // answered
   const discussionAnswered = document.createElement("div");
   discussionAnswered.className = "discussion__answered";
-
-  // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
-
-
+  discussionAnswered.innerHTML = `<p>${obj.answer === null ? "☐" : "☑"}</p>`;
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
@@ -24,7 +45,8 @@ const convertToDiscussion = (obj) => {
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
   for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
-    element.append(convertToDiscussion(agoraStatesDiscussions[i]));
+    const li = convertToDiscussion(agoraStatesDiscussions[i]);
+    element.append(li);
   }
   return;
 };
