4a044b26feac2b5a05b7be0fb34a6a6d19e27af7
leekoby <abc2752@naver.com>
2023-01-09 17:25:18 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // index.html을 열어서 agoraStatesDiscussions 배열 요소를 확인하세요.
-console.log(agoraStatesDiscussions);
-
+// console.log(agoraStatesDiscussions);
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
 const convertToDiscussion = (obj) => {
   const li = document.createElement("li"); // li 요소 생성
@@ -12,6 +11,8 @@ const convertToDiscussion = (obj) => {
   discussionContent.className = "discussion__content";
   const discussionAnswered = document.createElement("div");
   discussionAnswered.className = "discussion__answered";
+
+
 
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
 
@@ -41,13 +42,12 @@ const convertToDiscussion = (obj) => {
   checked.textContent = obj.answer ? "☑︎" : "☒";
   discussionAnswered.append(checked);
 
+
+
+
   li.append(avatarWrapper, discussionContent, discussionAnswered);
-
-
-
   return li;
 };
-
 
 
 
@@ -64,3 +64,42 @@ const render = (element) => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+
+const submitButton = document.querySelector("submit-button")
+
+submitButton.addEventListener("submit", (event) => {
+  event.preventDefault(); //submit 이벤트에서 써줘야한다고 함. 작동정지 방지
+
+  const newAuthor = document.querySelector("form__input--name > input")
+  console.log('author', newAuthor);
+  const newTitle = document.querySelector("form__input--title > input")
+  console.log('title', newTitle);
+  const newTextBox = document.querySelector("form__textbox > textarea")
+  console.log('textBox', newTextBox);
+
+  const newObj = {
+    id: "new id",
+    createdAt: new Date().toLocaleTimeString(),
+    title: newTitle.value,
+    url: "https://github.com/codestates-seb/agora-states-fe/discussions",
+    author: newAuthor.value,
+    bodyHTML: newTextBox.value,
+    avatarUrl: "https://avatars.githubusercontent.com/u/118284808?v=4",
+  }
+
+
+
+  agoraStatesDiscussions.unshift(newObj);
+
+  const discussion = convertToDiscussion(newObj);
+
+  ul.prepend(discussion);
+
+  // submit 후 빈칸 리셋
+  author.value = ""
+  title.value = ""
+  textBox.value = ""
+
+})
+
