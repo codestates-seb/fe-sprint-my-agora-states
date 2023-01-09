7f1dea80784e3933cfd33df57a71759c80263a84
chae030 <ychae1997@gmail.com>
2023-01-09 16:35:53 +0900

    discussion component 완료

--- script.js
+++ script.js
@@ -3,32 +3,69 @@ console.log(agoraStatesDiscussions);
 
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
 const convertToDiscussion = (obj) => {
-  const li = document.createElement("li"); // li 요소 생성
-  li.className = "discussion__container"; // 클래스 이름 지정
-
+  const li = document.createElement("li");
+  li.className = "discussion__container";
+  // * li.discussion__container 생성
   const avatarWrapper = document.createElement("div");
   avatarWrapper.className = "discussion__avatar--wrapper";
   const discussionContent = document.createElement("div");
   discussionContent.className = "discussion__content";
   const discussionAnswered = document.createElement("div");
   discussionAnswered.className = "discussion__answered";
-
+  // * li 자식으로 div 생성
+  
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
+  const avatarImg = document.createElement("img");
+  avatarImg.className = "discussion__avatar--image";
+  avatarImg.src = obj.avatarUrl;
+  avatarImg.alt = "avatar of " + obj.author;
+  avatarWrapper.append(avatarImg);
+  // ** div.discussion__avatar--wrapper Fin
 
+  const discussionTitle = document.createElement("h2");
+  const link = document.createElement("a");
+  discussionTitle.className = "discussion__title"
+  link.href = obj.url
+  link.textContent = obj.title;
+  discussionTitle.append(link)
+  // *** content-title
+  const discussionInformation = document.createElement("div");
+  discussionInformation.className = "discussion__information";
+  discussionInformation.textContent = `${obj.author} / ${obj.createdAt}`
+  discussionContent.append(discussionTitle, discussionInformation);
+  // *** content-info
+  // ** div.discussion__content Fin
 
+  const p = document.createElement("p")
+  p.textContent = obj.answer ? '⭕️' : '❌';
+  discussionAnswered.append(p)
+  // ** div.discussion__answered Fin
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
+  // * li에 data값을 넣은 div 3개를 붙임
   return li;
 };
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
   for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
-    element.append(convertToDiscussion(agoraStatesDiscussions[i]));
+    element.append(convertToDiscussion(agoraStatesDiscussions[i])); // return li
   }
   return;
 };
 
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
-render(ul);
+render(ul); // return ul.append(li)
+
+
+/*
+1. render 함수에 ul정보 전달
+2. render 함수에서 data.js에 있는 배열의 길이만큼 돌면서,
+   converToDiscussion 함수에 배열의 값들을 인수로 차례로 전달 (매개변수 : obj),
+   converToDiscussion return 값들을 ul에 붙인다.
+3. converToDiscussion return 값은 li.
+   그리고 그 li 안에 세 개의 div가 있다,
+   이 div에는 obj의 프로퍼티 값을 넣음
+*/
+
