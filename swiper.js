5cba05db1100487d9a04663aa0ecedaf0c86d095
jiin9999 <cji3604@gmail.com>
2023-01-09 19:54:47 +0900

    Feat: "bare minimum 조건 충족 커밋"

---
+++ swiper.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 3,
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
