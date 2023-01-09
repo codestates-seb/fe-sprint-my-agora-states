300787210cb7ffb9fd213b29d49fb9147d64cd55
leekoby <abc2752@naver.com>
2023-01-09 15:51:54 +0900

    save

--- script.js
+++ script.js
@@ -15,11 +15,37 @@ const convertToDiscussion = (obj) => {
 
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
 
+  const face = document.createElement('img')
+
+  face.src = obj.avatarUrl;
+  face.className = "discussion__avatar--image"
+  face.alt = "avatar of " + obj.author
+  avatarWrapper.append(face)
+
+
+  const discussionTitle = document.createElement("h2");
+  const titleAnchor = document.createElement("a");
+  titleAnchor.href = obj.url;
+  titleAnchor.textContent = obj.title;
+  discussionTitle.append(titleAnchor);
+  discussionContent.append(discussionTitle);
+
+
+  const discussionInfo = document.createElement("div");
+  discussionInfo.textContent = `${obj.author} / ${new Date(obj.createAt).toLocaleTimeString()}` // 
+  discussionContent.append(discussionTitle, discussionInfo);
+
+  const checked = document.createElement("p");
+  checked.textContent = obj.answer ? "☑︎" : "☒";
+  discussionAnswered.append(checked);
+
+  li.append(avatarWrapper, discussionContent, discussionAnswered);
 
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
 };
+
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
