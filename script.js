4d61a050542e2ad88ab4645c178042c0446cd33b
dev-Jaewon <app235naver@gmail.com>
2023-01-09 17:55:26 +0900

    refact discussion render 함수

--- script.js
+++ script.js
@@ -1,32 +1,47 @@
-// index.html을 열어서 agoraStatesDiscussions 배열 요소를 확인하세요.
-console.log(agoraStatesDiscussions);
+const createEle = (tag, className, options = {}) => {
+    const Element = document.createElement(tag);
+    Element.className = className;
+
+    for (const [key, value] of Object.entries(options)) {
+        Element[key] = value;
+    }
+
+    return Element;
+};
 
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
 const convertToDiscussion = (obj) => {
-  const li = document.createElement("li"); // li 요소 생성
-  li.className = "discussion__container"; // 클래스 이름 지정
+    const li = createEle("li", "discussion__container"); // li 요소 생성
 
-  const avatarWrapper = document.createElement("div");
-  avatarWrapper.className = "discussion__avatar--wrapper";
-  const discussionContent = document.createElement("div");
-  discussionContent.className = "discussion__content";
-  const discussionAnswered = document.createElement("div");
-  discussionAnswered.className = "discussion__answered";
+    const avatarWrapper = createEle("div", "discussion__avatar__wrapper");
+    avatarWrapper.style.backgroundImage = `url(${obj.avatarUrl})`;
 
-  // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
+    const discussionAuthor = createEle("h3", "discussion__title", {
+        textContent: obj.author,
+    });
+    const discussionCreateAt = createEle("div", "discussion__create__At", {
+        textContent: obj.createdAt,
+    });
 
+    const discussionWrittenInfo = createEle("div", "discussion__Written__Info");
+    discussionWrittenInfo.append(discussionCreateAt, discussionAuthor);
 
+    const discussionContent = createEle("div", "discussion__content");
+    const title = createEle("h3", "title", { textContent: obj.title });
 
-  li.append(avatarWrapper, discussionContent, discussionAnswered);
-  return li;
+    discussionContent.append(title, discussionWrittenInfo);
+
+    const discussionAnswered = createEle("div", "discussion__answered");
+
+    li.append(avatarWrapper, discussionContent, discussionAnswered);
+    return li;
 };
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
-  for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
-    element.append(convertToDiscussion(agoraStatesDiscussions[i]));
-  }
-  return;
+    for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
+        element.append(convertToDiscussion(agoraStatesDiscussions[i]));
+    }
 };
 
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
