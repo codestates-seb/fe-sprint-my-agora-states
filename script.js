62407b228b8cf3acac43999c45b4fccaef90bc73
chochojj <csgy114@gmail.com>
2023-01-09 21:45:23 +0900

    css js
    확인

--- script.js
+++ script.js
@@ -47,9 +47,9 @@ const convertToDiscussion = (obj) => {
 
 
 
-  li.append(avatarWrapper, discussionContent, discussionAnswered);
-  return li;
-};
+//   li.append(avatarWrapper, discussionContent, discussionAnswered);
+//   return li;
+// };
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
