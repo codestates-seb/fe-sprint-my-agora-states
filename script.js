1aa29c865620ebc836d35d2bf7b5bb14d99497dc
kimsh322 <kimsh91322@gmail.com>
2023-01-09 17:18:15 +0900

    페이지네이션 추가

--- script.js
+++ script.js
@@ -47,13 +47,99 @@ const convertToDiscussion = (obj) => {
 };
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
-const render = (element) => {
-  for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
-    element.append(convertToDiscussion(agoraStatesDiscussions[i]));
-  }
-  return;
-};
+// const render = (element) => {
+//   for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
+//     element.append(convertToDiscussion(agoraStatesDiscussions[i]));
+//   }
+//   return;
+// };
 
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
-render(ul);
+//render(ul);
+
+// 페이지네이션 구현
+const buttons = document.querySelector(".buttons");
+const showContent = 6; //한페이지 최대 글 개수
+const showButton = 5; // 한페이지 페이지버튼 개수
+let page = 1; // 첫페이지
+const maxPage = Math.ceil(agoraStatesDiscussions.length / showContent); //최대 페이지 수
+
+// 페이지 버튼 생성
+const makeButton = (id) => {
+  const button = document.createElement("button");
+  button.classList.add("button");
+  button.dataset.num = id;
+  button.innerText = id;
+  button.addEventListener("click", (e) => {
+    //버튼 클릭시 class 조정
+    Array.prototype.forEach.call(buttons.children, (button) => {
+      if (button.dataset.num) button.classList.remove("active");
+    });
+    e.target.classList.add("active");
+    renderContent(parseInt(e.target.dataset.num));
+  });
+  return button;
+};
+
+const renderContent = (page) => {
+  // 목록 리스트 초기화
+  while (ul.hasChildNodes()) {
+    ul.removeChild(ul.lastChild);
+  }
+  // 글의 최대 개수를 넘지 않는 선에서, 화면에 최대 6개의 글 생성
+  for (
+    let id = (page - 1) * showContent + 1;
+    id <= page * showContent && id <= agoraStatesDiscussions.length;
+    id++
+  ) {
+    ul.appendChild(convertToDiscussion(agoraStatesDiscussions[id - 1]));
+  }
+};
+
+// 페이지이동
+const goPrevPage = () => {
+  page -= showButton;
+  render1(page);
+};
+
+const goNextPage = () => {
+  page += showButton;
+  render1(page);
+};
+
+const prev = document.createElement("button");
+prev.classList.add("button", "prev");
+prev.innerHTML = "<-";
+prev.addEventListener("click", goPrevPage);
+
+const next = document.createElement("button");
+next.classList.add("button", "next");
+next.innerHTML = "->";
+next.addEventListener("click", goNextPage);
+
+const renderButton = (page) => {
+  // 버튼 리스트 초기화
+  while (buttons.hasChildNodes()) {
+    buttons.removeChild(buttons.lastChild);
+  }
+  // 화면에 최대 5개의 페이지 버튼 생성
+  for (let id = page; id < page + showButton && id <= maxPage; id++) {
+    buttons.appendChild(makeButton(id));
+  }
+  // 첫 버튼 활성화(class="active")
+  buttons.children[0].classList.add("active");
+
+  buttons.prepend(prev);
+  buttons.append(next);
+
+  // 이전, 다음 페이지 버튼이 필요한지 체크
+  if (page - showButton < 1) buttons.removeChild(prev);
+  if (page + showButton > maxPage) buttons.removeChild(next);
+};
+
+const render1 = (page) => {
+  renderContent(page);
+  renderButton(page);
+};
+render1(page);
