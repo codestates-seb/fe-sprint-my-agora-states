47cf0dcf892d77e018ecb43c2b77e2163ba6b87e
unknown <kgsol3242@gmail.com>
2023-01-09 20:21:34 +0900

    나열하기 완료

--- script.js
+++ script.js
@@ -12,11 +12,41 @@ const convertToDiscussion = (obj) => {
   discussionContent.className = "discussion__content";
   const discussionAnswered = document.createElement("div");
   discussionAnswered.className = "discussion__answered";
-
+  // 이미지
+  const avaterImg = document.createElement('img');
+  avaterImg.className = "discussion__avatar--image"
+    avaterImg.src = obj.avatarUrl;
+    avaterImg.alt = "avatar of " + obj.author;
+  // 이미지 append
+  avatarWrapper.append(avaterImg);
+  
+  // 타이틀
+  const discussionTitle = document.createElement("h2")
+  discussionTitle.className = "discussion__title"
+  const titleHref = document.createElement("a");
+  titleHref.href = obj.url;
+  titleHref.textContent = obj.title
+  // 하이퍼링크 append
+  discussionTitle.append(titleHref)
+  // 날짜와 작성자
+  const discussionInformation = document.createElement("div")
+  discussionInformation.className = "discussion__information"
+    discussionInformation.textContent = obj.author + " / " + obj.createdAt
+  // 컨텐츠 어펜드
+  discussionContent.append(discussionTitle, discussionInformation)
+  // 답변
+  discussionAnswered.textContent = answer(obj.answer)
+  discussionAnswered.className = "discussion__answered"
+  function answer(nu){
+    if (nu === null){
+      return "☒"
+    } else {
+      return "☑"
+    }
+  } 
+  
+  
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
-
-
-
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
 };
@@ -32,3 +62,4 @@ const render = (element) => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
