4c4a238e68d233d017ba0f2c724ecd14432df813
zi0ne <jiwon0331j@gmail.com>
2023-01-10 02:03:33 +0900

    Bare Minimun js

--- script.js
+++ script.js
@@ -12,19 +12,43 @@ const convertToDiscussion = (obj) => {
   discussionContent.className = "discussion__content";
   const discussionAnswered = document.createElement("div");
   discussionAnswered.className = "discussion__answered";
-
+  
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
+  const discussionAvatarImg = document.createElement('img');
+  discussionAvatarImg.className = "discussion__avatar--image";
+  discussionAvatarImg.setAttribute('src',obj.avatarUrl);
+  
+  const discussionTitle = document.createElement('h2');
+  discussionTitle.className = "discussion__title";
+  
+  const discussionTitleA = document.createElement('a');
+  discussionTitleA.setAttribute('href',obj.url)
+  discussionTitleA.textContent = obj.author + '/' + obj.createdAt;
+  
+  const discussionInform = document.createElement('div');
+  discussionInform.className = "discussion__information";
+  discussionInform.textContent = obj.title;
+  
+  const discussionCheck = document.createElement('p');
+  discussionCheck.textContent = '☑';
 
-
+  discussionContent.appendChild(discussionTitle);
+  discussionTitle.appendChild(discussionTitleA);
+  discussionContent.appendChild(discussionInform);
+  avatarWrapper.appendChild(discussionAvatarImg);
+  discussionAnswered.appendChild(discussionCheck);
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
+
 };
+
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
   for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
     element.append(convertToDiscussion(agoraStatesDiscussions[i]));
+
   }
   return;
 };
