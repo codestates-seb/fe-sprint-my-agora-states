cbd286cc41d8cad624c35194835a32aeeb203ec3
YujunSun0 <cons202@gmail.com>
2023-01-10 10:22:39 +0900

    제출한 리스트의 시간 표시

--- script.js
+++ script.js
@@ -56,3 +56,32 @@ const render = (element) => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+// 질문 입력창에 입력 후 제출하면 화면에 표시하기.
+
+const form = document.querySelector(".form");
+const inputName = document.querySelector("div.form__input--name > input");
+const title = document.querySelector("div.form__input--title > input");
+const textBox = document.querySelector("#story");
+
+// submit 이벤트가 발생할 때 새로운 객체 생성 (입력한 값을 받는)
+form.addEventListener("submit", (event) => {
+  event.preventDefault(); // submit이 발생할 때 기본으로 실행되는 새로고침을 막아줌
+
+  const obj = {
+    id: "YujunSun0",
+    createdAt: new Date().toISOString(),
+    title: title.value,
+    url: "https://github.com/YujunSun0",
+    author: inputName.value,
+    answer: null,
+    bodyHTML: textBox.value,
+    avatarUrl:
+      "https://mblogthumb-phinf.pstatic.net/MjAxOTAyMjBfNjMg/MDAxNTUwNjQzMzUzODUx.wh73kZENlQlYp99uENRo5gNrJ54EWGiSg0lKIq35YLsg.CPJq7bL6r7AHcR0hTpE9bjtrOfjkbFUCpbOPFtVVGngg.JPEG.moonlee97/IMG_20180910_200713.jpg?type=w800",
+  };
+  agoraStatesDiscussions.unshift(obj); // 배열의 맨앞에 객체를 집어넣음
+  const newDiscussion = convertToDiscussion(obj); // 위에서 만든 discussion을 추가해주는 함수 적용
+  ul.prepend(newDiscussion); // ul의 first-child 앞에 추가
+});
+
+// 내일 할 것 : 시간 고치기(O) , submit 누르면 값 초기화 시켜주기, css++ / 심화과정
