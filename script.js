f28d0c53f2f3e626834952622335ebff3a97d4f8
moonjiyoung <umarg228@naver.com>
2023-01-10 22:15:34 +0900

    디스커션추가까지 완료

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ const convertToDiscussion = (obj) => {
   //alt= author
   photo.src = obj.avatarUrl;
   photo.alt = 'avatar of' + obj.author;
+  photo.title = obj.id; //마우스 오버하면 작성자의 아이디
   avatarWrapper.append(photo);
 
   //가운데내용
@@ -80,10 +81,10 @@ render(ul);
 //그게 공중부양하면 append하면 밑에 창에 보여지겠지
 
 /* 그럼 일단 create해보자 */
-const newForm = document.createElement("div") //밑에꺼 전체 집합이라고 생각했다
-const newName = document.createElement("form__input--name > input");
-const newTitle = document.createElement("form__input--title > input");
-const newTextbox = document.createElement("form__textbox > textarea");
+//const newForm = document.createElement("div") //밑에꺼 전체 집합이라고 생각했다
+//const newName = document.createElement("form__input--name > input");
+//const newTitle = document.createElement("form__input--title > input");
+//const newTextbox = document.createElement("form__textbox > textarea");
 //현재 공중부양상태이다 
 
 /*연결하게되면 */
@@ -98,3 +99,31 @@ const newTextbox = document.createElement("form__textbox > textarea");
 
  // }
 //)
+const form = document.querySelector("form.form");
+const author = form.querySelector("div.form__input--name > input");
+const title = form.querySelector("div.form__input--title > input");
+const textbox = form.querySelector("div.form__textbox > textarea");
+
+//submit이라는 이벤트가 있다.
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const obj = {
+    id: "new id" ,
+    createdAt : new Date().toISOString(),  //toISOString() datajs에 써잇는 시간식으로 표현하는방법때문이다
+    title: title.value, 
+    url: "https://github.com/codestates-seb/agora-states-fe/discussions" ,
+    author: author.value, 
+    answer : null,
+    bodyHTML: textbox.value ,
+    avatarUrl: "https://avatars.githubusercontent.com/u/90553688?s=64&u=3c4e4dc2053d4977ac12b9cfc2667582f986d3d8&v=4",
+  };
+  //만든 배열을 data더미의 배열에 추가해준다(맨앞요소로)
+  agoraStatesDiscussions.unshift(obj);
+//전체 데이터 더미를 ul로 append해준다( 근데 맨앞으로니까 prepend)
+ul.prepend(convertToDiscussion(obj));
+//입력한 값을 submit에서 댓글창으로 append되면 값을 초기화한다.
+author.value = " ";
+title.value = " ";
+textbox.value = " ";
+
+});
