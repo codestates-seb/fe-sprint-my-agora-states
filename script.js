c58db87b912af26f8db01fae6ee87568feedf5e7
hyena <108929456+hazellee0914@users.noreply.github.com>
2023-01-10 10:59:29 +0900

    🔧update

--- script.js
+++ script.js
@@ -13,6 +13,9 @@ const convertToDiscussion = (obj) => {
   const discussionAnswered = document.createElement("div");
   discussionAnswered.className = "discussion__answered";
 
+
+
+
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
 
   const avatarImg = document.createElement('img');
@@ -32,7 +35,7 @@ const convertToDiscussion = (obj) => {
 
   const discussionInfo = document.createElement("div");
   discussionInfo.className = "discussion__information";
-  discussionInfo.textContent = obj.author + " / " + obj.createdAt;
+  discussionInfo.textContent = `${obj.author} / ${new Date(obj.createdAt).toLocaleTimeString()}`;
   discussionContent.append(discussionTitle, discussionInfo);
 
   const answeredIcon = document.createElement("p");
@@ -48,6 +51,7 @@ const convertToDiscussion = (obj) => {
 const render = (element) => {
   for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
     element.append(convertToDiscussion(agoraStatesDiscussions[i]));
+    // console.log(agoraStatesDiscussions[i].answer);
   }
   return;
 };
@@ -55,3 +59,44 @@ const render = (element) => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+//submit 처리 함수
+let inputName = document.querySelector(".form__input--name input");
+let inputTitle = document.querySelector(".form__input--title input");
+let inputStory = document.querySelector(".form__textbox textarea");
+let submitForm = document.querySelector("form");
+let submitBtn = document.querySelector(".form__submit input");
+
+function handleSubmit(e) {
+  // 브라우저 기본 동작 멈추기
+  e.preventDefault();
+  const newDiscussion = {
+      Id: null,
+      createdAt: null,
+      title: null,
+      url: null,
+      author: null,
+      answer: null,
+      bodyHTML: null,
+      avatarUrl:"avatar.jpeg"
+  };
+
+  newDiscussion.title = inputTitle.value;
+  newDiscussion.author = inputName.value;
+  newDiscussion.createdAt = new Date().toISOString();  //현재 시간
+
+  inputTitle.value = '';
+  inputName.value = '';
+  inputStory.value = '';
+  
+  agoraStatesDiscussions.unshift(newDiscussion);
+  console.log("동작");
+  console.log(agoraStatesDiscussions);
+  const ul = document.querySelector("ul.discussions__container");
+  ul.prepend(convertToDiscussion(agoraStatesDiscussions[0]));
+  console.log(submitForm);
+
+}
+console.log(handleSubmit);
+submitForm.addEventListener('submit', handleSubmit);
+
