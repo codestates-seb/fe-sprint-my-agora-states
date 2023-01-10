2698b870f2f9dd4512fe554babb775e338256603
leekc3813 <93689662+leekc3813@users.noreply.github.com>
2023-01-10 10:52:10 +0900

    bareminium
    베어미니엄

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const convertToDiscussion = (obj) => {
   const li = document.createElement("li"); // li 요소 생성
   li.className = "discussion__container"; // 클래스 이름 지정
 
+  /* index객체에 data.js를 넣을 함수지정  */
   const avatarWrapper = document.createElement("div");
   avatarWrapper.className = "discussion__avatar--wrapper";
   const discussionContent = document.createElement("div");
@@ -14,12 +15,34 @@ const convertToDiscussion = (obj) => {
   discussionAnswered.className = "discussion__answered";
 
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
+  // 이미지 부분 
+  const face = document.createElement("img") // 프로필 사진
+  face.src = obj.avatarUrl;
+  face.alt = "avatar of" + obj.author;
+  avatarWrapper.append(face);
 
+  const discussionTitle = document.createElement("h2");
+  const titleAnchor = document.createElement("a");
+  titleAnchor.href = obj.url;
+  titleAnchor.textContent = obj.title;
+  discussionTitle.append(titleAnchor);
+  discussionContent.append(discussionTitle);
+  
+
+  const discussionInfo = document.createElement("div");
+  discussionInfo.textContent = `${obj.author} / ${new Date(obj.createAt).toLocaleTimeString()}` // 날짜 표현 형식이 여러개가 있는데 이걸 제일 많이  쓴다
+  discussionContent.append(discussionTitle, discussionInfo);
+
+  // 체크 부분
+  const checkBox = document.createElement('p');
+  checkBox.textContent = obj.answer ? "☑︎" : "☒";
+  discussionAnswered.append(checkBox);
 
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
-  return li;
+  return li; // il요소를 위의 함수를 통해 가공해서 append
 };
+
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
