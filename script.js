c3e72fa77ba62a769f55e91b72d9797b992b0e6c
zi0ne <jiwon0331j@gmail.com>
2023-01-10 16:28:46 +0900

    SEB_FE_43 박지원 과제제출합니다

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ const convertToDiscussion = (obj) => {
   // 아바타의 이미지가 없을때 고정 이미지 사용법
 
   
-  const discussionTitle = document.createElement('h2');
+  const discussionTitle = document.createElement('h3');
   discussionTitle.className = "discussion__title";
   
   const discussionTitleA = document.createElement('a');
@@ -32,7 +32,10 @@ const convertToDiscussion = (obj) => {
   
   const discussionInform = document.createElement('div');
   discussionInform.className = "discussion__information";
-  discussionInform.textContent = obj.author + '/' + obj.createdAt; // 템플릿리터럴로도 가능
+  const koreanDate = new Date(obj.createdAt); //  obj.createAt의 UTC시간을 koreanDate에 저장
+  discussionInform.textContent = `${obj.author} / ${koreanDate.toLocaleTimeString()}`;
+  //toLocalTimeString은 UTC로 표현된 시간을 한국의 표준 시간 오전(후) h:m:s로 나타내는 Date객체의 내장 메서드이다.
+  //discussionInform.textContent = obj.author + '/' + obj.createdAt; // 템플릿리터럴로도 가능
   
   const discussionCheck = document.createElement('p');
   // 조건문으로 answer이 null인지 아닌지 확인해야함.
@@ -49,16 +52,77 @@ const convertToDiscussion = (obj) => {
 
 };
 
+//디스커션유지
+
+
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
   for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
     element.append(convertToDiscussion(agoraStatesDiscussions[i])); // 아고라 디스커션의 0번째 배열부터 함수시작
-
   }
   return;
 };
 
+// const render = (element,arr) => {
+//   for(let i of arr){
+//     element.append(convertToDiscussion(i));
+//   }
+//   return;
+// };
+
+//페이지네이션 10개씩
+
+function pagenation() {
+  let dataLength = agoraStatesDiscussions.length;
+  if(dataLength <= 10) return;
+  let totalPage = Math.ceil(dataLength/10); //5
+  let pageGroup = Math.ceil(totalPage/3); //2
+
+
+
+
+  let makepage = document.querySelector('.pageNum');
+
+  for(let i = 1;i <=totalPage;i++){
+    const page = document.createElement('li');
+    page.className= `page${i}`;
+    const pagesrc = document.createElement('a');
+    pagesrc.setAttribute('target','__blank');
+    pagesrc.textContent = `${i}`;
+    pagesrc.addEventListener('click',evt1);
+    page.append(pagesrc);
+    makepage.append(page);
+  }
+    let pagingArr= [];
+    if(totalPage >= 1)
+    for(let i =0;i <= dataLength; i+=10){
+
+      pagingArr.push(agoraStatesDiscussions.slice(i,i+10));
+      console.table(pagingArr);
+  } 
+      console.table(pagingArr.length)
+    return pagingArr
+}
+
+function evt1() {
+
+  const num = this.textContent;
+  let qnum = document.querySelector("discussions__container").childElementCount;
+  console.log(document.querySelector('discussions__container').hasChildNodes())
+  if(document.querySelector('.discussions__container').childElementCount){
+    while(document.querySelector('.discussions__container').childElementCount !== 0){
+      document.querySelector('.discussions__container').removeChild(document.querySelector('.discussion__container'));
+    }
+
+  }
+  render(ul,pagearr[num-1]);
+}
+
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+pagearr = pagenation();
+
+
