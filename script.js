4064173550ae7508bb71f3d741683b5bba9f041e
pizzaYami <cyj9407@naver.com>
2023-01-10 16:59:24 +0900

    last

--- script.js
+++ script.js
@@ -35,7 +35,24 @@ const convertToDiscussion = (obj) => {
   // discussionContent에 작성자와 날짜 넣기
   const discussion__information__maker = document.createElement("div");
   discussion__information__maker.className =  "discussion__information"
-  discussion__information__maker.textContent = obj.author + ' / ' + obj.createdAt;
+  //현지시간으로 바꾸기
+  let submitTime = obj.createdAt.slice(-9,-1);
+  let submitTimes = submitTime.split(":");
+  let hour = submitTimes[0]
+  let min = submitTimes[1]
+  let sec = submitTimes[2]
+console.log(hour,min,sec)
+  if(hour >= 12){
+    console.log(hour)
+    hour = hour - 12;
+    console.log(hour)
+    submitTime = `오후 ${hour}:${min}:${sec}`;
+  }else{
+    submitTime 
+    = `오전 ${hour}:${min}:${sec}`;
+  }
+
+  discussion__information__maker.textContent = obj.author + ' / ' + submitTime;
   discussionContent.append(discussion__information__maker);
 
   // discussionAnswered에 체크박스 넣기 
@@ -56,14 +73,14 @@ const convertToDiscussion = (obj) => {
 };
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
-
 const render = (element) => {
-  for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
+  for (let i = agoraStatesDiscussions.length-1; i >= 1; i--) {
     // return li를 안해서 여기서 자꾸 undefined가 떳다.
     element.append(convertToDiscussion(agoraStatesDiscussions[i]));
   }
   return;
 }
+
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
@@ -71,15 +88,49 @@ render(ul);
 
 // date.js양식처럼 값을가져와 convertToDiscussion로 돌린다.
 // let으로 변경가능하게 바꾼다.
-// unshift를 통해서 넣어준다. 
-let submitForm = {
-  name:"1",
-  createdAt:"2",
-  titie:"3",
-  url:"4",
-  author:"5",
-  answer:"6",
-  bodyHTML:"7",
-  avatarUrl:"https://avatars.githubusercontent.com/u/79903256?s=64&v=4",
+// unshift로 넣어준다.
+// const submitForm = {
+//   id : "1",
+//   createdAt :"2",
+//   titie : 'story',
+//   url :"",
+//   author : 'aa',
+//   answer : "",
+//   bodyHTML: "",
+//   avatarUrl : "https://avatars.githubusercontent.com/u/116322645?v=4",
+// }
+
+// 이게 맞나?
+function InputInform(name, title, story){
+this.id = name;
+this.author = name;
+this.createdAt = new Date().toLocaleDateString();
+this.title = title;
+this.story = story;
+this.story = story;
+this.avatarUrl = "https://avatars.githubusercontent.com/u/116322645?v=4";
 }
 
+// 날짜 양식 바꾸기
+// let today = new Date();
+// console.log(today)
+// let dateFormat1 = today.getFullYear()+'-'+(today.getMonth()+1)+'T'+date.getHours();+':'+date.getMinutes();+':'+today.getSeconds()+'Z'
+
+const submitBtn = document.querySelector('.form')
+
+submitBtn.addEventListener('submit', (e) => {
+
+e.preventDefault();
+//submit 입력 값 뽑아내기 
+nameText = document.querySelector('#name');
+titleText = document.querySelector('#title');
+storyText = document.querySelector('#story');
+
+const newObj = new InputInform(nameText.value, titleText.value, storyText.value);
+agoraStatesDiscussions.unshift(newObj);
+
+const discussion = convertToDiscussion(agoraStatesDiscussions[0],0);
+ul.prepend(discussion)
+
+storyText.value = titleText.value = nameText.value = '';
+});
