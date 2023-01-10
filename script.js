a11a17e7723f1855c773148ae0792c677f6b2bc5
moonjiyoung <umarg228@naver.com>
2023-01-10 14:24:47 +0900

    일단 베이미니멈까지완료한듯

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ const convertToDiscussion = (obj) => {
   discussionInformation.textContent = `${obj.author} / ${new Date(obj.createdAt).toLocaleTimeString()}`;
   //author / createdAt
   // toLocaleDateString() 
-  // new Date(obj.createdAt).toLocaleTimeString()
+  // new Date().toLocaleTimeString()
   discussionContent.append(discussionTitle, discussionInformation);
 
 
@@ -62,3 +62,23 @@ const render = (element) => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+//submit을 클릭하면 밑에항목에 추가된다.
+//이름은 <div class="discussion__information">kimploo 
+//title은 <h2 class="discussion__title">
+//your question의 textarea는 a href
+
+let article_index = document.querySelectorAll("label");
+let article_name = article_index[0];
+discussionTitle = article_index[1];
+
+article_name.onclick = handleClick;
+discussionTitle.onclick = handleClick;
+
+function handleClick() {
+
+}
+
+// handleclick하면 name이 kimploo 자리에 채워지고
+// title이 좋은질문하는법에 채워지고
+//question이 url로 연결된다
