4337b99e89e6cdaf9c316a80be05344d3a0d2561
yunhwan98 <79241793+yunhwan98@users.noreply.github.com>
2023-01-10 09:30:56 +0900

    깃허브 이미지 불러오기 기능

--- modal.js
+++ modal.js
@@ -3,9 +3,20 @@ const formInputName = formInput.querySelector(".form__input--name");
 const formInputTitle = formInput.querySelector(".form__input--title");
 const formInputTextbox = formInput.querySelector(".form__textbox");
 const formBtn = document.querySelector(".form__submit").querySelector("input");
+let formImg;
 
-console.log(formBtn);
+//깃허브 이미지 불러오기
+fetch("https://api.github.com/users/yunhwan98")
+  .then((response) => response.json())
+  .then((data) => {
+    formImg = data;
+
+    console.log(data);
+  });
+
+//제출 버튼 클릭시 실행
 formBtn.onclick = function (event) {
+  //새로고침 방지
   event.preventDefault();
   const inputName = formInputName.querySelector("#name").value;
   const inputTitle = formInputTitle.querySelector("#name").value;
@@ -13,14 +24,13 @@ formBtn.onclick = function (event) {
   const obj = {
     author: inputName,
     title: inputTitle,
+    avatarUrl: formImg.avatar_url,
     createdAt: Date(),
   };
+  //
   if (inputName && inputTitle && inputTextbox) {
-    console.log(inputName, inputTitle, inputTextbox);
-    //console.log(convertToDiscussion(obj));
-
-    ul.append(convertToDiscussion(obj));
-    //console.log(ul);
-    //render(ul);
+    //console.log(inputName, inputTitle, inputTextbox);
+    //글을 맨앞으로 추가
+    ul.prepend(convertToDiscussion(obj));
   }
 };
