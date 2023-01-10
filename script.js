3fff191af96ea2bed3e60b9600e7ed12ec32954b
SEB_FE_43_김현진 <sossos1191@gmail.com>
2023-01-10 15:33:30 +0900

    css수정 추가

--- script.js
+++ script.js
@@ -6,14 +6,11 @@ console.log(agoraStatesDiscussions);
 const convertToDiscussion = (obj) => {
   const li = document.createElement("li"); // li 요소 생성
   li.className = "discussion__container"; // 클래스 이름 지정
-  // avatarWrapper > img
-  const avatarWrapper = document.createElement("div");  // div요소 생성
-  avatarWrapper.className = "discussion__avatar--wrapper";  // div요소 클랙스명 지정
-  // discussionContent > info, title>href
-  const discussionContent = document.createElement("div");
+  const avatarWrapper = document.createElement("div");  // div요소 생성 avatarWrapper > img
+  avatarWrapper.className = "discussion__avatar--wrapper"; 
+  const discussionContent = document.createElement("div"); // discussionContent > info, title>href
   discussionContent.className = "discussion__content";
-  // discussionAnswered > p태그
-  const discussionAnswered = document.createElement("div");
+  const discussionAnswered = document.createElement("div"); // discussionAnswered > p태그
   discussionAnswered.className = "discussion__answered";
 
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
@@ -60,15 +57,14 @@ const convertToDiscussion = (obj) => {
   return li;
 };
 
-// ul을 인자로 render실행 
-// -> 해당ul에 더미배열 [i]값 반환 
-// -> convertToDiscussion함수가 더미[i]를 인자로 사용
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
-const render = (element) => {
+const render = (element) => { // ul > 더미배열 [i]값 반환
   for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
-    element.append(convertToDiscussion(agoraStatesDiscussions[i]));
+    // console.log(JSON.stringify(agoraStatesDiscussions[i]));
+    element.append(convertToDiscussion(agoraStatesDiscussions[i])); // 더미[i]를 인자로 사용
   }
   return;
+  
 };
 
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
@@ -82,10 +78,13 @@ render(ul);
 // 1. 선택 할 쿼리셀렉터(클랙스명 or 아이디) -> 서브밋 클릭시 추가시킬 함수.
 const form = document.querySelector('form'); // 작성세션 전체
 form.addEventListener('submit', (event) => {
-  event.preventDefault();
+  // const target = event.target;
+  // const action = target.classList[0]; // 클릭된 HTML 엘리먼트에 클레스 정보를 가져옵니다.
+  // const buttonContent = target.textContent; // 클릭된 HTML 엘리먼트의 텍스트 정보를 가져옵니다.
+  event.preventDefault(); // 기본동작 정지
   // 대상쿼리 잡기
-  const formName = document.querySelector('.form__input--name #name') // .value;  .value까지 쓰면 초기화코드가 길어짐.
-  const formTitle = document.querySelector('.form__input--title #title')  // .value;  생각해보면 메소드까지 변수화시키면 별로인듯. 매번 변수만들기 귀찮음
+  const formName = document.querySelector('.form__input--name #name') // .value;  .value까지 쓰면 초기화코드가 길어짐. 정적인 변수가 됨.
+  const formTitle = document.querySelector('.form__input--title #title')  // .value;  매번 변수만들기 귀찮음
   const formBox = document.querySelector('.form__textbox #story') // .value;
   // console.log(formName);
   const obj = {
@@ -112,4 +111,56 @@ form.addEventListener('submit', (event) => {
 
 
 // 로컬 스토리지 구축해보기
+// localStorage.setItem(local['qwe'], 'qweqwe')
+// localStorage.getItem(local['qwe'])
 // 페이징처리.
+
+
+
+// const obj = {}
+//   localStorage.setItem(obj['id'], 'id 불명'),
+//   localStorage.setItem(obj['createdAt'], new Date()),
+//   localStorage.setItem(obj['title'], formTitle.value),
+//   localStorage.setItem(obj['url'], "https://github.com/codestates-seb/agora-states-fe/discussions/44"),
+//   localStorage.setItem(obj['author'], formName.value),
+//   localStorage.setItem(obj['answer'], null),
+//   localStorage.setItem(obj['bodyHTML'], formBox.value),
+//   localStorage.setItem(obj['avatarUrl'], "https://avatars.githubusercontent.com/u/97888923?s=64&u=12b18768cdeebcf358b70051283a3ef57be6a20f&v=4"),
+
+//   localStorage.getItem(obj['id']),
+//   localStorage.setItem(obj['createdAt']),
+//   localStorage.setItem(obj['title']),
+//   localStorage.setItem(obj['url']),
+//   localStorage.setItem(obj['author']),
+//   localStorage.setItem(obj['answer']),
+//   localStorage.setItem(obj['bodyHTML']),
+//   localStorage.setItem(obj['avatarUrl'])
+
+  // localStorage에 저장할 객체
+// const obj = {
+//   name : 'anna',
+//   age : 20
+// }
+ 
+// // localStorage에 저장할 배열
+// const arr = [1, 2, 3];
+ 
+// // 객체, 배열을 JSON 문자열로 변환
+// const objString = JSON.stringify(obj);
+// const arrObj = JSON.stringify(agoraStatesDiscussions[i]);
+// console.log(objArr);
+// 아고라스테이츠디스커션[i번]['키'] -> string변환
+// 변환 한 string타입 변수에 잘 담아서 뿌려주기.
+// 입력한 정보 로컬에 넣기.
+// 문자열변환 -> 셋아이템 키:값 넣기 -> 겟아이템 키로 가져오기.
+ 
+// // setItem
+// window.localStorage.setItem('person', objString);
+ 
+// // getItem
+// const personString = window.localStorage.getItem('person');
+// const numsString = window.localStorage.getItem('nums');
+ 
+// // JSON 문자열을 객체, 배열로 변환
+// const personObj = JSON.parse(personString);
+// const numsArr = JSON.parse(numsString);
