d91940a6f224b08c0efc1956741310f809023a7b
dohyun98 <kch9554@naver.com>
2023-01-10 16:31:50 +0900

    Bare Minimum Requirement

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // index.html을 열어서 agoraStatesDiscussions 배열 요소를 확인하세요.
-console.log(agoraStatesDiscussions);
+// console.log(agoraStatesDiscussions);
 
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
 const convertToDiscussion = (obj) => {
@@ -16,22 +16,26 @@ const convertToDiscussion = (obj) => {
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
 
   const avatar = document.createElement('img');
+  avatar.className = 'discussion__avatar--image';
   avatar.src = obj.avatarUrl;
+  avatar.alt = 'avatar of' + obj.author;
   avatarWrapper.append(avatar);
 
   const content = document.createElement('h2');
   const title = document.createElement('a');
+  title.className = 'discussion__title';
   title.href = obj.url;
   title.innerText = obj.title;
-  content.append(title); // <h2>안에 <a>가 있음.
+  content.append(title);
   discussionContent.append(content);
 
   const information = document.createElement('div');
+  information.className = 'discussion__information';
   information.innerText = `${obj.author} / ${obj.createdAt}`;
   discussionContent.append(information);
 
   const answer = document.createElement('p');
-  answer.innerText = `${obj.answer !== null ? '☑' : '❎'}`;
+  answer.innerText = `${obj.answer !== null ? '✅' : '✏️'}`;
   discussionAnswered.append(answer);
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
@@ -48,3 +52,42 @@ const render = (element) => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector('ul.discussions__container');
 render(ul);
+
+// submit
+const form = document.querySelector('form.form');
+const author = document.querySelector('div.form__input--name > input');
+const title = document.querySelector('div.form__input--title > input');
+const textbox = document.querySelector('div.form__textbox > textarea');
+
+// function saveObj() {
+//   localStorage.setItem(TODOS_KEY, JSON.stringify(obj));
+// }
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const newData = {
+    id: 'user',
+    createdAt: new Date().toLocaleTimeString(),
+    title: title.value, // 타이틀
+    url: 'https://github.com/codestates-seb/agora-states-fe/discussions/45#discussioncomment-2756236',
+    author: author.value, // 이름
+    bodyHTML: textbox.value, // 질문
+    avatarUrl: 'https://avatars.githubusercontent.com/u/79903256?s=64&v=4',
+  };
+
+  agoraStatesDiscussions.unshift(newData);
+  // localStorage.setItem('save', JSON.stringify(agoraStatesDiscussions));
+  // convertToDiscussion(newData);
+
+  const discussion = convertToDiscussion(newData);
+  ul.prepend(discussion);
+
+  // while (ul.firstChild) {
+  //   ul.removeChild(ul.firstChild);
+  // }
+  // render(ul);
+
+  title.value = '';
+  author.value = '';
+  textbox.value = '';
+});
