a1962d6a3fe9e329bf58681e1bdfd420c5e1d5a0
leekoby <abc2752@naver.com>
2023-01-10 11:21:22 +0900

    수정

--- script.js
+++ script.js
@@ -51,14 +51,15 @@ const convertToDiscussion = (obj) => {
 };
 
 
-
-// agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
+// // // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
   for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
     element.append(convertToDiscussion(agoraStatesDiscussions[i]));
   }
   return;
 };
+
+
 
 
 
