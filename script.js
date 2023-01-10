69c1503c67458e440171844e9a1cfcb9a9df56c0
YujunSun0 <cons202@gmail.com>
2023-01-10 15:28:57 +0900

    로컬스토리지 구현 완료

--- script.js
+++ script.js
@@ -65,29 +65,37 @@ const title = document.querySelector("div.form__input--title > input");
 const textBox = document.querySelector("#story");
 
 // submit 이벤트가 발생할 때 새로운 객체 생성 (입력한 값을 받는)
-form.addEventListener("submit", (event) => {
-  event.preventDefault(); // submit이 발생할 때 기본으로 실행되는 새로고침을 막아줌
+// 저장소에 saveObj라는 키의 값이 있을 때 , 없을때를 구분함
+// localStorage 사용
 
-  const obj = {
-    id: "YujunSun0",
-    createdAt: new Date().toISOString(),
-    title: title.value,
-    url: "https://github.com/YujunSun0",
-    author: inputName.value,
-    answer: null,
-    bodyHTML: textBox.value,
-    avatarUrl:
-      "https://mblogthumb-phinf.pstatic.net/MjAxOTAyMjBfNjMg/MDAxNTUwNjQzMzUzODUx.wh73kZENlQlYp99uENRo5gNrJ54EWGiSg0lKIq35YLsg.CPJq7bL6r7AHcR0hTpE9bjtrOfjkbFUCpbOPFtVVGngg.JPEG.moonlee97/IMG_20180910_200713.jpg?type=w800",
-  };
-  agoraStatesDiscussions.unshift(obj); // 배열의 맨앞에 객체를 집어넣음
-  const newDiscussion = convertToDiscussion(obj); // 위에서 만든 discussion을 추가해주는 함수 적용
+if (!localStorage.getItem("saveObj")) {
+  form.addEventListener("submit", (event) => {
+    event.preventDefault(); // submit이 발생할 때 기본으로 실행되는 새로고침을 막아줌
+
+    const obj = {
+      id: "YujunSun0",
+      createdAt: new Date().toISOString(),
+      title: title.value,
+      url: "https://github.com/YujunSun0",
+      author: inputName.value,
+      answer: null,
+      bodyHTML: textBox.value,
+      avatarUrl:
+        "https://mblogthumb-phinf.pstatic.net/MjAxOTAyMjBfNjMg/MDAxNTUwNjQzMzUzODUx.wh73kZENlQlYp99uENRo5gNrJ54EWGiSg0lKIq35YLsg.CPJq7bL6r7AHcR0hTpE9bjtrOfjkbFUCpbOPFtVVGngg.JPEG.moonlee97/IMG_20180910_200713.jpg?type=w800",
+    };
+
+    const saveObj = localStorage.setItem("saveObj", JSON.stringify(obj));
+
+    agoraStatesDiscussions.unshift(obj); // 배열의 맨앞에 객체를 집어넣음
+    const newDiscussion = convertToDiscussion(obj); // 위에서 만든 discussion을 추가해주는 함수 적용
+    ul.prepend(newDiscussion); // ul의 first-child 앞에 추가
+  });
+} else {
+  const loadObj = JSON.parse(window.localStorage.getItem("saveObj"));
+  agoraStatesDiscussions.unshift(loadObj); // 배열의 맨앞에 객체를 집어넣음
+  const newDiscussion = convertToDiscussion(loadObj); // 위에서 만든 discussion을 추가해주는 함수 적용
   ul.prepend(newDiscussion); // ul의 first-child 앞에 추가
-
-  // localStorage 를 이용한 브라우저에 데이터 저장하기
-  window.localStorage.setItem("name", inputName.value);
-  window.localStorage.setItem("title", title.value);
-  window.localStorage.setItem("question", textBox.value);
-});
+}
 
 // 내일 할 것 : 시간 고치기(O) , submit 누르면 값 초기화 시켜주기, css++ / 심화과정
 
@@ -105,9 +113,6 @@ let pageGroup = Math.ceil(currentPage / pageCount); // ex) 1/5 = 0.2 올림 => 1
 
 //pageGroup의 첫번째, 마지막 숫자 구하여 페이지 넘기기
 let lastNum = pageGroup * pageCount;
-if (lastNum > totalPage) {
-  lastNum = totalPage;
-}
 
 let firstNum = lastNum - (pageCount - 1);
 
@@ -116,6 +121,10 @@ for (let i = firstNum; i <= lastNum; i++) {
   const navButton = document.createElement("button");
   navButton.className = "pageNum";
   navButton.id = `page__${i}`;
-  navButton.innerText = `${i}`;
+  navButton.textContent = `${i}`;
   nav.append(navButton);
+
+  navButton.addEventListener("click", function () {
+    console.log(navButton.textContent);
+  });
 }
