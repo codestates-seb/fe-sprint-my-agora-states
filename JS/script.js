0349c68d86b30fa9500c06b329e3a030d24380ac
rickyshu <hoinleekk@naver.com>
2023-01-10 15:03:19 +0900

    Feat: 추가 localstorage 저장 기능 추가

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,7 @@
 // index.html을 열어서 agoraStatesDiscussions 배열 요소를 확인하세요.
-console.log(agoraStatesDiscussions);
+//console.log(agoraStatesDiscussions);
 
+let newData = agoraStatesDiscussions;
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
 const convertToDiscussion = (obj) => {
   const li = document.createElement("li"); // li 요소 생성
@@ -22,7 +23,7 @@ const convertToDiscussion = (obj) => {
   avatar.alt = `avatar of ${obj["author"]}`;
   avatarWrapper.appendChild(avatar);
 
-  //title
+  //title-hyperlink로 해결해야 한다!
   const title = document.createElement("h2");
   title.className = "discussion__title";
   const anchor = document.createElement("a");
@@ -46,12 +47,19 @@ const convertToDiscussion = (obj) => {
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
 };
-console.log(convertToDiscussion);
+//console.log(convertToDiscussion);
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
+//rendering 전에 localstorage확인하기!
+
+if (localStorage.getItem("Data")) {
+  newData = JSON.parse(localStorage.getItem("Data"));
+}
+
+console.log(newData);
 const render = (element) => {
-  for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
-    element.append(convertToDiscussion(agoraStatesDiscussions[i]));
+  for (let i = 0; i < newData.length; i += 1) {
+    element.append(convertToDiscussion(newData[i]));
   }
   return;
 };
@@ -60,8 +68,6 @@ const inputName = document.querySelector(".form__input--name input");
 const inputTitle = document.querySelector(".form__input--title input");
 const inputAnswer = document.querySelector(".form__textbox textarea");
 const submitForm = document.querySelector(".form");
-console.log(inputName);
-console.log(inputTitle);
 
 submitForm.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -72,13 +78,16 @@ submitForm.addEventListener("submit", (event) => {
     bodyHTML: null,
     avatarUrl:
       "https://cdn.pixabay.com/photo/2016/06/15/15/25/loudspeaker-1459128__480.png",
-    createdAt: localTime.toLocaleTimeString(),
+    createdAt: localTime.toISOString(),
     id: "unique id+ " + Math.round(Math.random * 10000),
     title: inputTitle.value,
     url: "https://github.com/codestates-seb/agora-states-fe/discussions/45",
   };
+
   // 밑과 같이 했을 때 왜 작동이 안 하는지 명확하게 파악해야 한다!
   agoraStatesDiscussions.unshift(obj);
+
+  localStorage.setItem("Data", JSON.stringify(agoraStatesDiscussions));
 
   while (ul.firstChild) {
     ul.removeChild(ul.firstChild);
