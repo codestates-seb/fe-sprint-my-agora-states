8def9f27e953d4f95b2b803d9e646d712434dcfb
khkh0109 <ist200120@gmail.com>
2023-02-01 00:09:37 +0900

    feat: createNewDiscussion 날짜 및 시간 생성 방식 변경

--- script.js
+++ script.js
@@ -73,15 +73,8 @@ const createNewDiscussion = (name, title) => {
   const newDiscussion = {};
 
   const date = new Date();
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, "0");
-  const day = String(date.getDate()).padStart(2, "0");
-  const hours = String(date.getHours()).padStart(2, "0");
-  const minutes = String(date.getMinutes()).padStart(2, "0");
-  const seconds = String(date.getSeconds()).padStart(2, "0");
 
-  newDiscussion.createdAt = `${year}-${month}-${day} 
-  ${hours}:${minutes}:${seconds}`;
+  newDiscussion.createdAt = date;
   newDiscussion.title = title;
   newDiscussion.url = "";
   newDiscussion.author = name;
