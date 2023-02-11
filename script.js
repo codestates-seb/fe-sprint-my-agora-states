bd0bdc1cb050ac2d3314515c201b2a708655b9d6
Aroma-oh <on002way@gmail.com>
2023-02-11 15:26:49 +0900

    feat: 페이지네이션 수정

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
+
 let agoraStatesDiscussions = []; // 데이터를 담을 곳
-fetch(`http://localhost:4000/discussions`)
+fetch(`http://localhost:4000/discussions`) // 데이터를 가져올 곳
   .then((res) => res.json())
   .then((json) => {
-    json.map((data) => {
-      agoraStatesDiscussions = json;
-      const ul = document.querySelector("ul.discussions__container");
-      render(ul);
-    });
+    agoraStatesDiscussions = json;
+    const ul = document.querySelector("ul.discussions__container");
+    render(ul);
+    paging();
   });
 
-// convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
+// 📌 agoraStatesDiscussions 데이터를 DOM으로 변환
 const convertToDiscussion = (obj) => {
-  const li = document.createElement("li"); // li 요소 생성
-  li.className = "discussion__container"; // 클래스 이름 지정
+  const li = document.createElement("li");
+  li.className = "discussion__container";
 
   const avatarWrapper = document.createElement("div");
   avatarWrapper.className = "discussion__avatar--wrapper";
@@ -21,14 +21,14 @@ const convertToDiscussion = (obj) => {
   const discussionAnswered = document.createElement("div");
   discussionAnswered.className = "discussion__answered";
 
-  // avatarWrapper
+  // ✨ avatarWrapper
   const avatarImg = document.createElement('img');
   avatarImg.className = 'discussion__avatar--image';
   avatarImg.src = obj.avatarUrl;
   avatarImg.alt = 'avatar of' + obj.author;
   avatarWrapper.append(avatarImg);
 
-  // discussionContent
+  // ✨ contentTitle
   const contentTitle = document.createElement('h2');
   contentTitle.className = 'discussion__title';
   const contentTitleLink = document.createElement('a');
@@ -36,13 +36,14 @@ const convertToDiscussion = (obj) => {
   contentTitleLink.textContent = obj.title;
   contentTitle.append(contentTitleLink);
 
+  // ✨ discussionContent
   const contentInfo = document.createElement('div');
   contentInfo.className = 'discussion__information';
-  const dt = new Date(obj.createdAt)
+  const dt = new Date(obj.createdAt); //
   contentInfo.textContent = dt.toLocaleString();
   discussionContent.append(contentTitle, contentInfo);
 
-  // discussionAnswered
+  // ✨ discussionAnswered
   const answered = document.createElement('p');
   answered.textContent = '●';
   answered.className = "answered"
@@ -59,7 +60,7 @@ const convertToDiscussion = (obj) => {
   return li;
 };
 
-// agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
+// 📌 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
   for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
     element.append(convertToDiscussion(agoraStatesDiscussions[i]));
@@ -68,11 +69,11 @@ const render = (element) => {
 };
 
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
-const ul = document.querySelector("ul.discussions__container");
+ul = document.querySelector("ul.discussions__container");
 render(ul);
 
 /* ----------------------------Local storage 만들기 ------------------------------ */
-// submit 이벤트를 사용
+// submit 이벤트를 사용 
 // 입력된 값을 객체에 저장
 // 객체를 로컬 스토리지에 저장
 // data.js의 0번째 인덱스로 로컬스토리지 저장 내용을 불러와서 집어 넣음
@@ -113,7 +114,7 @@ form.addEventListener("submit", (event) => {
   while (ul.firstChild) {
     ul.removeChild(ul.firstChild);
   }
-  render(ul);
+  render(ul); // 비동기 삐이~
 
   author.value = '';
   title.value = '';
@@ -133,47 +134,51 @@ window.onbeforeunload = function () {
 
 
 /*-----------------------------------------------------------------------------*/
+const paging = () => {
+  const rowsPerPage = 10; // 한 페이지에 게시글 10개
+  const rows = [...agoraStatesDiscussions]; //querySelectAll은 유사 배열로 리턴함
+  const rowsCount = agoraStatesDiscussions.length;
+  const pageCount = Math.ceil(rowsCount / rowsPerPage);
+  const numbers = document.querySelector('.pagination')
 
-// paging
-const rowsPerPage = 10; // 한 페이지에 게시글 10개
-const rows = [...agoraStatesDiscussions]; //querySelectAll은 유사 배열로 리턴함
-const rowsCount = agoraStatesDiscussions.length;
-const pageCount = Math.ceil(rowsCount / rowsPerPage);
-const numbers = document.querySelector('.pagination')
+  // 1. 페이징 생성
+  // 아래 li를 페이지 개수만큼 생성 -> pagecount
+  // 대상.innerHtml = <li><a href =''>1</a></li>
+  for (let i = 1; i <= pageCount; i++) {
+    numbers.innerHTML += `<li class = li__page ><a href = '' class = page ${i}>${i}</a></li>` // a 태그 안에 페이징 정보 담김
+  } // 📌 여기까지 fetching 완료되고 나서 즉시실행됨
 
-// 페이지네이션 생성
-// 아래 li를 페이지 개수만큼 생성 -> pagecount
-// 대상.innerHtml = <li><a href =''>1</a></li>
+  // 2. 페이지 번호 클릭하면 이동하기
+  const numberBtn = numbers.querySelectorAll('a'); // a 태그 요소만 저장
+  numberBtn.forEach((item, index) => {   //numbers의 모든 아이템과 인덱스를 받아서
+    item.addEventListener('click', (el) => { // item이 클릭될 때마다 이벤트를 실행시킴
+      el.preventDefault(); // .preventDefault()메소드는 a 요소의 기본 기능을 없앤다. (새로고침 방지)
+      for (nb of numberBtn) { // 클릭된 페이지만 active를 위해
+        nb.classList.remove('active'); // 모든 페이지를 비활성화하고 
+      }
+      el.target.classList.add('active'); // el.target은 function의 this와 같은 역할(즉, item) // 클릭한 요소에 active 클래스를 추가함 
 
-for (let i = 1; i <= pageCount; i++) {
-  numbers.innerHTML += `<li class = li__page ><a href = '' class = page ${i}>${i}</a></li>` // a 태그 안에 페이징 정보 담김
+      // 테이블 출력함수 만들기
+      displayRow(index);
+    });
+  }); // numberBtn // 📌 페이지 클릭하면 실행됨
+
+  // fetching 완료 -> paging 실행 -> displayRow 실행 아닌가? 
+  // 왜 paging 밖으로 빼면 참조를 못하는거지?? 
+  // 바보... rowsPerPage가 스코프로 paging 내에 있어서 참조 못했음... ㅠ
+  const displayRow = (index) => {
+    let start = index * rowsPerPage; // 1이라면 discussions data 10 인덱스부터 출력
+    let end = start + rowsPerPage; // start에서 10개 더 보여주기 위함
+
+    while (ul.firstChild) {
+      ul.removeChild(ul.firstChild); // 1. 페이지 싹다 지우고
+    }
+
+    for (i = start; i < end; i++) {
+      ul.append(convertToDiscussion(agoraStatesDiscussions[i])); // 2. 페이지네이션만 다시 추가 
+    }
+  } //displayRow
 }
 
-// 페이지 번호 클릭하면 이동하기
-const numberBtn = numbers.querySelectorAll('a'); // a 태그 요소만 저장
-numberBtn.forEach((item, index) => { //numbers의 모든 아이템과 인덱스를 받아서
-  item.addEventListener('click', (el) => { // item이 클릭될 때마다 이벤트를 실행시킴
-    el.preventDefault(); // .preventDefault()메소드는 a 요소의 기본 기능을 없앤다. 
-    for (nb of numberBtn) { // 클릭된 페이지만 active를 위해
-      nb.classList.remove('active'); // 모든 페이지를 비활성화하고 
-    }
-    el.target.classList.add('active'); // el.target은 function의 this와 같은 역할 // 클릭한 요소에 active 클래스를 추가함 
 
-    // 테이블 출력함수 만들기
-    displayRow(index);
-  });
-}); // numberBtn
-
-function displayRow(index) {
-  let start = index * rowsPerPage; // 1이라면 discussions data 10 인덱스부터 출력
-  let end = start + rowsPerPage; // start에서 10개 더 보여주기 위함
-
-  while (ul.firstChild) {
-    ul.removeChild(ul.firstChild);
-  }
-
-  for (i = start; i < end; i++) {
-    ul.append(convertToDiscussion(agoraStatesDiscussions[i]));
-  }
-} //displayRow
 
