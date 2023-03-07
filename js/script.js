e93902e68a6bec23576606f5b4d7eddafb7b72cf
XionWCFM <101111364+XionWCFM@users.noreply.github.com>
2023-03-07 15:43:23 +0900

    페이지네이션 구현 / but 페이지네이션 버튼 오류 (마지막 버튼) 눌렀을때 오류 발생함.

--- js/script.js
+++ js/script.js
@@ -30,7 +30,7 @@ const propertyMaker = (element, property, content) => {
   return element;
 };
 
-const dateMaker = (element) => {
+const dateMaker = () => {
   const date = new Date();
   const year = String(date.getFullYear());
   const month = String(date.getMonth() + 1).padStart(2, '0');
@@ -38,10 +38,9 @@ const dateMaker = (element) => {
   const hours = String(date.getHours()).padStart(2, '0');
   const minutes = String(date.getMinutes()).padStart(2, '0');
   const seconds = String(date.getSeconds()).padStart(2, '0');
-  hours >= 12
-    ? (element.textContent += `오후 ${hours}:${minutes}:${seconds}`)
-    : (element.textContent += `오전 ${hours}:${minutes}:${seconds}`);
-  return element;
+  let result = '';
+  result = `${year}-${month}-${day}T${hours}:${minutes}:${seconds}Z`;
+  return result;
 };
 
 const avatarMaker = (obj) => {
@@ -59,10 +58,10 @@ const avatarMaker = (obj) => {
 
 const parserMaker = (obj) => {
   const regex = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})/;
-  const [year, month, day, hours, minutes, seconds] = obj.match(regex);
-  return hours >= 12
-    ? `오후 ${hours}:${minutes}:${seconds}`
-    : `오전 ${hours}:${minutes}:${seconds}`;
+  const [year, month, day, dummy, hours, minutes, seconds] = obj.match(regex);
+  if (hours < 12) return `오전 ${hours}:${minutes}:${seconds}`;
+  if (hours < 22) return `오후 0${hours - 12}:${minutes}:${seconds}`;
+  return `오후 ${hours - 12}:${minutes}:${seconds}`;
 };
 
 const discussInfoMaker = (obj) => {
@@ -74,7 +73,7 @@ const discussInfoMaker = (obj) => {
   } else {
     discussInfo = propertyMaker(discussInfo, 'textContent', obj.author);
     discussInfo.textContent += ' / ';
-    discussInfo = dateMaker(discussInfo);
+    discussInfo.textContent += dateMaker();
   }
   return discussInfo;
 };
@@ -130,17 +129,17 @@ const submitPusher = (event) => {
     title: titleInput.value,
     content: storyInput.value,
     author: nameInput.value,
+    createdAt: dateMaker(),
   };
   let newDiscuss = convertToDiscussion(obj);
-  console.log(newDiscuss);
-  agoraStatesDiscussions.unshift(newDiscuss);
-  ul.prepend(newDiscuss);
-  ul.removeChild(ul.lastChild);
-  console.log(agoraStatesDiscussions.length);
+  agoraStatesDiscussions.unshift(obj);
+  console.log(agoraStatesDiscussions);
+  document.querySelector(`#page_${activePage}`).click();
   return;
 };
 
-//페이지네이션 파트
+//페이지네이션 파트 class = "animate__backOutDown"
+
 const buttonMaker = (i) => {
   let btn = createElementWithClass('button', 'pageNumber');
   btn = propertyMaker(btn, 'id', `page_${i}`);
@@ -149,7 +148,7 @@ const buttonMaker = (i) => {
 };
 
 const paintButton = (firstNum, lastNum) => {
-  for (let i = firstNum; i < lastNum; i++) {
+  for (let i = firstNum; i <= lastNum; i++) {
     let btn = buttonMaker(i);
     pageButtonContainer.append(btn);
   }
@@ -176,7 +175,8 @@ const pageButtonNumber = (cursor, total) => {
 
 const pageButtonOnclick = (event) => {
   if (event.target.classList[0] !== 'pageNumber') return;
-  const nowPage = Number(event.target.textContent);
+  console.log(event);
+  const nowPage = Number(event.target.textContent); //
   activePage = nowPage;
   removeChild(ul);
   render(ul, nowPage * 10 - 10, nowPage * 10);
