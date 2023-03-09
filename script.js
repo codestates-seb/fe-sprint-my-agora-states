b8ac30df421a35fec2c6c0028da661b3c4675c36
minsun0714 <woooooooow22@gmail.com>
2023-03-09 21:51:15 +0900

    🎨  div 생성 및 클래스 지정하여 더미 데이터들을 li tag에 추가 🎨 글 쓴 시간 확장형식으로 변경 🎨 글쓴이와 time을 info 변수에 묶어 저장 🎨 title에 url 적용

--- script.js
+++ script.js
@@ -4,20 +4,55 @@ console.log(agoraStatesDiscussions);
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
 const convertToDiscussion = (obj) => {
   const li = document.createElement("li"); // li 요소 생성
-  li.className = "discussion__container"; // 클래스 이름 지정
+  li.classList.add("discussion__container");
 
+  // avatarWrapper, discussionInfo, discussionContent, discussionAnswered div 태그 생성
   const avatarWrapper = document.createElement("div");
-  avatarWrapper.className = "discussion__avatar--wrapper";
+  const discussionInfo = document.createElement("div");
   const discussionContent = document.createElement("div");
-  discussionContent.className = "discussion__content";
   const discussionAnswered = document.createElement("div");
+
+  // avatarWrapper, discussionInfo, discussionContent, discussionAnswered 클래스명 지정
+  avatarWrapper.className = "discussion__avatar--wrapper";
+  discussionInfo.className = "discussion__information";
+  discussionContent.className = "discussion__content";
   discussionAnswered.className = "discussion__answered";
 
-  // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
+  // img 태그 생성, 아바타 이미지 src에 URL 추가하여 avatarWrapper 자식 태그로 추가
+  const avatarImg = document.createElement("img");
+  avatarImg.src = obj.avatarUrl;
+  avatarWrapper.append(avatarImg);
 
+  // 인자로 받은 객체의 제목, 글쓴이, 글쓴 시간 선언
+  const title = obj.title;
+  const author = obj.author;
+  let time = obj.answer?.createdAt
+    .slice(11)
+    .match(/[0-9]+/g)
+    .join(":");
+  if (time) {
+    time = Number(time.slice(0, 2)) >= 12 ? "오전 " + time : "오후 " + time;
+  } else {
+    time = "";
+  }
 
+  // author와 time을 info 변수에 저장
+  const info = author + " / " + time;
 
+  // title에 url 적용
+  const linkedTitle = document.createElement("A");
+  linkedTitle.setAttribute("href", obj.answer?.url);
+  linkedTitle.append(title);
+
+  // li tag 자식 노드로 추가
   li.append(avatarWrapper, discussionContent, discussionAnswered);
+
+  // discussion__content 태그에 제목, info 변수(author, time) 자식 노드로 추가
+  const discussion = document.createElement("div");
+  discussion.className = "discussion__content";
+  discussionInfo.innerText = info;
+  discussionContent.append(linkedTitle, discussionInfo);
+
   return li;
 };
 
