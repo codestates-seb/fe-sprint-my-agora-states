b8b85bd0c4411ce70da10af528666c4b52a1b249
jungo <mkk90645@gmail.com>
2023-03-09 18:34:04 +0900

    feat: discussion

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // index.html을 열어서 agoraStatesDiscussions 배열 요소를 확인하세요.
 console.log(agoraStatesDiscussions);
-
+// 댭변 상태에 따라 아이콘 사용
+const ANSWERED_MARK = "☑︎";
+const UNANSERED_MARK = "◻︎";
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
 const convertToDiscussion = (obj) => {
   const li = document.createElement("li"); // li 요소 생성
@@ -8,19 +10,51 @@ const convertToDiscussion = (obj) => {
 
   const avatarWrapper = document.createElement("div");
   avatarWrapper.className = "discussion__avatar--wrapper";
+ 
+  const avatarImg = document.createElement("img");
+
+  avatarImg.src = obj.avatarUrl;
+  avatarImg.alt = "avatar of" + obj.author;
+  avatarImg.className = "discussion__avatar--image";
+
+  avatarWrapper.append(avatarImg);
+
   const discussionContent = document.createElement("div");
   discussionContent.className = "discussion__content";
+
+  const discussionTitle = document.createElement("h2");
+  discussionTitle.className = "discussion__title";
+
+  const discussionUrl = document.createElement("a");
+  discussionUrl.href = obj.url;
+  discussionUrl.textContent = obj.title;
+
+  discussionTitle.append(discussionUrl);
+
+  const discussionInformation = document.createElement("div");
+  discussionInformation.className = "discussion__information";
+
+  discussionInformation.textContent = obj.createdAt;
+
+  discussionContent.append(discussionTitle);
+  discussionContent.append(discussionInformation);
+
   const discussionAnswered = document.createElement("div");
   discussionAnswered.className = "discussion__answered";
 
-  // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
-
-
+  const discussionIcon = document.createElement("p");
+  discussionIcon.textContent = "☑";
+  discussionAnswered.append(discussionIcon);
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
+  if (obj.answer === null) {
+    discussionIcon.textContent = UNANSERED_MARK;
+  } else {
+    discussionIcon.textContent = ANSWERED_MARK;
+  }
+
   return li;
 };
-
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
   for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
@@ -28,7 +62,6 @@ const render = (element) => {
   }
   return;
 };
-
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
