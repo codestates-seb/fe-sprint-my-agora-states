f79d241dc081a6663de8d0b89efa8979b79563ba
ujujungbuck <garam8u2@gmail.com>
2023-03-09 17:20:01 +0900

    써밋 제외 뼈대 작성

--- script.js
+++ script.js
@@ -15,6 +15,39 @@ const convertToDiscussion = (obj) => {
 
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
 
+  const avatarImg = document.createElement('img')
+  avatarImg.className = "discussion__avatar--image"
+  avatarImg.src = obj.avatarUrl
+  avatarImg.alt = 'avatar of ' + obj.author
+  avatarWrapper.append(avatarImg)
+
+//title
+  const contentTitle = document.createElement('h2')
+  contentTitle.className = "discussion__title"
+  discussionContent.append(contentTitle)
+//링크
+  const contentLink = document.createElement('a')
+  contentLink.href = obj.url
+  contentLink.textContent = obj.title
+  contentTitle.append(contentLink)
+//정보
+  const contentInformation = document.createElement('div')
+  contentInformation.className = "discussion__information"
+  contentInformation.textContent = obj.author
+  discussionContent.append(contentInformation)
+
+//체크박스
+  const contentAnswered = document.createElement('p')
+  contentAnswered.className = "discussion__answered"
+  discussionAnswered.append(contentAnswered)
+  if( obj.answer === null ){
+    contentAnswered.textContent = "☑"
+  }else{
+    contentAnswered.textContent = "x"
+  }
+  //날짜
+  contentInformation.textContent = `${obj.author} / ${new Date(obj.createdAt).toLocaleString("ko-KR")}`
+
 
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
@@ -23,6 +56,7 @@ const convertToDiscussion = (obj) => {
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
+  // element.innerHTML = ""
   for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
     element.append(convertToDiscussion(agoraStatesDiscussions[i]));
   }
