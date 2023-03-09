42f4062a98d0619a1356b9277bee7951cd78a6f3
mule-heo <zaq98114@gmail.com>
2023-03-09 14:57:21 +0900

    avatarWrapper, discussionContent 함수 정의 및 렌더링

--- script.js
+++ script.js
@@ -1,22 +1,48 @@
 // index.html을 열어서 agoraStatesDiscussions 배열 요소를 확인하세요.
-console.log(agoraStatesDiscussions);
+console.log(agoraStatesDiscussions[0]);
 
+const fillAvatarWrapper = (obj) => {
+  const avatarWrapper = document.createElement("div");
+  avatarWrapper.className = "discussion__avatar--wrapper";
+  // child
+  const avatar = document.createElement("img");
+  avatar.src = obj.avatarUrl;
+  avatarWrapper.appendChild(avatar);
+  return avatarWrapper;
+};
+
+const fillDiscussionContent = (obj) => {
+  const discussionContent = document.createElement("div");
+  discussionContent.className = "discussion__content";
+  // children
+  // h2 - discussion__title obj.title
+  const h2Title = document.createElement("h2");
+  // h2 > a - link and obj.title
+  const aLink = document.createElement("a");
+  aLink.href = obj.url;
+  aLink.textContent = obj.title;
+  // div - discussion__information obj.author, obj.createdAt
+  const divInfo = document.createElement("div");
+  divInfo.textContent = `${obj.author} / ${obj.createdAt}`;
+
+  h2Title.appendChild(aLink);
+  discussionContent.appendChild(h2Title);
+  discussionContent.appendChild(divInfo);
+  return discussionContent;
+};
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
 const convertToDiscussion = (obj) => {
   const li = document.createElement("li"); // li 요소 생성
   li.className = "discussion__container"; // 클래스 이름 지정
-
-  const avatarWrapper = document.createElement("div");
-  avatarWrapper.className = "discussion__avatar--wrapper";
-  const discussionContent = document.createElement("div");
-  discussionContent.className = "discussion__content";
+  const avatarWrapper = fillAvatarWrapper(obj);
+  const discussionContent = fillDiscussionContent(obj);
   const discussionAnswered = document.createElement("div");
   discussionAnswered.className = "discussion__answered";
 
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
-  const avatar = document.createElement("img");
-  avatar.src = obj["avatarUrl"];
-  avatarWrapper.appendChild(avatar);
+
+  // answer discussionAnswered
+  // object.answer 가져와서 위의 과정을 반복한 후 parent만 discussionAnswered로 넣기
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
