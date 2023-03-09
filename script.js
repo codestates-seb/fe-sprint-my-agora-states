9d5752128534681abd8a5fc8ca0434013572aa70
sungjihyun <jhsung23@gmail.com>
2023-03-09 18:03:57 +0900

    feat: 질문하기 버튼 클릭 시 질문 작성 폼 보여주기

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const ul = document.querySelector('ul.discussions__container');
 
-const submitButton = document.querySelector('.form__submit').childNodes[1];
+const askingButton = document.getElementById('asking');
+
+const formContainer = document.querySelector('.form__container');
 const inputName = document.querySelector('#name');
 const inputTitle = document.querySelector('#title');
 const inputQuestion = document.querySelector('#story');
+const submitButton = document.querySelector('#submit-button');
+
+askingButton.addEventListener('click', () => {
+  formContainer.classList.toggle('show');
+});
 
 submitButton.addEventListener('click', (e) => {
   e.preventDefault();
@@ -19,7 +26,7 @@ submitButton.addEventListener('click', (e) => {
     })
   );
 
-  initializeInput();
+  formContainer.classList.toggle('show');
 });
 
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
