29f06deed3a7f56bf2d24370a199a56eaf97eb11
hyeincha <runprogrmm@gmail.com>
2023-03-10 00:48:43 +0900

    feat: UTC 시간대 현지 시간 적용 기능 구현

--- functions.js
+++ functions.js
@@ -1,9 +1,13 @@
 const dateConverter = (date) => {
-  let createDate = new Date(date);
-  let year = createDate.getFullYear();
-  let month = createDate.getMonth();
-  let day = createDate.getDate();
-  return `${year}년 ${month}월 ${day}일`;
+  const createDate = new Date(date);
+  const year = createDate.getFullYear();
+  const month = createDate.getMonth() + 1;
+  const day = createDate.getDate();
+  const time = createDate.getHours();
+  const minute = createDate.getMinutes();
+  const minutes = minute < 10 ? '0' + minute : minute;
+  const ampm = time >= 12 ? '오후' : '오전';
+  return `${year}년 ${month}월 ${day}일 ${ampm} ${time}:${minutes}`;
 };
 
 const appendToStorage = (name, data) => {
