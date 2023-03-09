500905d1ec0453a0c022fc4668abbf7b149927e8
lhyjjg <leehyun147@gmail.com>
2023-03-09 23:34:48 +0900

    test

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // index.html을 열어서 agoraStatesDiscussions 배열 요소를 확인하세요.
 console.log(agoraStatesDiscussions);
 
+
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
 const convertToDiscussion = (obj) => {
   const li = document.createElement("li"); // li 요소 생성
@@ -14,8 +15,40 @@ const convertToDiscussion = (obj) => {
   discussionAnswered.className = "discussion__answered";
 
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
+  // avatar 이미지
+  const avatarImg = document.createElement('img');
+  avatarImg.src = obj.avatarUrl;
+  avatarImg.alt = 'avatar of ' + obj.author;
+  avatarImg.className = "discussion__avatar--image"
+  avatarWrapper.append(avatarImg);
 
+  // title (질문 제목, 링크연결)
+  const title = document.createElement("h2");
+  const titleUrl = document.createElement("a");
+  titleUrl.href = obj.url;
+  titleUrl.textContent = obj.title;
+  // titleUrl.className = "aTag";
+  title.className = "discussion__title";
+  title.append(titleUrl);
+  discussionContent.append(title);
 
+  // information(닉네임, 날짜)
+  const information = document.createElement('div');
+  information.textContent = `${obj.author} / ${obj.createdAt}`;
+  information.className = "discussion__information";
+  discussionContent.append(information);
+
+  // 답변여부
+  const answered = document.createElement('p')
+  if(obj.answer === null) {
+    answered.textContent = "☑"
+  } else
+  answered.textContent = "O"
+  discussionAnswered.append(answered);
+
+  
+  
+  
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
