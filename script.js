19413c07a81e64e13b2bcbccf96adf65cc1f96a8
Y.N Choe <dev.ynchoe@gmail.com>
2023-03-09 17:28:35 +0900

    Submit 버튼 클릭 시 Component 추가되도록 설정

--- script.js
+++ script.js
@@ -47,3 +47,57 @@ const render = (element) => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const container = document.querySelector("div.discussions__container");
 render(container);
+
+
+// 진짜 나으 아고라 스테이츠 만들기
+
+
+
+const submitBtn = document.querySelector('.form__submit')
+
+const nameInput = document.querySelector('#name')
+const titleInput = document.querySelector('#title')
+const contentInput = document.querySelector('#story')
+
+submitBtn.addEventListener('click', event => {
+  event.preventDefault()
+
+  const component = document.createElement('div')
+  component.className = "discussion__container"
+
+
+  const avatarWrapper = document.createElement("div");
+  avatarWrapper.className = "discussion__avatar--wrapper";
+
+  const discussionContent = document.createElement("div");
+  discussionContent.className = "discussion__content";
+
+  const discussionAnswered = document.createElement("div");
+  discussionAnswered.className = "discussion__answered";
+
+
+
+  avatarWrapper.innerHTML = `<img class="discussion__avatar--image"
+  src="https://vehrcommunications.com/wp-content/uploads/2021/12/Grinch.jpg"
+  alt="avatar of ${nameInput.value}">`
+
+  discussionContent.innerHTML = `<h2 class="discussion__title"><a
+  href='https://www.google.com/'>${titleInput.value}</a></h2>
+  <div class="discussion__information">2022-05-16T01:02:17Z
+  </div >`
+
+  discussionAnswered.innerHTML = `<p>❌</p>`
+
+
+  component.append(avatarWrapper, discussionContent, discussionAnswered);
+
+  container.prepend(component)
+
+  nameInput.value = '';
+  titleInput.value = '';
+  contentInput.value = '';
+
+  nameInput.focus();
+
+  console.log('Button Clicked!')
+})
