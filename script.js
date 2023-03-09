3e4e70101b93787117a784f562e316380442e632
Seunghyo <shyo0218@naver.com>
2023-03-09 16:56:40 +0900

    기본 기능 구현

--- script.js
+++ script.js
@@ -15,11 +15,35 @@ const convertToDiscussion = (obj) => {
 
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
 
+const avatarImage = document.createElement("img") // 태그이름
+avatarImage.className = 'discussion__avatar--image';
+avatarImage.src = obj.avatarUrl;
+avatarImage.alt = 'avatar of' + obj.author;
+avatarWrapper.append(avatarImage);
 
 
-  li.append(avatarWrapper, discussionContent, discussionAnswered);
+const discussionTitle = document.createElement("h2");
+const discussionTitletext = document.createElement("a");
+discussionTitletext.href = obj.url;
+discussionTitletext.textContent = obj.title;
+discussionTitle.append(discussionTitletext);
+
+
+const discussionInformation  = document.createElement("div");
+discussionInformation.className = "discussion__information";
+discussionInformation.textContent = `${obj.author} / ${new Date(obj.createdAt).toLocaleTimeString()}`
+discussionContent.append(discussionTitle, discussionInformation);
+
+
+const checked = document.createElement("p");
+checked.textContent = obj.answer ? "🙄" : "✅";
+discussionAnswered.append(checked);
+
+li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
 };
+
+
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
@@ -32,3 +56,32 @@ const render = (element) => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+const form = document.querySelector('form.form');
+const displayauthor = document.querySelector('.form__input--name > input');
+const displayTitle = document.querySelector('.form__input--title > input');
+const displayQuestion = document.querySelector('.form__textbox > textarea')
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const obj = {
+    id : "new id",
+    createdAt : new Date(),
+    title: displayTitle.value,
+    url:"https://github.com/codestates-seb/agora-states-fe/discussions",
+    author: displayauthor.value,
+    bodyHTML: displayQuestion.value,
+    avatarUrl:"https://blog.kakaocdn.net/dn/GHYFr/btrsSwcSDQV/UQZxkayGyAXrPACyf0MaV1/img.jpg"
+
+  };
+// 기존 discuusions 데이터의 맨 앞에 입력받은 정보 추가
+agoraStatesDiscussions.unshift(obj);
+ul.prepend(convertToDiscussion(obj));
+
+
+//form 초기화
+displayauthor.value = '';
+displayTitle .value = '';
+displayQuestion.value = '';
+});
+
