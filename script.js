2103df76c59531e03bf63d26df519965194c3283
tnsdlznf23 <112317745+tnsdlznf23@users.noreply.github.com>
2023-03-10 15:16:04 +0900

    1차

--- script.js
+++ script.js
@@ -1,121 +1,43 @@
-// index.html을 열어서 agoraStatesDiscussions 배열 요소를 확인하세요.
-console.log(agoraStatesDiscussions);
-
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
 const convertToDiscussion = (obj) => {
   //agoraStatesDiscussions[i]
-  // li 요소를 생성합니다.
-
+  // li 요소 생성
   const li = document.createElement("li");
-  li.classList.add("discussion__container", "swiper-slide");
-
-  // div 요소를 생성합니다.
-  const div = document.createElement("div");
-  div.classList.add("discussion__content");
-
-  // discussion__main 클래스를 가진 div 요소를 생성합니다.
-  const discussionMain = document.createElement("div");
-  discussionMain.classList.add("discussion__main");
-
-  // h2 요소를 생성합니다.
-  const h2 = document.createElement("h2");
-  h2.classList.add("discussion__title");
-
-  // a 요소를 생성합니다.
-  const a = document.createElement("a");
-  a.href = "https://github.com/codestates-seb/agora-states-fe/discussions/6";
-  a.textContent = obj.title;
-
-  // a 요소를 h2 요소에 추가합니다.
-  h2.appendChild(a);
-
-  // discussion__name 클래스를 가진 div 요소를 생성합니다.
+  li.classList.add("discussion__list");
+  // h2 요소 생성
+  const discussionTitle = document.createElement("h2");
+  discussionTitle.classList.add("discussion__title");
+  discussionTitle.textContent = obj.title;
+  li.appendChild(discussionTitle);
+  // div 요소 생성
   const discussionName = document.createElement("div");
   discussionName.classList.add("discussion__name");
   discussionName.textContent = obj.author;
-
-  // discussionMain에 h2와 discussionName을 추가합니다.
-  discussionMain.appendChild(h2);
-  discussionMain.appendChild(discussionName);
-
-  // discussion__information 클래스를 가진 div 요소를 생성합니다.
-  const discussionInformation = document.createElement("div");
-  discussionInformation.classList.add("discussion__information");
-  discussionInformation.textContent = obj.info;
-
-  // discussion__content에 discussionMain과 discussionInformation을 추가합니다.
-  div.appendChild(discussionMain);
-  div.appendChild(discussionInformation);
-
-  // li 요소에 div 요소를 추가합니다.
-  li.appendChild(div);
-
-  return li;
-};
-const convertToDiscussion2 = (obj) => {
-  //agoraStatesDiscussions[i]
-  // li 요소를 생성합니다.
-
-  const li = document.createElement("li");
-  li.classList.add("discussion__container", "swiper-slide");
-
-  // div 요소를 생성합니다.
-  const div = document.createElement("div");
-  div.classList.add("discussion__content");
-
-  // discussion__main 클래스를 가진 div 요소를 생성합니다.
-  const discussionMain = document.createElement("div");
-  discussionMain.classList.add("discussion__main");
-
-  // h2 요소를 생성합니다.
-  const h2 = document.createElement("h2");
-  h2.classList.add("discussion__title");
-
-  // a 요소를 생성합니다.
-  const a = document.createElement("a");
-  a.href = "https://github.com/codestates-seb/agora-states-fe/discussions/6";
-  a.textContent = obj.title;
-
-  // a 요소를 h2 요소에 추가합니다.
-  h2.appendChild(a);
-
-  // discussion__name 클래스를 가진 div 요소를 생성합니다.
-  const discussionName = document.createElement("div");
-  discussionName.classList.add("discussion__name");
-  discussionName.textContent = obj.author;
-
-  // discussionMain에 h2와 discussionName을 추가합니다.
-  discussionMain.appendChild(h2);
-  discussionMain.appendChild(discussionName);
-
-  // discussion__information 클래스를 가진 div 요소를 생성합니다.
-  const discussionInformation = document.createElement("div");
-  discussionInformation.classList.add("discussion__information");
-  discussionInformation.textContent = obj.info;
-
-  // discussion__content에 discussionMain과 discussionInformation을 추가합니다.
-  div.appendChild(discussionMain);
-  div.appendChild(discussionInformation);
-
-  // li 요소에 div 요소를 추가합니다.
-  li.appendChild(div);
-
+  li.appendChild(discussionName);
+  // div 요소 생성
+  const discussionBtn = document.createElement("div");
+  discussionBtn.classList.add("discussion__btn");
+  li.appendChild(discussionBtn);
+  // 생성된 요소를 적절한 위치에 추가
+  const parentElement = document.querySelector(".discussions__container");
+  parentElement.appendChild(li);
   return li;
 };
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
-  for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
-    element.append(convertToDiscussion(agoraStatesDiscussions[i]));
+  let localdata = JSON.parse(localStorage.getItem("data"));
+
+  for (i = 0; i < localdata.length; i += 1) {
+    element.append(convertToDiscussion(localdata[i]));
   }
   return;
 };
 
-// ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
 
-//입력 폼 데이터 받기
+//2. 폼 제출 시 데이터 출력
 const $form = document.querySelector(".form");
 $form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -124,32 +46,55 @@ $form.addEventListener("submit", (e) => {
 
   const name = formdata.get("name"); //get 메서드로 value 값 가져오기
   const title = formdata.get("title");
-  const info = formdata.get("story");
 
   const newdata = {
     author: name,
     title: title,
-    info: info,
   };
-  agoraStatesDiscussions.unshift(newdata);
-  ul.prepend(convertToDiscussion2(agoraStatesDiscussions[0]));
-  
-  window.localStorage.setItem('newdata', newdata);
-  console.log(localStorage.getItem('newdata'))
 
+  let localdata = JSON.parse(localStorage.getItem("data")); //로컬 데이터 가져오기
+  localdata.unshift(newdata); //가져온 로컬 데이터에 새로운 데이터 추가해주기 (currentdata는 배열형태)
+  localStorage.setItem("data", JSON.stringify(localdata)); //추가된 로컬데이터 다시 등록해주기
+
+  ul.prepend(convertToDiscussion(localdata[0])); //추가된 데이터 렌더해주기
+
+  document.querySelector("#title").value = "";
+  document.querySelector("#name").value = "";
 });
 
+// 3. x버튼 누르면 지워지게
+//문제: delete 먼저 잡고 추가하면 추가된 delete 버튼 인식 못함. > document에 이벤트 리스너 달고 클래스명 포함되어 있을때만 찾기
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("discussion__btn")) {
+    let localdata = JSON.parse(localStorage.getItem("data"));
+    let target = e.target;
+    const targetlist = target.parentElement;
+    const childarr = Array.from(ul.children);
+    const index = childarr.indexOf(targetlist); //클릭한 버튼의 리스트가 몇번째 인덱스인지 찾기
 
+    localdata.splice(index, 1);
+    localStorage.setItem("data", JSON.stringify(localdata)); //로컬에 데이터 삭제
 
+    console.log(index);
 
-/*
- agoraStatesDiscussions.unshift(newdata);
- ul.prepend(convertToDiscussion(agoraStatesDiscussions[0]));
+    target.parentElement.remove();
+  }
+});
 
+// 마우스 따라다니는 원
 
-while (ul.firstChild) {
-  ul.removeChild(ul.firstChild);
-}
-render(ul);
+// 원을 가리키는 변수
+const circle = document.querySelector(".circle");
 
-*/
+// 마우스 이벤트 처리
+document.addEventListener("mousemove", (e) => {
+  // 스크롤 위치 고려하기
+  const scrollX = window.scrollX;
+  const scrollY = window.scrollY;
+  const mouseX = e.pageX - scrollX;
+  const mouseY = e.pageY - scrollY;
+
+  // 원 위치 설정
+  circle.style.left = mouseX + "px";
+  circle.style.top = mouseY + "px";
+});
