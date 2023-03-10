c8f32139b830c685e23e967cecf1feaf9ea3998b
DanoHwang <danohwang00@gmail.com>
2023-03-10 14:53:52 +0900

    Feat: 페이지네이션 기능 구현

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ const questionUsername = document.querySelector("#name");
 const questionTitle = document.querySelector("#title");
 const questionStory = document.querySelector("#story");
 const submitButton = document.querySelector(".form__submit");
+const pageBar = document.querySelector(".page-bar");
+const ul = document.querySelector(".discussions__ul");
+
+let currentPage = 1;
 
 const changeUtcToLocal = (date) => {
   let localDate = new Date(date);
@@ -78,9 +82,108 @@ const render = (element) => {
   return;
 };
 
-// ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
-const ul = document.querySelector("ul.discussions__container");
-render(ul);
+const makeButtonElement = (className, textContent, appendTarget, id) => {
+  const button = document.createElement("button");
+  if (id) button.id = id;
+  button.className = className;
+  button.textContent = textContent;
+  appendTarget.append(button);
+};
+
+const resetPage = () => {
+  const pageBarButtons = document.querySelectorAll(".page-bar__button");
+  const prevButton = document.querySelector(".page-bar__button--prev");
+  const nextButton = document.querySelector(".page-bar__button--next");
+
+  pageBarButtons.forEach((button) => {
+    button.remove();
+  });
+
+  prevButton.remove();
+  nextButton.remove();
+
+  const discussions = document.querySelectorAll(".discussion__container");
+  discussions.forEach((element) => {
+    element.remove();
+  });
+};
+
+const paginate = (clickType) => {
+  const numOfData = agoraStatesDiscussions.length; // 총 데이터 갯수
+  const pageLimit = 5; // 화면에 나타날 페이지 갯수
+  const dataLimit = 5; // 한 페이지 당 나타날 데이터 갯수
+  const totalPage = Math.ceil(numOfData / dataLimit);
+  const half = Math.floor(pageLimit / 2);
+  let firstNumberOfPage, lastNumberOfPage;
+
+  // currentPage (현재 페이지) 계산
+  if (clickType === "prev") {
+    currentPage = (currentPage === 1)
+    ? 1
+    : currentPage - 1;
+  } else if (clickType === "next") {
+    currentPage = (currentPage !== totalPage)
+    ? currentPage + 1
+    : currentPage;
+  } else if (typeof(clickType) === "number") {
+    currentPage = clickType;
+  } else {
+    currentPage = 1;
+  }
+
+  // currentPage에 따른 pageBar 시작, 끝 계산
+  if (currentPage <= half + 1) {
+    firstNumberOfPage = 1;
+    lastNumberOfPage = firstNumberOfPage + pageLimit;
+  } else if (currentPage < totalPage - half) {
+    firstNumberOfPage = currentPage - half;
+    lastNumberOfPage = currentPage + half + 1;
+  } else {
+    firstNumberOfPage = totalPage - pageLimit + 1;
+    lastNumberOfPage = totalPage + 1;
+  }
+
+  // 버튼 생성
+  makeButtonElement("page-bar__button--prev", "이전", pageBar);
+
+  for (let i = firstNumberOfPage; i < lastNumberOfPage; i++) {
+    makeButtonElement("page-bar__button", i, pageBar, `page_${i}`);
+  }
+
+  makeButtonElement("page-bar__button--next", "다음", pageBar);
+
+  // 데이터 목록 렌더
+  const firstIndex = (currentPage - 1) * dataLimit;
+  const lastIndex = currentPage * dataLimit - 1;
+
+  for (let i = firstIndex; i <= lastIndex; i++) {
+    if (i >= numOfData) break;
+
+    ul.append(convertToDiscussion(agoraStatesDiscussions[i]));
+  }
+
+  // 페이지 버튼 클릭 시 배열의 모든 데이터를 다시 화면에 렌더링할 수 있도록 이벤트 부착
+  const pageBarButtons = document.querySelectorAll(".page-bar__button");
+  const prevButton = document.querySelector(".page-bar__button--prev");
+  const nextButton = document.querySelector(".page-bar__button--next");
+
+  pageBarButtons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      resetPage();
+      paginate(Number(event.target.textContent));
+    });
+  });
+
+  prevButton.addEventListener("click", (event) => {
+    resetPage();
+    paginate("prev");
+  });
+
+  nextButton.addEventListener("click", (event) => {
+    resetPage();
+    paginate("next");
+  });
+};
 
 const openModal = () => {
   modal.classList.remove("hide");
@@ -108,8 +211,8 @@ const addQuestion = () => {
   };
 
   agoraStatesDiscussions.push(newQuestion);
-  console.log(agoraStatesDiscussions)
-  render(ul);
+  resetPage();
+  paginate();
 };
 
 questionButton.addEventListener("click", openModal);
@@ -119,3 +222,6 @@ submitButton.addEventListener("click", () => {
   addQuestion();
   closeModal();
 });
+
+// ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
+paginate();
