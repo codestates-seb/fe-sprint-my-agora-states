480d3a6d814328e899895ddbb78f093a241d6cb3
youngyun213 <k12377733@gmail.com>
2023-03-10 16:43:26 +0900

    css 수정

--- script.js
+++ script.js
@@ -14,12 +14,66 @@ const convertToDiscussion = (obj) => {
   discussionAnswered.className = "discussion__answered";
 
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
+  const avatarImg = document.createElement("img"); //img 요소 생성
+  avatarImg.className = "discussion__avatar--image";
+  avatarImg.src = obj.avatarUrl;
+  avatarImg.alt = `avatar of ${obj.author}`;
+  avatarWrapper.append(avatarImg);
 
+  // discussionContent 내부
+  // -> 제목 h3 요소 생성, 추가
+  const discussionTitle = document.createElement("h3");
+  discussionTitle.className = "discussion__title";
+  discussionContent.append(discussionTitle);
+  // 제목 url -> a 요소 생성, 추가
+  const titleAnchor = document.createElement("a");
+  titleAnchor.href = obj.url;
+  titleAnchor.textContent = obj.title;
+  discussionTitle.append(titleAnchor);
+  // -> 저자, 생성일 div 요소 생성, 추가
+  const discussionInfo = document.createElement("div");
+  discussionInfo.className = "discussion__information";
+  discussionInfo.textContent = `${obj.author} / ${new Date(obj.createdAt).toLocaleString()}`;
+  discussionContent.append(discussionInfo);
 
+  // discussionAnswered -> p요소 생성, 추가
+  const checked = document.createElement("p");
+  checked.textContent = obj.answer ? "🟢" : "🔴";
+  discussionAnswered.append(checked);
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
 };
+
+const form = document.querySelector("form.form");
+//submit하면 동작할 함수
+form.addEventListener("submit", (event) => {
+  const title = document.querySelector("input#title");
+  const author = document.querySelector("input#name");
+  const story = document.querySelector("textarea#story");
+  //폼 요소는 submit 동작이 일어나면 자동으로 새로고침을 한다.
+  event.preventDefault(); // 새로고침 안함
+  // 하나의 객체를 만들어서 convertToDiscussion함수에 넣어서 li로 만든 다음, ul요소에 append
+  const newDiscussion = {
+    id: "unique value",
+    createdAt: new Date(),
+    title: title.value,
+    url: "",
+    author: author.value,
+    answer: null,
+    bodyHTML: story.value,
+    avatarUrl: "",
+  };
+  ul.prepend(convertToDiscussion(newDiscussion)); //리스트 앞에 넣기
+
+  //배열에 추가
+  agoraStatesDiscussions.unshift(newDiscussion);
+
+  //input 칸 초기화
+  title.value = "";
+  author.value = "";
+  story.value = "";
+});
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
