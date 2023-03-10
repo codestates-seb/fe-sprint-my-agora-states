45f1cb368e4bb100cb9a25ccf8af867bba01e920
jungo <mkk90645@gmail.com>
2023-03-10 14:13:44 +0900

    Reproject

--- script.js
+++ script.js
@@ -1,67 +1,178 @@
 // index.html을 열어서 agoraStatesDiscussions 배열 요소를 확인하세요.
-console.log(agoraStatesDiscussions);
-// 댭변 상태에 따라 아이콘 사용
-const ANSWERED_MARK = "☑︎";
-const UNANSERED_MARK = "◻︎";
-// convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
+// console.log(agoraStatesDiscussions);
+//  pagination기능, 데이터 10개씩 출력
+const pagination = (discussions) => {
+  const count = 10;
+  const discussion = [...discussions.children];
+  const pages = document.querySelector('.page__num');
+  const prevPage = document.querySelector('#prevPage');
+  const nextPage = document.querySelector('#nextPage');
+  const Fullpages = Math.ceil(discussion.length / count);
+  let FirstPage = 1;
+
+//이전,다음버튼 활성,비활성화
+  const buttonActive = () => {
+    if(FirstPage === 1) {
+      prevPage.setAttribute('disabled', "")   // 이전버튼 비활성화 ""는 true의 의미
+    }
+    else {
+      prevPage.removeAttribute('disabled')  
+    }
+    if(FirstPage === Fullpages) {             
+      nextPage.setAttribute('disabled', "")   // 마지막페이지일때 다음버튼 비활성화
+    }
+    else {
+      nextPage.removeAttribute('disabled')
+    }
+}
+  //페이지 번호 버튼 생성 
+  const showPagenumber = (index) => {
+    const pageNum = document.createElement('button');
+    pageNum.className = 'page__btn pagination__number';
+    pageNum.textContent = index;
+    pageNum.setAttribute('page-index', index)
+    pageNum.setAttribute('aria-label', 'pageNum' + index)
+    pages.append(pageNum);
+  }
+  //페이지 수 카운트
+  const PageNumber = () => {
+    for(let i = 1; i <= Fullpages; i++) {
+      showPagenumber(i)
+    }
+  }
+  
+  const pageNumber =() => {
+    const pageBtn = [...pages.children];  //page버튼 
+    pageBtn.forEach(button => {
+      button.onclick = function(event) {
+       FirstPage = event.target.getAttribute('page-index')
+      }
+    })
+  }
+
+  // 현재 페이지에 맞춰 discussion 출력
+  const setFirstPage = (pageNum) => {
+    FirstPage = pageNum;  //현재 페이지 설정
+    buttonActive();       //버튼 활성화
+    pageNumber();         
+    const prevRange = (pageNum - 1) * count; // 0, 10
+    const currRange = pageNum * count // 10, 20
+    
+    discussion.forEach((item, index) => {
+      item.classList.add('hide')
+      if(index >= prevRange && index < currRange) {
+        item.classList.remove('hide')
+      }
+    });
+    
+  }
+//pagination
+  window.addEventListener('load', () => {
+    PageNumber();
+    setFirstPage(1);
+    // 이전 클릭 페이지-1
+    prevPage.addEventListener('click', () => {
+      setFirstPage(FirstPage - 1)
+    })
+    //다음 클릭 페이지 +1
+    nextPage.addEventListener('click', () => {
+      setFirstPage(FirstPage + 1)
+    })
+    //페이지 넘버 클릭
+    const pageBtn = [...pages.children];
+    pageBtn.forEach(button => {
+      const pageIndex = Number(button.getAttribute('page-index'))
+      button.addEventListener('click', (e) => {
+        setFirstPage(pageIndex);
+      })  
+    })
+  });
+}
+
 const convertToDiscussion = (obj) => {
-  const li = document.createElement("li"); // li 요소 생성
-  li.className = "discussion__container"; // 클래스 이름 지정
-
-  const avatarWrapper = document.createElement("div");  //이미지 묶음
+  const li = document.createElement("li");
+  li.className = "discussion__container";
+  //  li.discussion__container 생성, div  자식 생성
+  const avatarWrapper = document.createElement("div");
   avatarWrapper.className = "discussion__avatar--wrapper";
- 
-  const avatarImg = document.createElement("img");  //이미지
-
-  avatarImg.src = obj.avatarUrl;  //data.js와 연동
-  avatarImg.alt = "avatar of" + obj.author;
+  const discussionContent = document.createElement("div");
+  discussionContent.className = "discussion__content";
+  const discussionAnswered = document.createElement("div");
+  discussionAnswered.className = "discussion__answered";
+  
+  // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
+  // 아바타 이미지 할당
+  const avatarImg = document.createElement("img");
   avatarImg.className = "discussion__avatar--image";
+  avatarImg.src = obj.avatarUrl;
+  avatarImg.alt = "avatar of " + obj.author;
   avatarWrapper.append(avatarImg);
 
-  const discussionContent = document.createElement("div");  //컨텐츠 박스
-  discussionContent.className = "discussion__content";
+  // 제목
+  const discussionTitle = document.createElement("h2");
+  const titleAnchor = document.createElement("a");
+  discussionTitle.className = "discussion__title"
+  titleAnchor.href = obj.url;
+  titleAnchor.textContent = obj.title;
+  discussionTitle.append(titleAnchor);
 
-  const discussionTitle = document.createElement("h2"); //컨텐츠 제목
-  discussionTitle.className = "discussion__title";
-
-  const discussionUrl = document.createElement("a");  //링크 연결
-  discussionUrl.href = obj.url;   //data.js와 연동
-  discussionUrl.textContent = obj.title;
-
-  discussionTitle.append(discussionUrl);
-
+  // 정보
   const discussionInformation = document.createElement("div");
   discussionInformation.className = "discussion__information";
-  discussionInformation.textContent = obj.createdAt;
-  discussionContent.append(discussionTitle);
-  discussionContent.append(discussionInformation);
-
-  const discussionAnswered = document.createElement("div"); //질문 체크
-  discussionAnswered.className = "discussion__answered";
-
-  const discussionIcon = document.createElement("p"); //질문 체크
-  discussionIcon.textContent = "☑";
-  discussionAnswered.append(discussionIcon);
-
-  const discussionPreview = document.createElement("button"); //미리보기
-  discussionPreview.className = "discussion__preview";
-  discussionPreview.textContent = "미리 보기";
-
-  li.append(avatarWrapper, discussionContent, discussionAnswered,discussionPreview);  //질문 체크 o,x
-  if (obj.answer === null) {
-    discussionIcon.textContent = UNANSERED_MARK;
-  } else {
-    discussionIcon.textContent = ANSWERED_MARK;
-  }
+  discussionInformation.textContent = `${obj.author} / ${obj.createdAt}`
+  discussionContent.append(discussionTitle, discussionInformation);
+  
+// 댭변 상태에 따라 아이콘 사용  img src
+  const answerImg= document.createElement("img")
+  answerImg.src = obj.answer ? 'image/sun.png' : 'image/cloud.png';
+  discussionAnswered.append(answerImg)
+  // li에 div 3개 할당
+  li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
 };
-// agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
+
+// --- agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
   for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
     element.append(convertToDiscussion(agoraStatesDiscussions[i]));
   }
+  pagination(element)
   return;
 };
-// ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
+
+// --- ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
+ // return ul.append(li)
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+
+//discussion 추가
+
+const form = document.querySelector('form.form')
+const title = document.querySelector('input#title')
+const author = document.querySelector('input#name')
+const story = document.querySelector('textarea#story')
+
+const images = ["profile_1.png","profile_2.png","profile_3.png","profile_4.png"];
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+const bgImage = document.createElement("img");
+
+
+form.addEventListener('submit', (event) =>{
+  event.preventDefault();
+
+  let newDiscussion = {
+    id : "unique value",
+    createdAt: new Date(),
+    title: title.value,
+    url: "#",
+    author: author.value,
+    answer: null,
+    bodyHTML: story.value,
+    avatarUrl: author.value ? `img/${chosenImage}` : 'image/Ghost.gif'
+  };
+  ul.prepend(convertToDiscussion(newDiscussion))
+  title.value ="";
+  author.value = "";
+  story.value = "";
+})
