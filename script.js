b4cf6b791e88b055b1f1bc884868c86742890ebe
minwoogwak99 <minwoogwak99@gmail.com>
2023-03-10 12:54:38 +0900

    added scroll animation

--- script.js
+++ script.js
@@ -49,6 +49,10 @@ const convertToDiscussion = (obj) => {
   return li;
 };
 
+
+
+
+
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
   for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
@@ -60,3 +64,21 @@ const render = (element) => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+
+
+//스크롤 애니메이션
+let observer = new IntersectionObserver((e) => {
+  e.forEach(element => {
+    if(element.isIntersecting) {
+      element.target.style.opacity = 1;
+    } else {
+      element.target.style.opacity = 0;
+    }
+  })
+})
+
+const discussion_container = document.querySelectorAll('.discussion__container');
+discussion_container.forEach((discussion, i) => {
+  observer.observe(discussion);
+})
