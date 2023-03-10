eb94f53f132fc4ea075a278ce9e4038dc0df6125
jina <wlsdk7245@gmail.com>
2023-03-10 14:27:48 +0900

    feat: style 수정 및 플로팅 추가

--- script.js
+++ script.js
@@ -188,7 +188,6 @@ discussionHref.addEventListener("click", (event) => {
 });
 
 const reRender = (event) => {
-  window.scroll(0, 0);
   render(ul, event.target.textContent);
 };
 
@@ -236,3 +235,12 @@ if (!localStorage.getItem("agoraStates")) {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+const topFloating = document.querySelector(".top-floating");
+
+topFloating.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  let PageLocation = document.querySelector(".body").offsetTop;
+  window.scrollTo({ top: PageLocation, behavior: "smooth" });
+});
