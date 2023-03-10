b7e55f1b6f0a1ad54ba3fcb3150f8bd4817191ad
coaudtn0276 <coaudtn0276@gmail.com>
2023-03-10 09:00:45 +0900

    ✅ 더미데이터 나열, 배치 X

--- script.js
+++ script.js
@@ -15,6 +15,41 @@ const convertToDiscussion = (obj) => {
 
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
 
+  // 반복문으로 그림을 다 찾아서  
+  // 만약 첫번째 배열에 그림이 들어있으면 다음 배열에 그림의 인덱스 +1 한 그림 넣기
+
+  // for(let i = 0; i < agoraStatesDiscussions.length; i += 1){
+    
+  // }
+
+
+  // 일단 다른 데이터들도 반복문 없이 넣어보기
+  // 왼쪽 엘리먼트
+  const avatarImg = document.createElement('img');
+  avatarImg.src = obj.avatarUrl;
+  avatarImg.alt = 'avatar of ' + obj.author;
+  avatarWrapper.append(avatarImg);
+
+  // 가운데 엘리먼트
+  const avatitle = document.createElement('h2');
+  avatitle.className = "discussion__title"
+  const ava_a = document.createElement('a');
+  const avainfromation = document.createElement('div');
+  avainfromation.className = "discussion__information";
+  ava_a.href = obj.url;
+  ava_a.textContent = obj.title;
+  avainfromation.textContent = obj.author + obj.createdAt;
+  avatitle.append(ava_a);
+  discussionContent.append(avatitle);
+  discussionContent.append(avainfromation);
+
+  // 오른쪽 엘리먼트
+const ava_p = document.createElement('p')
+discussionAnswered.append(ava_p);
+ava_p.textContent = '☑';
+
+// const ul = document.querySelector('ul.discussions__container')
+// ul.append(li);
 
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
