e1824a9ab0aa24d32ebae4baaa881cc17102a110
taehyeon83 <wxg5483@naver.com>
2023-03-10 11:08:36 +0900

    📝 submit 구현

--- script.js
+++ script.js
@@ -19,32 +19,62 @@ const convertToDiscussion = (obj) => {
   const avatarImg = document.createElement("img"); // img 요소 할당
   avatarImg.classList.add("discussion__avatar--image"); // class 추가
   avatarImg.src = obj.avatarUrl;
+  avatarImg.alt = `avatar of ${obj.author}`;
   avatarWrapper.append(avatarImg);
 
-  
   //타이틀
   const discussionTitle = document.createElement("h2");
   const titleAnchor = document.createElement("a");
-  discussionTitle.classList.add("discussion__title")
+  discussionTitle.classList.add("discussion__title");
   titleAnchor.textContent = obj.title; // 제목표시
-  titleAnchor.href = obj.url; //링크 
+  titleAnchor.href = obj.url; //링크
   discussionTitle.append(titleAnchor);
 
   //날짜 시간
   const discussionInformation = document.createElement("div");
-  discussionInformation.classList.add("discussion__information")
-  discussionInformation.textContent = `${obj.author} / ${obj.createdAt}`; //이름 / 
-  discussionContent.append(discussionTitle,discussionInformation);
+  discussionInformation.classList.add("discussion__information");
+  discussionInformation.textContent = `${obj.author} / ${new Date(obj.createdAt).toLocaleString()}`;
+  discussionContent.append(discussionTitle, discussionInformation);
 
   //체크박스
   const checked = document.createElement("p");
-  checked.textContent = obj.answer ? "💜" : "🤍";
+  checked.textContent = obj.answer ? "💜" : "🤍"; // 삼항연산자
   discussionAnswered.append(checked);
-
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
 };
+
+const form = document.querySelector("form.form");
+const title = document.querySelector("input#title");
+const author = document.querySelector("input#name");
+const story = document.querySelector("textarea#story");
+form.addEventListener("submit", (event) => {
+  //addEventListner : 중복가능
+  event.preventDefault(); // 자동으로 새로고침 막음
+  const newDiscussion = {
+    id: "unique value",
+    createdAt: new Date(),
+    title: title.value,
+    url: "https://github.com/codestates-seb/agora-states-fe/discussions/45",
+    author: author.value,
+    answer: {
+      id: "DC_kwDOHOApLM4AKg6M",
+      createdAt: "2022-05-16 02:09:52",
+      url: "https://github.com/codestates-seb/agora-states-fe/discussions/45#discussioncomment-2756236",
+      author: "Kingsenal",
+      bodyHTML:
+        '<p dir="auto">안녕하세요. <a class="user-mention notranslate" data-hovercard-type="user" data-hovercard-url="/users/dubipy/hovercard" data-octo-click="hovercard-link-click" data-octo-dimensions="link_type:self" href="https://github.com/dubipy">@dubipy</a> 님!<br>\n코드스테이츠 교육 엔지니어 권준혁 입니다. <g-emoji class="g-emoji" alias="raised_hands" fallback-src="https://github.githubassets.com/images/icons/emoji/unicode/1f64c.png">🙌</g-emoji></p>\n<p dir="auto">질문 주신 내용은 노드 환경이 구성되어 있지 않기 때문에 발생되는 문제로 확인됩니다.</p>\n<p dir="auto"><code class="notranslate">brew unlink node &amp;&amp; brew link node</code></p>\n<p dir="auto">노드를 연결해 보시고 안된다면</p>\n<p dir="auto"><code class="notranslate">brew link --overwrite node</code></p>\n<p dir="auto">이 명령어를 그 다음에도 안된다면 접근권한 문제일 가능성이 큽니다.</p>\n<p dir="auto"><code class="notranslate">$ sudo chmod 776 /usr/local/lib</code> 접근 권한 변경 후<br>\n<code class="notranslate">$ brew link --overwrite node</code> 다시 연결을 해보세요 !</p>\n<p dir="auto">그럼에도 안된다면 다시 한 번 더 질문을 남겨주세요 !</p>\n<p dir="auto">답변이 되셨다면 내용을 간략하게 정리해서 코멘트를 남기고 answered를 마크해주세요 <g-emoji class="g-emoji" alias="white_check_mark" fallback-src="https://github.githubassets.com/images/icons/emoji/unicode/2705.png">✅</g-emoji><br>\n감사합니다.<g-emoji class="g-emoji" alias="rocket" fallback-src="https://github.githubassets.com/images/icons/emoji/unicode/1f680.png">🚀</g-emoji><br>\n코드스테이츠 교육 엔지니어 권준혁</p>',
+      avatarUrl: "https://avatars.githubusercontent.com/u/79903256?s=64&v=4",
+    },
+    bodyHTML: story.value,
+    avatarUrl: "https://avatars.githubusercontent.com/u/97888923?s=64&u=12b18768cdeebcf358b70051283a3ef57be6a20f&v=4",
+  };
+  ul.prepend(convertToDiscussion(newDiscussion)); //prepend : 맨앞으로
+  title.value = "";
+  author.value = "";
+  story.value = "";
+});
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
@@ -57,4 +87,3 @@ const render = (element) => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
-
