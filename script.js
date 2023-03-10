65d21bbeb40d8093a572095910e6b3341eb6d68c
minsun0714 <woooooooow22@gmail.com>
2023-03-10 10:48:17 +0900

    ⚡️ form에 addEventListner 적용

--- script.js
+++ script.js
@@ -20,11 +20,11 @@ const convertToDiscussion = (obj) => {
 
   // img 태그 생성, 아바타 이미지 src에 URL 추가하여 avatarWrapper 자식 태그로 추가
   const avatarImg = document.createElement("img");
+  avatarImg.className = "discussion__avatar--image";
   avatarImg.src = obj.avatarUrl;
   avatarWrapper.append(avatarImg);
 
   // 인자로 받은 객체의 제목, 글쓴이, 글쓴 시간 선언
-  const title = obj.title;
   const author = obj.author;
   let time = obj.answer?.createdAt
     .slice(11)
@@ -37,27 +37,29 @@ const convertToDiscussion = (obj) => {
   }
 
   // author와 time을 info 변수에 저장
-  const info = author + " / " + time;
 
   // 돋보기 그림  img 태그 생성 (on/off)
   const on = document.createElement("img");
-  on.src = "./img/idea-305335_640.png";
+  on.src = "./img/cartoon-1294877_640.png";
   const off = document.createElement("img");
-  off.src = "./img/cartoon-1294877_640.png";
+  off.src = "./img/idea-305335_640.png";
 
   // 응답 있을 시 전구가 켜지도록 구현
   const response = obj.answer?.bodyHTML;
   response ? discussionAnswered.append(on) : discussionAnswered.append(off);
 
   // title에 url 적용
+  const title = obj.title;
   const linkedTitle = document.createElement("A");
   linkedTitle.setAttribute("href", obj.answer?.url);
   linkedTitle.append(title);
 
   // li tag 자식 노드로 추가
   li.append(avatarWrapper, discussionContent, discussionAnswered);
+  li.className = "discussion__container";
 
   // discussion__content 태그에 제목, info 변수(author, time) 자식 노드로 추가
+  const info = author + " / " + time;
   const discussion = document.createElement("div");
   discussion.className = "discussion__content";
   discussionInfo.innerText = info;
@@ -65,6 +67,31 @@ const convertToDiscussion = (obj) => {
 
   return li;
 };
+
+// submit 클릭 시 질문 리스트에 추가되도록 구현
+const form = document.querySelector("form.form");
+const title = document.querySelector("input#title");
+const author = document.querySelector("input#name");
+const story = document.querySelector("textarea#story");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  // 하나의 객체를 만들어서, cd함수에 넣어서 li로 만든 다음 ul요소에 append
+  const newDiscussion = {
+    id: Math.random(),
+    createdAt: new Date(),
+    title: title.value,
+    url: "https://github.com/codestates-seb/agora-states-fe/discussions/6",
+    author: author.value,
+    answer: null,
+    bodyHTML: story?.value,
+    avatarUrl:
+      "https://avatars.githubusercontent.com/u/12145019?s=64&u=5c97f25ee02d87898457e23c0e61b884241838e3&v=4",
+  };
+  ul.prepend(convertToDiscussion(newDiscussion));
+  title.value = "";
+  author.value = "";
+  story.value = "";
+});
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
