501bab8f071a2c09ace3865fb32e1aae87456ec0
shun-is-me <rhkrtlgus@naver.com>
2023-03-10 17:14:33 +0900

    fullpage를 이용해서 슬라이드 형식의 css 구성

--- script.js
+++ script.js
@@ -81,3 +81,11 @@ const render = (element) => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+
+new fullpage('#fullpage', {
+//하단은 옵션
+    autoScrolling: true,
+    scrollHorizontally: true,
+    sectionsColor: ['black', 'black', 'black', 'black'],
+});
