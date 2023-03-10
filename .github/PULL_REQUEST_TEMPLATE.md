4064ce4ab84795ba8e9a8a78bdaff014edd9fc10
Ella Yeonsu Choi <happymondaysu@gmail.com>
2023-03-10 16:08:35 +0900

    feat: Add agorastates project

--- .github/PULL_REQUEST_TEMPLATE.md
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -12,17 +12,17 @@ Github Page 배포 링크를 아래 작성합니다.
 
 스스로 구현 완료한 부분까지 체크하여 제출합니다.
 
-- [ ] 디스커션 나열 기능
-    - [ ] `script.js`를 수정하여 `agoraStatesDiscussions` 배열의 데이터를 나열할 수 있게 구현합니다.
+- [✔] 디스커션 나열 기능
+    - [✔] `script.js`를 수정하여 `agoraStatesDiscussions` 배열의 데이터를 나열할 수 있게 구현합니다.
 - [ ] CSS
-    - [ ] 아고라 스테이츠 질문 리스트가 중앙으로 와야 합니다.
-    - [ ] `style.css`를 수정하여 멋지고 아름답게 나만의 아고라 스테이츠를 꾸밉니다.
-    - [ ] [colorhunt](https://colorhunt.co/palettes/popular), [dribbble](https://dribbble.com/)에서 적절한 색 조합, 디자인을 참고합니다.
+    - [✔] 아고라 스테이츠 질문 리스트가 중앙으로 와야 합니다.
+    - [🔛] `style.css`를 수정하여 멋지고 아름답게 나만의 아고라 스테이츠를 꾸밉니다.
+    - [✔] [colorhunt](https://colorhunt.co/palettes/popular), [dribbble](https://dribbble.com/)에서 적절한 색 조합, 디자인을 참고합니다.
 - [ ] 디스커션 추가 기능
-    - [ ] `script.js`를 수정하여 디스커션 추가 기능을 구현합니다.
-    - [ ] `section.form__container` 요소에 새로운 아고라 스테이츠 질문을 추가할 수 있는 입력 폼을 제작합니다. 형식은 자유입니다.
+    - [✔] `script.js`를 수정하여 디스커션 추가 기능을 구현합니다.
+    - [✔] `section.form__container` 요소에 새로운 아고라 스테이츠 질문을 추가할 수 있는 입력 폼을 제작합니다. 형식은 자유입니다.
     - [ ] 아이디, 본문을 입력하고 버튼을 누르면 실제 화면에 디스커션이 추가되어야 합니다.
-    - [ ] `agoraStatesDiscussions` 배열에 추가한 데이터가 실제 쌓여야 합니다.
+    - [✔] `agoraStatesDiscussions` 배열에 추가한 데이터가 실제 쌓여야 합니다.
 - [ ] Github Page 배포
   - [ ] Github Page 배포 기능을 이용하여 누구나 볼 수 있게 배포합니다.
 - [ ] [코드스테이츠 fe-sprint-my-agora-states 리포지토리](https://github.com/codestates-seb/fe-sprint-my-agora-states)로 Pull Request
@@ -34,9 +34,9 @@ Github Page 배포 링크를 아래 작성합니다.
 스스로 구현 완료한 부분까지 체크하여 제출합니다.
 
 - [ ] 현지 시간 적용
-    - [ ] 샘플 시간을 잘 변형하여, 현지 시간에 맞게 표현합니다. (ex. 오전 10:02:17)
+    - [✔] 샘플 시간을 잘 변형하여, 현지 시간에 맞게 표현합니다. (ex. 오전 10:02:17)
 - [ ] 페이지네이션 기능
-    - [ ] 페이지네이션에 대해서 스스로 학습합니다.
+    - [✔] 페이지네이션에 대해서 스스로 학습합니다.
     - [ ] 한 페이지에 10개씩 디스커션이 보여야 합니다.
     - [ ] 다음 페이지로 넘어갈 수 있어야 합니다.
     - [ ] 이전 페이지로 돌아올 수 있어야 합니다.
@@ -56,7 +56,7 @@ Github Page 배포 링크를 아래 작성합니다.
 
 아래 예시를 지우고, 자신의 과제 시연 화면을 추가합니다.
 
- ![image](https://s3.ap-northeast-2.amazonaws.com/urclass-images/NB0JkuHQnLg8X1woSRS84-1652915757557.gif)
+ ![최연수_아고라스테이츠.png](../최연수_아고라스테이츠.png)
  
 ### 가장 자랑하고 싶은 기능
 
@@ -64,8 +64,10 @@ Github Page 배포 링크를 아래 작성합니다.
 
 ### 구현하고 싶었는데 하지 못한 아쉬운 기능
 
-> 적어주세요
+> 자바스크립트 전체적으로 구현이 어려웠습니다.<br/>
+> 모르는 개념들을 찾고 익히느라 시간이 오래걸려서 욕심에 비해 완성도가 떨어진게 아쉽습니다.<br/>
+> 이후에 시간을 더 내서 CSS, JS: pagination까지 차차 완성하려고 합니다.
 
 ### 도움을 받고 싶은 부분
 
-> 적어주세요
+> submit이 왜 안되는지 모르겠어요.
--- script.js
+++ script.js
@@ -1,27 +1,89 @@
-// index.html을 열어서 agoraStatesDiscussions 배열 요소를 확인하세요.
-console.log(agoraStatesDiscussions);
-
-// convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
+// AgoraStates Data -> DOM
 const convertToDiscussion = (obj) => {
-  const li = document.createElement("li"); // li 요소 생성
-  li.className = "discussion__container"; // 클래스 이름 지정
+  const li = document.createElement("li"); // li elements
+  li.className = "discussion__big--container"; // class name setting
+  
+  const elContainer = document.createElement('div');
+  elContainer.className = 'discussion__container'
+  const avatarWrapper = document.createElement('div');
+  avatarWrapper.className = 'discussion__avatar--wrapper';
+  const discussionContent = document.createElement('div');
+  discussionContent.className = 'discussion__content';
+  const discussionAnswered = document.createElement('div');
+  discussionAnswered.className = 'discussion__answered';
 
-  const avatarWrapper = document.createElement("div");
-  avatarWrapper.className = "discussion__avatar--wrapper";
-  const discussionContent = document.createElement("div");
-  discussionContent.className = "discussion__content";
-  const discussionAnswered = document.createElement("div");
-  discussionAnswered.className = "discussion__answered";
-
-  // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
-
-
-
-  li.append(avatarWrapper, discussionContent, discussionAnswered);
+  // Image
+  const avatarImage = document.createElement('img');
+  avatarImage.className = 'discussion__avatar--image'
+  avatarImage.alt = `avatar of ${obj.author}`
+  avatarWrapper.append(avatarImage)
+  // Title
+  const title = document.createElement('h2');
+  title.className = 'discussion__title'
+  // Information
+  const information = document.createElement('div');
+  information.className = 'discussion__information'
+  // URL
+  const url = document.createElement('a');
+  // Checkbox
+  const checkbox = document.createElement('p');
+  checkbox.className = 'checkbox';
+  
+  // setAttribute
+  avatarImage.setAttribute('src', obj.avatarUrl);
+  url.textContent = obj.title;
+  information.textContent = `${obj.author} / ${new Date(obj.createdAt).toLocaleString()}`;
+  url.setAttribute('href', obj.url);
+  checkbox.textContent = '✅';
+  
+  // Append
+  li.append(elContainer);
+  elContainer.append(avatarWrapper, discussionContent, discussionAnswered);
+  avatarWrapper.append(avatarImage);
+  discussionContent.append(title, information);
+  discussionAnswered.append(checkbox);
+  title.append(url);
+  
+  // Answer -----------------------
+  if(obj.answer !== null) {
+    const ansWrapper = document.createElement('ul');
+    ansWrapper.className = 'answer__wrapper';
+    // First answer
+    const answer = document.createElement('li');
+    answer.className = 'answer';
+    // Avatar
+    const ansAvatarWrapper = document.createElement('div');
+    ansAvatarWrapper.className = 'answer__avatar--wrapper';
+    const ansAvatarImg = document.createElement('img');
+    ansAvatarImg.className = 'answer__avatar--image'
+    // Answer Title
+    const ansTitle = document.createElement('h2');
+    ansTitle.className = 'answer__title'
+    const url = document.createElement('a');
+    // Answer Contents
+    const ansContent = document.createElement('div');
+    ansContent.className = 'answer__content';
+    // Answer Information
+    const ansInformation = document.createElement('div');
+    ansInformation.className = 'answer__Information';
+    // Text Contents
+    url.textContent = '답변';
+    checkbox.textContent = '✅';
+    url.href = obj.answer.url;
+    ansAvatarImg.src = obj.answer.avatarUrl;
+    ansInformation.textContent = `${obj.answer.author} / ${new Date(obj.answer.createdAt).toLocaleString()}`
+    // Append
+    li.append(ansWrapper);
+    ansWrapper.append(answer);
+    answer.append(ansAvatarWrapper, ansContent);
+    ansAvatarWrapper.append(ansAvatarImg);
+    ansContent.append(ansTitle,ansInformation);
+    ansTitle.append(url); 
+  }
   return li;
 };
 
-// agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
+//  Function: Rendering array data of agoraStatesDiscussions
 const render = (element) => {
   for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
     element.append(convertToDiscussion(agoraStatesDiscussions[i]));
@@ -29,6 +91,45 @@ const render = (element) => {
   return;
 };
 
-// ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
-const ul = document.querySelector("ul.discussions__container");
+// IF: Rendering all agoraStatesDiscussions data of -> ul elements
+function getLocalStorage() {
+  if(localStorage.length === 0) {  
+  }
+  else {
+    agoraStatesDiscussions = JSON.parse(localStorage.getItem('json'));
+  }
+}
+getLocalStorage();
+
+const ul = document.querySelector('ul.discussions__container');
 render(ul);
+
+// New Question -----------------------
+const newTitle = document.querySelector('#title');
+const newName = document.querySelector('#name');
+const newStory = document.querySelector('#story');
+const form = document.querySelector('form.form');
+
+// Submit -----------------------
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const obj = {
+    id: "new id",
+    createdAt: new Date(),
+    title: newTitle.value,
+    url: undefined,
+    author: newName.value,
+    answer: null,
+    bodyHTML: newStory.value,
+    avatarUrl:myAvatar,
+  }
+  agoraStatesDiscussions.unshift(obj);
+  ul.prepend(convertToDiscussion(obj));
+
+  newName.value = '';
+  newStory.value = '';
+  newTitle.value = '';
+  
+  listItems = document.querySelectorAll("li.discussion__big--container");
+  setCurrentPage(1);
+});
