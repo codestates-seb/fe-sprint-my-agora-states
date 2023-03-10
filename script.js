3ad21b1ed0da772c7c523675c27dffa82c74bdd0
Mooobi <qkrantod@gmail.com>
2023-03-10 11:28:14 +0900

    디스커션 정렬, 추가, 현지시 적용

--- script.js
+++ script.js
@@ -13,9 +13,32 @@ const convertToDiscussion = (obj) => {
   const discussionAnswered = document.createElement("div");
   discussionAnswered.className = "discussion__answered";
 
-  // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
+  const discussionTitle = document.createElement('h2');
+  discussionTitle.className = 'discussion__title';
+  const discussionInformation = document.createElement('div');
+  discussionInformation.className = 'discussion__information';
+  discussionContent.append(discussionTitle, discussionInformation);
+  discussionInformation.textContent = `${obj['author']} / ${new Date(obj['createdAt']).toLocaleString()}` ;
 
+  const discussionTitleLink = document.createElement('a');
+  discussionTitleLink.href = obj['url'];
+  discussionTitleLink.textContent = obj['title'];
+  discussionTitle.append(discussionTitleLink);
 
+  const avaterImg = document.createElement('img')
+  avaterImg.className = 'discussion__avatar--image';
+  avaterImg.src = obj['avatarUrl'];
+  avaterImg.art = obj['author'];
+  avatarWrapper.append(avaterImg);
+
+  const answerCheck = document.createElement('img');
+  answerCheck.className = 'answerCheck'
+  if (obj['answer']) {
+    answerCheck.src = 'https://cdn.pixabay.com/photo/2017/01/13/01/22/ok-1976099_1280.png';
+  } else {
+    answerCheck.src = 'https://cdn.pixabay.com/photo/2017/02/12/21/29/false-2061131_1280.png';
+  }
+  discussionAnswered.append(answerCheck);
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
@@ -32,3 +55,28 @@ const render = (element) => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+const form = document.querySelector('.form');
+
+const inputName = document.querySelector('#name');
+const inputTitle = document.querySelector('#title');
+const inputQuesiton = document.querySelector('#story');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const newdiscussion = {
+    id: 'none',
+    createdAt: new Date().toLocaleString(),
+    title: inputTitle.value,
+    url: "https://sebfe-codestates.zendesk.com/hc/ko",
+    author: inputName.value,
+    answer: null,
+    bodyHTML: inputQuesiton.value,
+    avatarUrl: "https://cdn.pixabay.com/photo/2016/09/01/08/24/smiley-1635449_1280.png",
+  };
+
+  ul.prepend(convertToDiscussion(newdiscussion));
+
+inputName.value = inputTitle.value = inputQuesiton.value = '';
+})
