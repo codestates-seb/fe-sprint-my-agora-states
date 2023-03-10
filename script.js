bf72870239d44f7e2883c5c4f3227fc6da6f9029
shun-is-me <rhkrtlgus@naver.com>
2023-03-10 12:26:55 +0900

    입력한 폼을 목록에 추가하기

--- script.js
+++ script.js
@@ -46,6 +46,26 @@ const convertToDiscussion = (obj) => {
   return li;
 };
 
+//입력한 질문도 li에 넣기
+const form = document.querySelector('form.form');
+const title = document.querySelector('input#title');
+const author = document.querySelector('input#name');
+const story = document.querySelector('textarea#story');
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const newDiscussion = {
+    id: "unique value",
+    createdAt: new Date,
+    title: title.value,
+    url: "https://github.com/codestates-seb/agora-states-fe/discussions/45",
+    author: author.value,
+    answer: '',
+    bodyHTML: story.value,
+    avatarUrl:
+      "https://avatars.githubusercontent.com/u/97888923?s=64&u=12b18768cdeebcf358b70051283a3ef57be6a20f&v=4",
+  };
+  ul.prepend(convertToDiscussion(newDiscussion))
+})
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
