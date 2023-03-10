f4c3ad3373c26e88ef91ce88c975baf655fd2523
KingGyeongHoo <fmaxgo@naver.com>
2023-03-10 10:49:44 +0900

    restore

--- script.js
+++ script.js
@@ -84,8 +84,6 @@ const render = (element, first, last) => {
   return;
 };
 
-
-
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul, 0, 10);
