5b8b28072e26067a91acaa129c438adb168866d3
crowcrow07 <skathd3402@gmail.com>
2023-03-10 15:10:07 +0900

    페이지네이션 구현

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const userSubmit = document.querySelector("form.form");
+const questionButton = document.querySelector(".question_button");
+const buttons = document.querySelector(".buttons");
+
+const numOfContent = agoraStatesDiscussions.length;
+const showContent = 10;
+const showButton = 5;
 
 // console.log(agoraStatesDiscussions);
 
 //submit 버튼이 눌리는지를 알고싶다.
+
+function newPage() {
+  window.location.href = "https://urclass.codestates.com/";
+}
 
 userSubmit.addEventListener("submit", function (event) {
   event.preventDefault();
@@ -15,19 +25,25 @@ userSubmit.addEventListener("submit", function (event) {
     title: titleInput.value,
     url: "#",
     author: nameInput.value,
-    answer: {
-      id: "DC_kwDOHOApLM4AKg6M",
-      createdAt: "2022-05-16T02:09:52Z",
-      url: "https://github.com/codestates-seb/agora-states-fe/discussions/45#discussioncomment-2756236",
-      author: "Kingsenal",
-      bodyHTML:
-        '<p dir="auto">안녕하세요. <a class="user-mention notranslate" data-hovercard-type="user" data-hovercard-url="/users/dubipy/hovercard" data-octo-click="hovercard-link-click" data-octo-dimensions="link_type:self" href="https://github.com/dubipy">@dubipy</a> 님!<br>\n코드스테이츠 교육 엔지니어 권준혁 입니다. <g-emoji class="g-emoji" alias="raised_hands" fallback-src="https://github.githubassets.com/images/icons/emoji/unicode/1f64c.png">🙌</g-emoji></p>\n<p dir="auto">질문 주신 내용은 노드 환경이 구성되어 있지 않기 때문에 발생되는 문제로 확인됩니다.</p>\n<p dir="auto"><code class="notranslate">brew unlink node &amp;&amp; brew link node</code></p>\n<p dir="auto">노드를 연결해 보시고 안된다면</p>\n<p dir="auto"><code class="notranslate">brew link --overwrite node</code></p>\n<p dir="auto">이 명령어를 그 다음에도 안된다면 접근권한 문제일 가능성이 큽니다.</p>\n<p dir="auto"><code class="notranslate">$ sudo chmod 776 /usr/local/lib</code> 접근 권한 변경 후<br>\n<code class="notranslate">$ brew link --overwrite node</code> 다시 연결을 해보세요 !</p>\n<p dir="auto">그럼에도 안된다면 다시 한 번 더 질문을 남겨주세요 !</p>\n<p dir="auto">답변이 되셨다면 내용을 간략하게 정리해서 코멘트를 남기고 answered를 마크해주세요 <g-emoji class="g-emoji" alias="white_check_mark" fallback-src="https://github.githubassets.com/images/icons/emoji/unicode/2705.png">✅</g-emoji><br>\n감사합니다.<g-emoji class="g-emoji" alias="rocket" fallback-src="https://github.githubassets.com/images/icons/emoji/unicode/1f680.png">🚀</g-emoji><br>\n코드스테이츠 교육 엔지니어 권준혁</p>',
-      avatarUrl: "./1.jpg",
-    },
+    answer: null,
     bodyHTML: storyInput.value,
     avatarUrl: "./1.jpg",
   };
   ul.prepend(convertToDiscussion(obj));
+  nameInput.value = "";
+  titleInput.value = "";
+  storyInput.value = "";
+});
+let cnt = 0; //버튼 눌린 횟수
+questionButton.addEventListener("click", function () {
+  cnt += 1;
+  if (cnt % 2 === 1) {
+    userSubmit.classList.remove("hide");
+    questionButton.textContent = "질문 안할래요!";
+  } else {
+    userSubmit.classList.add("hide");
+    questionButton.textContent = "질문할래요!";
+  }
 });
 
 const date = () => {
@@ -102,3 +118,51 @@ const render = (element) => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+const makeButton = (id) => {
+  const button = document.createElement("button");
+  button.classList.add("button");
+  button.dataset.num = id;
+  button.innerText = id;
+  button.addEventListener("click", (e) => {
+    Array.prototype.forEach.call(buttons.children, (button) => {
+      if (button.dataset.num) button.classList.remove("active");
+    });
+    e.target.classList.add("active");
+    renderContent(parseInt(e.target.dataset.num));
+  });
+  return button;
+};
+
+// 페이지네이션 버튼 그룹 생성 함수
+const makeButtonGroup = (start, end) => {
+  const group = document.createElement("div");
+  group.classList.add("button-group");
+  for (let i = start; i <= end; i++) {
+    group.append(makeButton(i));
+  }
+  return group;
+};
+
+// 화면에 보여줄 데이터를 계산하는 함수
+const getDisplayData = (page) => {
+  const start = (page - 1) * showContent;
+  const end = start + showContent;
+  return agoraStatesDiscussions.slice(start, end);
+};
+
+// 화면에 데이터를 렌더링하는 함수
+const renderContent = (page) => {
+  const displayData = getDisplayData(page);
+  ul.innerHTML = "";
+  for (let i = 0; i < displayData.length; i += 1) {
+    ul.append(convertToDiscussion(displayData[i]));
+  }
+};
+
+// 페이지네이션 버튼 그룹 생성
+const buttonGroup = makeButtonGroup(1, Math.ceil(numOfContent / showContent));
+buttons.append(buttonGroup);
+
+// 초기 페이지 설정
+renderContent(1);
