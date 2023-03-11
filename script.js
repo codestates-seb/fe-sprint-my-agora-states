7c7cb950a0845092ddd5bf90b82a4d3f02950997
Jeon_Ye_Hun <yahun722@naver.com>
2023-03-11 18:15:56 +0900

    css 추가

--- script.js
+++ script.js
@@ -14,8 +14,29 @@ const convertToDiscussion = (obj) => {
   discussionAnswered.className = "discussion__answered";
 
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
+  // 이미지 만들기
+  const avatarimg = document.createElement('img');
+  avatarimg.className = "discussion__avatar--image";
+  avatarimg.src = obj.avatarUrl;
+  avatarimg.alt = 'avatar ${obj.author}';
+  avatarWrapper.append(avatarimg)
+  // 제목 만들기
+  const contentTitle = document.createElement('h2');
+  contentTitle.className = "discussion__title";
 
-
+  const TitleAchor = document.createElement('a');
+  TitleAchor.href = obj.url;
+  TitleAchor.textContent = obj.title;
+  discussionContent.append(TitleAchor);
+  // 글쓴이 날짜 만들기 
+  const contentInfo = document.createElement('div');
+  contentInfo.className = "discussion__information";
+  contentInfo.textContent = `${obj.author} / ${new Date(obj.createdAt).toLocaleString("ko-KR")}`;
+  discussionContent.append(contentTitle, contentInfo);
+  // 체크박스 만들기
+  const checked = document.createElement('p');
+  checked.textContent = obj.answer ? "✅" : "❌"
+  discussionAnswered.append(checked);
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
@@ -28,6 +49,35 @@ const render = (element) => {
   }
   return;
 };
+//  문서 내용 가져오기
+const form = document.querySelector("form.form");
+const title = document.querySelector("input#title");
+const author = document.querySelector("input#name");
+const question = document.querySelector("textarea#story");
+
+form.addEventListener('submit',(event) => {
+  event.preventDefault(); // 서브밋 이벤트로 사용시 꼭 사용해야함
+  const newdic = {
+    id : "new id",
+    createdAt : new Date().toISOString(),
+    title : title.value ,
+    url : "https://github.com/codestates-seb/agora-states-fe/discussions/44",
+    author : author.value,
+    answer : null,
+    bodyHTML : question.value,
+    avatarUrl : "https://avatars.githubusercontent.com/u/90553688?s=64&u=3c4e4dc2053d4977ac12b9cfc2667582f986d3d8&v=4"
+  }
+  ul.prepend(convertToDiscussion(newdic));
+  title.value = "";
+  author.value = "";
+  question.value = "";
+
+})
+  
+
+
+
+
 
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
