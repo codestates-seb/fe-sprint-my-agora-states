3834d53180d5805610764731398f60867d7fdac3
Mooobi <qkrantod@gmail.com>
2023-03-12 13:50:06 +0900

    local storage 기능 구현

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 // index.html을 열어서 agoraStatesDiscussions 배열 요소를 확인하세요.
 console.log(agoraStatesDiscussions);
 
+// local storage 구현을 위한 data 변수 선언
+let data;
+const dataFromLocalStorage = localStorage.getItem('agoraStatesDiscussions')
+if (dataFromLocalStorage) {
+  data = JSON.parse(dataFromLocalStorage);
+} else {
+  data = agoraStatesDiscussions.slice();
+}
+
+
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
 const convertToDiscussion = (obj) => {
   const li = document.createElement("li"); // li 요소 생성
@@ -45,47 +55,21 @@ const convertToDiscussion = (obj) => {
 };
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
+// local storage 구현을 위해 배열을 data로 수정
 const render = (element, from, to) => {
   console.log(from. to);
   if (!from && !to) {
     from = 0;
-    to = agoraStatesDiscussions.length -1;
+    to = data.length -1;
   }
 
   while (element.firstChild) {
     element.removeChild(element.firstChild);
   }
   for (let i = from; i < to; i++) {
-    element.append(convertToDiscussion(agoraStatesDiscussions[i]));
+    element.append(convertToDiscussion(data[i]));
   }
 };
-
-// 디스커션 추가 기능 구현
-
-const form = document.querySelector('.form');
-
-const inputName = document.querySelector('#name');
-const inputTitle = document.querySelector('#title');
-const inputQuesiton = document.querySelector('#story');
-
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-
-  const newdiscussion = {
-    id: 'none',
-    createdAt: new Date(),
-    title: inputTitle.value,
-    url: "https://sebfe-codestates.zendesk.com/hc/ko",
-    author: inputName.value,
-    answer: null,
-    bodyHTML: inputQuesiton.value,
-    avatarUrl: "https://cdn.pixabay.com/photo/2016/09/01/08/24/smiley-1635449_1280.png",
-  };
-
-  ul.prepend(convertToDiscussion(newdiscussion));
-
-inputName.value = inputTitle.value = inputQuesiton.value = '';
-});
 
 // 페이지네이션 이전,다음 기능 구현
 
@@ -129,18 +113,62 @@ buttons.lastElementChild.addEventListener("click", () => {
 
 // 페이지네이션 (1,2,3,..) 기능 구현
 
-
-// 페이지네이션 1,2,3,.. UI 구현
 const totalItems = agoraStatesDiscussions.length;
 const totalPages = Math.ceil(Number(totalItems)/Number(limit));
 
 const paginationContainer = document.querySelector('.numberingButtons');
 
+const createPaginationClickHandler = (i) => {
+  return () => {
+    page = i;
+    const { pageStart, pageEnd } = getPageStartEnd(limit, page);
+    render(ul, pageStart, pageEnd);
+  };
+};
+
 for (let i = 1; i <= totalPages; i++) {
-const paginationItem = document.createElement('button');
-paginationItem.textContent = i;
-paginationContainer.appendChild(paginationItem);
-}
+  const paginationItem = document.createElement('button');
+  paginationItem.textContent = i;
+  paginationContainer.appendChild(paginationItem);
+
+  paginationItem.addEventListener('click', createPaginationClickHandler(i));
+};
+
+// 디스커션 추가 기능 구현
+// limit 변수 선언 뒤로 위치 변경
+
+const form = document.querySelector('.form');
+
+const inputName = document.querySelector('#name');
+const inputTitle = document.querySelector('#title');
+const inputQuesiton = document.querySelector('#story');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const newdiscussion = {
+    id: 'none',
+    createdAt: new Date(),
+    title: inputTitle.value,
+    url: "https://sebfe-codestates.zendesk.com/hc/ko",
+    author: inputName.value,
+    answer: null,
+    bodyHTML: inputQuesiton.value,
+    avatarUrl: "https://cdn.pixabay.com/photo/2016/09/01/08/24/smiley-1635449_1280.png",
+  };
+
+  // ul.prepend(convertToDiscussion(newdiscussion));
+  // ul의 자식으로 넣는 대신 앞에서부터 data 배열에 추가
+  data.unshift(newdiscussion);
+
+  // local storage에 저장
+  localStorage.setItem('agoraStatesDiscussions', JSON.stringify(data));
+
+  render(ul, 0, limit);
+  
+  inputName.value = inputTitle.value = inputQuesiton.value = '';
+});
+
 
 // const pagination = document.querySelector('.paginaton');
 
