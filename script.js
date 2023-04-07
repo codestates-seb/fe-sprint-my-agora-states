8bc7d2747d3ea4171e6131cd724cfa73108a7ce7
KimSuSung0326 <suseong326@naver.com>
2023-04-07 15:57:54 +0900

    ✅ change

--- script.js
+++ script.js
@@ -65,14 +65,21 @@ form.addEventListener('submit',(event) => {
 })
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
+let serverDiscussions = []; // 서버에서 받은 데이터를 담을 변수를 준비한다.
+fetch('http://localhost:4000/discussions')
+  .then(res => res.json())
+  .then(json => {
+    serverDiscussions = json; //받아온 데이터를 담아준다.
+    const ul = document.querySelector('ul.discussions__container');
+  //ul이라는 변수로 ul.discussions__container 를 담아온다.
+    render(ul); //render한다
+  });
+
+// ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const render = (element) => {
   for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
     element.append(convertToDiscussion(agoraStatesDiscussions[i]));
+    //ul.append와 같다.
   }
   return;
 };
-
-// ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
-const ul = document.querySelector("ul.discussions__container");
-render(ul);
-ul.append(li)
