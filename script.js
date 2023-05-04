718c4ee32cbee5ec483506a2c91a3fa1d5e8e716
agnes0304 <jiwoochoi0304@gmail.com>
2023-05-04 14:13:31 +0900

    project layout setting and background image

--- script.js
+++ script.js
@@ -49,3 +49,6 @@ const render = (element) => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+// 일단 초기 스타일링 전 모습 완료
+
