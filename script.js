3c3b2b19339e4982afe6251119f6e17b0b52ebf1
hanbinchoi <wkghskak@naver.com>
2023-05-04 16:37:36 +0900

    feat. post처리

--- script.js
+++ script.js
@@ -53,3 +53,89 @@ const render = (element) => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+function remove_children(parent) {
+  while (parent.firstChild) {
+    parent.firstChild.remove();
+  }
+}
+
+const form = document.querySelector("form");
+
+function getTime(date) {
+  const y = date.getFullYear();
+  const m = date.getMonth() + 1;
+  const d = date.getDate();
+  const h = date.getHours();
+  const minute = date.getMinutes();
+  const s = date.getSeconds();
+  return `${y}-${String(m).padStart(2, "0")}-${String(d).padStart(
+    2,
+    "0"
+  )}T${String(h).padStart(2, "0")}:${String(minute).padStart(2, "0")}:${String(
+    s
+  ).padStart(2, "0")}Z`;
+}
+class IDGenerator {
+  #count = 0;
+  getID() {
+    const timestamp = new Date().getTime().toString(16);
+    const randomNumber = Math.floor(Math.random() * 2 ** 20).toString(16);
+
+    const count = this.#count.toString(16);
+    this.#count = (this.#count + 1) % 2 ** 12;
+
+    return timestamp + randomNumber.padStart(5, "0") + count.padStart(3, "0");
+  }
+}
+
+function handleFormEvent(event) {
+  event.preventDefault();
+
+  // 입력받은 값을 오브젝트에 저장
+  // 저장할 것들 (id, 시간, 제목, url, 작성자, 답변 유무, 내용, 작성자 사진, )
+
+  const target = event.target;
+
+  // id
+  const id = new IDGenerator().getID();
+
+  // 시간
+  const date = new Date();
+  const createdAt = getTime(date);
+
+  // 제목
+  const title = target.querySelector(".form__input--title > input").value;
+
+  // url
+  const url = null;
+
+  // 작성자
+  const author = target.querySelector(".form__input--name > input").value;
+
+  // 답변
+  const answer = null;
+
+  // 내용
+  const bodyHTML = target.querySelector("#story").value;
+
+  // 작성자 사진
+  const avatarUrl = null;
+
+  // 객체화
+  const item = {
+    id,
+    createdAt,
+    title,
+    url,
+    author,
+    answer,
+    bodyHTML,
+    avatarUrl,
+  };
+  agoraStatesDiscussions.unshift(item);
+  remove_children(ul);
+  render(ul);
+}
+
+form.addEventListener("submit", handleFormEvent);
