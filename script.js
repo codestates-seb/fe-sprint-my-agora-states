ce4bf777d56184d3977e9359731eb4b6d1a7809c
agnes0304 <jiwoochoi0304@gmail.com>
2023-05-04 16:03:45 +0900

    form submit ongoing

--- script.js
+++ script.js
@@ -14,24 +14,24 @@ const convertToDiscussion = (obj) => {
   discussionAnswered.className = "discussion__answered";
 
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
-  const avatarImg = document.createElement('img');
+  const avatarImg = document.createElement("img");
   avatarImg.src = obj.avatarUrl;
-  avatarImg.alt = 'avatar of '+obj.author;
+  avatarImg.alt = "avatar of " + obj.author;
   avatarWrapper.append(avatarImg);
 
-  const titleEl = document.createElement('h2');
-  const titleLink = document.createElement('a');
+  const titleEl = document.createElement("h2");
+  const titleLink = document.createElement("a");
   titleLink.href = obj.url;
   titleEl.textContent = obj.title;
   titleLink.append(titleEl);
   discussionContent.append(titleLink);
 
-  const discussionInfo = document.createElement('div');
-  discussionInfo.textContent = obj.author + ' / ' + obj.createdAt;
+  const discussionInfo = document.createElement("div");
+  discussionInfo.textContent = obj.author + " / " + obj.createdAt;
   discussionContent.append(discussionInfo);
 
-  const discussionChecked = document.createElement('p');
-  discussionChecked.textContent = obj.answer ? '☑' : '☒';
+  const discussionChecked = document.createElement("p");
+  discussionChecked.textContent = obj.answer ? "☑" : "☒";
   discussionAnswered.append(discussionChecked);
 
   li.append(avatarWrapper, discussionContent, discussionAnswered); // 단일 요소 하나를 생성하는 애
@@ -48,7 +48,56 @@ const render = (element) => {
 
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
+const firstLi = ul.firstChild;
 render(ul);
 
+console.log(firstLi);
+
 // 일단 초기 스타일링 전 모습 완료
+
+// 폼에 입력하면 그 정보를 받아와야 함: 버튼 클릭에서도 사용해야함. 폼 가지고 오기
+const form = document.querySelector("form");
+
+const title = form.querySelector(".form__input--title > input");
+const author = form.querySelector(".form__input--name > input");
+const discussionText = form.querySelector(".form__textbox > input");
+ 
+console.log(firstLi.value)
+console.log(title.value)
+console.log(author.value)
+console.log(discussionText.value)
+// 사용자가 인풋에 입력을 하고 제출 버튼을 누르면 -> 일단 작성자, 제목만 수집, 아바타는 기본 아바타 이미지 주기
+// li 항목으로 생성해서
+// 가장 첫번째 항목으로 추가하고 / HTML에 렌더링하기
+// 리스트를 전부 다 계속 새로고침해서 렌더링하게 하는 건 좀 문제가 있음.
+
+// 랜덤한 id생성하는 방법(이건 나중에 직접 작성해보기: 지금은 gpt)
+function generateRandomId() {
+  const chars =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let randomId = "";
+  for (let i = 0; i < 10; i++) {
+    randomId += chars[Math.floor(Math.random() * chars.length)];
+  }
+};
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const obj = {
+    id: generateRandomId(),
+    createdAt: new Date(),
+    title: title.value,
+    url: "DEFAULT",
+    author: author.value,
+    answer: {},
+    bodyHTML: discussionText.value,
+    avatarUrl: "",
+  };
+  console.log(obj);
+  agoraStatesDiscussions.unshift(obj);
+  const itemLi = convertToDiscussion(obj);
+  // render(itemLi);
+  // 위에처럼 하면 결국 어레이 다 돌고 로드하잖아. 그냥 html에 삽입해주는 방식으로 해야 할듯.
+  ul.insertBefore(itemLi, firstLi);
+});
 
