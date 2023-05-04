f26b44f544185478a2cab9327bcc9bec9795d149
dongyunkim96 <dongyunkim96@gmail.com>
2023-05-04 00:51:30 -0400

    test

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const convertToDiscussion = (obj) => {
 
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
 
-
+//hello world
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
