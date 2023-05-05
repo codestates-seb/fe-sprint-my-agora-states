6fc40b33206e28d3978b3fb76b1c50f66f647490
hanbinchoi <wkghskak@naver.com>
2023-05-05 19:01:08 +0900

    modal 적용 (시간, 작성자, 아바타 이미지)

--- modal.js
+++ modal.js
@@ -7,9 +7,31 @@ const openModal = (event) => {
   const targetData = agoraStatesDiscussions.find((x) => x.id === id);
 
   const modalTitle = document.querySelector(".modal__title > h2");
-  const modalContent = document.querySelector(".modal__story");
   modalTitle.textContent = targetData.title;
+
+  const modalContent = document.querySelector(".modal__story");
   modalContent.innerHTML = targetData.bodyHTML;
+
+  const modalAvatar = document.querySelector(".modal__avatar--wrapper");
+  while (modalAvatar.firstChild) {
+    modalAvatar.firstChild.remove();
+  }
+  const avatarImg = document.createElement("img");
+  avatarImg.src = targetData.avatarUrl;
+  avatarImg.alt = `avatar of ${targetData.author}`;
+  modalAvatar.append(avatarImg);
+
+  const modalAuthor = document.querySelector(".modal__author--name");
+  modalAuthor.textContent = targetData.author;
+
+  const modalRecordTime = document.querySelector(".modal__record__time");
+  const time = targetData.createdAt;
+  console.log(time);
+  modalRecordTime.textContent = `${Number(time.slice(0, 4))}년 ${Number(
+    time.slice(5, 7)
+  )}월 ${Number(time.slice(8, 10))}일  ${Number(
+    time.slice(11, 13)
+  )}:${time.slice(14, 16)}`;
 
   modal.classList.remove("hidden");
   document.body.style.overflow = "hidden";
