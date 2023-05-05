cd395e2941cf3dd81e5faea6fdba65b7321da36f
hanbinchoi <wkghskak@naver.com>
2023-05-05 22:44:09 +0900

    제목으로 search

--- search.js
+++ search.js
@@ -1,3 +1,28 @@
 const searchInput = document.querySelector(".search--input");
 
-console.log(searchInput);
+function handleSearch(event) {
+  const container = document.querySelector(".discussions__search__container");
+  const titles = document.querySelectorAll(".discussion__title");
+  const stories = document.querySelectorAll(".discussion__story");
+  const authors = document.querySelectorAll(".discussion__author");
+
+  while (container.firstChild) {
+    container.firstChild.remove();
+  }
+  const searchKey = event.target.value;
+  if (searchKey === "") {
+    render(ul);
+    return;
+  }
+  const discussions = ul.childNodes;
+  console.dir(discussions);
+
+  for (i of titles) {
+    if (i.textContent.toUpperCase().includes(searchKey.toUpperCase())) {
+      container.append(i.closest("li"));
+    }
+  }
+  render(container);
+}
+
+searchInput.addEventListener("keyup", handleSearch);
