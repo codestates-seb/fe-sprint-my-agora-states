fce6e78b1097ac047e36034e57448f526fb340c5
YangSaekyul <covetsky1@gmail.com>
2023-05-05 16:18:33 +0900

    Bare Minimum

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // index.html을 열어서 agoraStatesDiscussions 배열 요소를 확인하세요.
 console.log(agoraStatesDiscussions);
 
+// 0. 배열 불러오기
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
 const convertToDiscussion = (obj) => {
   const li = document.createElement("li"); // li 요소 생성
@@ -13,9 +14,28 @@ const convertToDiscussion = (obj) => {
   const discussionAnswered = document.createElement("div");
   discussionAnswered.className = "discussion__answered";
 
+  const avatarImg = document.createElement('img');
+  avatarImg.className = "discussion__avatar--image";
+  avatarImg.src = obj.avatarUrl;
+  avatarImg.alt = 'avatar of ' + obj.author;
+  avatarWrapper.append(avatarImg);
+
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
 
+  const discussionTitle = document.createElement('h2');
+  discussionTitle.className = 'discussion__title';
+  discussionTitle.textContent = obj.title;
+  discussionContent.append(discussionTitle);
 
+  const discussionInfo = document.createElement('div');
+  discussionInfo.className = 'discussion__information';
+  discussionInfo.textContent = `${obj.author} / ${obj.createdAt}`;
+
+  discussionContent.append(discussionInfo);
+
+  const discussionCheckbox = document.createElement("p");
+  discussionCheckbox.textContent = "☑";
+  discussionAnswered.append(discussionCheckbox);
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
@@ -32,3 +52,35 @@ const render = (element) => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+// 1. 새로운 값을 입력했을 때 배열 추가
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault(); // 기본 동작(페이지 새로고침) 방지
+
+  const name = document.querySelector('#name').value;
+  const title = document.querySelector('#title').value;
+  const story = document.querySelector('#story').value;
+
+  // 새로운 데이터 생성
+  const newDiscussion = {
+    author: name,
+    avatarUrl: "https://via.placeholder.com/150",
+    title: title,
+    createdAt: new Date().toLocaleString('ko-KR', { timeZone: 'Asia/Seoul' }),
+    content: story,
+  };
+
+  agoraStatesDiscussions.push(newDiscussion); // 새로운 데이터를 배열에 추가
+
+  // 화면에 렌더링
+  const ul = document.querySelector("ul.discussions__container");
+  const newDiscussionElement = convertToDiscussion(newDiscussion);
+  ul.insertBefore(newDiscussionElement, ul.firstChild);
+
+  // 입력창 초기화
+  document.querySelector('#name').value = "";
+  document.querySelector('#title').value = "";
+  document.querySelector('#story').value = "";
+});
