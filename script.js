a8f47b0462ca89ee6a1f4f91472fc65aa996cad0
Minsu Lee <dlalstn5789@gmail.com>
2023-05-05 15:37:12 +0900

    05.05 discussions을 localstorage에 추가하고 실제 화면에 추가

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 // index.html을 열어서 agoraStatesDiscussions 배열 요소를 확인하세요.
 console.log(agoraStatesDiscussions);
+for (let i = 0; i < window.localStorage.length; i++) {
+  agoraStatesDiscussions.unshift(
+    JSON.parse(window.localStorage.getItem(`${i}`))
+  );
+}
 
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
 const convertToDiscussion = (obj) => {
@@ -14,17 +19,45 @@ const convertToDiscussion = (obj) => {
   discussionAnswered.className = "discussion__answered";
 
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
+  //avatarWrapper - img 추가
+  const avatearImg = document.createElement("img");
+  avatearImg.className = "discussion__avatar--image";
+  avatearImg.src = obj.avatarUrl;
+  avatearImg.alt = `avatar of ${obj.author}`;
+  avatarWrapper.appendChild(avatearImg);
 
+  //discussionContent - h2 > a (title) 추가
+  const discussionTitle = document.createElement("h2");
+  discussionTitle.className = "discussion__title";
+  const titleA = document.createElement("a");
+  titleA.href = obj.url;
+  titleA.textContent = obj.title;
+  discussionTitle.appendChild(titleA);
 
+  discussionContent.appendChild(discussionTitle);
 
+  //discussionContent - div(discuccion information) 추가
+  const discuccionInformation = document.createElement("div");
+  discuccionInformation.className = "discussion__information";
+  discuccionInformation.textContent = `${obj.author} / ${obj.createdAt}`;
+  discussionContent.appendChild(discuccionInformation);
+
+  //discussionAnswered - p 추가
+  const discussionAnsweredP = document.createElement("p");
+  discussionAnsweredP.textContent = "☑";
+  if (obj.answer !== null) {
+    discussionAnswered.appendChild(discussionAnsweredP);
+  }
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
 };
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
+  //렌더할 때마다 로컬에 아이템도 같이 배열에 추가하기
   for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
     element.append(convertToDiscussion(agoraStatesDiscussions[i]));
+    // console.log(agoraStatesDiscussions[i]);
   }
   return;
 };
@@ -32,3 +65,48 @@ const render = (element) => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+//discusion추가 기능
+const formQuestionSubmitDiv = document.querySelector(".form");
+
+function handleSubmit(event) {
+  //submit때문에 페이지가 다시 렌더링 되는 것은 방지
+  // event.preventDefault();
+
+  const name = document.querySelector("#name");
+  const title = document.querySelector("#title");
+  const question = document.querySelector("#story");
+  console.log(
+    `질문자: ${name.value} / 제목: ${title.value} / 질문 내용: ${question.value}`
+  );
+
+  //현재시간
+  let today = new Date().toISOString();
+
+  const newQuestion = {
+    author: `${name.value}`,
+    avatarUrl: "./person.png",
+    title: `${title.value}`,
+    bodyHTML: `${question.value}`,
+    createdAt: `${today}`,
+    answer: null,
+  };
+
+  const objString = JSON.stringify(newQuestion);
+
+  window.localStorage.setItem(`${window.localStorage.length}`, objString);
+
+  console.log(newQuestion.createdAt);
+  // 배열에 등록
+  // agoraStatesDiscussions.unshift(newQuestion);
+
+  //ul을 다시 렌더...함..
+  ul.insertBefore(newQuestion);
+
+  //리셋!
+  name.value = "";
+  title.value = "";
+  question.value = "";
+}
+
+formQuestionSubmitDiv.addEventListener("submit", handleSubmit);
