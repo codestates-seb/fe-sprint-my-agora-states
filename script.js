1a855b161cc96642f06c988d0ec5b77f13d47869
Geneses96/My-first-github-repository.git <geneses96@naver.com>
2023-05-06 18:14:19 +0900

    나만의 아고라 스테이츠 최종

--- script.js
+++ script.js
@@ -2,24 +2,62 @@
 console.log(agoraStatesDiscussions);
 
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
-const convertToDiscussion = (obj) => {
+const convertToDiscussion = (agoraStatesDiscussions) => {
   const li = document.createElement("li"); // li 요소 생성
   li.className = "discussion__container"; // 클래스 이름 지정
 
   const avatarWrapper = document.createElement("div");
   avatarWrapper.className = "discussion__avatar--wrapper";
-  const discussionContent = document.createElement("div");
+
+  const discussionContent = document.createElement("h3");
   discussionContent.className = "discussion__content";
+
   const discussionAnswered = document.createElement("div");
   discussionAnswered.className = "discussion__answered";
 
+  const discussionTitle = document.createElement('a');
+  discussionTitle.className = "discussion__title";
+  discussionTitle.textContent = agoraStatesDiscussions.title;
+  discussionTitle.href = agoraStatesDiscussions.url
+
+  discussionContent.append(discussionTitle);
+
+  const discussionInformation = document.createElement('div');
+  discussionInformation.className = "discussion__information"
+  discussionInformation.textContent = agoraStatesDiscussions.createdAt;
+  discussionContent.appendChild(discussionInformation);
+
+  const discussionCheckbox = document.createElement('p');
+  discussionCheckbox.className = "discussion__answered"
+  discussionCheckbox.textContent = '☑'
+  discussionAnswered.append(discussionCheckbox);
+
+  
+
+  
+
+
+  
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
+  
+  const avatarImg = document.createElement('img');
+  avatarImg.src = agoraStatesDiscussions.avatarUrl;
+  avatarImg.alt = 'avatar of ' + agoraStatesDiscussions.author;
+  avatarWrapper.append(avatarImg);
 
+  
 
+  
+  
+ 
+  
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
+
+  
 };
+
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
@@ -29,6 +67,8 @@ const render = (element) => {
   return;
 };
 
+
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
