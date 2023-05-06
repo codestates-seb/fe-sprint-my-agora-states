59caf8169846fa2eafee78b43cc2b5d6cadac8cb
Geneses96/My-first-github-repository.git <geneses96@naver.com>
2023-05-06 19:49:58 +0900

    페이지네이션추가된 최종본

--- script.js
+++ script.js
@@ -58,7 +58,6 @@ const convertToDiscussion = (agoraStatesDiscussions) => {
   
 };
 
-
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
   for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
@@ -72,3 +71,82 @@ const render = (element) => {
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
 
+const discussionsPerPage = 10; // 한 페이지에 보여줄 데이터 개수
+const discussionsContainer = document.querySelector('.discussions__container');
+const pagination = document.querySelector('.page-btns');
+
+// 페이지 개수 계산
+const totalPages = Math.ceil(agoraStatesDiscussions.length / discussionsPerPage);
+
+// 페이지 버튼 생성
+for (let i = 1; i <= totalPages; i++) {
+  const btn = document.createElement('button');
+  btn.textContent = i;
+  pagination.appendChild(btn);
+}
+
+// 페이지 버튼 클릭 시 해당 페이지 데이터 보여주기
+pagination.addEventListener('click', (event) => {
+  if (event.target.tagName === 'BUTTON') {
+    const pageNum = parseInt(event.target.textContent);
+    const start = (pageNum - 1) * discussionsPerPage;
+    const end = start + discussionsPerPage;
+    const discussions = agoraStatesDiscussions.slice(start, end);
+    renderDiscussions(discussions);
+  }
+});
+
+// 초기 데이터 렌더링
+renderDiscussions(agoraStatesDiscussions.slice(0, discussionsPerPage));
+
+// 데이터 렌더링 함수
+function renderDiscussions(discussions) {
+  let html = '';
+  discussions.forEach((discussion) => {
+    const { avatarUrl, title, url, author, createdAt, isAnswered } = discussion;
+    html += `
+      <li class="discussion__container">
+        <div class="discussion__avatar--wrapper">
+          <img class="discussion__avatar--image"
+            src="${avatarUrl}"
+            alt="avatar of ${author}">
+        </div>
+        <div class="discussion__content">
+          <h3 class="discussion__title"><a href="${url}">${title}</a></h3>
+          <div class="discussion__information">${author} / ${createdAt}</div>
+        </div>
+        ${isAnswered ? '<div class="discussion__answered"><p>☑</p></div>' : ''}
+      </li>
+    `;
+  });
+  discussionsContainer.innerHTML = DOMPurify.sanitize(html);
+}
+function handleSubmit(event) {
+  event.preventDefault(); // 폼의 기본 동작(새로고침)을 막는다
+  const nameInput = document.querySelector("#name");
+  const titleInput = document.querySelector("#title");
+  const storyInput = document.querySelector("#story");
+
+  const newDiscussion = {
+    author: nameInput.value,
+    title: titleInput.value,
+    content: storyInput.value,
+    date: new Date(),
+    comments: [],
+  };
+
+  agoraStatesDiscussions.unshift(newDiscussion);
+  
+  renderDiscussions();
+  nameInput.value = "";
+  titleInput.value = "";
+  storyInput.value = "";
+}
+function init() {
+  // ... 이전 코드들
+
+  const form = document.querySelector(".form");
+  form.addEventListener("submit", handleSubmit);
+}
+
+init();
