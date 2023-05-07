5c8b43c5f38c8d48b7c518b98ef7a5ccd351a9ed
poco124 <parksh9612@gmail.com>
2023-05-07 20:23:05 +0900

    중간단계

--- script.js
+++ script.js
@@ -14,8 +14,29 @@ const convertToDiscussion = (obj) => {
   discussionAnswered.className = "discussion__answered";
 
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
+  const avatarImg = document.createElement('img');
+  // 이미지 요소 넣어주기
+  avatarImg.className = "discussion__avatar--image";
+  avatarImg.src = obj.avatarUrl;
+  avatarImg.alt = 'avatar of ' + obj.author;
+  // agoraStatesDiscussions[0].author
+  avatarWrapper.append(avatarImg);//만들고 넣기
 
 
+  const contentTitle = document.createElement('h2');
+  contentTitle.className = "discussion__title";
+  const contentA = document.createElement('a');
+  contentA.href = obj.urcl;
+  contentA.innerHTML = obj.title;
+  contentTitle.append(contentA);
+  discussionContent.append(contentTitle)
+  
+  const contentInfo = document.createElement('div');
+  const date = new Date(obj.createdAt);
+  const writeDate = `${date.getFullYear()}년 ${date.getMonth()}월 ${date.getDay()}일 ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+  contentInfo.className = "discussion__information";
+  contentInfo.textContent = obj.author + " / " + writeDate;
+  discussionContent.append(contentInfo);
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
@@ -32,3 +53,8 @@ const render = (element) => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+const form = document.querySelector('form.form');
+const title = document.querySelector('input#title')
+const writer = document.querySelector('input#name')
+const story = document.querySelector('textarea#story')
