883f94140acaedc0f2d9041dfbfc299c54ea028d
Jihun Song <songjihun940409@gmail.com>
2023-05-08 13:25:39 +0900

    add data with input, validation

--- script.js
+++ script.js
@@ -23,11 +23,8 @@ const convertToDiscussion = (obj) => {
   // data.link
   const aLink = document.createElement("a");
   aLink.setAttribute("href", obj.url);
-  aLink.setAttribute("style", "text-decoration: none");
-
-  aLink.setAttribute("line-height", 0.8);
-
   aLink.textContent = obj.title;
+
   const title = document.createElement("h2");
   title.append(aLink);
   discussionContent.append(title);
@@ -35,12 +32,12 @@ const convertToDiscussion = (obj) => {
   // data.information
   const discussionInformation = document.createElement("div");
   discussionInformation.className = "discussion__information";
-  discussionInformation.textContent = `${obj.author} / ${obj.createdAt}`;
+  discussionInformation.textContent = `${obj.author} / ${new Date(obj.createdAt).toLocaleString()}`;
   discussionContent.append(discussionInformation);
 
   // checkBox
   const checkBox = document.createElement("p");
-  checkBox.textContent = "☑";
+  checkBox.textContent = obj.answer ? "☑" : "☒";
   discussionAnswered.append(checkBox);
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
@@ -60,62 +57,47 @@ const ul = document.querySelector("ul.discussions__container");
 render(ul);
 
 // input에서 데이터 가져오기
-const submitBtn = document.querySelector(".form__submit");
+const form = document.querySelector("form.form");
 const elInputName = document.querySelector(".form__input--name");
 const elInputTitle = document.querySelector(".form__input--title");
 const elInputQuestion = document.querySelector(".form__textbox");
 
-submitBtn.onclick = function (event) {
+// 실제 배열 추가
+form.addEventListener("submit", (event) => {
   event.preventDefault();
   const date = new Date();
 
   // input value
-  let name = elInputName.children[1].value;
+  let author = elInputName.children[1].value;
   let title = elInputTitle.children[1].value;
   let text = elInputQuestion.children[1].value;
 
-  // img
-  const imgElement = document.createElement("img");
-  imgElement.className = "discussion__avatar--image";
-  imgElement.setAttribute("src", "https://avatars.githubusercontent.com/u/12145019?s=64&u=5c97f25ee02d87898457e23c0e61b884241838e3&v=4");
-  imgElement.setAttribute("alt", "avatar of kimploo");
+  if (validName) {
+    alert("Your name must be longer more than 2 characters");
+    return;
+  }
+  if (validTitle) {
+    alert("Your title must be longer more than 5 characters");
+    return;
+  }
+  if (validText) {
+    alert("Your Question must be longer more than 10 characters");
+    return;
+  }
 
-  const imgDiv = document.createElement("div");
-  imgDiv.append(imgElement);
-  imgDiv.className = "discussion__avatar--wrapper";
-
-  // content
-
-  const a = document.createElement("a");
-  a.textContent = title;
-
-  const h2 = document.createElement("h2");
-  h2.append(a);
-  h2.className = "discussion__title";
-
-  const infoDiv = document.createElement("div");
-  infoDiv.textContent = `${name} / ${date.toLocaleTimeString()}`;
-  infoDiv.className = "discussion__information";
-
-  const contentDiv = document.createElement("div");
-  contentDiv.className = "discussion__content";
-  contentDiv.append(h2, infoDiv);
-
-  // checkBox
-  const p = document.createElement("p");
-  p.innerHTML = "☑";
-  const checkBoxDiv = document.createElement("div");
-  checkBoxDiv.className = "discussion__answered";
-
-  //li
-  const li = document.createElement("li");
-  li.className = "discussion__container";
-  li.append(imgDiv, contentDiv, checkBoxDiv);
-  const ul = document.querySelector(".discussions__container");
-  ul.prepend(li);
-
-  // reset input value
-  elInputName.children[1].value = "";
-  elInputTitle.children[1].value = "";
-  elInputQuestion.children[1].value = "";
-};
+  let newQuestion = {
+    answer: null,
+    author,
+    avatarUrl: "https://avatars.githubusercontent.com/u/12145019?s=64&u=5c97f25ee02d87898457e23c0e61b884241838e3&v=4",
+    bodyHTML: text,
+    createdAt: date.toISOString(),
+    id: date.getTime(),
+    title,
+    url: "https://github.com/codestates-seb/agora-states-fe/discussions/45",
+  };
+  agoraStatesDiscussions.unshift(newQuestion);
+  while (ul.firstChild) {
+    ul.removeChild(ul.firstChild);
+  }
+  render(ul);
+});
