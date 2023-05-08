4fd028fa464018cbec989e4640fba542e8c2012a
HyerimKimm <50258232+HyerimKimm@users.noreply.github.com>
2023-05-08 16:35:12 +0900

    showAnswer, createNewDiscussion 메소드를 global method로 추출

--- script.js
+++ script.js
@@ -1,53 +1,9 @@
 // index.html을 열어서 agoraStatesDiscussions 배열 요소를 확인하세요.
 const ul = document.querySelector("ul.discussions__container");
 
-//input하면 질문을 추가해주는 함수입니다. 
+// 질문 등록 버튼을 눌렀을 때의 동작
 const btn_submit = document.querySelector('.form__submit--button');
-btn_submit.addEventListener('click',function(){
-  let inputName = document.querySelector('#name');
-  let inputTitle = document.querySelector('#title');
-  let inputStory = document.querySelector('#story');
-
-  if(inputName.value.length===0 || inputTitle.value.length===0 || inputStory.value.length===0) {
-    return;
-  }
-  // 입력 날짜 형식 맞추기
-  let today = new Date();
-  console.log(today)
-  let yyyy = today.getFullYear().toString();
-  let mm = (today.getMonth()+1).toString();
-  mm.length===1?mm='0'+mm:mm=mm;
-  let dd = (today.getDate()).toString();
-  dd.length===1?dd='0'+dd:dd=dd;
-  let HH = (today.getHours()).toString();
-  HH.length===1?HH='0'+HH:HH=HH;
-  let min = today.getMinutes().toString();
-  min.length===1?min='0'+min:min=min;
-  let sec = today.getSeconds().toString();
-  sec.length===1?sec='0'+sec:sec=sec;
-  todayFormat=`${yyyy}-${mm}-${dd}T${HH}:${min}:${sec}`;
-
-  //agoraStatesDiscussions 배열에 요소 추가
-  const agoraStatesDiscussion = {
-    id: '012345',
-    createdAt: todayFormat,
-    title: inputTitle.value,
-    url:null,
-    author: inputName.value,
-    answer: null,
-    bodyHTML: '<p></p>',
-    avatarUrl: 'assets/images/profile.png',
-  }
-  agoraStatesDiscussions.unshift(agoraStatesDiscussion);
-
-  //li에 새로 등록한 데이터 1개 추가하기
-  render_new(ul);
-
-  //input value 초기화
-  inputName.value='';
-  inputTitle.value='';
-  inputStory.value='';
-});
+btn_submit.addEventListener('click',createNewDiscussion());
 
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
 const convertToDiscussion = (obj) => {
@@ -61,7 +17,6 @@ const convertToDiscussion = (obj) => {
   const discussionAnswered = document.createElement("div");
   discussionAnswered.className = "discussion__answered";
 
-  // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
   //image 데이터
   const avatar = document.createElement('img');
   avatar.className='discussion__avatar--image';
@@ -91,16 +46,7 @@ const convertToDiscussion = (obj) => {
   const answer = document.querySelector('.answer');
 
   //답변완료 여부 선택 시 answer 정보 출력
-  answered.addEventListener('click',function(){
-    console.log(obj.answer['bodyHTML']);
-    const answer__author = document.querySelector('.answer__content--author');
-    answer__author.textContent = `작성자 : ${obj.answer['author']}`;
-    const answer__bodyHtml = document.querySelector('.answer__content--bodyHtml');
-    answer__bodyHtml.innerHTML = obj.answer['bodyHTML'];
-    const answer__questionTitle = document.querySelector('.answer__questionTitle');
-    answer__questionTitle.textContent = `Q. ${obj.title}`;
-    answer.classList.remove('hide');
-  })
+  answered.addEventListener('click',showAnswer(obj, answer))
 
   //answer의 ❌ 버튼 누르면 answer hide
   const answer__closeBtn = document.querySelector('.answer__title--closeBtn');
@@ -150,4 +96,66 @@ const render_new = function(element) {
 
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 render(ul);
+
+function showAnswer(obj, answer) {
+  return function () {
+    console.log(obj.answer['bodyHTML']);
+    const answer__author = document.querySelector('.answer__content--author');
+    answer__author.textContent = `작성자 : ${obj.answer['author']}`;
+    const answer__bodyHtml = document.querySelector('.answer__content--bodyHtml');
+    answer__bodyHtml.innerHTML = obj.answer['bodyHTML'];
+    const answer__questionTitle = document.querySelector('.answer__questionTitle');
+    answer__questionTitle.textContent = `Q. ${obj.title}`;
+    answer.classList.remove('hide');
+  };
+}
+
+//새로운 discussion을 등록하는 메소드
+function createNewDiscussion() {
+  return function () {
+    let inputName = document.querySelector('#name');
+    let inputTitle = document.querySelector('#title');
+    let inputStory = document.querySelector('#story');
+
+    if (inputName.value.length === 0 || inputTitle.value.length === 0 || inputStory.value.length === 0) {
+      return;
+    }
+    // 입력 날짜 형식 맞추기
+    let today = new Date();
+    console.log(today);
+    let yyyy = today.getFullYear().toString();
+    let mm = (today.getMonth() + 1).toString();
+    mm.length === 1 ? mm = '0' + mm : mm = mm;
+    let dd = (today.getDate()).toString();
+    dd.length === 1 ? dd = '0' + dd : dd = dd;
+    let HH = (today.getHours()).toString();
+    HH.length === 1 ? HH = '0' + HH : HH = HH;
+    let min = today.getMinutes().toString();
+    min.length === 1 ? min = '0' + min : min = min;
+    let sec = today.getSeconds().toString();
+    sec.length === 1 ? sec = '0' + sec : sec = sec;
+    todayFormat = `${yyyy}-${mm}-${dd}T${HH}:${min}:${sec}`;
+
+    //agoraStatesDiscussions 배열에 요소 추가
+    const agoraStatesDiscussion = {
+      id: '012345',
+      createdAt: todayFormat,
+      title: inputTitle.value,
+      url: null,
+      author: inputName.value,
+      answer: null,
+      bodyHTML: '<p></p>',
+      avatarUrl: 'assets/images/profile.png',
+    };
+    agoraStatesDiscussions.unshift(agoraStatesDiscussion);
+
+    //li에 새로 등록한 데이터 1개 추가하기
+    render_new(ul);
+
+    //input value 초기화
+    inputName.value = '';
+    inputTitle.value = '';
+    inputStory.value = '';
+  };
+}
 
