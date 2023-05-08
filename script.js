2113090396b131077d83e742fa8c933a15722837
wonjin <101559371+Ywonjin@users.noreply.github.com>
2023-05-08 18:32:09 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -15,7 +15,28 @@ const convertToDiscussion = (obj) => {
 
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
 
+  const avatarImg = document.createElement('img');
+  avatarImg.src = agoraStatesDiscussions[0].avatarUrl;
+  avatarImg.alt = 'avatar of ' + agoraStatesDiscussions[0].author;
+  avatarWrapper.append(avatarImg);
 
+  const discussionTitle = document.createElement("h2");
+  const titleAnchor = document.createElement("a");
+  titleAnchor.href = obj.url;
+  titleAnchor.textContent = obj.title;
+  discussionTitle.append(titleAnchor);
+  discussionContent.append(discussionTitle);
+
+
+  discussionAnswered.className = "discussion__answered";
+  const discussionInformation = document.createElement("div");
+  discussionInformation.className = "discussion__information";
+  discussionInformation.textContent = `${obj.author} / ${new Date(obj.createdAt || Date.now()).toISOString()}`;
+  discussionContent.append(discussionTitle, discussionInformation);
+
+  const checked = document.createElement("p");
+  checked.textContent = obj.answer ? "☑︎" : "☒";
+  discussionAnswered.append(checked);
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
@@ -32,3 +53,39 @@ const render = (element) => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+const form = document.querySelector("form.form");
+const author = form.querySelector("div.form__input--name > input");
+const title = form.querySelector("div.form__input--title > input");
+const textbox = form.querySelector("div.form__itextbox > textarea");
+
+form.addEventListener("submit", 
+(event) => {
+  event.preventDefault(); //서브밋 이벤트로 사용시 꼭 함께 사용해주어야함
+  // addEventListener 안으로 선언 위치 이동
+  const author = form.querySelector("div.form__input--name > input").value;
+  const title = form.querySelector("div.form__input--title > input").value;
+  const textbox = form.querySelector("div.form__textbox > textarea").value;
+
+  const Obj = {
+    id: "new id",
+    createdAt: new Date().toISOString(),
+    title: title,
+    url: "https://github.com/codestates-seb/agora-states-fe/discussions",
+    author: author,
+    bodyHTML: textbox,
+    avatarUrl: "https://avatars.githubusercontent.com/u/97888923?s=64&u=12b18768cdeebcf358b70051283a3ef57be6a20f&v=4"
+  }
+// addEventListener 안으로 선언 위치 이동
+  agoraStatesDiscussions.unshift(Obj);
+
+  const discussion = convertToDiscussion(Obj);
+  
+  ul.prepend(discussion);
+
+  // submit 후 입력란 빈칸으로 리셋하는 기능 추가
+  form.querySelector("div.form__input--name > input").value = "";
+  form.querySelector("div.form__input--title > input").value = "";
+  form.querySelector("div.form__textbox > textarea").value = "";
+}
+)
