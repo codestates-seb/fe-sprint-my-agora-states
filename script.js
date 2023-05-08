55566ecb9cccab1f70c3054bdef340a7192b2bdf
kimmand0o0 <yuwang0130@gmail.com>
2023-05-09 08:59:36 +0900

    :zap: FEAT : 페이지네이션 추가

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 // index.html을 열어서 agoraStatesDiscussions 배열 요소를 확인하세요.
 // console.log(agoraStatesDiscussions);
 
+let localData = JSON.parse(localStorage.getItem(agoraStatesDiscussions));
+
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
 const convertToDiscussion = (obj) => {
-  // console.log("obj = ", obj);
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
   const li = document.createElement("li"); // li 요소 생성
   li.className = "discussion__container"; // 클래스 이름 지정
@@ -39,25 +40,22 @@ const convertToDiscussion = (obj) => {
   discussionAnswered.className = "discussion__answered";
 
   const discussionAnsweredBox = document.createElement("p");
-  if (obj.answer !== null) discussionAnsweredBox.textContent = "☑";
+  if (obj.answer !== null) discussionAnsweredBox.textContent = "☑️";
+  else discussionAnsweredBox.textContent = "❌";
   discussionAnswered.append(discussionAnsweredBox);
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
 };
 
-// agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
-const render = (element) => {
-  for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
-    element.append(convertToDiscussion(agoraStatesDiscussions[i]));
+const render = (element, currentPage, limit) => {
+  let start = currentPage * 5 - 5;
+  let renderData = dataSetup(localData);
+  for (let i = start; i < start + limit; i += 1) {
+    element.append(convertToDiscussion(renderData[i]));
   }
-
   return;
 };
-
-// ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
-const ul = document.querySelector("ul.discussions__container");
-render(ul);
 
 const openFormButton = document.querySelector(".open__form");
 const formContainer = document.querySelector(".form__container");
@@ -99,20 +97,20 @@ formSubmit.addEventListener("submit", function (event) {
     createdAt: new Date().toISOString(),
     title: inputTitle.value,
     author: inputName.value,
+    url: "https://github.com/codestates-seb/agora-states-fe/discussions/6",
     answer: null,
     bodyHTML: inputTextBox.value,
-    avatarUrl: "https://cataas.com/cat",
+    avatarUrl:
+      "https://cdn.imweb.me/upload/S201712205a3a0910b89f5/9e19def61ad24.jpg",
   };
 
-  showCat(obj);
-
-  agoraStatesDiscussions.unshift(obj);
+  localData.unshift(obj);
 
   while (ul.firstChild) {
     ul.removeChild(ul.firstChild);
   }
 
-  render(ul);
+  render(ul, currentPage, limit);
 
   window.scrollTo(0, 0);
 });
@@ -126,19 +124,69 @@ function uuidv4() {
   );
 }
 
-function showCat(obj) {
-  $.ajax({
-    type: "GET",
-    url: "https://api.thecatapi.com/v1/images/search",
-    data: {},
-    success: function (response) {
-      imgUrl = response[0]["url"];
-      obj.url = imgUrl;
-    },
-  });
-  return obj;
+function dataSetup(data) {
+  if (data.length === 0) data = agoraStatesDiscussions;
+  localStorage.setItem(agoraStatesDiscussions, JSON.stringify(data));
+  return JSON.parse(localStorage.getItem(agoraStatesDiscussions));
 }
 
-function renderPagination(currentPage) {
-  if (_totalCount <= 5) return;
+let currentPage = 1;
+let limit = 5;
+let pageCount = 5;
+let totalCount = localData.length;
+let totalPage = Math.ceil(totalCount / limit);
+let pageGroup = Math.ceil(currentPage / pageCount);
+
+function pagination(currentPage, limit) {
+  currentPage = Number(currentPage);
+  render(ul, currentPage, limit);
 }
+
+function paginationButton(element, currentPage, totalPage, pageGroup) {
+  console.log(totalPage, pageGroup);
+
+  let start = pageGroup * 5 - 4;
+  let last = pageGroup * 5;
+
+  if (totalPage < last) last = totalPage;
+
+  const left = document.createElement("button");
+  left.className = "left__button";
+  left.textContent = "<";
+
+  element.append(left);
+
+  for (let i = start; i <= last; i++) {
+    const button = document.createElement("button");
+    button.className = "page__button";
+    button.textContent = i;
+    element.append(button);
+  }
+
+  const right = document.createElement("button");
+  right.className = "right__button";
+  right.textContent = ">";
+  element.append(right);
+}
+
+// ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
+const ul = document.querySelector("ul.discussions__container");
+const buttonUl = document.querySelector("ul.buttons__container");
+pagination(currentPage, limit);
+paginationButton(buttonUl, currentPage, totalPage, pageGroup);
+
+const pageButton = document.querySelector(".page__buttons");
+pageButton.addEventListener("click", function (event) {
+  const target = event.target;
+  const buttonContent = target.textContent;
+
+  currentPage = buttonContent;
+
+  if (buttonContent.indexOf("<")) {
+    while (ul.firstChild) {
+      ul.removeChild(ul.firstChild);
+    }
+
+    pagination(buttonContent, limit);
+  }
+});
