1c1195159689b8b169d728d4d8c39a92b821d7e9
developerpyk <developerpyk@gmail.com>
2023-05-08 13:02:10 +0900

    디스커션 기능, 현지 시간 적용, 기본 css 적용

--- script.js
+++ script.js
@@ -7,20 +7,52 @@ const convertToDiscussion = (obj) => {
   li.className = "discussion__container"; // 클래스 이름 지정
 
   const avatarWrapper = document.createElement("div");
-  avatarWrapper.className = "discussion__avatar--wrapper";
+    avatarWrapper.className = "discussion__avatar--wrapper";
+
+    const avatarImg = document.createElement("img");
+      avatarImg.className = "discussion__avatar--image";
+      avatarImg.src = obj.avatarUrl;
+      avatarImg.alt = "avatar of " + obj.author;
+      avatarWrapper.append(avatarImg)
+
   const discussionContent = document.createElement("div");
-  discussionContent.className = "discussion__content";
+    discussionContent.className = "discussion__content";
+
+    const title = document.createElement("h2");
+      title.className = "discussion__title";
+      discussionContent.append(title);
+
+        const anchor = document.createElement("a");
+          anchor.href = obj.url;
+          anchor.innerText = obj.title;
+          title.append(anchor);
+
+    const info = document.createElement("div");
+      info.className ="discussion__information";
+      info.innerText = obj.author + " / " + today(obj.createdAt);
+      discussionContent.append(info);
+
   const discussionAnswered = document.createElement("div");
-  discussionAnswered.className = "discussion__answered";
+    discussionAnswered.className = "discussion__answered";
+
+    const answer = document.createElement("div");
+      answer.className = "discussion__answered";
+      discussionAnswered.append(answer);
+
+        const checkBox = document.createElement("p");
+          checkBox.innerText = "☑"
+          answer.append(checkBox);
 
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
 
 
-
+  
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
 };
 
+
+var currentDate = new Date('1675397344299');
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
   for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
@@ -32,3 +64,52 @@ const render = (element) => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+// 날짜 적용 함수
+function today(){
+  let now=new Date()
+  let hour=now.getHours()
+  let min=now.getMinutes()
+  let sec=now.getSeconds()
+
+  if(hour < 12 && hour >= 00){
+     return `오전 ${hour}:${min}:${sec}`
+  }else if(hour >=12 && hour <=24){
+     return `오후 ${hour}:${min}:${sec}`
+  }
+}
+
+// input을 불러와야 한다.
+const form = document.querySelector("form.form");
+const author = form.querySelector("div.form__input--name > input");
+const title = form.querySelector("div.form__input--title > input");
+const textbox = form.querySelector("div.form__textbox > textarea");
+
+form.addEventListener("submit", (event) => {
+  // 폼 제출을 하면 완전히 새로운 html을 받아오는 새로고침 현상을 해결하려면,
+  event.preventDefault();
+    const obj = {
+      id: "unique id",
+      createdAt: new Date().toISOString(),
+      title: title.value,
+      url:"https://github.com/codestates-seb/agora-states-fe/discussions",
+      author: author.value,
+      answer: null,
+      bodyHTML: textbox.value,
+      avatarUrl:
+      "https://media.istockphoto.com/id/1137748653/ko/%EB%B2%A1%ED%84%B0/%EC%83%88%EC%8B%B9-%EC%83%9D%ED%83%9C-%EA%B8%B0%ED%98%B8-%EC%95%84%EC%9D%B4%EC%BD%98.jpg?s=612x612&w=is&k=20&c=h4nG_gXgt8xjhWOtTrS_RaR0XOT13UjoZ_SsmeVCDE8="
+    };
+    
+    //agoraStatesDiscussions 객체 추가
+    agoraStatesDiscussions.unshift(obj);
+
+    // 42개의 데이터가 계속 추가되지 않도록 사용자 입장에서 새로고침되어 보이게 화면 지우기
+    while (ul.firstChild) {
+      ul.removeChild(ul.firstChild);
+    }
+
+    //다시 agoraStatesDiscussions 기반으로 화면 보여주기 (렌더링)
+    render(ul);
+
+  })
+  
