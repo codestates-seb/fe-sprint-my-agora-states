5168c46b21b815890991e24c6c19647a4c9ceeca
hanbinchoi <wkghskak@naver.com>
2023-05-08 16:39:20 +0900

    search 버그 수정

--- src/js/search.js
+++ src/js/search.js
@@ -6,22 +6,27 @@ const authors = document.querySelectorAll(".discussion__author");
 // 제목, 내용, 작성자로 검색
 function handleSearch(event) {
   const searchKey = event.target.value;
+  if (searchKey === "") render(ul);
+  else {
+    while (ul.firstChild) {
+      ul.firstChild.remove();
+    }
+    const discussionDatas = JSON.parse(localStorage.getItem("data"));
+    console.log(discussionDatas);
+    for (let i = 0; i < discussionDatas.length; i++) {
+      const titleCheck = discussionDatas[i].title
+        .toUpperCase()
+        .replace(/ /g, "")
+        .includes(searchKey.toUpperCase().replace(/ /g, ""));
 
-  for (let i = 0; i < ul.childNodes.length; i++) {
-    const titleCheck = titles[i].textContent
-      .toUpperCase()
-      .replace(/ /g, "")
-      .includes(searchKey.toUpperCase().replace(/ /g, ""));
+      const authorCheck = discussionDatas[i].author
+        .toUpperCase()
+        .replace(/ /g, "")
+        .includes(searchKey.toUpperCase().replace(/ /g, ""));
 
-    const authorCheck = authors[i].textContent
-      .toUpperCase()
-      .replace(/ /g, "")
-      .includes(searchKey.toUpperCase().replace(/ /g, ""));
-
-    if (titleCheck || authorCheck) {
-      titles[i].closest("li").classList.remove("hidden");
-    } else {
-      titles[i].closest("li").classList.add("hidden");
+      if (titleCheck || authorCheck) {
+        ul.append(convertToDiscussion(discussionDatas[i]));
+      }
     }
   }
 }
