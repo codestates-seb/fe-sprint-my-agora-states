9949f98f461bab81e7a24aa0943c2a12861a8789
WONHO22 <dnjsgh2122@gmail.com>
2023-05-08 16:21:32 +0900

    desk 이미지 및 일부 디테일 수정, 답변 modal 기능 구현중

--- script.js
+++ script.js
@@ -2,11 +2,6 @@
 //console.log(agoraStatesDiscussions);
 
 
-
-
-
-
-
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
 const convertToDiscussion = (obj) => {
   const li = document.createElement("li"); // li 요소 생성
@@ -19,8 +14,6 @@ const convertToDiscussion = (obj) => {
    // div태그생성 => 클래스명 discussion__content 설정
   const discussionContent = document.createElement("div");
   discussionContent.className = "discussion__content";
-
-
 
 
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.3
@@ -50,22 +43,41 @@ const convertToDiscussion = (obj) => {
   content__information.innerHTML = `닉네임: ${obj.author} <br>작성일: ${new Date(obj.createdAt).toLocaleString("ko-KR")} `;
   discussionContent.append(content__information);
 
+
   //discussion__container li 자식요소인 discussion__answered div 태그 생성 => p태그 넣기 (답변여부 확인)
   // div태그생성 => 클래스명 discussion__answered 설정
+  // 답변 여부 및 답변창뜨는 모달 구현 부분
+
   const discussionAnswered = document.createElement("div");
   discussionAnswered.className = "discussion__answered";
-
   const answerCheck = document.createElement('p');
+  answerCheck.className="popupBtn";
+
+  const answerContainer = document.createElement('div');
+  answerContainer.className ="answer__container";
+
+  
+  const answerContent = document.createElement('div');
+  answerContent.className = "answer__content";
+
   if(obj.answer === null){
     answerCheck.textContent = '☒';
   } else{
     answerCheck.textContent = '☑';
+    answerContent.innerHTML = `<span id="closeBtn">&times;</span>` + obj.answer.bodyHTML;
+    console.log(answerContent);
+    
   }
   discussionAnswered.append(answerCheck);
+  discussionAnswered.append(answerContainer);
+  answerContainer.append(answerContent);
 
+  
   
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
+
+
 };
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
@@ -86,6 +98,7 @@ const title = form.querySelector("#title");
 const textbox = form.querySelector("#story");
 
 
+
 form.addEventListener("submit", (event) => {
   event.preventDefault(); // submit 기본값인 새로고침 방지
 
@@ -93,19 +106,16 @@ form.addEventListener("submit", (event) => {
     id: "new id",
     createdAt: new Date().toISOString(),
     title: title.value,
+    answer: null,
     url: "",
     author: author.value,
     bodyHTML: textbox.value,
     avatarUrl: "/imgs/head_image.png"
   }
 
-
-// agoraStatesDiscussions => local storage에 저장
   agoraStatesDiscussions.unshift(newObj);
   const NewDiscussion = convertToDiscussion(newObj);
   
-
-
   // agoraStatesDiscussions에 새로만들 배열 맨앞에 삽입
   ul.prepend(NewDiscussion);
 
@@ -117,14 +127,27 @@ form.addEventListener("submit", (event) => {
 
 
 
-
-
-
-
-
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
 
 
 
+// 답변클릭시 모달 구현 -실패
+
+const btn = document.querySelector('.popupBtn');
+const modal = document.querySelector('.answer__container');
+const closeBtn = document.querySelector('.closeBtn');
+
+btn.onclick = function() {
+  modal.style.display = 'block';
+}
+// closeBtn.onclick = function() {
+//   modal.style.display = 'none';
+// }
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
