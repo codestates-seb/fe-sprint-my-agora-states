ab1b7aabd3e8675f89838d2938bd949d292bc864
Moses-Yu <mosesyumss@gmail.com>
2023-05-08 09:50:31 +0900

    색 변경

--- script.js
+++ script.js
@@ -115,3 +115,7 @@ const render = (element) => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+
+const send = document.querySelector(send)
+send.action = () => console.log(document.querySelector('form'))
