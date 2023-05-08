b04661ccc1a030728d658374805f7693ad2fb1c7
심도연 <shimdokite@gmail.com>
2023-05-08 14:01:00 +0900

    [Feat] 다크모드, top 버튼 구현
    Changes to be committed:
    	modified:   data.js
    	modified:   index.html
    	modified:   script.js
    	modified:   style.css
    	deleted:    styleA.css

--- script.js
+++ script.js
@@ -48,7 +48,14 @@ const convertToDiscussion = (obj) => {
   const answered = document.createElement("div");
   answered.className = "discussion__answered";
   const answeredCheck = document.createElement("p");
-  answeredCheck.textContent = obj.answer ? "☑" : "❏";
+  if (obj.answer !== null && "answer" in obj) {
+    // 답변이 있는 경우, 답변 화면 렌더링(Advanced)
+    answeredCheck.textContent = "☑";
+    answeredCheck.href = obj.answer.url;
+  } else {
+    answeredCheck.textContent = "❏";
+  }
+  // answeredCheck.textContent = obj.answer ? "☑" : "❏";
 
   answered.append(answeredCheck);
   discussionAnswered.append(answered);
@@ -59,7 +66,11 @@ const convertToDiscussion = (obj) => {
 
   const discussionAnswerButton = document.createElement("button");
   discussionAnswerButton.className = "discussion__answer__button";
-  discussionAnswerButton.textContent = "자세히 보기";
+  discussionAnswerButton.textContent = "show details";
+
+  discussionAnswerButton.addEventListener("click", () => {
+    alert("개발 진행 중...");
+  });
 
   contentArea.append(
     avatarWrapper,
@@ -77,22 +88,26 @@ const convertToDiscussion = (obj) => {
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
+  // 한 페이지당 출력할 데이터 갯수
   const pageCount = Math.ceil(agoraStatesDiscussions.length / 10);
   const container = document.querySelector(".pagination-button");
+  // 현재 페이지
   let currentPage = window.sessionStorage.getItem("currentPage")
     ? window.sessionStorage.getItem("currentPage")
     : 1;
 
+  // 페이지 갯수
   for (let i = 1; i < pageCount + 1; i++) {
     const button = document.createElement("button");
     button.className = `page-button-${i}`;
     button.textContent = i;
     container.append(button);
 
+    // 페이지 버튼 클릭 했을 때 reload, window.location.href가 reload를 한다는 의미!
     button.addEventListener("click", () => {
-      console.log(button.textContent);
+      // console.log(button.textContent);
       window.sessionStorage.setItem("currentPage", i); // reload로 인한 초기화를 방지 위해서 setItem 사용
-      window.location.href = `${window.location.origin}${window.location.pathname}?pageNo=${i}`; // end point를 바꿔서 새로고침을 함
+      window.location.href = `${window.location.origin}${window.location.pathname}?pageNo=${i}`;
       window;
     });
   }
@@ -100,8 +115,9 @@ const render = (element) => {
   const startIndex = (currentPage - 1) * 10;
   const endIndex = startIndex + 10;
 
+  // 몇번째 페이지에 출력될 데이터를 보여주는 for문
   for (let j = startIndex; j < endIndex; j += 1) {
-    console.log(agoraStatesDiscussions[j]);
+    // console.log(agoraStatesDiscussions[j]);
     element.append(convertToDiscussion(agoraStatesDiscussions[j]));
   }
   return;
@@ -121,7 +137,7 @@ const render = (element) => {
 
 // console.log(window.localStorage.length);
 if (window.localStorage.length) {
-  for (let i = 0; i < window.localStorage.length - 1; i++) {
+  for (let i = 0; i < window.localStorage.length - 2; i++) {
     // count 값을 뺴주기 위해서 -1 처리
     const discussionString = window.localStorage.getItem(`discussion${i}`);
     const discussionObj = JSON.parse(discussionString);
@@ -135,8 +151,9 @@ render(ul);
 
 // 아이디, 제목, 본문을 입력하고 누르면 실제 화면에 디스커션이 추가
 // agoraStatesDiscussions 배열에 들어가 있는 요소는 객체형태로 들어가 있다 { title, author, createdAt, avatarUrl, url }
-// agoraStatesDiscussions 배열에 추가한 데이터가 실제 쌓여야 한다(concat()을 사용해서 불변성을 지키고 새로운 요소 넣기)
+// agoraStatesDiscussions 배열에 추가한 데이터가 실제 쌓여야 한다
 // 등록한 시간까지 같이 나올 수 있게(advanced)
+
 const form = document.querySelector(".form");
 const inputName = document.querySelector("#name");
 const inputTitle = document.querySelector("#title");
@@ -161,14 +178,78 @@ form.addEventListener("submit", (e) => {
   agoraStatesDiscussions.unshift(newDiscussion);
   ul.prepend(convertToDiscussion(newDiscussion));
 
+  // 초기화
   inputName.value = "";
   inputTitle.value = "";
   inputText.value = "";
 
-  // submit 하면 객체 newDiscussion을 localStorage에 저장
-  const objString = JSON.stringify(newDiscussion);
-  // window.localStorage.setItem("discussion", objString);
-  window.localStorage.setItem(`discussion${count}`, objString);
-  // 기존 count에 +1 된 값
-  window.localStorage.setItem("count", ++count);
+  // localStorage 만드는 함수
+  function setLocalStorage() {
+    // submit 하면 객체 newDiscussion을 localStorage에 저장
+    const objString = JSON.stringify(newDiscussion);
+    // window.localStorage.setItem("discussion", objString);
+    window.localStorage.setItem(`discussion${count}`, objString);
+    // 기존 count에 +1 된 값
+    window.localStorage.setItem("count", ++count);
+  }
+  setLocalStorage();
+});
+
+/*
+ * 1. data.js -> script.js로 더미 데이터(agoraStatesDiscussions)를 가져옴
+ * 2. script.js 에서 agoraStatesDiscussions를 기준으로 render, 이때 localStorage에 데이터가 존재한다면 agoraStatesDiscussions에 추가 후 render
+ * 3. 새로 추가되는 데이터는 localStorage(setItem)에 추가
+ * 4. 화면에 어떻게 출력을 해줄건가?... 문제해결...
+ */
+
+//scrolltotop
+const scrollButton = document.querySelector(".scroll");
+let intervalId = 0;
+
+function scrollStep(scrollStepHeight) {
+  if (window.pageYOffset === 0) {
+    clearInterval(intervalId);
+  }
+  window.scrollTo(0, window.pageYOffset - scrollStepHeight);
+}
+
+function scrollToTop(scrollStepHeight, delay) {
+  if (scrollStepHeight <= 0) {
+    alert("The specified scroll step height must be positive!");
+  } else if (delay <= 0) {
+    alert("The specified scroll delay must be positive!");
+  }
+  intervalId = setInterval(() => scrollStep(scrollStepHeight), delay);
+}
+
+scrollButton.addEventListener("click", () => scrollToTop(90, 16.6));
+
+// dark mode
+const themeMode = document.querySelector(".mode");
+const isUserColorTheme = localStorage.getItem("color-theme");
+
+// 최초 접속 시 테마 적용 시켜두기
+window.onload = function () {
+  if (isUserColorTheme === "dark") {
+    // localStorage.setItem("color-theme", "dark");
+    document.documentElement.setAttribute("color-theme", "dark");
+    themeMode.setAttribute("checked", true);
+    themeMode.textContent = "dark";
+  } else {
+    // localStorage.setItem("color-theme", "light");
+    document.documentElement.setAttribute("color-theme", "light");
+    themeMode.textContent = "light";
+  }
+};
+// 체크박스 클릭 시 테마 변경
+themeMode.addEventListener("click", (e) => {
+  if (themeMode.textContent === "light") {
+    document.documentElement.setAttribute("color-theme", "dark");
+    localStorage.setItem("color-theme", "dark");
+    themeMode.textContent = "dark";
+  } else {
+    document.documentElement.setAttribute("color-theme", "light");
+    localStorage.setItem("color-theme", "light");
+    themeMode.textContent = "light";
+  }
 });
