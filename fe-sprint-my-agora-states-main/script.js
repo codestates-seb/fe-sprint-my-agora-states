ae08281226b79fa23b6b30904ee763a563d536ce
wonjin <101559371+Ywonjin@users.noreply.github.com>
2023-05-08 18:31:14 +0900

    Add files via upload

---
+++ fe-sprint-my-agora-states-main/script.js
@@ -0,0 +1,91 @@
+// index.html을 열어서 agoraStatesDiscussions 배열 요소를 확인하세요.
+console.log(agoraStatesDiscussions);
+
+// convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
+const convertToDiscussion = (obj) => {
+  const li = document.createElement("li"); // li 요소 생성
+  li.className = "discussion__container"; // 클래스 이름 지정
+
+  const avatarWrapper = document.createElement("div");
+  avatarWrapper.className = "discussion__avatar--wrapper";
+  const discussionContent = document.createElement("div");
+  discussionContent.className = "discussion__content";
+  const discussionAnswered = document.createElement("div");
+  discussionAnswered.className = "discussion__answered";
+
+  // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
+
+  const avatarImg = document.createElement('img');
+  avatarImg.src = agoraStatesDiscussions[0].avatarUrl;
+  avatarImg.alt = 'avatar of ' + agoraStatesDiscussions[0].author;
+  avatarWrapper.append(avatarImg);
+
+  const discussionTitle = document.createElement("h2");
+  const titleAnchor = document.createElement("a");
+  titleAnchor.href = obj.url;
+  titleAnchor.textContent = obj.title;
+  discussionTitle.append(titleAnchor);
+  discussionContent.append(discussionTitle);
+
+
+  discussionAnswered.className = "discussion__answered";
+  const discussionInformation = document.createElement("div");
+  discussionInformation.className = "discussion__information";
+  discussionInformation.textContent = `${obj.author} / ${new Date(obj.createdAt || Date.now()).toISOString()}`;
+  discussionContent.append(discussionTitle, discussionInformation);
+
+  const checked = document.createElement("p");
+  checked.textContent = obj.answer ? "☑︎" : "☒";
+  discussionAnswered.append(checked);
+
+  li.append(avatarWrapper, discussionContent, discussionAnswered);
+  return li;
+};
+
+// agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
+const render = (element) => {
+  for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
+    element.append(convertToDiscussion(agoraStatesDiscussions[i]));
+  }
+  return;
+};
+
+// ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
+const ul = document.querySelector("ul.discussions__container");
+render(ul);
+
+const form = document.querySelector("form.form");
+const author = form.querySelector("div.form__input--name > input");
+const title = form.querySelector("div.form__input--title > input");
+const textbox = form.querySelector("div.form__itextbox > textarea");
+
+form.addEventListener("submit", 
+(event) => {
+  event.preventDefault(); //서브밋 이벤트로 사용시 꼭 함께 사용해주어야함
+  // addEventListener 안으로 선언 위치 이동
+  const author = form.querySelector("div.form__input--name > input").value;
+  const title = form.querySelector("div.form__input--title > input").value;
+  const textbox = form.querySelector("div.form__textbox > textarea").value;
+
+  const Obj = {
+    id: "new id",
+    createdAt: new Date().toISOString(),
+    title: title,
+    url: "https://github.com/codestates-seb/agora-states-fe/discussions",
+    author: author,
+    bodyHTML: textbox,
+    avatarUrl: "https://avatars.githubusercontent.com/u/97888923?s=64&u=12b18768cdeebcf358b70051283a3ef57be6a20f&v=4"
+  }
+// addEventListener 안으로 선언 위치 이동
+  agoraStatesDiscussions.unshift(Obj);
+
+  const discussion = convertToDiscussion(Obj);
+  
+  ul.prepend(discussion);
+
+  // submit 후 입력란 빈칸으로 리셋하는 기능 추가
+  form.querySelector("div.form__input--name > input").value = "";
+  form.querySelector("div.form__input--title > input").value = "";
+  form.querySelector("div.form__textbox > textarea").value = "";
+}
+)
