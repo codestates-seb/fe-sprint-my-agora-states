437c3d58d314987d59c7defd86da0b56e13bb856
YongILTree1 <kyi4698@LAPTOP-8U5AUD4I>
2023-05-08 16:54:32 +0900

    my agora states 작성 문서

--- script.js
+++ script.js
@@ -8,14 +8,34 @@ const convertToDiscussion = (obj) => {
 
   const avatarWrapper = document.createElement("div");
   avatarWrapper.className = "discussion__avatar--wrapper";
+  const avatarImg = document.createElement("img");
+  avatarImg.src = obj.avatarUrl;
+  avatarImg.alt = "avatar of" + obj.author;
+  avatarImg.className = "discussion__avatar--image";
+  avatarWrapper.append(avatarImg);
+
   const discussionContent = document.createElement("div");
   discussionContent.className = "discussion__content";
+  const discussionTitle = document.createElement("h2");
+  discussionTitle.className = "discussion__title";
+  const titleLink = document.createElement("a");
+  titleLink.href = obj.url;
+  titleLink.textContent = obj.title;
+  discussionTitle.append(titleLink);
+
+  const discussionInfo = document.createElement("div");
+  discussionInfo.className = "discussion__information";
+  discussionInfo.textContent = `${obj.author} / ${new Date(obj.createdAt || Date.now()).toLocaleTimeString()}`;
+  discussionContent.append(discussionTitle, discussionInfo);
+
   const discussionAnswered = document.createElement("div");
   discussionAnswered.className = "discussion__answered";
-
+  const answerBox = document.createElement("div");
+  answerBox.className = "discussion__answered";
+  const answerCheck = document.createElement("p");
+  answerCheck.textContent = obj.answer ? "☑" : "☒";
+  discussionAnswered.append(answerCheck);
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
-
-
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
@@ -32,3 +52,34 @@ const render = (element) => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+// 새로운 질문을 입력하면 리스트에 해당 질문이 쌓여야 합니다.
+const form = document.querySelector("form.form");
+const author = document.querySelector("div.form__input--name > input");
+const title = document.querySelector("div.form__input--title > input");
+const textBox = document.querySelector("div.form__textbox > textarea");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  console.log("어렵네..");
+  console.log(author.value, title.value, textBox.value);
+
+  const obj = {
+    id: "unique id",
+    createdAt: new Date(),
+    title: title.value,
+    url: "https://github.com/codestates-seb/agora-states-fe/discussions",
+    author: author.value,
+    answer: null,
+    bodyHTML: textBox.value,
+    avatarUrl: "https://avatars.githubusercontent.com/u/97888923?s=64&u=12b18768cdeebcf358b70051283a3ef57be6a20f&v=4",
+  };
+
+  agoraStatesDiscussions.unshift(obj);
+
+  while (ul.firstChild) {
+    ul.removeChild(ul.firstChild);
+  }
+
+  render(ul);
+});
