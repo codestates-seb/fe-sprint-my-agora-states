a3c2a8821e94eab7b6e953990d12ef158a686a37
Jun Seong <drawbeat0112@gmail.com>
2023-05-08 22:06:49 +0900

    수정사항

--- script.js
+++ script.js
@@ -100,7 +100,7 @@ form.addEventListener("submit", (event) => {
     answer: null,
     bodyHTML: textbox.value,
     avatarUrl:
-      "https://avatars.githubusercontent.com/u/12145019?s=64&u=5c97f25ee02d87898457e23c0e61b884241838e3&v=4",
+      "images/main-img.png",
   };
 
   // agoraStatesDiscussions 객체 추가
