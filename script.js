1c76ff2a47ff817d7ac360315d68a6e756f1d14b
udaeng8286 <udaenglvup@gmail.com>
2023-05-09 00:00:31 +0900

    페이지네이션 구현

--- script.js
+++ script.js
@@ -20,35 +20,39 @@ const convertToDiscussion = (obj) => {
   const discussionContent = document.createElement("div");
   discussionContent.className = "discussion__content";
 
-  // discussion title
+  // discussion content
   const discussionTitle = document.createElement("h2");
   const titleAnchor = document.createElement("a");
   titleAnchor.className = "discussion__title";
   titleAnchor.href = obj.url;
   titleAnchor.textContent = obj.title;
+  const discussionInformation = document.createElement("div");
+  discussionInformation.className = "discussion__information";
+  discussionInformation.textContent = `${obj.author} / ${new Date(obj.createdAt).toLocaleDateString()} ${new Date(obj.createdAt).toLocaleTimeString()}`;
+  discussionContent.append(discussionTitle, discussionInformation);
 
   discussionTitle.append(titleAnchor);
 
   //discussion answered
   const discussionAnswered = document.createElement("div");
   discussionAnswered.className = "discussion__answered";
-  const discussionInformation = document.createElement("div");
-  discussionInformation.className = "discussion__information";
-  discussionInformation.textContent = `${obj.author} / ${new Date(obj.createdAt).toLocaleDateString()} ${new Date(obj.createdAt).toLocaleTimeString()}`;
-  discussionContent.append(discussionTitle, discussionInformation);
-
-
-
   const checked = document.createElement("p");
   checked.textContent = obj.answer ? "✅" : "❎";
+
   discussionAnswered.append(checked);
 
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
 
-
-
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
+
+  const totalPages = Math.ceil(agoraStatesDiscussions.length / pageLimit);
+  if (thisPage === totalPages) {
+    // 현재 페이지가 마지막 페이지인 경우 다음 페이지로 이동
+    changePage(totalPages + 1);
+  } else {
+    loadList();
+  }
 };
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
@@ -70,11 +74,14 @@ const author = form.querySelector("div.form__input--name > input");
 const title = form.querySelector("div.form__input--title > input");
 const textbox = form.querySelector("div.form__textbox > textarea");
 
+//local
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const obj = {
     id: "unique id",
+    createdAt: new Date().toISOString(),
     createdAt: new Date().toLocaleDateString(),
     title: title.value,
     url: "https://github.com/codestates-seb/agora-states-fe/discussions",
@@ -95,7 +102,64 @@ form.addEventListener("submit", (event) => {
 
   // 다시 agoraStatesDiscussions 기반으로 화면에 보여주기 (렌더링)
   render(ul);
-
-
 })
+
+
+//페이지네이션
+let thisPage = 1; //현재 페이지
+let pageLimit = 8; //한 페이지에 보이는 글 수
+let list = document.querySelectorAll('.discussion__container');
+
+function loadList() {
+  let startContent = pageLimit * (thisPage - 1); // 현재 페이지에서 보여줄 첫 번째 글 인덱스 계산
+  let lastContent = pageLimit * thisPage - 1; // 현재 페이지에서 보여줄 마지막 글의 인덱스를 계산 
+
+  // 현재 페이지에서 보여줄 글만 화면에 표시
+  list.forEach((item, key) => {
+    if (key >= startContent && key <= lastContent) {
+      item.style.display = 'flex';
+    } else {
+      item.style.display = 'none';
+    }
+  })
+  listPage();
+}
+loadList();
+
+
+// 현재 페이지 번호와 전체 페이지 수를 나타내는 페이지 번호 링크를 생성
+function listPage() {
+  /* 41/8 => 페이지 수 */
+  let count = Math.ceil(list.length / pageLimit);
+  document.querySelector('.listPage').innerHTML = '';
+  // 
+  if (thisPage !== 1) {
+    let prev = document.createElement('li');
+    prev.innerText = '<';
+    prev.setAttribute('onclick', "changePage(" + (thisPage - 1) + ")");
+    document.querySelector('.listPage').appendChild(prev);
+  }
+
+  for (let i = 1; i <= count; i++) {
+    let newPage = document.createElement('li');
+    newPage.innerText = i;
+    if (i === thisPage) {
+      newPage.classList.add('active');
+    }
+    newPage.setAttribute('onclick', "changePage(" + i + ")");
+    document.querySelector('.listPage').appendChild(newPage);
+  }
+
+  if (thisPage !== count) {
+    let next = document.createElement('li');
+    next.innerText = '>';
+    next.setAttribute('onclick', "changePage(" + (thisPage + 1) + ")");
+    document.querySelector('.listPage').appendChild(next);
+  }
+}
+
+function changePage(i) {
+  thisPage = i;
+  loadList();
+}
 
