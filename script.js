5a4db2876412dbc64540aaca1aa271fcbad839c8
hyeonjin <jhjin0630@gmail.com>
2023-05-08 16:01:27 +0900

    FIX: CSS

--- script.js
+++ script.js
@@ -13,21 +13,31 @@ const convertToDiscussion = (obj) => {
   const discussionAnswered = document.createElement("div");
   discussionAnswered.className = "discussion__answered";
   
+  
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
 
-  li.append(avatarWrapper, discussionContent, discussionAnswered);
+  
 
-  const avatarImage = document.createElement("div");
+  const avatarImage = document.createElement("img");
   avatarImage.className = "discussion__avatar--image";
-  avatarWrapper.append(avatarImage); //avatar wrapper에 image div 추가 ((유저명을 여기 쓸지 고민))
+  avatarImage.src = obj.avatarUrl;
+  avatarImage.alt = `avatar of ${obj.author}`
+  avatarWrapper.appendChild(avatarImage); 
 
-  const discussionTitle = document.createElement("div");
+  const discussionTitle = document.createElement("h2");
   discussionTitle.className = "discussion__title";
+  discussionTitle.innerHTML = `<a href=${obj.url}>${obj.title}</a>`;
+  
   const discussionInformation = document.createElement("div");
   discussionInformation.className = "discussion__information";
-  discussionContent.append(discussionTitle, discussionInformation); //discussionContent에 제목, 저자명, 시간 div 추가
+  discussionInformation.textContent = `${obj.author}  /  ${obj.createdAt}`
 
-//디스커션에 대답이 달린 것을 어떻게 표현할지? 
+  const discussionCheck = document.createElement("div");
+  discussionCheck.className = "check";
+  discussionCheck.textContent = "☑";
+  discussionAnswered.append(discussionCheck);
+discussionContent.append(discussionTitle, discussionInformation);
+li.append(avatarWrapper, discussionContent, discussionAnswered);
 
 
   return li;
@@ -35,7 +45,7 @@ const convertToDiscussion = (obj) => {
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
-  for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
+  for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {  
     element.append(convertToDiscussion(agoraStatesDiscussions[i]));
   }
   return;
@@ -43,4 +53,48 @@ const render = (element) => {
 
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
+
+
 render(ul);
+
+
+//폼 작성시 discussion 목록에 포함시키기 
+// 1) 폼 DOM 하기 
+
+const inputName = document.querySelector("#name");
+const inputTitle = document.querySelector("#title");
+const inputStory = document.querySelector("#story");
+const inputSubmit = document.querySelector("#submit_q");
+
+// 2) 제출 버튼 누르면 닉네임, 제목, 내용이 함께 새로운 li를 생성해 맨 위에 위치한다
+
+const handleSubmit = (e) => {
+  e.preventDefault(); // 폼 기본 동작 방지
+  const name = inputName.value;
+  const title = inputTitle.value;
+  const story = inputStory.value;
+
+  // 새로운 객체 생성
+  const newDiscussion = {
+    createdAt: new Date().toISOString(),
+    title: title,
+    author: name,
+    bodyHTML: story,
+    avatarUrl: "ast4.png"
+  };
+
+  // agoraStatesDiscussions 배열에 새로운 객체 추가
+  agoraStatesDiscussions.unshift(newDiscussion);
+
+  // 새로운 객체를 DOM으로 렌더링
+  const ul = document.querySelector("ul.discussions__container");
+  ul.prepend(convertToDiscussion(newDiscussion));
+
+  // 입력 필드 초기화
+  inputName.value = "";
+  inputTitle.value = "";
+  inputStory.value = "";
+};
+
+inputSubmit.addEventListener("click", handleSubmit);
+
