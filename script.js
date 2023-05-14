309ba286ac72854bfc6fd8202a9162d2db9c9a5d
YEONSOOK-KO <dustnr0121@gmail.com>
2023-05-14 22:40:41 +0900

    add initial code

--- script.js
+++ script.js
@@ -23,12 +23,52 @@ const convertToDiscussion = (obj) => {
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
+  // 초기화
+  element.innerHTML = '';
   for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
-    element.append(convertToDiscussion(agoraStatesDiscussions[i]));
+    let data = agoraStatesDiscussions[i];
+
+    let liEl = document.createElement('li');
+    liEl.classList.add('discussion__container');
+
+    let discAvaWrap = document.createElement('div');
+    discAvaWrap.classList.add('discussion__avatar--wrapper');
+
+    let discAvaImg = document.createElement('img');
+    discAvaImg.classList.add('discussion__avatar--image');
+    discAvaImg.src = data.avatarUrl;
+    discAvaWrap.appendChild(discAvaImg);
+
+    let discCont = document.createElement('div');
+    discCont.classList.add('discussion__content');
+
+    let discTit = document.createElement('h2');
+    let hrefTit = document.createElement('a');
+    hrefTit.href = data.url;
+    hrefTit.textContent = data.title;
+    discTit.append(hrefTit);
+    
+    let discInfo = document.createElement('div');
+    discInfo.classList.add('discussion__information');
+    discInfo.textContent = data.author + ' / ' + data.createdAt;
+    
+    discCont.appendChild(discTit);
+    discCont.appendChild(discInfo);
+
+    let discAnsw = document.createElement('div');
+    discAnsw.classList.add('discussion__answered');
+    discAnsw.textContent = '☑';
+
+    liEl.appendChild(discAvaWrap);
+    liEl.appendChild(discCont);
+    liEl.appendChild(discAnsw);
+
+    element.appendChild(liEl);
+
   }
   return;
 };
 
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
-const ul = document.querySelector("ul.discussions__container");
-render(ul);
+const ulEl = document.querySelector("ul.discussions__container"); // ul태그의 ciscussions__container 클래스
+render(ulEl);
