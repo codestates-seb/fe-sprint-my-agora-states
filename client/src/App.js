649e2d916e885bca1f339815ac56354bad985d79
widrns15 <85780501+widrns15@users.noreply.github.com>
2023-06-07 16:48:04 +0900

    ♻️ refactor: client 폴더 코드 및 CSS 리팩토링

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="form__info">
+        <h1>My Agora States</h1>
+        <section className="form__container">
+          <form action="" method="get" className="form">
+            <div className="form__input--wrapper">
+              <div className="form__input--name">
+                <label htmlFor="name">Enter your name: </label>
+                <input type="text" name="name" id="name" required />
+              </div>
+              <div className="form__input--title">
+                <label htmlFor="title">Enter your title: </label>
+                <input type="text" name="title" id="title" required />
+              </div>
+              <div className="form__textbox">
+                <label htmlFor="story">Your question: </label>
+                <textarea
+                  id="story"
+                  name="story"
+                  placeholder="질문을 작성하세요"
+                  required
+                ></textarea>
+              </div>
+            </div>
+            <div className="form__submit">
+              <input type="submit" value="submit" />
+            </div>
+          </form>
+        </section>
+      </div>
+      <div className="form__active">
+        <section className="discussion__wrapper">
+          <ul className="discussions__container">
+            <li className="discussion__container">
+              <div className="discussion__avatar--wrapper">
+                <img
+                  className="discussion__avatar--image"
+                  src="https://avatars.githubusercontent.com/u/12145019?s=64&u=5c97f25ee02d87898457e23c0e61b884241838e3&v=4"
+                  alt="avatar of kimploo"
+                />
+              </div>
+              <div className="discussion__content">
+                <h2 className="discussion__title">
+                  <a href="https://github.com/codestates-seb/agora-states-fe/discussions/6">
+                    [notice] 좋은 질문하는 법
+                  </a>
+                </h2>
+                <div className="discussion__information">
+                  kimploo / 2022-04-22T14:08:33Z
+                </div>
+              </div>
+              <div className="discussion__answered">
+                <p>✅</p>
+              </div>
+            </li>
+          </ul>
+        </section>
+      </div>
+    </main>
   );
-}
+};
 
 export default App;
