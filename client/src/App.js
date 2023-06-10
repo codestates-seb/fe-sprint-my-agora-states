3658bc1b7a55de4a06b4f2a0bb93d9f178ae1853
widrns15 <85780501+widrns15@users.noreply.github.com>
2023-06-10 23:47:16 +0900

    🔥 feat: 클라이언트에 서버 메서드 및 기능 추가
    - ♻️ refactor: 서버 메서드를 추가하기 위한 리팩토링

--- client/src/App.js
+++ client/src/App.js
@@ -1,97 +1,72 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
-
-const DiscussionItem = ({ avatarUrl, title, author, createdAt, answer }) => {
-  return (
-    <li className="discussion__container">
-      <div className="discussion__avatar--wrapper">
-        <img
-          className="discussion__avatar--image"
-          src={avatarUrl}
-          alt="avatar"
-        />
-      </div>
-      <div className="discussion__content">
-        <h2 className="discussion__title">
-          <a href={title}>
-            {title.length > 53 ? `${title.slice(0, 53)}...` : title}
-          </a>
-        </h2>
-        <div
-          className="discussion__information"
-          dangerouslySetInnerHTML={{
-            __html: `${author} / ${new Date(createdAt).toLocaleTimeString()}`,
-          }}
-        ></div>
-      </div>
-      <div className="discussion__answered">
-        <p>{answer ? "✅" : "❌"}</p>
-      </div>
-    </li>
-  );
-};
+import { Form, Discussions } from "./components";
 
 const App = () => {
   const [discussions, setDiscussions] = useState([]);
+  const [error, setError] = useState(null);
 
-  useEffect(() => {
+  const addDiscussion = ({ title, author, bodyText }) => {
+    const newDiscussionData = {
+      title,
+      author,
+      bodyHTML: bodyText,
+    };
     axios
-      .get("http://localhost:4000/discussions")
+      .post("http://localhost:4000/discussions/", newDiscussionData)
+      .then((response) => {
+        if (response.status === 201) {
+          getDiscussion();
+        }
+      })
+      .catch((error) => {
+        setError("Failed to add discussion.");
+      });
+  };
+  const getDiscussion = () => {
+    axios
+      .get("http://localhost:4000/discussions/")
       .then((response) => {
         setDiscussions(response.data);
       })
       .catch((error) => {
-        console.error("Error fetching discussions:", error);
+        setError("Failed to fetch discussions.");
       });
+  };
+
+  const deleteDiscussion = (id) => {
+    axios
+      .delete(`http://localhost:4000/discussions/${id}`)
+      .then((response) => {
+        if (response.status === 202 || response.status === 204) {
+          getDiscussion();
+        }
+      })
+      .catch((error) => {
+        setError("Failed to delete discussion.");
+      });
+  };
+
+  useEffect(() => {
+    getDiscussion();
   }, []);
 
   return (
     <main>
       <div className="form__info">
         <h1>My Agora States</h1>
-        <section className="form__container">
-          <form action="" method="get" className="form">
-            <div className="form__input--wrapper">
-              <div className="form__input--name">
-                <label htmlFor="name">Enter your name: </label>
-                <input type="text" name="name" id="name" required />
-              </div>
-              <div className="form__input--title">
-                <label htmlFor="title">Enter your title: </label>
-                <input type="text" name="title" id="title" required />
-              </div>
-              <div className="form__textbox">
-                <label htmlFor="story">Your question: </label>
-                <textarea
-                  id="story"
-                  name="story"
-                  placeholder="질문을 작성하세요"
-                  required
-                ></textarea>
-              </div>
-            </div>
-            <div className="form__submit">
-              <input type="submit" value="submit" />
-            </div>
-          </form>
-        </section>
+        <Form addDiscussion={addDiscussion} />
       </div>
       <div className="form__active">
-        <section className="discussion__wrapper">
-          <ul className="discussions__container">
-            {discussions.map((discussion) => (
-              <DiscussionItem
-                key={discussion.id}
-                avatarUrl={discussion.avatarUrl}
-                title={discussion.title}
-                author={discussion.author}
-                createdAt={discussion.createdAt}
-                answer={discussion.answer}
-              />
-            ))}
-          </ul>
-        </section>
+        {error ? (
+          <div className="error">{error}</div>
+        ) : (
+          <Discussions
+            discussions={discussions}
+            deleteDiscussion={deleteDiscussion}
+          />
+        )}
       </div>
     </main>
   );
