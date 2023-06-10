d32064d3d7a53394ba15c91e34b9a0a0f17a5ca2
widrns15 <85780501+widrns15@users.noreply.github.com>
2023-06-10 23:40:51 +0900

    🔥 feat: 서버에 post, put, delete 메서드 및 기능 추가

--- server/controller/index.js
+++ server/controller/index.js
@@ -1,6 +1,14 @@
 const { agoraStatesDiscussions } = require("../repository/discussions");
 const discussionsData = agoraStatesDiscussions;
 
+const handleRequestBody = (req, res) => {
+  if (!req.body) return res.status(400).send("no request body");
+  const { title, author, bodyHTML } = req.body;
+  if (!title && !author && !bodyHTML)
+    return res.status(400).send("bad request");
+  return true;
+};
+
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
@@ -23,6 +31,59 @@ const discussionsController = {
       } else {
         res.status(404).json({ error: "Discussion not found." });
       }
+    }
+  },
+
+  createOne: (req, res) => {
+    const { title, author, bodyHTML } = req.body;
+
+    if (handleRequestBody(req, res) !== true) return;
+    const id = parseInt(Math.random() * 10000);
+    const avartarId = parseInt(Math.random() * 100);
+    const url =
+      "https://github.com/codestates-seb/agora-states-fe/discussions/" + id;
+    const newDiscussion = {
+      id,
+      createdAt: new Date().toISOString(),
+      title,
+      url,
+      author,
+      answer: null,
+      bodyHTML,
+      avatarUrl: `https://randomuser.me/api/portraits/men/${avartarId}.jpg`,
+    };
+    discussionsData.unshift(newDiscussion);
+    return res.status(201).send("resource created successfully: ID " + id);
+  },
+
+  updateById: (req, res) => {
+    if (handleRequestBody(req, res) !== true) return;
+    const idx = discussionsData.findIndex(
+      (d) => d.id === Number(req.params.id)
+    );
+    const updated = {
+      ...discussionsData[idx],
+      ...req.body,
+      updatedAt: new Date().toISOString(),
+    };
+
+    if (idx !== -1) {
+      discussionsData.splice(idx, 1, updated);
+      return res.status(200).send("resource updated successfully");
+    } else {
+      return res.status(404).send("Not found");
+    }
+  },
+
+  deleteById: (req, res) => {
+    const idx = discussionsData.findIndex(
+      (d) => d.id === Number(req.params.id)
+    );
+    if (idx !== -1) {
+      discussionsData.splice(idx, 1);
+      return res.status(202).send("resource deleted successfully");
+    } else {
+      return res.status(404).send("Not found");
     }
   },
 };
