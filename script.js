75e27374479b9fbabc4df608f63ef668f3a06b0e
CHOI JAHYUN <0912jh@gmail.com>
2023-10-16 17:36:04 +0900

    적당한완료

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
-//기본 배열
+const ul = document.querySelector("ul.discussions__container");
+let getNewData = JSON.parse(localStorage.getItem("discussions"));
 
-console.log(agoraStatesDiscussions.length);
-
-// 폼 추가 함수
-
+// 댓글 작성버튼 누를때
+// 객체를 만들고 배열에 추가된다.
 const makeNewDiscussion = (event) => {
   event.preventDefault();
 
-  console.log("makeNewQuestion내부텍스트1");
-  //폼 작성버튼 누르면 돔을 통해 작성값을 가져온다
-  //작성값을 넣는 배열을 만든다
-  //agoraStatesDiscussions에 생성된 배열을 추가한다.
-
-  //폼 작성요소 가져옴
   const newName = document.getElementById("name").value;
   const newTitle = document.getElementById("title").value;
   const newContent = document.getElementById("story").value;
 
-  //객체생성, 배열에 추가
+  let time = new Date();
+  function getToday() {
+    var date = new Date();
+    var year = date.getFullYear();
+    var month = ("0" + (1 + date.getMonth())).slice(-2);
+    var day = ("0" + date.getDate()).slice(-2);
+    return "작성시간 : " + year + "년 " + month + "월 " + day + "일";
+  }
+
+  // let randomNum = {
+  //   Math.floor(Math.random() * 899999999 + 10000;)
+  // }
+
   const newObj = {
     id: "new",
-    createdAt: "2022-05-16T01:02:17Z",
+    createdAt: getToday(),
     title: newTitle,
     avatarUrl:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Unknown_person.jpg/434px-Unknown_person.jpg",
@@ -29,98 +34,92 @@ const makeNewDiscussion = (event) => {
     bodyHTML: `<p>${newContent}</p>`,
   };
 
-  //배열에 추가해서 리턴하기
   agoraStatesDiscussions.unshift(newObj);
+  localStorage.setItem("discussions", JSON.stringify(agoraStatesDiscussions));
 
-  const ul = document.querySelector("ul.discussions__container");
- 
-  //프리펜드안해도 화면에 그려주는.
-  ul.innerHTML = ""; //랜더의 전 내용 지워줌 중복안되게.
-  render(ul);//추가한것만 띄워줌?.
+  ul.innerHTML = "";
+  render(ul);
 };
 
-
-
+window.onload = () => {
+  getNewData = localStorage.getItem("discussions");
+  if (getNewData !== null) {
+    agoraStatesDiscussions = JSON.parse(getNewData);
+  }
+  render(ul);
+};
 
 const formButton = document.getElementById("submit");
 formButton.addEventListener("click", makeNewDiscussion);
 
-//로컬 스토리지를 만들고
-//내부의 값을 불러온다
-//새로 생성되는 배열은
-
-// index.html을 열어서 agoraStatesDiscussions 배열 요소를 확인하세요.
-console.log(agoraStatesDiscussions);
-console.log(agoraStatesDiscussions.length);
-
-// convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
 const convertToDiscussion = (obj) => {
-  const li = document.createElement("li"); // li 요소 생성
-  li.className = "discussion__container"; // 클래스 이름 지정
+  const li = document.createElement("li");
+  li.className = "discussion__container";
 
-  //생성요소들
+  const deleteButton = document.createElement("button");
+  deleteButton.className = "discussion__content--delete";
+  deleteButton.textContent = "삭제";
+  deleteButton.addEventListener("click", function () {
+    const index = agoraStatesDiscussions.indexOf(obj);
+    if (index > -1) {
+      agoraStatesDiscussions.splice(index, 1);
+      localStorage.setItem(
+        "discussions",
+        JSON.stringify(agoraStatesDiscussions)
+      );
+      ul.innerHTML = "";
+      render(ul);
+    }
+  });
+
   const avatarWrapper = document.createElement("div");
   avatarWrapper.className = "discussion__avatar--wrapper";
+
   const discussionContent = document.createElement("div");
   discussionContent.className = "discussion__content";
+
   const discussionAnswered = document.createElement("div");
   discussionAnswered.className = "discussion__answered";
 
   const discussionTitle = document.createElement("h2");
   const discussionAnchor = document.createElement("a");
-  const avatarImg = document.createElement("img"); //함수값 img
-  avatarImg.classList.add["discussion__avatar--image"];
-
-  const discusstionId = document.createElement("div");
-  const discusstionCreatedAt = document.createElement("div");
-  const discusstionContentText = document.createElement("div");
-  const discussionAuthor = document.createElement("div");
-  const discussionAnswer = document.createElement("div");
-
-  // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
   discussionAnchor.textContent = obj.title;
   discussionAnchor.href = obj.url;
   discussionAnchor.target = "_blank";
+
+  const avatarImg = document.createElement("img");
+  avatarImg.classList.add("discussion__avatar--image");
   avatarImg.src = obj.avatarUrl;
 
-  discusstionCreatedAt.textContent = obj.createdAt;
+  const discusstionId = document.createElement("div");
   discusstionId.textContent = obj.id;
+
+  const discusstionCreatedAt = document.createElement("div");
+  discusstionCreatedAt.textContent = obj.createdAt;
+
+  const discussionAuthor = document.createElement("div");
   discussionAuthor.textContent = obj.author;
-  // discusstionContentText.innerHTML = obj.answer.bodyHTML; // 이거 켜두면...메인 태그에 바디가 이상해짐
 
-  //타이틀
   discussionTitle.append(discussionAnchor);
+  discussionContent.append(
+    discussionTitle,
+    discussionAuthor,
+    discusstionCreatedAt
+  );
 
-  //콘텐트
-  discussionContent.append(discussionTitle); // 제목
-  // discussionContent.append(discusstionContentText); //내용
-  discussionContent.append(discussionAuthor); // 글쓴이
-  discussionContent.append(discusstionCreatedAt); //생성시간
-  //만일 answer 배열이 존재한다면
   if (obj.answer === null) {
     discussionContent.append("답변없음");
   } else {
     discussionContent.append("답변있음");
   }
 
-  //아바타부분
   avatarWrapper.append(avatarImg);
-
-  li.append(avatarWrapper, discussionContent, discussionAnswered);
+  li.append(avatarWrapper, discussionContent, discussionAnswered, deleteButton);
   return li;
 };
 
-// agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
-  for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
+  for (let i = 0; i < agoraStatesDiscussions.length; i++) {
     element.append(convertToDiscussion(agoraStatesDiscussions[i]));
   }
-  return;
 };
-
-// ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
-const ul = document.querySelector("ul.discussions__container");
-render(ul);
-
-
-
